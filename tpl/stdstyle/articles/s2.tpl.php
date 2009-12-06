054744c725f926c859232951483b74e3adc93161
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2009-12-06 17:09:47 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@60 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/articles/s2.tpl.php
+++ tpl/stdstyle/articles/s2.tpl.php
@@ -1,12 +1,12 @@
-<table width=600 class="content2-pagetitle">
+<table class="content">
 	<colgroup>
 		<col width="100">
 	</colgroup>
-	<tr><td width=600 class="content2-pagetitle"><img src="tpl/stdstyle/images/blue/stat2.png" border="0" width="32" height="32" alt="Statystyki" title="Statystyki" align="middle"><font size="4">  <b>Statystyki OC PL</b></font></td></tr>
+	<tr><td class="content2-pagetitle"><img src="tpl/stdstyle/images/blue/stat1.png" border="0" width="32" height="32" alt="Statystyki" title="Statystyki" align="middle"><font size="4">  <b>Statystyki OC PL</b></font></td></tr>
 	<tr><td class="spacer"></td></tr>
 </table>
 
-<table width="600">
+<table>
 <tr>
 <td><?php include ("t2.php");?>
 </td></tr>
