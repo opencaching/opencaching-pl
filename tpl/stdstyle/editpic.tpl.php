4a9e51bd7fed471e57b8cbf0d0585bd996bbdab5
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2009-12-06 20:19:10 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@98 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/editpic.tpl.php
+++ tpl/stdstyle/editpic.tpl.php
@@ -52,7 +52,7 @@ function checkForm()
 		<col width="100">
 		<col>
 	</colgroup>
-	<tr><td class="content2-pagetitle" colspan="2"><img src="tpl/stdstyle/images/blue/picture.png" border="0" width="32" height="32" alt="" title="edit picture" align="middle"> <b>Edycja {pictypedesc} <a href="/viewcache.php?cacheid={cacheid}">{cachename}</a></b></td></tr>
+	<tr><td class="content2-pagetitle" colspan="2"><img src="tpl/stdstyle/images/blue/picture.png" border="0" width="32" height="32" alt="" title="edit picture" align="middle"> <b>Edit {pictypedesc} </b><br><a href="/viewcache.php?cacheid={cacheid}">{cachename}</a></td></tr>
 	<tr><td class="spacer" colspan="2"><br></td></tr>
 
 	<tr>
