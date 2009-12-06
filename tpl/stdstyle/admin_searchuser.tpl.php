a6a985e7e62588fd26735036e3fa5736aae03376
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2009-12-06 17:51:45 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@69 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/admin_searchuser.tpl.php
+++ tpl/stdstyle/admin_searchuser.tpl.php
@@ -3,10 +3,10 @@ global $usr;
 ?>
 <form name="optionsform" style="display:inline;" action='admin_searchuser.php' method="POST">
 <table class="content" border="0" cellspacing="0px" cellpadding="0px">
-<tr><td class="content2-pagetitle"><img src="tpl/stdstyle/images/blue/profile.png" border="0" width="32" height="32" alt="" title="" align="middle"><img src="tpl/stdstyle/images/blue/search3.png" border="0" width="32" height="32" alt="" title="" align="middle"><font size="4">  <b>Szukaj użytkownika</b></font></td></tr>
+<tr><td class="content2-pagetitle"><img src="tpl/stdstyle/images/blue/search3.png" border="0" width="32" height="32" alt="" title="" align="middle"><img src="tpl/stdstyle/images/blue/profile.png" border="0" width="32" height="32" alt="" title="" align="middle"><font size="4">  <b>Szukaj użytkownika</b></font></td></tr>
 <tr><td class="spacer"></td></tr>
 <tr>
-	<td>
+	<td><br><br>
 		{not_found}
 		<input type="text" name="username" value="{username}" class="input200" /> <input type="submit" value="Szukaj" class="formbuttons" /><br/>
 	</td>
