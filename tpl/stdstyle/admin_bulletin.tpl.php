bcbaac2a7a7cc668ac55a552212cf42d69b42acb
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2009-12-06 16:28:03 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@51 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/admin_bulletin.tpl.php
+++ tpl/stdstyle/admin_bulletin.tpl.php
@@ -1,14 +1,14 @@
 <table class="content">
 <tr>
 	<td class="content2-pagetitle">
-		<img src="tpl/stdstyle/images.blue/write.png" border="0" width="32" height="32" alt="" title="" align="middle"><font size="4">  <b>Wyślij biuletyn</b></font>
+		<img src="tpl/stdstyle/images/blue/write.png" border="0" width="32" height="32" alt="" title="" align="middle"><font size="4">  <b>Wyślij biuletyn</b></font>
 	</td>
 </tr>
 <tr><td class="spacer" colspan="2"></td></tr>
 <tr>
 	<td>
 		<form action='admin_bulletin.php' method='POST'>
-		<textarea name='bulletin' cols='110' rows='10'></textarea>
+		<textarea name='bulletin' cols='80' rows='15'></textarea>
 		<br>
 		<input type='submit' value='Wyślij biuletyn'>
 		</form>
