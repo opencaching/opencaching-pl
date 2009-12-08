9fa1ad789d03be3a6ebd264805e4275e607be671
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2009-12-08 19:38:06 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@154 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/newcache.tpl.php
+++ tpl/stdstyle/newcache.tpl.php
@@ -250,7 +250,7 @@ function toggleAttr(id)
 		<td><div class="notice" style="width:500px;height:44px;">{waypoint_other_portal_info}</div><div class="buffer"></div></td>
 	</tr>
 	<tr>
-		<td style="width:769px;margin:0 0 0 0;border-top:1px solid blue;border-left:1px solid blue;border-right:1px solid blue;border-bottom:1px solid blue;" colspan="2">
+		<td class="bg-white" style="width:769px;margin:0 0 0 0;border-top:1px solid rgb(54,83,151);border-left:1px solid rgb(54,83,151);border-right:1px solid rgb(54,83,151);border-bottom:1px solid rgb(54,83,151);" colspan="2">
 			<p class="content-title-noshade-size1"><img src="tpl/stdstyle/images/blue/attributes.png" width="32" height="32" align="middle" border="0" alt=""/>&nbsp;{cache_attributes}</p>
 		
 		</td>
@@ -263,7 +263,7 @@ function toggleAttr(id)
 		<td colspan="2"><div class="notice" style="width:500px;min-height:24px;height:auto;">{attributes_edit_hint} {additional_attributes_hint} <a href="cache-atr.php" target="_BLANK">{additional_attributes}</a>. {attributes_desc_hint}</div></td></tr>
 	<tr><td colspan="2"><div class="buffer"></div></td></tr>
 	<tr>
-		<td style="width:769px;margin:0 0 0 0;border-top:1px solid blue;border-left:1px solid blue;border-right:1px solid blue;border-bottom:1px solid blue;" colspan="2">
+		<td class="bg-white" style="width:769px;margin:0 0 0 0;border-top:1px solid blue;border-left:1px solid blue;border-right:1px solid blue;border-bottom:1px solid blue;" colspan="2">
 			<p class="content-title-noshade-size1"><img src="tpl/stdstyle/images/blue/describe.png" width="32" height="32" align="middle" border="0" alt=""/>&nbsp;{description}</p>
 			
 			</td>
@@ -327,7 +327,7 @@ function toggleAttr(id)
 		</td>
 	</tr>
 	<tr>
-		<td style="width:769px;margin:0 0 0 0;border-top:1px solid blue;border-left:1px solid blue;border-right:1px solid blue;border-bottom:1px solid blue;" colspan="2">
+		<td class="bg-white" style="width:769px;margin:0 0 0 0;border-top:1px solid blue;border-left:1px solid blue;border-right:1px solid blue;border-bottom:1px solid blue;" colspan="2">
 		
 			<p class="content-title-noshade-size1"><img src="tpl/stdstyle/images/blue/crypt.png" width="32" height="32" align="middle" border="0" alt=""/>
 				{other}
