90186281a348df28c2a62f9430a5f94bdd0d6a4e
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2009-12-09 08:52:11 +0000

    dodanie naglowka
    git-svn-id: https://opencaching-pl.googlecode.com/svn@165 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/add_rr_comment.tpl.php
+++ tpl/stdstyle/add_rr_comment.tpl.php
@@ -1,4 +1,12 @@
-Dodaj adnotację RR do skrzynki "<a href="viewcache.php?cacheid={cacheid}">{cachename}</a>"<br>
+<table class="content">
+<tr>
+	<td class="content2-pagetitle">
+		<img src="tpl/stdstyle/images/blue/write.png" border="0" width="32" height="32" alt="" title="" align="middle"><font size="4">  <b>Dodaj adnotację RR do skrzynki "<a href="viewcache.php?cacheid={cacheid}">{cachename}</a>"</b></font>
+	</td>
+</tr>
+<tr><td class="spacer" colspan="2"></td></tr>
+<tr>
+	<td>
 <form action='viewcache.php' method='POST'>
 	<input type='hidden' name='cacheid' value='{cacheid}'>
 	<textarea name='rr_comment' cols='110' rows='10'></textarea>
@@ -6,3 +14,7 @@ Dodaj adnotację RR do skrzynki "<a href="viewcache.php?cacheid={cacheid}">{cach
 	<input type='submit' value='Dodaj komentarz'>
 	<a href='viewcache.php?cacheid={cacheid}'>Anuluj</a>
 </form>
+	</td>
+</tr>
+</table>
+
