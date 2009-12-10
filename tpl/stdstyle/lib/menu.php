a93f950a37c275f99e8c79830679064f3bcf57ff
ocpl <ocpl@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2009-12-10 01:52:50 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@201 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/lib/menu.php
+++ tpl/stdstyle/lib/menu.php
@@ -206,7 +206,7 @@ $menu = array(
 		'menustring' => 'IRC',
 		'siteid' => 'irc',
 		'visible' => true,
-		'filename' => 'http://webchat.freenode.net/?nick='.$usr['username'].'&channels=opencaching.pl&prompt=1',
+		'filename' => 'http://webchat.freenode.net/?nick='.$usr['username'].'&amp;channels=opencaching.pl&amp;prompt=1',
 		'newwindow' => true
 	),
 	array(
