12eda81a4185c7ff82077efe46ee5b282443e404
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2009-12-11 19:04:27 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@314 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/viewcache.tpl.php
+++ tpl/stdstyle/viewcache.tpl.php
@@ -68,7 +68,7 @@
 <!-- End Text Container -->
 <!-- Text container -->
 			<div class="content2-container">
-				<div class="content2-container-2col-left" style="width:400px;">
+				<div class="content2-container-2col-left" style="width:390px;">
 					<p class="content-title-noshade-size3">
 						<img src="tpl/stdstyle/images/blue/compas.png" class="icon32" alt="" title="" />
 						<b>{coords}</b> <span class="content-title-noshade-size0">(WGS84)</span><br />
@@ -122,9 +122,9 @@ else
 							{rating_stat}
 							</p>
 						</div>
-						<div id="viewcache-map" class="content2-container-2col-right">
+						<div id="viewcache-map" class="content2-container-2col-right"><div class="img-shadow">
 							<img src="http://maps.google.com/staticmap?center={latitude},{longitude}&amp;zoom=8&amp;size=170x170&amp;maptype=terrain&amp;key={googlemap_key}&amp;sensor=false&amp;markers={latitude},{longitude},blue{typeLetter}&amp;format=png" alt="{{map}}" />
-						</div>
+						</div></div>
 					<?php
 							}
 					if ($usr == false && $hide_coords)
