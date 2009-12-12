c31e73b51ef7d09f20c0ed22c14d0fb2e1b2ee6f
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2009-12-12 14:43:09 +0000

    xhtml fix
    git-svn-id: https://opencaching-pl.googlecode.com/svn@342 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/add_rr_comment.tpl.php
+++ tpl/stdstyle/add_rr_comment.tpl.php
@@ -1,17 +1,17 @@
 <table class="content">
 <tr>
 	<td class="content2-pagetitle">
-		<img src="tpl/stdstyle/images/blue/write.png" border="0" width="32" height="32" alt="" title="" align="middle"><font size="4"> Dodaj adnotację RR do skrzynki "<a href="viewcache.php?cacheid={cacheid}">{cachename}</a>"</font>
+		<img src="tpl/stdstyle/images/blue/write.png" border="0" width="32" height="32" alt="" title="" align="middle"/><font size="4"> {{add_comment_OCTeam_label}} "<a href="viewcache.php?cacheid={cacheid}">{cachename}</a>"</font>
 	</td>
 </tr>
 <tr><td class="spacer" colspan="2"></td></tr>
 <tr>
 	<td>
 <form action='viewcache.php' method='POST'>
-	<input type='hidden' name='cacheid' value='{cacheid}'>
+	<input type='hidden' name='cacheid' value='{cacheid}' />
 	<textarea name='rr_comment' cols='80' rows='15'></textarea>
 	<br />
-	<input type='submit' value='Dodaj komentarz'>
+	<input type='submit' value='Dodaj komentarz' />
 	<a href='viewcache.php?cacheid={cacheid}'>Anuluj</a>
 </form>
 	</td>
