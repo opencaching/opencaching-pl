3800679c1ea75244eb62990eb5c89498300a67dd
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2009-12-12 12:45:59 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@339 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/newcachesrest.tpl.php
+++ tpl/stdstyle/newcachesrest.tpl.php
@@ -2,7 +2,7 @@
 <img src="tpl/stdstyle/images/blue/cache.png" border="0" width="32" height="32" alt="Cache" title="Cache" align="middle"/>&nbsp;{{abroad_caches}}
 </div>
 <!-- Text container -->
-<div class="content2-container line-box">
+<div class="content2-container">
 		{newcachesrest}
 </div>
 <!-- End Text Container -->
