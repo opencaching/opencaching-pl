9ef37c6917254a256d82411f45df619664bbdf4b
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2009-12-17 09:13:56 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@423 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/users-stats.tpl.php
+++ tpl/stdstyle/users-stats.tpl.php
@@ -1,6 +1,6 @@
  	<!-- 	CONTENT -->
 <div class="content2">
-<div class="content2-pagetitle"><img src="tpl/stdstyle/images/blue/stat1.png" class="icon32" alt="{title_text}" title="{title_text}" />&nbsp;{{Statystyki usera}}</div>
+<div class="content2-pagetitle"><img src="tpl/stdstyle/images/blue/stat1.png" class="icon32" alt="{title_text}" title="{title_text}" />&nbsp;{{Statystyki usera}}: </div>
 
 
 <img src="graphs/user-stat.php?userid=449&title=ccy" />
