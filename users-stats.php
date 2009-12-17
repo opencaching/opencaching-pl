c04fdcfea42e59f11505e926b5bbc58287d34617
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2009-12-17 12:03:56 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@432 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- users-stats.php
+++ users-stats.php
@@ -17,28 +17,30 @@
 	$rsGeneralStat =sql("SELECT hidden_count, founds_count, log_notes_count, username FROM `user` WHERE user_id=&1 ",$user_id);
 	if ($rsGeneralStat !== false){
 			$user_record = sql_fetch_array($rsGeneralStat);
-			mysql_free_result($rsGeneralStat);
+
 			tpl_set_var('username',$user_record['username']);
 			$content .= '<table style="border-collapse: collapse" border="1" width="500"><tr><td colspan="4" bgcolor="#C6E2FF"><b>General statistics </b></td></tr><tr><td> Total Found </td> <td>' . $user_record['founds_count'] . '</td> <td> Find Rate </td> <td>  </td></tr><tr><td> Caches created </td> <td>' . $user_record['hidden_count'] . '</td> <td> Total logs entry </td> <td>' . $user_record['log_notes_count'] . '</td></tr><tr><td> Avg cache/day </td> <td> </td> <td>First Cache</td><td> </td></tr><tr><td> Most cache/day </td> <td></td> <td>Latest Cache</td><td></td></tr></table><br /><br />';	
 
 		}
+			mysql_free_result($rsGeneralStat);
 
 	$rsCreateCachesYear= sql("SELECT COUNT(*) `count`,YEAR(`date_created`) `year` FROM `caches` WHERE user_id=&1 GROUP BY YEAR(`date_created`) ORDER BY YEAR(`date_created`) ASC",$user_id);
 
 	if ($rsCreateCachesYear !== false){
 	$content .= '<tr><td><img src="graphs/BarGraphustat.php?userid=' . $user_id . '&t=ccy" border="0" alt="" /></td></tr>';
-	mysql_free_result($rsCreateCachesYear);
 			
 	}
+	mysql_free_result($rsCreateCachesYear);
+
 	$rsCreateCachesMonth = sql("SELECT COUNT(*) `count`, MONTH(`date_created`) `month`, YEAR(`date_created`) `year` FROM `caches` WHERE user_id=&1 GROUP BY MONTH(`date_created`), YEAR(`date_created`) ORDER BY YEAR(`date_created`) ASC, MONTH(`date_created`) ASC",$user_id);
 
  				if ($rsCreateCachesMonth !== false) {
 				$rccm = mysql_fetch_array($rsCreateCachesMonth); 
 		$content .= '<tr><td><img src="graphs/BarGraphustat.php?userid=' . $user_id . '&t=ccm' . $rccm['year'] . '" border="0" alt="" /></td></tr>';
 
- 			mysql_free_result($rsCreateCachesMonth);
-		}
 
+		}
+ 			mysql_free_result($rsCreateCachesMonth);
 
 
 	$rsCachesFindYear = sql("SELECT COUNT(*) `count`,YEAR(`date_created`) `year` FROM `cache_logs` WHERE type=1 AND user_id=&1 GROUP BY YEAR(`date_created`) ORDER BY YEAR(`date_created`) ASC",$user_id);
@@ -46,8 +48,9 @@
   				if ($rsCachesFindYear !== false) {
 
 		$content .= '<tr><td><img src="graphs/BarGraphustat.php?userid=' . $user_id . '&t=cfy"  border="0" alt="" /></td></tr>';					
-				mysql_free_result($rsCachesFindYear);
+
 			}
+				mysql_free_result($rsCachesFindYear);
 
 $rsCachesFindMonth= sql("SELECT COUNT(*) `count`,YEAR(`date_created`) `year` , MONTH(`date_created`) `month` FROM `cache_logs` WHERE type=1 AND user_id=&1 GROUP BY MONTH(`date_created`) , YEAR(`date_created`) ORDER BY YEAR(`date_created`) ASC, MONTH(`date_created`) ASC",$user_id);
 
@@ -55,9 +58,9 @@ $rsCachesFindMonth= sql("SELECT COUNT(*) `count`,YEAR(`date_created`) `year` , M
 				$rcfm = mysql_fetch_array($rsCachesFindMonth); 
 
 		$content .= '<tr><td><img src="graphs/BarGraphustat.php?userid=' . $user_id . '&t=cfm' . $rcfm['year'] . '"  border="0" alt="" /></td></tr>';					
-				mysql_free_result($rsCachesFindMonth);
-		}
 
+		}
+				mysql_free_result($rsCachesFindMonth);
 
 
 	tpl_set_var('content',$content);
