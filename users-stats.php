a0db57ce58cbcab219b9dcbe659d2dbcefa3ff39
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2009-12-17 17:09:35 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@436 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- users-stats.php
+++ users-stats.php
@@ -14,13 +14,15 @@
 
 	$content="";
 
+	$dd=sqlValue(sql("select to_days(`current_date`)-to_days(`date_created`) from tabela `user` WHERE user_id=&1 ",$user_id));
+	echo $dd;
 	$rsGeneralStat =sql("SELECT hidden_count, founds_count, log_notes_count, username FROM `user` WHERE user_id=&1 ",$user_id);
 	if ($rsGeneralStat !== false){
 			$user_record = sql_fetch_array($rsGeneralStat);
 
 			tpl_set_var('username',$user_record['username']);
 
-			$content .= '<table style="border-collapse: collapse" border="1" width="500"><tr><td colspan="4" bgcolor="#C6E2FF"><b>General statistics </b></td></tr><tr><td> Total Found </td> <td>' . $user_record['founds_count'] . '</td> <td> Find Rate </td> <td>  </td></tr><tr><td> Caches created </td> <td>' . $user_record['hidden_count'] . '</td> <td> Total logs entry </td> <td>' . $user_record['log_notes_count'] . '</td></tr><tr><td> Avg cache/day </td> <td> </td> <td>First Cache</td><td> </td></tr><tr><td> Most cache/day </td> <td></td> <td>Latest Cache</td><td></td></tr></table><br /><br />';	
+			$content .= '<table style="border-collapse: collapse" border="1" width="500"><tr><td colspan="4" bgcolor="#C6E2FF"><b>Caches created statistics </b></td></tr><tr><td> Total created caches</td> <td>' . $user_record['hidden_count'] . '</td> <td> Create Rate </td> <td> .... </td></tr><tr><td> Avg cache/day </td> <td> ....</td> <td>First Cache created</td><td>.... </td></tr><tr><td> Most cache/day </td> <td>....</td> <td>Latest Cache created</td><td>....</td></tr></table><br /><br />';	
 
 		}
 			mysql_free_result($rsGeneralStat);
@@ -35,9 +37,8 @@
 
 	$rsCreateCachesMonth = sql("SELECT COUNT(*) `count`, MONTH(`date_created`) `month`, YEAR(`date_created`) `year` FROM `caches` WHERE user_id=&1 GROUP BY MONTH(`date_created`), YEAR(`date_created`) ORDER BY YEAR(`date_created`) ASC, MONTH(`date_created`) ASC",$user_id);
 
- 				if ($rsCreateCachesMonth !== false) {
+ 				if (mysql_num_rows($result)==0) {
 				$rccm = mysql_fetch_array($rsCreateCachesMonth); 
-	
 
 		$content .= '<tr><td><img src="graphs/BarGraphustat.php?userid=' . $user_id . '&t=ccm' . $rccm['year'] . '" border="0" alt="" /></td></tr>';
 			
@@ -49,6 +50,7 @@
 	$rsCachesFindYear = sql("SELECT COUNT(*) `count`,YEAR(`date_created`) `year` FROM `cache_logs` WHERE type=1 AND user_id=&1 GROUP BY YEAR(`date_created`) ORDER BY YEAR(`date_created`) ASC",$user_id);
 
   				if ($rsCachesFindYear !== false) {
+		$content .= '<br><br><table style="border-collapse: collapse" border="1" width="500"><tr><td colspan="4" bgcolor="#C6E2FF"><b>Caches found statistics </b></td></tr><tr><td> Total Found it Caches</td> <td>' . $user_record['founds_count'] . '</td> <td> Find Rate </td> <td> &nbsp;.... </td></tr><tr><td> Avg cache/day </td> <td> &nbsp;....</td> <td>  First Found it Cache  </td> <td>&nbsp;....</td></tr><tr><td> Most cache/day</td> <td> &nbsp;....</td> <td> Latest Found it Cache</td><td>&nbsp;....</td></tr></table><br /><br />';	
 
 		$content .= '<tr><td><img src="graphs/BarGraphustat.php?userid=' . $user_id . '&t=cfy"  border="0" alt="" /></td></tr>';					
 
@@ -65,7 +67,7 @@ $rsCachesFindMonth= sql("SELECT COUNT(*) `count`,YEAR(`date_created`) `year` , M
 		}
 				mysql_free_result($rsCachesFindMonth);
 
-			$content .='<tr><td>Visted</td></tr><tr<td><img=images/PLmapa250.jpg alt="" /></td</tr>';
+			$content .='<br><br><table style="border-collapse: collapse" border="0" width="500"><tr><td colspan="4" bgcolor="#C6E2FF"><b>Visted PL regions </b></td></tr><tr><td width="500"><center><img src=images/PLmapa250.jpg alt="" /></center></td</tr></table>';
 	tpl_set_var('content',$content);
 	$tplname = 'users-stats';
 }
