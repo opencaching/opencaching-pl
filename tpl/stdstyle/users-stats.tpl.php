8f563223f956c388a6767b84e8d8630ac92a4996
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2009-12-17 09:15:56 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@424 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/users-stats.tpl.php
+++ tpl/stdstyle/users-stats.tpl.php
@@ -3,14 +3,14 @@
 <div class="content2-pagetitle"><img src="tpl/stdstyle/images/blue/stat1.png" class="icon32" alt="{title_text}" title="{title_text}" />&nbsp;{{Statystyki usera}}: </div>
 
 
-<img src="graphs/user-stat.php?userid=449&title=ccy" />
+<img src="graphs/BarGraphustat.php?userid=449&title=ccy" />
 <br><br>
-<img src="graphs/user-stat.php?userid=449&title=ccm2008" /> <br />
-<img src="graphs/user-stat.php?userid=449&title=ccm2009" /><br />
+<img src="graphs/BarGraphustat.php?userid=449&title=ccm2008" /> <br />
+<img src="graphs/BarGraphustat.php?userid=449&title=ccm2009" /><br />
 <br><br>
-<img src="graphs/user-stat.php?userid=449&title=cfy" /><br />
-<img src="graphs/user-stat.php?userid=449&title=cfm2008" /> <br />
-<img src="graphs/user-stat.php?userid=449&title=cfm2009" />
+<img src="graphs/BarGraphustat.php?userid=449&title=cfy" /><br />
+<img src="graphs/BarGraphustat.php?userid=449&title=cfm2008" /> <br />
+<img src="graphs/BarGraphustat.php?userid=449&title=cfm2009" />
 
 </div>
 
