f64ac5f73a4e8571f0ff614c4209bac14169a46e
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2009-12-21 08:11:32 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@473 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- graphs/BarGraphustat.php
+++ graphs/BarGraphustat.php
@@ -1,19 +1,10 @@
 <?php
 setlocale(LC_TIME, 'pl_PL.utf-8');
-  //prepare the templates and include all neccessary
-	require_once('./lib/common.inc.php');
+  
 	
 	//Preprocessing
 	if ($error == false)
 	{
-			//user logged in?
-		if ($usr == false)
-		{
-		    $target = urlencode(tpl_get_current_page());
-		    tpl_redirect('login.php?target='.$target);
-		}
-		else
-		{
 require("../lib/jpgraph/src/jpgraph.php");
 require('../lib/jpgraph/src/jpgraph_bar.php');
 require('../lib/jpgraph/src/jpgraph_date.php');
@@ -155,6 +146,6 @@ $bplot->value->SetFormat('%d');
   $graph->Stroke();
    
   }
-  }
+ 
   
   ?>
