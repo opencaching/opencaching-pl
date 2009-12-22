d3abe55e1c895f7bde4a5f74ee180230dd7000ab
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2009-12-22 16:16:21 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@484 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- graphs/PieGraphustat.php
+++ graphs/PieGraphustat.php
@@ -15,16 +15,10 @@ require("../lib/jpgraph/src/jpgraph_pie3d.php");
   sql('USE `ocpl`');
 		$year='';
 		// check for old-style parameters
-		if (isset($_REQUEST['userid']) && isset($_REQUEST['title']))
+		if (isset($_REQUEST['userid']) && isset($_REQUEST['t']))
 		{
 			$user_id = $_REQUEST['userid'];
-			$titles = $_REQUEST['title'];
-			if (strlen($titles) >3) {
-			$year = substr ($titles,-4);
-			$tit= substr($titles,0,-4);
-			}
-			else
-			{ $tit=$titles;}
+			$tit = $_REQUEST['t'];
 		}
 		
   $y=array();
@@ -32,37 +26,22 @@ require("../lib/jpgraph/src/jpgraph_pie3d.php");
   
   
 
-if ($tit == "ccy") {
-$rsCreateCachesYear= sql("SELECT COUNT(*) `count`,YEAR(`date_created`) `year` FROM `caches` WHERE user_id=&1 GROUP BY YEAR(`date_created`) ORDER BY YEAR(`date_created`) ASC",$user_id);
+if ($tit == "cc") {
+$rsCreateCachesYear= sql("SELECT COUNT(`caches`.`type`) `count`, `cache_type`.`pl` `type` FROM `caches` INNER JOIN `cache_type` ON (`caches`.`type`=`cache_type`.`id`) WHERE `user_id`=&1 AND `status`!=5 GROUP BY `caches`.`type` ORDER BY `count` DESC",$user_id);
 
 				if ($rsCreateCachesYear !== false){
 				$descibe="Roczna statystyka";
 				$xtitle="";
 					while ($ry = mysql_fetch_array($rsCreateCachesYear)){
 					$y[] = $ry['count'];
-					$x[] = $ry['year'];}
+					$x[] = $ry['type'];}
 					}
 				mysql_free_result($rsCreateCachesYear);
 		}
 
 
-if ($tit == "ccm") {
-$rsCreateCachesMonth = sql("SELECT COUNT(*) `count`, MONTH(`date_created`) `month`, YEAR(`date_created`) `year` FROM `caches` WHERE user_id=&1 AND YEAR(`date_created`)=&2 GROUP BY MONTH(`date_created`), YEAR(`date_created`) ORDER BY YEAR(`date_created`) ASC, MONTH(`date_created`) ASC",$user_id,$year);
-
- 				if ($rsCreateCachesMonth !== false) {
-				$descibe="Miesiêczna statystyka za rok";
-				$xtitle=$year;
-				while ($rm = mysql_fetch_array($rsCreateCachesMonth)){
-					$y[] = $rm['count'];
-					$x[] = $rm['month'];}
-					}
-
- mysql_free_result($rsCreateCachesMonth);
-				
-				}
-
-if ($tit == "cfy") {
-$rsCachesFindYear = sql("SELECT COUNT(*) `count`,YEAR(`date_created`) `year` FROM `cache_logs` WHERE type=1 AND user_id=&1 GROUP BY YEAR(`date_created`) ORDER BY YEAR(`date_created`) ASC",$user_id);
+if ($tit == "cf") {
+$rsCachesFindYear = sql("SELECT COUNT(`caches`.`type`) `count`, `cache_type`.`pl` `type` FROM `cache_logs` INNER JOIN `cache_type` ON (`caches`.`type`=`cache_type`.`id`) WHERE `user_id`=&1 AND `cache_logs`=1 GROUP BY `caches`.`type` ORDER BY `count` DESC",$user_id);
 
   				if ($rsCachesFindYear !== false) {
 				$descibe="Roczna statystyka";
@@ -74,49 +53,41 @@ $rsCachesFindYear = sql("SELECT COUNT(*) `count`,YEAR(`date_created`) `year` FRO
 				mysql_free_result($rsCachesFindYear);
 }
 
-if ($tit == "cfm") {
-$rsCachesFindMonth= sql("SELECT COUNT(*) `count`,YEAR(`date_created`) `year` , MONTH(`date_created`) `month` FROM `cache_logs` WHERE type=1 AND user_id=&1 AND YEAR(`date_created`)=&2 GROUP BY MONTH(`date_created`) , YEAR(`date_created`) ORDER BY YEAR(`date_created`) ASC, MONTH(`date_created`) ASC",$user_id,$year);
-
- 				if ($rsCachesFindMonth !== false){
-				$descibe="Miesiêczna statystyka za rok ";
-				$describe .= $year;
-				$xtitle=$year;
-				while ($rfm = mysql_fetch_array($rsCachesFindMonth)){
-					$y[] = $rfm['count'];
-					$x[] = $rfm['month'];}
-				}
-				mysql_free_result($rsCachesFindMonth);
-}
-
 
 				
 /// A new pie graph
-$graph = new PieGraph(250,200,"auto");
-$graph->SetShadow();
- 
+$graph = new PieGraph(500,300,"auto");
+$graph->SetScale('textint');
+//$graph->SetShadow();
+
+
+
+
 // Title setup
-$graph->title->Set("Exploding all slices");
+$graph->title->Set("Wg typów skrzynek");
 $graph->title->SetFont(FF_FONT1,FS_BOLD);
 //$graph ->legend->Pos( 0.25,0.8,"right" ,"bottom"); 
  
 // Setup the pie plot
-$p1 = new PiePlot($data);
+$p1 = new PiePlot($y);
+$p1->SetTheme("earth");
+$p1->value->SetFormat("%d");
 $p1->SetLabelType(PIE_VALUE_ABS);
 
 // Adjust size and position of plot
 $p1->SetSize(0.35);
-$p1->SetCenter(0.5,0.52);
+$p1->SetCenter(0.25,0.52);
  
 // Setup slice labels and move them into the plot
+
 $p1->value->SetFont(FF_FONT1,FS_BOLD);
 $p1->value->SetColor("darkred");
 $p1->SetLabelPos(0.65);
+$p1->SetLegends($x);
+
  
 // Explode all slices
-$p1->ExplodeAll(10);
- 
-// Add drop shadow
-$p1->SetShadow();
+$p1->ExplodeAll(20);
  
 // Finally add the plot
 $graph->Add($p1);
