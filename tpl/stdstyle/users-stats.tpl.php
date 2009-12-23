67cc75ef7a49cde4cf54d7db12eee6471cd4dfb1
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2009-12-23 07:44:06 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@503 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/users-stats.tpl.php
+++ tpl/stdstyle/users-stats.tpl.php
@@ -14,7 +14,7 @@
 <div class="content-title-noshade">
 <table border="0" cellspacing="2" cellpadding="1" style="margin-left: 10px;" width="97%">
 <tr><td ><img src="tpl/stdstyle/images/blue/profile1.png"  alt="" title="Profile" align="middle"/></td>
-<td>	{username} {{registered_since_label}} {registered}</td>
+<td>{username} {{registered_since_label}} {registered}<br>{{description_user}}:<br>{description_start}{description}{description_end}</td>
 <td>
 	<img src="tpl/stdstyle/images/blue/email.png" class="icon32" alt="Email" title="Email" align="middle"/>&nbsp;<a href="mailto.php?userid={userid}">{{email_user}}</a><br/>
 	<img src="tpl/stdstyle/images/blue/world.png" class="icon32" alt="Mapa" title="Map" align="middle"/>&nbsp;<a href="cachemap2.php?userid={userid}">{{show_user_map}}</a>
