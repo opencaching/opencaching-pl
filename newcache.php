6daa7fdceede841e18da11d9c506505c1ec5d9b7
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2009-12-25 21:26:40 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@576 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- newcache.php
+++ newcache.php
@@ -192,15 +192,17 @@
 					$name = iconv("utf-8", "UTF-8", $name);
 			}
 
-			// Text / normal HTML / HTML editor
-			tpl_set_var('use_tinymce', (($descMode == 3) ? 1 : 0));
+					// Text / normal HTML / HTML editor
+					tpl_set_var('use_tinymce', (($desc_htmledit == 1) ? 1 : 0));
 
-			if ($descMode == 1)
-				tpl_set_var('descMode', 1);
-			else if ($descMode == 2)
-				tpl_set_var('descMode', 2);
-			else
-			{
+					if (($desc_html == 1) && ($desc_htmledit == 1))
+					{
+						tpl_set_var('descMode', 3);
+					}
+					else if ($desc_html == 1)
+						tpl_set_var('descMode', 2);
+					else
+						tpl_set_var('descMode', 1);
 				// TinyMCE
 					$headers = tpl_get_var('htmlheaders') . "\n";
 					$headers .= '<script language="javascript" type="text/javascript" src="lib/phpfuncs.js"></script>' . "\n";
@@ -208,8 +210,8 @@
 					$headers .= '<script language="javascript" type="text/javascript" src="lib/tinymce/config/desc.js.php?lang='.$lang.'&amp;cacheid=' . ($desc_record['cache_id']+0) . '"></script>' . "\n";
 					tpl_set_var('htmlheaders', $headers);
 
-				tpl_set_var('descMode', 3);
-			}
+//				tpl_set_var('descMode', 3);
+//			}
 
 			//effort
 			$search_time = isset($_POST['search_time']) ? $_POST['search_time'] : '0';
