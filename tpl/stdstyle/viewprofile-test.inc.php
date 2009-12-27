581fe0c3009b420f5cf754f5647348b790660915
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2009-12-27 14:10:25 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@651 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/viewprofile-test.inc.php
+++ tpl/stdstyle/viewprofile-test.inc.php
@@ -8,11 +8,6 @@
 	*
 	***************************************************************************/
 
-
-
- $no_hiddens = "<tr><td>".tr('no_hidden_caches')."</td></tr>";
- $no_notpublished = "<tr><td>".tr('no_not_published_caches')."</td></tr>";
- $no_logs = "<tr><td>".tr('no_logs')."</td></tr>";
  $no_time_set = tr('no_time_indicated');
 
  $logtype[1] = tr('found');
@@ -20,7 +15,7 @@
  $logtype[3] = tr('comment');
 
  $cache_line = '<li>{cacheimage}&nbsp;{cachestatus} &nbsp; {date} &nbsp; <a href="viewcache.php?cacheid={cacheid}">{cachename}</a></li>';
- $cache_notpublished_line = '<li class="linklist-noindent">{cacheimage}&nbsp;{cachestatus} &nbsp; <a href="editcache.php?cacheid={cacheid}">{date}</a> &nbsp; <a href="viewcache.php?cacheid={cacheid}">{cachename}</a></li>';
+ $cache_notpublished_line = '<li>{cacheimage}&nbsp;{cachestatus} &nbsp; <a href="editcache.php?cacheid={cacheid}">{date}</a> &nbsp; <a href="viewcache.php?cacheid={cacheid}">{cachename}</a></li>';
  $log_line = '<li>{logimage}&nbsp;{logtype} &nbsp; {date} &nbsp; <a href="viewcache.php?cacheid={cacheid}">{cachename}</a></li>';
- $cache_line_my_caches = '<li class="linklist-noindent">{logimage}&nbsp;{logtype}&nbsp; {date} &nbsp; <a href="viewcache.php?cacheid={cacheid}">{cachename}</a> - <a href="viewprofile.php?userid={userid}">{username}</a></li>';
+ $cache_line_my_caches = '<li>{logimage}&nbsp;{logtype}&nbsp; {date} &nbsp; <a href="viewcache.php?cacheid={cacheid}">{cachename}</a> - <a href="viewprofile.php?userid={userid}">{username}</a></li>';
 ?>
