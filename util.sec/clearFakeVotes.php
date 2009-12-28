0647b3753587c9acb5520944d254f44bf5477053
trojmiasto <trojmiasto@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2009-12-28 16:03:16 +0000

    fixed svn version crap.
    git-svn-id: https://opencaching-pl.googlecode.com/svn@703 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- util.sec/clearFakeVotes.php
+++ util.sec/clearFakeVotes.php
@@ -41,7 +41,6 @@ class ClearFakeVotes
 		$logs_query = mysql_query($sql2);
 		if( mysql_num_rows($logs_query)==0)
 		{
-<<<<<<< .mine
 			$sql3 = "SELECT username FROM user WHERE user_id = ".$scores['user_id'];
 			$fakeUser = mysql_result(mysql_query($sql3),0);
 			//echo "uzytkownik: ".$scores['user_id']."=".$fakeUser." ocenil skrzynke: ".$scores['cache_id']." na ".$scores['score'];
@@ -66,37 +65,6 @@ class ClearFakeVotes
 			
 			$sql4 = "UPDATE caches SET votes='".sql_escape($liczba)."', score='".sql_escape($srednia)."' WHERE cache_id='".sql_escape($scores['cache_id'])."'";
 			mysql_query($sql4);
-
-			
-=======
-			while( $logs = mysql_fetch_array($logs_query))
-			{
-				$sql3 = "SELECT username FROM user WHERE user_id = ".sql_escape($scores['user_id']);
-				$fakeUser = mysql_result(mysql_query($sql3),0);
-				//echo "uzytkownik: ".$scores['user_id']."=".$fakeUser." ocenil skrzynke: ".$scores['cache_id']." na ".$scores['score'];
-				//echo "<br />";
-				$sql_del = "DELETE FROM scores WHERE user_id = ".sql_escape($scores['user_id'])." AND cache_id = ".sql_escape($scores['cache_id']);
-				mysql_query($sql_del);
-				
-				$sql4 = "SELECT count(*) FROM scores WHERE cache_id='".sql_escape($scores['cache_id'])."'";
-				$liczba = mysql_result(mysql_query($sql4),0);
-				
-				$sql4 = "SELECT score FROM scores WHERE cache_id='".sql_escape($scores['cache_id'])."'";
-				$score = mysql_query($sql4);
-				$suma = 0;
-				
-				// obliczenie nowej sredniej
-				while( $res = mysql_fetch_array($score))
-					$suma += $res['score'];
-				
-				if( $liczba != 0)
-					$srednia = $suma / $liczba;
-				else $srednia = 0;
-				
-				$sql4 = "UPDATE caches SET votes='".sql_escape($liczba)."', score='".sql_escape($srednia)."' WHERE cache_id='".sql_escape($scores['cache_id'])."'";
-				mysql_query($sql4);
-			}
->>>>>>> .r772
 		}
 	}
 	
