c3cdb38a9f77c2841ab9345f1bf1a80fcf8836a5
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2009-12-28 14:19:09 +0000

    exclude cache stat for Events
    git-svn-id: https://opencaching-pl.googlecode.com/svn@700 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- viewcache.php
+++ viewcache.php
@@ -310,7 +310,10 @@
 				$coords = tr('hidden_coords');
 				$coords_other = "";
 			}
+			if ($cache_record['type'] != 6) {
 			$cache_stats = "<a href=\"#\" onclick=\"javascript:window.open('cache_stats.php?cacheid=".$cache_record['cache_id']."&amp;popup=y','Cache_Statistics','width=450,height=750,resizable=no,scrollbars=1')\"><img src=\"tpl/stdstyle/images/blue/stat1.png\" alt=\"\" title=\"Zobacz statystyke skrzynki\" /></a>";
+			} else {
+			$cache_stats="";}
 			tpl_set_var('cache_stats', $cache_stats);
 			tpl_set_var('googlemap_key', $googlemap_key);
 			tpl_set_var('map_msg', $map_msg);
