1df05737d7af54ca9504c4a2aa822e5fcd2301e6
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2009-12-29 10:26:01 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@726 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- viewprofile.php
+++ viewprofile.php
@@ -347,7 +347,7 @@
 //			$content .= '<p><span class="content-title-noshade txt-blue08" >Milestones czyli "kamienie milowe":</span></p><ul class="linklist-indent">';			
 			$content .= '<br /><table style="border-collapse: collapse; font-size: 110%;" width="250" border="1"><tr><td colspan="3" align="center" bgcolor="#DBE6F1"><b> Milestones "kamienie milowe"</b></td> </tr><tr><td bgcolor="#EEEDF9"><b> Nr </b></td> <td bgcolor="#EEEDF9"><b> Data </b></td> <td bgcolor="#EEEDF9"><b> Geocache</b> </td> </tr>';
 			$rsms=sql("SELECT cache_logs.cache_id cache_id,  DATE_FORMAT(cache_logs.date,'%Y-%m-%d') data, caches.wp_oc cache_wp FROM cache_logs, caches WHERE caches.cache_id=cache_logs.cache_id AND cache_logs.type='1' AND cache_logs.user_id=&1 AND cache_logs.deleted='0' ORDER BY cache_logs.date ASC",$user_id);
-			if (mysql_num_rows($rsms) <= 100) {
+			if (mysql_num_rows($rsms) < 101) {
 			for ($i = 0; $i < mysql_num_rows($rsms); $i+=10)
 				{		
 				$ii=$i;
