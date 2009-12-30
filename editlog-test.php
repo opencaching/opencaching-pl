3f8e1a93a1eb4a9dc3b631973ce8f503a0ba9cfc
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2009-12-30 15:51:55 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@793 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- editlog-test.php
+++ editlog-test.php
@@ -519,16 +519,14 @@
 
 
 					// Text / normal HTML / HTML editor
-					tpl_set_var('use_tinymce', (($desc_htmledit == 1) ? 1 : 0));
+					tpl_set_var('use_tinymce', (($descMode == 3) ? 1 : 0));
 
-					if (($desc_html == 1) && ($desc_htmledit == 1))
-					{
-						tpl_set_var('descMode', 3);
-					}
-					else if ($desc_html == 1)
+					if ($descMode == 1)
+						tpl_set_var('descMode', 1);
+					else if ($descMode == 2)
 						tpl_set_var('descMode', 2);
 					else
-						tpl_set_var('descMode', 1);
+					{
 					// TinyMCE
 					$headers = tpl_get_var('htmlheaders') . "\n";
 					$headers .= '<script language="javascript" type="text/javascript" src="lib/phpfuncs.js"></script>' . "\n";
@@ -536,7 +534,8 @@
 					$headers .= '<script language="javascript" type="text/javascript" src="lib/tinymce/config/log.js.php?lang='.$lang.'&amp;logid=0"></script>' . "\n";
 					tpl_set_var('htmlheaders', $headers);
 
-
+						tpl_set_var('descMode', 3);
+					}
 					if ($use_log_pw == true && $log_pw != '')
 					{
 						if ($pw_not_ok == true && isset($_POST['submitform']))
