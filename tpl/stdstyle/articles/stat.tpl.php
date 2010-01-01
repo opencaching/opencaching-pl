b8b72012bec1b2160554b44e427231c467b2f13f
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-01-01 15:31:27 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@876 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/articles/stat.tpl.php
+++ tpl/stdstyle/articles/stat.tpl.php
@@ -1,12 +1,11 @@
 <div class="content2-pagetitle"><img src="tpl/stdstyle/images/blue/stat1.png" class="icon32" alt="{title_text}" title="{title_text}" />&nbsp;{{stats}}</div>
-<div style=" font-size: 115%;">
-<img src="tpl/stdstyle/images/blue/stat2.png" alt="" /><a class="content-title-noshade" href="articles.php?page=s1">{{ranking_by_number_of_created_caches}}</a><br/>
-<img src="tpl/stdstyle/images/blue/stat2.png" alt="" /><a class="content-title-noshade" href="articles.php?page=s2">{{ranking_by_number_of_finds}}</a>
-<br/>
-<img src="tpl/stdstyle/images/blue/stat2.png" alt="" /><a href="articles.php?page=s4">{{cache_ranking_by_number_of_finds}}</a><br/>
-<img src="tpl/stdstyle/images/blue/stat2.png" alt="" /><a href="articles.php?page=s5">{{cache_ranking_by_calculated_indicator}}</a><br/>
-<img src="tpl/stdstyle/images/blue/stat2.png" alt="" /><a href="articles.php?page=s3">{{user_ranking_by_number_of_finds_of_their_caches}}</a><br/>
+<div style="line-heogh: 1.6em; font-size: 12px;">
+<img src="tpl/stdstyle/images/blue/stat2.png" alt="" /><a class="links" href="articles.php?page=s1">{{ranking_by_number_of_created_caches}}</a><br />
+<img src="tpl/stdstyle/images/blue/stat2.png" alt="" /><a class="links" href="articles.php?page=s2">{{ranking_by_number_of_finds}}</a><br />
+<img src="tpl/stdstyle/images/blue/stat2.png" alt="" /><a class="links" href="articles.php?page=s4">{{cache_ranking_by_number_of_finds}}</a><br />
+<?php if ($usr !== false){ echo '<img src="tpl/stdstyle/images/blue/stat2.png" alt="" /><a class="links" href="articles.php?page=s5">{{cache_ranking_by_calculated_indicator}}</a><br />';} ?>
+<img src="tpl/stdstyle/images/blue/stat2.png" alt="" /><a class="links" href="articles.php?page=s3">{{user_ranking_by_number_of_finds_of_their_caches}}</a><br /><br />
 <div class="img-shadow"><img src="graphs/new-caches-oc.php" alt="{{oc_statistics}}" /></div><br/>
 <div class="buffer"></div>
-<img src="graphs/cachetypes-oc.php" alt="{{oc_statistics}}" /><br/>
+<img src="graphs/cachetypes-oc.php" alt="{{oc_statistics}}" /><br />
 </div>
