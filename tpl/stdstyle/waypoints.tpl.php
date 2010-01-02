730096a0150fd6729ef802f8e16a009138740594
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-01-02 05:54:14 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@897 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/waypoints.tpl.php
+++ tpl/stdstyle/waypoints.tpl.php
@@ -1,3 +1,14 @@
+<?php
+/***************************************************************************
+	*                                         				                                
+	*   This program is free software; you can redistribute it and/or modify  	
+	*   it under the terms of the GNU General Public License as published by  
+	*   the Free Software Foundation; either version 2 of the License, or	    	
+	*   (at your option) any later version.
+	*   
+	*  UTF-8 ąść
+	***************************************************************************/
+?>
 <div class="content2-pagetitle"><img src="tpl/stdstyle/images/blue/cache.png" class="icon32" alt="" />&nbsp;{{waypoints_cache}} &#8211; {name}</div>
 
 <table>
