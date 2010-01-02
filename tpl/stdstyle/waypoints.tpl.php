d1196605435562fef5dbb4f69f74ac9452d88e70
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-01-02 05:48:43 +0000

    template for add aditional waypoints for cache
    git-svn-id: https://opencaching-pl.googlecode.com/svn@895 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

---
+++ tpl/stdstyle/waypoints.tpl.php
@@ -0,0 +1,3 @@
+<div class="content2-pagetitle"><img src="tpl/stdstyle/images/blue/cache.png" class="icon32" alt="" />&nbsp;{{waypoints_cache}} &#8211; {name}</div>
+
+
---
+++ waypoints.php
@@ -0,0 +1,54 @@
+<?php
+/***************************************************************************
+	*                                         				                                
+	*   This program is free software; you can redistribute it and/or modify  	
+	*   it under the terms of the GNU General Public License as published by  
+	*   the Free Software Foundation; either version 2 of the License, or	    	
+	*   (at your option) any later version.
+	*   
+	*  UTF-8 ąść
+	***************************************************************************/
+/*
+Add  additional waypoints to cache
+table db 'waypoints' struture 'id' 'cache_id' 'type_wp'   ????
+
+type_wp:
+Final location, Parking area, Question to answer, Reference point, Stages of Multicaches, Trialhead.
+
+*/
+//prepare the templates and include all neccessary
+	require_once('./lib/common.inc.php');
+	
+	$tplname = 'waypoints';
+
+	//Preprocessing
+	if ($error == false)
+	{
+			//user logged in?
+		if ($usr == false)
+		{
+		    $target = urlencode(tpl_get_current_page());
+		    tpl_redirect('login.php?target='.$target);
+		}
+		else
+		{
+
+	// check for old-style parameters
+		if (isset($_REQUEST['cacheid']))
+		{
+			$cache_id = $_REQUEST['cacheid'];
+		
+		}
+
+		
+		
+		
+		
+	
+		tpl_set_var("sample", "xxxx");
+	
+
+		}
+	}
+	tpl_BuildTemplate();
+?>
