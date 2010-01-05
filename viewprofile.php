bfca3d80a372392191383b8276399d07158a1221
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-01-05 16:30:38 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@1025 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- viewprofile.php
+++ viewprofile.php
@@ -513,7 +513,7 @@
 				$content .= '<p>&nbsp;</p><p><span class="content-title-noshade txt-blue08" >Najnowsze wpisy do logów wprowadzone przez użytkownika:</span>';
 				if ($user_id == $usr['userid'] || $usr['admin']) 
 				{$content .='&nbsp;&nbsp;<img src="tpl/stdstyle/images/blue/arrow.png" alt="" /> [<a class="links" href="myhome2.php">'.tr('show_all').'</a>]</p><br /><div><ul style="margin: -0.9em 0px 0.9em 0px; padding: 0px 0px 0px 10px; list-style-type: none; line-height: 1.2em; font-size: 115%;">';
-				}else{$content .='</p><br /><div><ul style="margin: -0.9em 0px 0.9em 0px; padding: 0px 0px 0px 10px; list-style-type: none; line-height: 1.2em; font-size: 115%;">';
+				}else{$content .='</p><br /><div><ul style="margin: -0.9em 0px 0.9em 0px; padding: 0px 0px 0px 10px; list-style-type: none; line-height: 1.6em; font-size: 115%;">';
 				}
 				for ($i = 0; $i < mysql_num_rows($rs_logs); $i++)
 					{
