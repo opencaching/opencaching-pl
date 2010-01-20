0f2faa00e4866ca7db5bd8ade9e1806c7e636a79
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-01-20 10:15:32 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@1223 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- rss/newnews.php
+++ rss/newnews.php
@@ -19,12 +19,10 @@
 		//get the news
 		$perpage = 20;
 		
-		$content = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<rss version=\"2.0\">\n<channel>\n<title>Opencaching.pl - Najnowsze wiadomości</title>\n<link>http://www.opencaching.pl/news.php</link>\n <description><![CDATA[Najnowsze wiadomości]]></description>
-	<image>
+		$content = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<rss version=\"2.0\">\n<channel>\n<title>Opencaching.pl - Najnowsze wiadomości</title>\n<link>http://www.opencaching.pl/news.php</link>\n <description><![CDATA[Najnowsze wiadomości]]></description><image>
 		<title>OpenCaching.PL</title>
 		<url>http://www.opencaching.pl/images/oc_logo.png</url>
-		<link>http://www.opencaching.pl</link>
-					</image>\n";
+		<link>http://www.opencaching.pl</link><width>70</width><height>28</height></image>\n";
 		
 		
 			$rsNews = sql('SELECT `date_posted`, `content` FROM `news` WHERE `topic`=2 AND `display`=1 ORDER BY `date_posted` DESC LIMIT ' . $perpage);
