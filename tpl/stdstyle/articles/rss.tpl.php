e09c1bf2c73a2f1bab6270398107430b2a9302cd
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-01-20 08:39:41 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@1216 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

---
+++ tpl/stdstyle/articles/rss.tpl.php
@@ -0,0 +1,16 @@
+<table class="content">
+	<colgroup><col width="100"></colgroup>
+	<tr><td width="750" class="content2-pagetitle">
+	<img src="tpl/stdstyle/images/blue/rss.png" class="icon32" alt="OC" title="Kontakt" align="middle" />
+	<font size="4">  <b>Kontakt</b></font></td></tr>
+		<tr><td class="spacer"></td></tr>
+
+</table>
+<br />
+<h1>Kanały RSS</h1>
+<p>Najnowsze skrzynki http://www.opencaching.pl/rss/newcaches.xml </p>
+<p> Najnowsze logi http://www.opencaching.pl/rss/newlogs.xml </p>
+<p> Njanowsze wiadomośći http://www.opencaching.pl/rss/newnewss.xml </p>
+<br />
+
+
