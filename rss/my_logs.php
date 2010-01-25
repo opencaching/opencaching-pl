9c96d8227f658d53f034425085bd57673fe6873c
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-01-25 08:44:57 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@1262 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- rss/my_logs.php
+++ rss/my_logs.php
@@ -16,14 +16,10 @@
 		new logs
 
 	****************************************************************************/
-	global $lang, $rootpath;
-
-	if (!isset($rootpath)) $rootpath = '';
-
-	//include template handling
+	//prepare the templates and include all neccessary
+	$rootpath = '../';
 	require_once($rootpath . 'lib/common.inc.php');
-	require_once($rootpath . 'lib/cache_icon.inc.php');
-//	require_once($stylepath . '/lib/icons.inc.php');
+	
 
 //Preprocessing
 if ($error == false)
@@ -56,45 +52,43 @@ if ($error == false)
 							  user.user_id AS user_id,
 							  caches.wp_oc AS wp_name,
 							  caches.type AS cache_type,
-							  cache_type.icon_small AS cache_icon_small,
-							  log_types.icon_small AS icon_small,
 							  IF(ISNULL(`cache_rating`.`cache_id`), 0, 1) AS `recommended`,COUNT(gk_item.id) AS geokret_in
-	                  FROM ((cache_logs INNER JOIN caches ON (caches.cache_id = cache_logs.cache_id)) INNER JOIN user ON (cache_logs.user_id = user.user_id) INNER JOIN log_types ON (cache_logs.type = log_types.id) INNER JOIN cache_type ON (caches.type = cache_type.id) LEFT JOIN `cache_rating` ON `cache_logs`.`cache_id`=`cache_rating`.`cache_id` AND `cache_logs`.`user_id`=`cache_rating`.`user_id` 
+	                  FROM (cache_logs INNER JOIN caches ON (caches.cache_id = cache_logs.cache_id)) INNER JOIN user ON (cache_logs.user_id = user.user_id) INNER JOIN log_types ON (cache_logs.type = log_types.id) INNER JOIN cache_type ON (caches.type = cache_type.id) LEFT JOIN `cache_rating` ON `cache_logs`.`cache_id`=`cache_rating`.`cache_id` AND `cache_logs`.`user_id`=`cache_rating`.`user_id` 
 							LEFT JOIN	gk_item_waypoint ON gk_item_waypoint.wp = caches.wp_oc
 							LEFT JOIN	gk_item ON gk_item.id = gk_item_waypoint.id AND
 							gk_item.stateid<>1 AND gk_item.stateid<>4 AND gk_item.typeid<>2 AND gk_item.stateid !=5	
-					  WHERE cache_logs.deleted=0 AND `cache_logs`.`user_id`='" . sql_escape($_REQUEST['userid']) . "'
-	                   GROUP BY cache_logs.id ORDER BY cache_logs.date_created DESC LIMIT 20");
+					  WHERE cache_logs.deleted=0 AND `cache_logs`.`user_id`=&1
+	                   GROUP BY cache_logs.id ORDER BY cache_logs.date_created DESC LIMIT 20",$user_id);
 
 				for ($i = 0; $i < mysql_num_rows($rs); $i++)
 				{
 				$r = sql_fetch_array($rs);
 
-			$thisline = "<item>\n<title>{cachename}</title>\n<description>  Data: {date} - Wpis: {logtype} - Rekomendacja: {rate} - GeoKrety: {gk} </description>\n<link>http://www.opencaching.pl/viewlogs.php?cacheid={cacheid}</link>\n</item>\n";				
+			$thisline = "<item>\n<title>{cachename}</title>\n<description>  Data: {date} - Wpis: {logtype}{rate}{gk} </description>\n<link>http://www.opencaching.pl/viewlogs.php?cacheid={cacheid}</link>\n</item>\n";				
 
 
 
 			if ( $r['geokret_in'] !='0')
 					{
-					$thisline = str_replace('{gk}', 'Tak', $thisline);
+					$thisline = str_replace('{gk}', ' - GeoKrety: Tak', $thisline);
 					}
 					else
 					{
-					$thisline = str_replace('{gk}', 'Nie', $thisline);
+					$thisline = str_replace('{gk}', '', $thisline);
 					}					
 				
 				        //$rating_picture
 				if ($r['recommended'] == 1) 
 					{
-					$thisline = str_replace('{rate}', 'Tak', $thisline);
+					$thisline = str_replace('{rate}', ' - Rekomendacja: Tak', $thisline);
 					}
 					else
 					{
-					$thisline = str_replace('{rate}', 'Nie', $thisline);
+					$thisline = str_replace('{rate}', '', $thisline);
 					}
 			$thisline = str_replace('{cacheid}', $r['cache_id'], $thisline);;
 			$thisline = str_replace('{cachename}', htmlspecialchars($r['cache_name']), $thisline);
-			$thisline = str_replace('{logtype}', htmlspecialchars($r['log_name']), $thisline););
+			$thisline = str_replace('{logtype}', htmlspecialchars($r['log_name']), $thisline);
 			$thisline = str_replace('{date}', date('d-m-Y', strtotime($r['log_date'])), $thisline);
 
 			$content .= $thisline . "\n";
