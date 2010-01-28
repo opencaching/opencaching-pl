a2f2d40135f47806744e150815325f129f39878f
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-01-28 11:16:44 +0000

    correct sort of new caches by date published
    git-svn-id: https://opencaching-pl.googlecode.com/svn@1297 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- rss/newcaches.php
+++ rss/newcaches.php
@@ -22,7 +22,8 @@
 header('Content-type: application/xml; charset="utf-8"');		
 		$content = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<rss version=\"2.0\">\n<channel>\n<title>OC PL - Nowe skrzynki</title>\n<ttl>60</ttl><description>Nowe skrzynki</description>\n<link>http://www.opencaching.pl</link><image><title>OC PL - Nowe skrzynki</title>\n<url>http://www.opencaching.pl/images/oc.png</url><link>http://www.opencaching.pl</link><width>100</width><height>28</height></image>\n\n";
 
-		$rs = sql('SELECT `caches`.`cache_id` `cacheid`, `user`.`user_id` `userid`, `caches`.`country` `country`, `caches`.`name` `cachename`, `user`.`username` `username`, `caches`.`date_created` `date_created`, `cache_type`.`icon_large` `icon_large` FROM `caches`, `user`, `cache_type` WHERE `caches`.`status`=1 AND `caches`.`user_id`=`user`.`user_id` AND `caches`.`type`=`cache_type`.`id` ORDER BY `caches`.`date_created` DESC LIMIT ' . $perpage);
+		$rs = sql('SELECT `caches`.`cache_id` `cacheid`, `user`.`user_id` `userid`, `caches`.`country` `country`, `caches`.`name` `cachename`, `user`.`username` `username`, `caches`.`date_created` `date_created`, IF((`caches`.`date_hidden`>`caches`.`date_created`), `caches`.`date_hidden`, `caches`.`date_created`) AS `date`,
+`cache_type`.`icon_large` `icon_large` FROM `caches`, `user`, `cache_type` WHERE `caches`.`status`=1 AND `caches`.`user_id`=`user`.`user_id` AND `caches`.`type`=`cache_type`.`id` AND `caches`.`date_hidden` <= NOW() AND `caches`.`date_created` <= NOW() ORDER BY IF((`caches`.`date_hidden`>`caches`.`date_created`), `caches`.`date_hidden`, `caches`.`date_created`) DESC, `caches`.`cache_id` DESC LIMIT ' . $perpage);
 		while ($r = sql_fetch_array($rs))
 		{
 			$thisline = "<item>\n<title>{cachename}</title>\n<description>{cachename} - {username} - {date} - {country}</description>\n<link>http://www.opencaching.pl/viewcache.php?cacheid={cacheid}</link>\n</item>\n";
@@ -31,7 +32,7 @@ header('Content-type: application/xml; charset="utf-8"');
 			$thisline = str_replace('{userid}', $r['userid'], $thisline);
 			$thisline = str_replace('{cachename}', htmlspecialchars($r['cachename']), $thisline);
 			$thisline = str_replace('{username}', htmlspecialchars($r['username']), $thisline);
-			$thisline = str_replace('{date}', date('d-m-Y', strtotime($r['date_created'])), $thisline);
+			$thisline = str_replace('{date}', date('d-m-Y', strtotime($r['date'])), $thisline);
 			$thisline = str_replace('{country}', htmlspecialchars($r['country']), $thisline);
 			//$thisline = str_replace('{imglink}', 'tpl/stdstyle/images/'.getSmallCacheIcon($r['icon_large']), $thisline);
 
