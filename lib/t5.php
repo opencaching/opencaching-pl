67f58dbb1476b8c37b3b87716861c12c668d7ddd
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-01-28 09:22:00 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@1296 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- lib/t5.php
+++ lib/t5.php
@@ -31,11 +31,11 @@ if ($l2!=$l1)
 {
 $licznik=$licznik+1;
     echo "</td></tr><tr><td class=\"bgcolor2\" align=\"right\">&nbsp;&nbsp;<b>$licznik</b>&nbsp;&nbsp;</td><td class=\"bgcolor2\" align=\"right\">&nbsp;&nbsp;<b>$l1</b>&nbsp;&nbsp;</td>";
-    echo  "<td class=\"bgcolor2\"><a class=\"links\" href=\"viewprofile.php?userid=$line[user_id]\">".htmlspecialchars($line[username])."(R: $xy - <font color=\"green\">$x</font>/<font color=\"blue\">$y</font>)</a>";
+    echo  "<td class=\"bgcolor2\"><a class=\"links\" href=\"viewprofile.php?userid=$line[user_id]\">".htmlspecialchars($line[username])."(<font color=\"black\">R: $xy - </font><font color=\"green\">$x</font>/<font color=\"blue\">$y</font>)</a>";
     $l2=$l1;
 }
 else {
-    echo ", <a class=\"links\" href=\"viewprofile.php?userid=$line[user_id]\">".htmlspecialchars($line[username])."(R: $xy - <font color=\"green\">$x</font>/<font color=\"blue\">$y</font>)</a>";
+    echo ", <a class=\"links\" href=\"viewprofile.php?userid=$line[user_id]\">".htmlspecialchars($line[username])."(<font color=\"black\">R: $xy - </font><font color=\"green\">$x</font>/<font color=\"blue\">$y</font>)</a>";
     }
 
 }
