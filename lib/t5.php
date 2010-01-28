e139509c07396e9ce5ba9608968438448360c524
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-01-28 06:55:20 +0000

    update information in stat
    git-svn-id: https://opencaching-pl.googlecode.com/svn@1286 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- lib/t5.php
+++ lib/t5.php
@@ -7,10 +7,10 @@
 	require_once($rootpath . 'lib/common.inc.php');
 
   setlocale(LC_TIME, 'pl_PL.UTF-8');
-	echo '<table width="97%"><tr><td align="center"><center><b>Ranking użytkowników wg liczby otrzymanych rekomendacji</b></center></td></tr> </table>';
+	echo '<table width="97%"><tr><td align="center"><center><b>Ranking użytkowników wg liczby otrzymanych rekomendacji</b><br /><br />gdzie: (<b><font color="green">X</font>/<font color="blue">Y</font></b>) X- liczba skrzynek z rekomendacjami, Y - liczba wszystkich skrzynek<br /><br /></center></td></tr> </table>';
         echo '<table border="1" bgcolor="white" width="97%" style="font-size:11px; line-height:1.6em;">' . "\n";
 
-$t1="CREATE TEMPORARY TABLE ocpl.tmp (id INT(11) unsigned NOT NULL auto_increment PRIMARY KEY, count INT(11), username VARCHAR(60)) ENGINE=MEMORY SELECT count(*) count, user.username username, user.user_id user_id FROM caches,cache_rating,user WHERE `cache_rating`.`cache_id`=caches.cache_id AND caches.user_id=user.user_id GROUP BY `user`.`user_id` ORDER BY `count` DESC, `user`.`username` ASC"; 
+$t1="CREATE TEMPORARY TABLE ocpl.tmp (id INT(11) unsigned NOT NULL auto_increment PRIMARY KEY, count INT(11), username VARCHAR(60)) ENGINE=MEMORY SELECT count(*) count, user.username username, user.user_id user_id FROM caches,cache_rating,user WHERE `cache_rating`.`cache_id`=caches.cache_id AND caches.user_id=user.user_id AND caches.type <> 6 GROUP BY `user`.`user_id` ORDER BY `count` DESC, `user`.`username` ASC"; 
 //mysql_query("SET NAMES 'utf8'"); 
 $r=mysql_query($t1) or die(mysql_error());
 //mysql_query("SET NAMES 'utf8'"); 
@@ -23,15 +23,18 @@ $licznik=0;
 while ($line=mysql_fetch_array($r))
 {
 $l1=$line[count];
+$x=sqlValue("SELECT COUNT(*) FROM caches WHERE `caches`.`topratings` >= 1 AND caches.type <> 6 AND caches.user_id='$line[user_id]'",0);
+$y=sqlValue("SELECT COUNT(*) FROM caches WHERE user_id='$line[user_id]' AND status <> 4 AND status <> 5 AND status <> 6 AND type <> 6",0);
+//$y=mysql_result($ys);
 if ($l2!=$l1)
 {
 $licznik=$licznik+1;
     echo "</td></tr><tr><td class=\"bgcolor2\" align=\"right\">&nbsp;&nbsp;<b>$licznik</b>&nbsp;&nbsp;</td><td class=\"bgcolor2\" align=\"right\">&nbsp;&nbsp;<b>$l1</b>&nbsp;&nbsp;</td>";
-    echo  "<td class=\"bgcolor2\"><a class=\"links\" href=\"viewprofile.php?userid=$line[user_id]\">".htmlspecialchars($line[username])."</a>";
+    echo  "<td class=\"bgcolor2\"><a class=\"links\" href=\"viewprofile.php?userid=$line[user_id]\">".htmlspecialchars($line[username])."(<font color=\"green\">$x</font>/<font color=\"blue\">$y</font>)</a>";
     $l2=$l1;
 }
 else {
-    echo ", <a class=\"links\" href=\"viewprofile.php?userid=$line[user_id]\">".htmlspecialchars($line[username])."</a>";
+    echo ", <a class=\"links\" href=\"viewprofile.php?userid=$line[user_id]\">".htmlspecialchars($line[username])."(<font color=\"green\">$x</font>/<font color=\"blue\">$y</font>)</a>";
     }
 
 }
