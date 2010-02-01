e66d2e37c4c921fbf4b1934d369147c7e1bddfdc
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-02-01 12:37:26 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@1317 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

---
+++ tpl/stdstyle/articles/bog.tpl.php
@@ -0,0 +1,12 @@
+<div class="content2-pagetitle"><img src="tpl/stdstyle/images/blue/stat1.png" class="icon32" alt="{title_text}" title="{title_text}" />&nbsp;{{stats}}</div>
+<div style="line-height: 1.8em; font-size: 13px;">
+
+<?php 
+//prepare the templates and include all neccessary
+	require_once('./lib/common.inc.php');
+
+	if( $usr['admin'] )
+	{ 
+	echo '<br /><img src="graphs/BOGstat.php" alt="" title="" align="middle" />';
+	} ?>
+</div>
