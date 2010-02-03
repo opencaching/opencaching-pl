756c4b390450a54c540421eb3a70e8ae13c3025e
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-02-03 20:02:57 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@1348 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- util.sec/cache_locations/cache_location.class.php
+++ util.sec/cache_locations/cache_location.class.php
@@ -5,6 +5,21 @@
  *  Unicode Reminder メモ
  *
  *  Cleanup the table sys_temptables from entries of dead threads
+ 
+ 	sql_dropTrigger('cacheLocationBeforeInsert');
+	sql("CREATE TRIGGER `cacheLocationBeforeInsert` BEFORE INSERT ON `cache_location` 
+				FOR EACH ROW 
+					BEGIN 
+						SET NEW.`last_modified`=NOW();
+					END;");
+
+	sql_dropTrigger('cacheLocationBeforeUpdate');
+	sql("CREATE TRIGGER `cacheLocationBeforeUpdate` BEFORE UPDATE ON `cache_location` 
+				FOR EACH ROW 
+					BEGIN 
+						SET NEW.`last_modified`=NOW();
+					END;");
+
  *
  *                         run it once a day
  *
