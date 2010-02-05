8a459216aae1f7491e337df6d2523c2a22ef3822
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-02-05 11:53:45 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@1373 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/viewcache_print.tpl.php
+++ tpl/stdstyle/viewcache_print.tpl.php
@@ -49,23 +49,24 @@
 			<div class="content2-container">
 				<div class="content2-container-2col-left" id="viewcache-baseinfo">
 					<p class="content-title-noshade-size3">
-						<img src="tpl/stdstyle/images/blue/compas.png" class="icon32" alt="" title="" />
+						<img src="tpl/stdstyle/images/blue/kompas.png" class="icon32" alt="" title="" />
 						<b>{coords}</b> <br/>
-						<img src="tpl/stdstyle/images/blue/compas.png" class="icon32" alt="" title="" />
+						<img src="tpl/stdstyle/images/blue/kompas.png" class="icon32" alt="" title="" />
 						<b>{coords2}</b><br/>
-						<img src="tpl/stdstyle/images/blue/compas.png" class="icon32" alt="" title="" />
+						<img src="tpl/stdstyle/images/blue/kompas.png" class="icon32" alt="" title="" />
 						<b>{coords3}</b> <span class="content-title-noshade-size0">(WGS84)</span><br />
 					</p>
-					<p>
+					<p style="line-height: 1.6em;">
 <!--						{{location}}:<b><span style="color: rgb(88,144,168)"> {kraj} {dziubek} {woj}</span></b><br />-->
-						{{size}}: {cachesize}<br />
-						{hidetime_start}{{time}}: {search_time}&nbsp;&nbsp;{{length}}: {way_length}<br />{hidetime_end}
-						{{status_label}}: {status}<br />
-						{{date_hidden_label}}: {hidden_date}<br />
-						{{date_created_label}}: {date_created}<br />
-						{{last_modified_label}}: {last_modified}<br />
-						{{waypoint}}: {oc_waypoint}<br />
-						{hidelistingsites_start}{{listed_also_on}}: {listed_on}<br />{hidelistingsites_end}
+						<img src="tpl/stdstyle/images/free_icons/world.png" class="icon16" alt="" title="" align="middle" />&nbsp;{{location}}:<b><span style="color: rgb(88,144,168)"> {kraj} {dziubek1} {woj} {dziubek2} {miasto}</span></b><br /> 
+						<img src="tpl/stdstyle/images/free_icons/package.png" class="icon16" alt="" title="" align="middle" />&nbsp;{{size}}: <b>{cachesize}</b><br />
+						<img src="tpl/stdstyle/images/free_icons/page_go.png" class="icon16" alt="" title="" align="middle" />&nbsp;{{status_label}}: {status}<br />
+						{hidetime_start}<img src="tpl/stdstyle/images/free_icons/time.png" class="icon16" alt="" title="" align="middle" />&nbsp;{{time}}: {search_time}&nbsp;&nbsp;<img src="tpl/stdstyle/images/free_icons/arrow_switch.png" class="icon16" alt="" title="" align="middle" />&nbsp;{{length}}: {way_length}<br />{hidetime_end}		
+						<img src="tpl/stdstyle/images/free_icons/date_go.png" class="icon16" alt="" title="" align="middle" />&nbsp;{{date_hidden_label}}: {hidden_date}<br />
+						<img src="tpl/stdstyle/images/free_icons/date_add.png" class="icon16" alt="" title="" align="middle" />&nbsp;{{date_created_label}}: {date_created}<br />
+						<img src="tpl/stdstyle/images/free_icons/date_edit.png" class="icon16" alt="" title="" align="middle" />&nbsp;{{last_modified_label}}: {last_modified}<br />
+						<img src="tpl/stdstyle/images/free_icons/arrow_in.png" class="icon16" alt="" title="" align="middle" />&nbsp;{{waypoint}}: <b>{oc_waypoint}</b><br />
+						{hidelistingsites_start}<img src="tpl/stdstyle/images/free_icons/page_go.png" class="icon16" alt="" title="" align="middle" />&nbsp;{{listed_also_on}}: {listed_on}<br />{hidelistingsites_end}
 					</p>
 					<?php
 global $usr, $lang, $hide_coords;			
