2cd62caf0ffba1c3947cc9a58a4208559376e6fe
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-02-09 09:01:18 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@1392 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

---
+++ doc/sql/tables/search_words.sql
@@ -0,0 +1,13 @@
+SET NAMES 'utf8';
+DROP TABLE IF EXISTS `search_words`;
+CREATE TABLE IF NOT EXISTS `search_words` (
+  `id` bigint(20) unsigned NOT NULL auto_increment,
+  `word` varchar(255) NOT NULL,
+  `simple` varchar(30) NOT NULL,
+  `hash` int(10) unsigned NOT NULL,
+  PRIMARY KEY  (`id`),
+  KEY `hash` (`hash`,`word`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+
+
