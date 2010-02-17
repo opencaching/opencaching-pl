8cef4fbb53b3d06642a3aba871da3fb80114270b
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-02-17 08:47:19 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@1417 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- util.sec/cron/modules/cache_npa_areas.class.php
+++ util.sec/cron/modules/cache_npa_areas.class.php
@@ -35,40 +35,30 @@ class cache_npa_areas
 			exit;
 		}
 	/* end db connect */
-		$rsCache = sql("SELECT `cache_id`, `latitude`, `longitude` FROM `caches` WHERE `need_npa_recalc`=1");
+//		$rsCache = sql("SELECT `cache_id`, `latitude`, `longitude` FROM `caches` WHERE `need_npa_recalc`=1");
+		$rsCache = sql("SELECT `cache_id`, `latitude`, `longitude` FROM `caches`");
 		while ($rCache = mysql_fetch_assoc($rsCache))
 		{
-					$sql=sql("DELETE FROM `cache_npa_areas` WHERE `cache_id`='&1' AND `calculated`=1", $rCache['cache_id']);
-					mysql_query($sql);
-			$rsLayers = sql("SELECT `id`, `type_id`, AsText(`shape`) AS `geometry` FROM `npa_areas` WHERE `exclude`=0 AND WITHIN(GeomFromText('&1'), `shape`)", 'POINT(' . $rCache['longitude'] . ' ' . $rCache['latitude'] . ')');
+//					$sql=sql("DELETE FROM `cache_npa_areas` WHERE `cache_id`='&1' AND `calculated`=1", $rCache['cache_id']);
+//					mysql_query($sql);
+
+
+			$rsLayers = sql("SELECT `id`, `type_id`, AsText(`shape`) AS `geometry` FROM `npa_areas` WHERE WITHIN(GeomFromText('&1'), `shape`)", 'POINT(' . $rCache['longitude'] . ' ' . $rCache['latitude'] . ')');
+
 			while ($rLayers = mysql_fetch_assoc($rsLayers))
 			{
 				if (gis::ptInLineRing($rLayers['geometry'], 'POINT(' . $rCache['longitude'] . ' ' . $rCache['latitude'] . ')'))
 				{
-					$bExclude = false;
 
-					// prüfen, ob in ausgesparter Fläche
-					$rsExclude = sql("SELECT `id`, AsText(`shape`) AS `geometry` FROM `npa_areas` WHERE `exclude`=1 AND `type_id`='&1' AND WITHIN(GeomFromText('&2'), `shape`)", $rLayers['type_id'], 'POINT(' . $rCache['longitude'] . ' ' . $rCache['latitude'] . ')');
-					while (($rExclude = smyql_fetch_assoc($rsExclude)) && ($bExclude==false))
-					{
-						if (gis::ptInLineRing($rExclude['geometry'], 'POINT(' . $rCache['longitude'] . ' ' . $rCache['latitude'] . ')'))
-						{
-							$bExclude = true;
-						}
-					}
-					mysql_free_result($rsExclude);
-
-					if ($bExclude == false)
-					{
 						$sql=sql("INSERT INTO `cache_npa_areas` (`cache_id`, `npa_id`, `calculated`) VALUES ('&1', '&2', 1) ON DUPLICATE KEY UPDATE `calculated`=1", $rCache['cache_id'], $rLayers['id']);
 					mysql_query($sql);
-						}
+						
 				}
 			}
 			mysql_free_result($rsLayers);
 			
-			$sql=sql("UPDATE `caches` SET `need_npa_recalc`=0 WHERE `cache_id`='&1'", $rCache['cache_id']);
-					mysql_query($sql);
+//			$sql=sql("UPDATE `caches` SET `need_npa_recalc`=0 WHERE `cache_id`='&1'", $rCache['cache_id']);
+//					mysql_query($sql);
 		}
 		mysql_free_result($rsCache);
 	}
