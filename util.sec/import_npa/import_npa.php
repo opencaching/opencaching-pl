1ede2c5232ab142ec180743a7e9710e9ec805858
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-02-18 13:56:44 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@1429 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

---
+++ util.sec/import_npa/import_npa.php
@@ -0,0 +1,26 @@
+#!/usr/bin/php -q
+<?php
+/***************************************************************************
+ *  You can find the license in the docs directory
+ *
+ *  Unicode Reminder メモ
+ *
+ *  Import 
+ *
+
+ ***************************************************************************/
+
+  require('../lib/web.inc.php');
+  sql('USE `ocpl`');
+
+
+	$rsArea = sql("SELECT `OGR_FID`, `sitename` AS `sitename`, `sitetype` AS `sitetype`, `sitecode` AS `sitecode`, `SHAPE` AS `shape` FROM `gis`.`n2k100k_laea` WHERE `sitecode` LIKE 'PL%'");
+	while ($rArea = mysql_fetch_assoc($rsArea))
+	{
+		$sql=sql("INSERT INTO `npa_areas` (`sitename`, `sitecode`,`sitetype`,`shape`) VALUES ('&1', '&2', '&3','&4')", $rArea['sitename'], $rArea['sitecode'],$rArea['sitetype'], $rArea['shape']);
+		mysql_query($sql);
+
+	}
+	mysql_free_result($rsArea);
+	
+?>
