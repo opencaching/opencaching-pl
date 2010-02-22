d22027363d9b5036c34a822d0b97b14d4c5fc376
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-02-22 15:43:38 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@1450 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/selectlocid.tpl.php
+++ tpl/stdstyle/selectlocid.tpl.php
@@ -1,21 +1,15 @@
 <?php
-   // Unicode Reminder メモ
+   // Unicode Reminder メモśćą
 ?>
-<table border="0" cellspacing="0" cellpadding="0" width="100%">
-	<tr>
-		<td class="header">
-			<img src="tpl/stdstyle/images/cache/traditional.png" class="icon32" alt="Miejsce" title="Miejsce" align="middle" />
-			 <b>Liczba znnalezionych miejsc:</b> {resultscount}
-		</td>
-	</tr>
-</table>
+<div class="content2-pagetitle"><img src="tpl/stdstyle/images/blue/cache.png" class="icon32" alt="Wyszukiwanie" title="Suchergebnis" align="middle" />&nbsp;<b>Liczba znalezionych miejsc:</b> {resultscount}</div>
+
 <p>Dla podanych kryteriów poszukiwań nie znaleziono jednoznacznego wyniku. Proszę wybrać lokalizacje z listy:</p>
 <table border="0" cellspacing="0" cellpadding="0" width="100%">
-	<tr><td colspan="2" class="header-small" style="margin-bottom:1px;">{pages}</td></tr>
+	<tr><td colspan="2" class="header-small" style="margin-bottom:1px;"></td></tr>
 </table>
 <table border="0" cellspacing="0" cellpadding="0" width="100%">
 	{locations}
 </table>
 <table border="0" cellspacing="0" cellpadding="0" width="100%">
-	<tr><td colspan="2" class="header-small" style="margin-top:1px;">{pages}</td></tr>
+	<tr><td colspan="2" class="header-small" style="margin-top:1px;"></td></tr>
 </table>
