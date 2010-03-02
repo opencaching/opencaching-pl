401e1fccd50631d67a301a83371495740f6bb1e1
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-03-02 13:03:01 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@1474 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

---
+++ tpl/stdstyle/mycaches.tpl.php
@@ -0,0 +1,35 @@
+<?php
+/***************************************************************************
+	*                                         				                                
+	*   This program is free software; you can redistribute it and/or modify  	
+	*   it under the terms of the GNU General Public License as published by  
+	*   the Free Software Foundation; either version 2 of the License, or	    	
+	*   (at your option) any later version.
+	*   
+	*  UTF-8 ąść
+	***************************************************************************/
+?>
+<div class="content2-pagetitle"><img src="tpl/stdstyle/images/blue/cache.png" class="icon32" alt="" title="" align="middle"/>&nbsp;Skrzynki użytkownika: {username}</div>
+<!-- Text container -->
+	<p>
+		{pages}
+	</p>
+<table border="0" cellspacing="2" cellpadding="1" style="margin-left: 10px; line-height: 1.4em; font-size: 13px;" width="97%">
+<tr>
+<td colspan="2"><strong>Data ukrycia</strong></td>
+<td><strong>Status</strong></td>
+<td>&nbsp;</td>
+<td><strong>Cache</strong></td>
+</tr>
+<tr>
+<td colspan="5"><hr></hr></td>
+</tr>
+		{file_content}
+<tr>
+<td colspan="5"><hr></hr></td>
+</tr>
+</table>
+	<p>
+		{pages}
+	</p>
+
