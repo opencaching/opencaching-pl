b926a669b0f793c780af64445e54e4e1742c6949
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-03-08 08:12:38 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@1498 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

---
+++ lib/t6.php
@@ -0,0 +1,49 @@
+<?php
+	global $lang, $rootpath;
+
+	if (!isset($rootpath)) $rootpath = './';
+
+	//include template handling
+	require_once($rootpath . 'lib/common.inc.php');
+
+  setlocale(LC_TIME, 'pl_PL.UTF-8');
+
+  $rsU = sql('SELECT COUNT(*) `count` FROM (SELECT COUNT(caches.user_id) FROM `caches` WHERE `status`=1 GROUP BY `user_id`) `users_with_founds`');
+  $fC = sql('SELECT COUNT(*) `count` FROM `caches` WHERE `status`=1');
+    $rsUs = mysql_fetch_array($rsU);
+    $fCt = mysql_fetch_array($fC);
+ 
+ $rsfCR = sql("SELECT COUNT(*) `count`, `cache_location`.`adm3` region FROM `cache_location` INNER JOIN caches ON cache_location.cache_id=caches.cache_id WHERE `cache_location`.`code1`='PL' AND caches.status='1' GROUP BY `cache_location`.`code3` ORDER BY count DESC");
+
+	echo '<table width="97%"><tr><td align="center"><center><b> '.tr('number_of_caches_by_region').'</b> <br />'.tr('only_active_caches').'<br /> '.tr('users_who_created_caches').': ';
+	echo $rsUs[count]; 
+	echo ' .::. '.tr('number_of_active_caches').': ';
+	echo $fCt[count]; 
+	echo '</center></td></tr></table><br><table border="1" bgcolor="white" width="97%">' . "\n";
+
+ 
+echo '
+<tr class="bgcolor2">
+	<td width="20%" align="right">
+		&nbsp;&nbsp;<b>'.tr('number_of_caches').'</b>&nbsp;&nbsp;
+	</td>
+	<td align="right">
+		&nbsp;&nbsp;<b>'.tr('region').'</b>&nbsp;&nbsp;
+	</td>
+</tr><tr><td height="2"></td></tr>';
+
+while ($line=mysql_fetch_array($rsfCR))
+{
+    echo '<tr class="bgcolor2">
+			<td align="right">
+				&nbsp;&nbsp;<b>'.$line[count].'</b>&nbsp;&nbsp;
+			</td>
+			<td align="right">
+				&nbsp;&nbsp;<b>'.$line[region].'</b>&nbsp;&nbsp;
+			</td>';
+
+}
+
+	echo '</table>' . "\n";
+
+?>
