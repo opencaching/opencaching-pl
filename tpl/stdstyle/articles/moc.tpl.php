efe513b8edc6063de9e2e21751db1c69a98c19a0
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-03-15 06:28:54 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@1509 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/articles/moc.tpl.php
+++ tpl/stdstyle/articles/moc.tpl.php
@@ -3,7 +3,7 @@
 		<col width="100">
 	</colgroup>
 	<tr><td width="97%" class="content2-pagetitle">
-	<img src="tpl/stdstyle/images/blue/newspaper.png" class="icon32" alt="ABC" title="MOC" align="middle" />
+	<img src="tpl/stdstyle/images/blue/newspaper.png" class="icon32" alt="" title="MOC" align="middle" />
 	<font size="4">  <b>Magazyn <b>OPENCACHING</b></b></font></td></tr>
 	<tr><td class="spacer"></td></tr></table>
 
@@ -12,6 +12,6 @@
 <h1> Pobierz najnowszy numer Magazynu OPENCACHING</h1>
 </p><br /><br />
 <p>
-<img src="/doc/MoG.jpg" alt="" title="MOC" width="200" />
+<img src="/doc/MoG.jpg" alt="" title="Magazyn OPENCACHING" width="200" />
 </p>
 </center>
