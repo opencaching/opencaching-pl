ec427be96c285a1614942ca81ac99682d200c8da
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-03-15 06:15:50 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@1506 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

---
+++ tpl/stdstyle/articles/moc.tpl.php
@@ -0,0 +1,9 @@
+<table width="600" class="content">
+	<colgroup>
+		<col width="100">
+	</colgroup>
+	<tr><td width="600" class="content2-pagetitle">
+	<img src="tpl/stdstyle/images/blue/newspaper.png" class="icon32" alt="ABC" title="MOC" align="middle" />
+	<font size="4">  <b>Magazyn <b>OPENCACHING</b></b></font></td></tr>
+	<tr><td class="spacer"></td></tr></table>
+
