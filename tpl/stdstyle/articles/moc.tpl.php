f9bedb033b9cbf1af50976998869bb3bb1b3cfca
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-03-18 06:09:55 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@1514 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/articles/moc.tpl.php
+++ tpl/stdstyle/articles/moc.tpl.php
@@ -12,7 +12,7 @@
 <h1> Pobierz najnowszy numer Magazynu OPENCACHING</h1>
 </p><br /><br />
 <p>
-<img src="/doc/MoG.jpg" alt="" title="Magazyn OPENCACHING" width="200" />
+<img src="/doc/Moc-3.jpg" alt="" title="Magazyn OPENCACHING" width="200" />
 </p>
 <p><b>Liczba pobrań: </b></p>
 </center>
@@ -25,4 +25,9 @@
 	<img src="tpl/stdstyle/images/blue/archive.png" class="icon32" alt="" title="MOC" align="middle" />
 	<font size="4">  Numery archiwalne</b></font></td></tr>
 	<tr><td class="spacer"></td></tr></table>
+<br /><br /><center>
+<p>
+<img src="/doc/MoG.jpg" alt="" title="Magazyn OPENCACHING" width="200" />
+</p>
 
+</center>
