7156ea261f4a277a5ef17d98f8fe67b4cd85e79c
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-03-21 17:22:55 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@1516 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/newcache.tpl.php
+++ tpl/stdstyle/newcache.tpl.php
@@ -143,7 +143,7 @@ function nearbycache()
 		var lon_h = document.forms['newcacheform'].lon_h.value;
 		var lon_min = document.forms['newcacheform'].lon_min.value;
 				if (document.newcacheform.lat_h.value == "0" && document.newcacheform.lon_h.value == "0" ) {
-		alert("Proszę wprowadzić poprawne współrzędne punktu"); 
+		alert("Prosze wprowadziv poprawne wspolrzedne punktu"); 
 			} else {
 	window.open('http://www.opencaching.pl/search.php?searchto=searchbydistance&showresult=1&expert=0&output=HTML&sort=bydistance&f_userowner=0&f_userfound=0&f_inactive=0&distance=0.15&unit=km&latNS=' + latNS + '&lat_h=' + lat_h + '&lat_min=' + lat_min + '&lonEW=' + lonEW + '&lon_h=' + lon_h + '&lon_min=' + lon_min);
 	}
@@ -162,7 +162,7 @@ function nearbycachemapOC()
 		var lon ;
 		lon=(lon_h*1)+(lon_min/60);
 				if (document.newcacheform.lat_h.value == "0" && document.newcacheform.lon_h.value == "0" ) {
-		alert("Proszę wprowadzić poprawne współrzędne punktu"); 
+		alert("Prosze wprowadzic poprawne wspolrzedne punktu"); 
 			} else {
 		window.open('http://www.opencaching.pl/cachemap3.php?inputZoom=17&lat=' + lat + '&lon=' + lon);}
 	return false;
