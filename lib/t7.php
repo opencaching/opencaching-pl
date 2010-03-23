846267f5cde85694dfabd61f1c2167adb8df844b
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-03-23 17:18:26 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@1530 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- lib/t7.php
+++ lib/t7.php
@@ -9,23 +9,23 @@
   setlocale(LC_TIME, 'pl_PL.UTF-8');
 
   $rsU = sql('SELECT COUNT(*) `count` FROM (SELECT COUNT(cache_logs.user_id) FROM `cache_logs` WHERE `type`=1 AND `deleted`=0 GROUP BY `user_id`) `users_with_founds`');
-  $fC = sql('SELECT COUNT(*) `count` FROM `cache_logs` WHERE `type`=1 AND `deleted`=0');
+  $fC = sql('SELECT COUNT(*) `count` FROM `cache_logs` WHERE (`type`=1 OR `type`=2) AND `deleted`=0');
     $rsUs = mysql_fetch_array($rsU);
     $fCt = mysql_fetch_array($fC);
  
- $rsfCR = sql("SELECT COUNT(*) `count`, `cache_location`.`adm3` region FROM `cache_location` INNER JOIN cache_logs ON cache_location.cache_id=cache_logs.cache_id WHERE `cache_location`.`code1`='PL' AND cache_logs.type='1' AND cache_logs.deleted='0' GROUP BY `cache_location`.`code3` ORDER BY count DESC");
+ $rsfCR = sql("SELECT COUNT(*) `count`, `cache_location`.`adm3` region FROM `cache_location` INNER JOIN cache_logs ON cache_location.cache_id=cache_logs.cache_id WHERE `cache_location`.`code1`='PL' AND (cache_logs.type='1' OR cache_logs.type='2') AND cache_logs.deleted='0' GROUP BY `cache_location`.`code3` ORDER BY count DESC");
 
-	echo '<table width="97%"><tr><td align="center"><center><b> '.tr('activity_by_region').'</b> <br /><br /> '.tr('number_of_found_caches').': ';
+	echo '<table width="97%"><tr><td align="center"><center><b> '.tr('activity_by_region').'</b> <br /><br /> '.tr('number_of_visit_caches').' (znalezione + nie znalezione): <b>';
 	echo $fCt[count];
-	echo ' .::. '.tr('users_who_found_caches').': ';
+	echo ' </b><br />'.tr('users_active').':<b> ';
  	echo $rsUs[count]; 
-	echo '</center></td></tr></table><br><table border="1" bgcolor="white" width="97%">' . "\n";
+	echo '</b></center></td></tr></table><br><table border="1" bgcolor="white" width="97%">' . "\n";
 
  
 echo '
 <tr class="bgcolor2">
-	<td width="20%" align="center">
-		&nbsp;&nbsp;<b>'.tr('number_of_found_caches').'</b>&nbsp;&nbsp;
+	<td width="20%">
+		<center><b>'.tr('number_of_visit_caches').'</b></center>
 	</td>
 	<td align="right">
 		&nbsp;&nbsp;<b>'.tr('region').'</b>&nbsp;&nbsp;
