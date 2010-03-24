955965fa92bc80775aa2e619a679078e60b44be7
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-03-24 08:53:34 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@1540 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- news.php
+++ news.php
@@ -43,13 +43,13 @@
 		$rsNewsTopics = sql('SELECT `name`, `id` FROM `news_topics` ORDER BY `id` ASC');
 		while ($rNewsTopics = sql_fetch_array($rsNewsTopics))
 		{
-			$newscontent .= mb_ereg_replace('{topic}', htmlspecialchars($rNewsTopics['name'], ENT_COMPAT, 'UTF-8'), $tpl_newstopic_header) . "\n";
+//			$newscontent .= mb_ereg_replace('{topic}', htmlspecialchars($rNewsTopics['name'], ENT_COMPAT, 'UTF-8'), $tpl_newstopic_header) . "\n";
 		
 			$rsNews = sql("SELECT `date_posted`, `content` FROM `news` WHERE `topic`='&1' AND `display`=1 ORDER BY `date_posted` DESC LIMIT 0, 20", $rNewsTopics['id']);
 			while ($rNews = sql_fetch_array($rsNews))
 			{
 				$thisnewscontent = $tpl_newstopic_without_topic;
-				$thisnewscontent = mb_ereg_replace('{date}',date('d.m.Y H:i:s', strtotime($rNews['date_posted'])), $thisnewscontent);
+				$thisnewscontent = mb_ereg_replace('{date}',date('d.m.Y', strtotime($rNews['date_posted'])), $thisnewscontent);
 				$thisnewscontent = mb_ereg_replace('{message}', $rNews['content'], $thisnewscontent);
 				$newscontent .= $thisnewscontent . "\n";
 			}
