336ca1cde70fd367c05450f053bea6bdb68a48e1
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-03-26 16:12:43 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@1591 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/articles/beginner.tpl.php
+++ tpl/stdstyle/articles/beginner.tpl.php
@@ -8,7 +8,8 @@
 niezbędne informacje które przybliżą Ci zasady zakładania skrzynki, wyboru pojemników, typu skrzynki itd, dzięki temu Twoja skrzynka będzie poprawnie założona z ogólnie przyjętymi zasadami.
 </p>
 <br /><ul>
-<p style="font-size: 20px;"><a class="links" href="http://wiki.opencaching.pl/index.php/Dla_pocz%C4%85tkuj%C4%85cych" target="_blank">Dla początkujących</a></p>
-<p style="font-size: 20px;"><a class="links" href="http://wiki.opencaching.pl/index.php/Zak%C5%82adanie_skrzynki" target="_blank">Jak przygotować i schować skrzynkę</a></p>
-<p style="font-size: 20px;"><a class="links" href="http://wiki.opencaching.pl/index.php/Jako%C5%9B%C4%87_skrzynki" target="_blank">Jakość skrzynki</a></p></ul>
+<li><p style="font-size: 16px;"><a class="links" href="http://wiki.opencaching.pl/index.php/Dla_pocz%C4%85tkuj%C4%85cych" target="_blank">Dla początkujących</a></p>
+<li><p style="font-size: 16px;"><a class="links" href="http://wiki.opencaching.pl/index.php/Zak%C5%82adanie_skrzynki" target="_blank">Jak przygotować i schować skrzynkę</a></p>
+<li><p style="font-size: 16px;"><a class="links" href="http://wiki.opencaching.pl/index.php/Jako%C5%9B%C4%87_skrzynki" target="_blank">Jakość skrzynki</a></p>
+</ul>
 </div>
