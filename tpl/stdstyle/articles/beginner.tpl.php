154687270db14803b1521a5d23dec0e046c080a4
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-03-26 09:41:10 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@1576 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

---
+++ tpl/stdstyle/articles/beginner.tpl.php
@@ -0,0 +1,13 @@
+<table class="content" border="0">
+	<tr><td class="content2-pagetitle"><img src="tpl/stdstyle/images/blue/cache.png" class="icon32" alt="" title="{{new_cache}}" align="middle" /><font size="4" <b>Nowa skrzynka - materiały pomocne</b></font></td></tr>
+	<tr><td class="spacer"></td></tr>
+</table>
+<br /><br />
+<div style="width: 750px;">
+<p style="font-size: 14px; line-height:1.6em; text-align: justify;">Poniżej przedstawiamy wykaz materiałów pomocnych które zawierają 
+niezbędne informacje które przybliżą Ci zasady zakładania skrzynki, wyboru pojemników, typu skrzynki itd, dzięki temu Twoja skrzynka będzie poprawnie założona z ogólnie przyjętymi zasadami.
+</p>
+<br />
+<p style="font-size: 20px;"><a class="links" href="http://wiki.opencaching.pl/index.php/Dla_pocz%C4%85tkuj%C4%85cych" target="_blank">Dla początkujących</a></p>
+<p style="font-size: 20px;><a class="links" href="http://wiki.opencaching.pl/index.php/Zak%C5%82adanie_skrzynki" target="_blank">Jak przygotować i schować skrzynkę</a></p>
+<p style="font-size: 20px;><a class="links" href="http://wiki.opencaching.pl/index.php/Jako%C5%9B%C4%87_skrzynki" target="_blank">Jakość skrzynki</a></p></div>
