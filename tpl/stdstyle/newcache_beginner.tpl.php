500fc9d5bbc99ee2739f2e0af9888baeaa22d8bc
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-03-26 07:02:45 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@1571 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/newcache_beginner.tpl.php
+++ tpl/stdstyle/newcache_beginner.tpl.php
@@ -20,10 +20,13 @@
 <table class="content" border="0">
 	<tr><td class="content2-pagetitle"><img src="tpl/stdstyle/images/blue/cache.png" class="icon32" alt="" title="{{new_cache}}" align="middle" /><font size="4" <b>Nowa skrzynka</b></font></td></tr>
 	<tr><td class="spacer"></td></tr>
-	<tr>
-		<td>Ponieważ zkładasz pierwsze swoje skrzynki na serwise OpenCaching.pl polecamy Ci abyś zapoznał się z lekturą artykułów na naszym OpenGeoWiki serwisie. Artykuły te przybliżą Ci zasady zakładania skrzynki, wyboru pojemników, typu skrzynki itd dzieki temu Twoj askrzynka będzie poprawnie założona i OC Team który będzie weryfikował te skrzynki nie bedzie mial uwag. Pamietaj że Twoja skrzynka jest Twoja wizytówką i inni będą ją oceniać w logach Twojej skrzynki.</td>
-	</tr>
-<tr><td> Czy chcesz zapoznać się z proponowanymi artykułami zanim założysz swoja skrzynkę?</td></tr>
-<tr><td><a href="articles.php?page=beginner">TAK</a></td><td> <a href="newcache-test.php?beginner=no">NIE</a></td></tr>
-	<tr><td class="spacer"></td></tr>
 </table>
+<br /><br />
+<div style="width: 750px;">
+<p style="font-size: 14px; line-height:1.6em; text-align: justify;">Ponieważ zkładasz pierwsze swoje skrzynki na serwise OpenCaching.pl polecamy Ci abyś zapoznał się z lekturą artykułów na naszym <a class="links" href="wiki.opencaching.pl">OpenGeoWiki</a> serwisie. Artykuły te przybliżą Ci zasady zakładania skrzynki, wyboru pojemników, typu skrzynki itd dzieki temu Twoj skrzynka będzie poprawnie założona i OC Team który będzie weryfikował te skrzynki nie będzie miał uwag. Pamiętaj że Twoja skrzynka jest Twoja wizytówką i inni użytkownicy będą ją oceniać w logach Twojej skrzynki.
+</p>
+<br />
+<center>
+<H1> Czy chcesz zapoznać się z proponowanymi artykułami zanim założysz swoja skrzynkę?</H1>
+<p style="font-size: 20px;"><a class="links" href="articles.php?page=beginner">TAK</a>&nbsp;&nbsp;/&nbsp;&nbsp;<a class="links" href="newcache-test.php?beginner=no">NIE</a></p>
+</center></div>
