a7d5ebede8d5b6ac21968ba435ef1b9a4bf7ce86
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-03-26 05:53:34 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@1568 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

---
+++ tpl/stdstyle/newcache_beginner.tpl.php
@@ -0,0 +1,29 @@
+<?php
+
+/***************************************************************************
+	*
+	*   This program is free software; you can redistribute it and/or modify
+	*   it under the terms of the GNU General Public License as published by
+	*   the Free Software Foundation; either version 2 of the License, or
+	*   (at your option) any later version.
+	*
+	***************************************************************************/
+
+/****************************************************************************
+
+   Unicode Reminder ąść
+
+
+ ****************************************************************************/
+?>
+
+<table class="content" border="0">
+	<tr><td class="content2-pagetitle"><img src="tpl/stdstyle/images/blue/cache.png" class="icon32" alt="" title="{{new_cache}}" align="middle" /><font size="4" <b>Nowa skrzynka</b></font></td></tr>
+	<tr><td class="spacer"></td></tr>
+	<tr>
+		<td>Ponieważ zkładasz pierwsze swoje skrzynki na serwise OpenCaching.pl polecamy Ci abyś zapoznał się z lekturą artykułów na naszym OpenGeoWiki serwisie. Artykuły te przybliżą Ci zasady zakładania skrzynki, wyboru pojemników, typu skrzynki itd dzieki temu Twoj askrzynka będzie poprawnie założona i OC Team który będzie weryfikował te skrzynki nie bedzie mial uwag. Pamietaj że Twoja skrzynka jest Twoja wizytówką i inni będą ją oceniać w logach Twojej skrzynki.</td>
+	</tr>
+<tr><td> Czy chcesz zapoznać się z proponowanymi artykułami zanim założysz swoja skrzynkę?</td></tr>
+<tr><td><a href="articles.php?page=beginner">TAK</a></td><td> <a href="newcache-test.php?beginner=no">NIE</a></td></tr>
+	<tr><td class="spacer"></td></tr>
+</table>
