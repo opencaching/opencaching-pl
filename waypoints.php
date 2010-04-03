a29113ab00831637e8a558f2e596a968304f5c57
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-04-03 17:39:44 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@1616 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- waypoints.php
+++ waypoints.php
@@ -14,7 +14,7 @@ Add  additional waypoints to cache:
 
 ----------------------------------
 table db 'waypoints' struture 
-'cache_id', 'type', 'status' , 'longitude', 'latitude', 'name', 'describe', 
+'id','cache_id', 'type', 'status' , 'longitude', 'latitude', 'name', 'describe', 
 
 table db waypoints_type
 'id','pl','en','icon'
