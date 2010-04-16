eb0f0480bb8efa6cb324d0acb50f557972526c93
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-04-16 09:28:54 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@1730 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- doc/sql/tables/cache_notes.sql
+++ doc/sql/tables/cache_notes.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS `cache_notes` (
   `note_id` int(11) NOT NULL auto_increment,
   `cache_id` int(11) default NULL default '0',
   `date` timestamp NOT NULL default CURRENT_TIMESTAMP,
-  `desc_html` tinyint(1) NOT NULL default '1',
+  `desc_html` tinyint(1) NOT NULL default '0',
   `desc` text NOT NULL,
   PRIMARY KEY  (`note_id`,`cache_id`),
   KEY `cache_id` (`cache_id`)
