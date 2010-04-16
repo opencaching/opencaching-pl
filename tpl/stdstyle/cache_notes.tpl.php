62bfeb88aa8c3b883c0d4ea98865c44f0c84ad51
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-04-16 08:18:50 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@1719 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

---
+++ tpl/stdstyle/cache_notes.tpl.php
@@ -0,0 +1,23 @@
+<?php
+
+/***************************************************************************
+	*
+	*   This program is free software; you can redistribute it and/or modify
+	*   it under the terms of the GNU General Public License as published by
+	*   the Free Software Foundation; either version 2 of the License, or
+	*   (at your option) any later version.
+	*
+	***************************************************************************/
+
+?>
+<div class="content2-pagetitle"><img src="tpl/stdstyle/images/blue/logs.png" class="icon32" alt="" />&nbsp;Moje notatki dla skrzynek</div>
+<div class="buffer"></div>
+<p class="content-title-noshade"><img src="images/actions/list-add-20.png" align="middle" border="0" alt=""/>&nbsp;<a href="new_cachenotes.php?cacheid={cacheid}">Dodaj nową notatkę</a></p>
+<p>&nbsp;</p>
+	{no_notes_message}
+
+	{notes_content}
+
+
+
+
