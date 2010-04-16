70b1c3954575d6a7465afcf9a7b43b9dbcce4cef
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-04-16 08:48:14 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@1725 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/cache_notes.tpl.php
+++ tpl/stdstyle/cache_notes.tpl.php
@@ -13,7 +13,7 @@
 <div class="content2-pagetitle"><img src="tpl/stdstyle/images/blue/logs.png" class="icon32" alt="" />&nbsp;Moje notatki dla skrzynek</div>
 <div class="buffer"></div>
 <p>&nbsp;</p>
-	{no_notes_message}
+
 
 	{notes_content}
 
