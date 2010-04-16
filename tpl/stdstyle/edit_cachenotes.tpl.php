eb69be060b04e859f32f11dbf019219ba4425f2b
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-04-16 09:45:35 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@1733 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/edit_cachenotes.tpl.php
+++ tpl/stdstyle/edit_cachenotes.tpl.php
@@ -26,7 +26,7 @@
 	</tr>
 	<tr><td class="buffer" colspan="2"></td></tr>
 	<tr><td valign="top" class="content-title-noshade">&nbsp;</td>
-	<td valign="top"><input type="checkbox" name="newshtml" id="newshtml" value="1" style="border:0;" {newshtml} /> <label for="newshtml"><b>Treść notatki zawiera kod HTML</b></label></td></tr>
+	<td valign="top"><input type="checkbox" name="newshtml" id="newshtml" value="{newshtml}" style="border:0;" /> <label for="newshtml"><b>Treść notatki zawiera kod HTML</b></label></td></tr>
 
 
 <tr><td class="buffer" colspan="2"></td></tr>
