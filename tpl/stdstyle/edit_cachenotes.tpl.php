3109834d0f8a42230d4b3cca6eef50270f8bc065
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-04-16 05:17:15 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@1714 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

---
+++ tpl/stdstyle/edit_cachenotes.tpl.php
@@ -0,0 +1,44 @@
+<?php
+/***************************************************************************
+	*                                         				                                
+	*   This program is free software; you can redistribute it and/or modify  	
+	*   it under the terms of the GNU General Public License as published by  
+	*   the Free Software Foundation; either version 2 of the License, or	    	
+	*   (at your option) any later version.
+	*   
+	*  UTF-8 ąść
+	***************************************************************************/
+?>
+
+<div class="content2-pagetitle"><img src="tpl/stdstyle/images/blue/logs.png" class="icon32" alt="" />&nbsp;Edycja notatki dla skrzynki: {cache_name}</div>
+	{general_message}
+<form action="new_cachenotes.php" method="post" enctype="application/x-www-form-urlencoded" name="waypoints_form" dir="ltr">
+<input type="hidden" name="cacheid" value="{cacheid}"/>
+
+<table width="90%" class="table" border="0">
+	<tr><td class="buffer" colspan="2"></td></tr>
+	<tr>
+		<td valign="top" class="content-title-noshade">Treść notatki:</td>
+		<td class="content-title-noshade">
+		<textarea name="desc" rows="10" cols="80">{desc}</textarea>{desc_message}</td>
+	</td>
+	</tr>
+<tr><td><input type="checkbox" name="newshtml" id="newshtml" value="1" style="border:0;" {newshtml} /> <label for="newshtml">Treść zawiera kod HTML</label></td></tr>
+<tr>
+	<td>
+		<img src="tpl/stdstyle/images/misc/hint.gif" border="0" width="15" height="11" alt="Hinweis" title="Hinweis" align="middle" />
+		<span style="color:#666666; font-size:10px;">
+		</span>
+	</td>
+</tr>
+
+<tr><td class="buffer" colspan="2"></td></tr>
+	<tr>
+		<td colspan="2">
+			<button type="submit" name="delete" value="delete" style="font-size:14px;width:140px"><b>Usuń notatkę</b></button> &nbsp;&nbsp;
+			<button type="submit" name="submitform" value="submit" style="font-size:14px;width:140px"><b>Zapisz notatkę</b></button>
+		<br /><br /></td>
+	</tr>
+
+</table>
+</form>
