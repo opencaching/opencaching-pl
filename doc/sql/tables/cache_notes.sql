3c9fee5cfff930e88673704cc93752a94010bd76
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-04-16 11:40:52 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@1738 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- doc/sql/tables/cache_notes.sql
+++ doc/sql/tables/cache_notes.sql
@@ -2,10 +2,13 @@ SET NAMES 'utf8';
 DROP TABLE IF EXISTS `cache_notes`;
 CREATE TABLE IF NOT EXISTS `cache_notes` (
   `note_id` int(11) NOT NULL auto_increment,
-  `cache_id` int(11) default NULL default '0',
+  `cache_id` int(11) default NULL,
+  `user_id` int(11) default NULL,
   `date` timestamp NOT NULL default CURRENT_TIMESTAMP,
   `desc_html` tinyint(1) NOT NULL default '0',
   `desc` text NOT NULL,
-  PRIMARY KEY  (`note_id`,`cache_id`),
-  KEY `cache_id` (`cache_id`)
+  PRIMARY KEY  (`note_id`),
+  KEY `cache_id` (`cache_id`),
+  KEY `user_id` (`user_id`),
+  KEY `date` (`date`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 COMMENT='cache notes' ;
