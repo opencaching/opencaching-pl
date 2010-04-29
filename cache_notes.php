b87d387ca32d29bcbbdd115328c382cac689627f
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-04-29 06:37:35 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@1778 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- cache_notes.php
+++ cache_notes.php
@@ -33,6 +33,9 @@
 			if (isset($_REQUEST['cacheid']))
 			{
 			$cache_id = $_REQUEST['cacheid'];	
+			$cache_rs = sql("SELECT `caches`.`name` `cache_name` FROM `caches` WHERE cache_id=&1",$cache_id);
+			$cache_record = sql_fetch_array($cache_rs);
+			tpl_set_var('cache_name', 'dla skrzynki: '.$cache_record['cache_name']);
 
 			tpl_set_var('notes_links', '<br /><img src="tpl/stdstyle/images/free_icons/add.png" title=""/>&nbsp;<a class="links" href="new_cachenotes.php?cacheid='.$cache_id.'">Dodaj nową notatkę dla skrzynki</a> &nbsp;&nbsp;<img src="tpl/stdstyle/images/free_icons/table_go.png" title=""/>&nbsp;<a class="links" href="viewcache.php?cacheid='.$cache_id.'">Wróć do skrzynki</a><br /><br />');
 			$cache_param='AND cache_notes.cache_id='.$cache_id;	
@@ -48,7 +51,7 @@
 							{
 							
 							$notes_record = sql_fetch_array($notes_rs);
-				tpl_set_var('cache_name', 'dla skrzynki: '.$notes_record['cache_name']);
+
 				$note_desc = $notes_record['desc'];
 				if ($note_desc != ''){
 				if ($notes_record['desc_html'] == '0')
