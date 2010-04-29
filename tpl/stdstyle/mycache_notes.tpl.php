653096d62dec28d1eb4aa44e9bceef9fdf48b6fd
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-04-29 06:47:50 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@1782 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

---
+++ tpl/stdstyle/mycache_notes.tpl.php
@@ -0,0 +1,19 @@
+<?php
+
+/***************************************************************************
+	*
+	*   This program is free software; you can redistribute it and/or modify
+	*   it under the terms of the GNU General Public License as published by
+	*   the Free Software Foundation; either version 2 of the License, or
+	*   (at your option) any later version.
+	*
+	***************************************************************************/
+
+?>
+<div class="content2-pagetitle"><img src="tpl/stdstyle/images/blue/logs.png" class="icon32" alt="" />&nbsp;Moje notatki {cache_name}</div>
+<p>{notes_links}</p>
+	{notes_content}
+
+
+
+
