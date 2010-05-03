e03bd046aba8617c65d0f52f1bdb7bd66c0c7d29
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-05-03 09:26:25 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@1830 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- editcache-test.php
+++ editcache-test.php
@@ -1,14 +1,5 @@
 <?php
 /***************************************************************************
-																./editlog.php
-															-------------------
-		begin                : July 5 2004
-		copyright            : (C) 2004 The OpenCaching Group
-		forum contact at     : http://www.opencaching.com/phpBB2
-
-	***************************************************************************/
-
-/***************************************************************************
 	*
 	*   This program is free software; you can redistribute it and/or modify
 	*   it under the terms of the GNU General Public License as published by
