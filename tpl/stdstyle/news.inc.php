7bf6747e093660adc6d989452e7c4419c97d01a8
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-05-11 04:49:14 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@1855 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/news.inc.php
+++ tpl/stdstyle/news.inc.php
@@ -26,7 +26,7 @@
  ****************************************************************************/
  
  $tpl_newstopic = '<p class="content-title-noshade-size3">{date}</p><p class="content-txtbox-noshade-size5">{message}</p><br/>';
- $tpl_newstopic_without_topics = '<p class="content-title-noshade-size2"><span style="color: black;">{date}</span></p><p style="line-height: 1.6em;">{message} &nbsp; <b><a class="links" href="news.php">[więcej nowości ...]</a></b></p><p>&nbsp;</p>';
+ $tpl_newstopic_without_topics = '<p class="content-title-noshade-size2"><span style="color: black;">{date}</span></p><p style="line-height: 1.6em;">{message} <br/> <b><a class="links" href="news.php">[więcej nowości ...]</a></b></p><p>&nbsp;</p>';
  $tpl_newstopic_without_topic = '<p class="content-title-noshade-size1">{date}</p><p style="line-height: 1.6em;">{message}</p>';
  $tpl_newstopic_header = '<p class="content-title-noshade-size3"><b>Aktualności</b></p><div class="line-box"></div>';
 ?>
