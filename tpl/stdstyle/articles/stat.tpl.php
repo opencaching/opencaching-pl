2e3b12eabdfa1a2dfb1cf5b3326119929fc74550
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-05-13 10:05:12 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@1870 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/articles/stat.tpl.php
+++ tpl/stdstyle/articles/stat.tpl.php
@@ -4,11 +4,11 @@
 <img src="tpl/stdstyle/images/free_icons/chart_bar.png" class="icon16" alt="" title="" align="middle" />&nbsp;<a class="links" href="articles.php?page=s2">{{ranking_by_number_of_finds}}</a><br />
 <img src="tpl/stdstyle/images/free_icons/chart_bar.png" class="icon16" alt="" title="" align="middle" />&nbsp;<a class="links" href="articles.php?page=s6">{{ranking_by_number_of_recommnedations}}</a><br />
 <img src="tpl/stdstyle/images/free_icons/chart_bar.png" class="icon16" alt="" title="" align="middle" />&nbsp;<a class="links" href="articles.php?page=s3">{{user_ranking_by_number_of_finds_of_their_caches}}</a><br />
-<hr align ="left" style="border: 0; width: 80%;color: #000000; background-color: #000000;height: 1px;"/>
+<hr align ="left" style="border: 0; width: 500px;color: #000000; background-color: #000000;height: 1px;"/>
 <img src="tpl/stdstyle/images/free_icons/chart_bar.png" class="icon16" alt="" title="" align="middle" />&nbsp;<a class="links" href="articles.php?page=s4">{{cache_ranking_by_number_of_finds}}</a><br />
 <img src="tpl/stdstyle/images/free_icons/chart_bar.png" class="icon16" alt="" title="" align="middle" />&nbsp;<a class="links" href="articles.php?page=s11a">Ranking skrzynek według liczby odkryć w województwie</a><br />
 <?php if ($usr !== false){ echo '<img src="tpl/stdstyle/images/free_icons/chart_bar.png" class="icon16" alt="" title="" align="middle" />&nbsp;<a class="links" href="articles.php?page=s5">{{cache_ranking_by_calculated_indicator}}</a><br />';} ?>
-<hr align ="left" style="border: 0; width: 80%;color: #000000; background-color: #000000;height: 1px;"/>
+<hr align ="left" style="border: 0; width: 500px;color: #000000; background-color: #000000;height: 1px;"/>
 <img src="tpl/stdstyle/images/free_icons/chart_bar.png" class="icon16" alt="" title="" align="middle" />&nbsp;<a class="links" href="articles.php?page=s7">{{number_of_caches_by_region}}</a><br />
 <img src="tpl/stdstyle/images/free_icons/chart_bar.png" class="icon16" alt="" title="" align="middle" />&nbsp;<a class="links" href="articles.php?page=s8">{{activity_by_region}}</a><br />
 <br/>
