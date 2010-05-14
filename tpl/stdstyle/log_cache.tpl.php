c666d4226d79759dd2638ce581dfef0c645dbe57
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-05-14 11:50:56 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@1877 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/log_cache.tpl.php
+++ tpl/stdstyle/log_cache.tpl.php
@@ -115,6 +115,10 @@ function toogleLayer( whichLayer, val )
 <table class="content" style="font-size: 12px; line-height: 1.6em;">
 	<tr><td colspan="2">&nbsp;</td></tr>
 	<tr>
+		<td colspan="2"><div class="notice" style="width:500px;height:44px">Możesz także zgłośić problem związany z skrzynką do własciciela np że skrzynka wymaga serwisu (lub do Zespołu OC PL) wykorzystując ten formularz <img src="/tpl/stdstyle/images/blue/arrow.png" alt="" title=""/>&nbsp; <a class="links" href="reportcache.php?cacheid={cacheid}">Zgłoś problem</a> </div></td>
+	</tr>
+
+	<tr>
 		<td width="180px"><img src="tpl/stdstyle/images/free_icons/page_go.png" class="icon16" alt="" title="" align="middle" />&nbsp;<strong>{{type_of_log}}:</strong></td>
 		<td>
 			<select onLoad="javascript:toogleLayer('ocena');" name="logtype" onChange="toogleLayer('ocena');">
