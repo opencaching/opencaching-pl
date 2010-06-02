e59fe246c4d7ba57e8dfffb050c2ed0c8ad9d465
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-06-02 07:29:36 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@1904 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/add_rr_comment.tpl.php
+++ tpl/stdstyle/add_rr_comment.tpl.php
@@ -1,7 +1,7 @@
 <table class="content">
 <tr>
 	<td class="content2-pagetitle">
-		<img src="tpl/stdstyle/images/blue/write.png" class="icon32" alt="" /><font size="4"> {{add_comment_OCTeam_label}} "<a href="viewcache.php?cacheid={cacheid}">{cachename}</a>"</font>
+		<img src="tpl/stdstyle/images/blue/write.png" class="icon32" alt="" /><font size="4"> {{add_rr_comment}} "<a href="viewcache.php?cacheid={cacheid}">{cachename}</a>"</font>
 	</td>
 </tr>
 <tr><td class="spacer" colspan="2"></td></tr>
@@ -10,9 +10,9 @@
 <form action='viewcache.php' method='post'>
 	<input type='hidden' name='cacheid' value='{cacheid}' />
 	<textarea name='rr_comment' cols='80' rows='15'></textarea>
-	<br />
-	<input type='submit' value='Dodaj komentarz' />
-	<a href='viewcache.php?cacheid={cacheid}'>Anuluj</a>
+	<br/><br/>
+	<button type="submit" value="Dodaj komentarz" style="font-size:12px;width:140px"><b>Dodaj komentarz</b></button>&nbsp;&nbsp;
+	<button type="submit" value="Anuluj" style="font-size:12px;width:140px"><a class="links" href='viewcache.php?cacheid={cacheid}'>Anuluj</a></button>
 </form>
 	</td>
 </tr>
