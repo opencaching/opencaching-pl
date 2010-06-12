5800366fa31bc8774817f252ec1b03f30cf23538
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-06-12 11:48:49 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@1981 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/viewlogs.inc.php
+++ tpl/stdstyle/viewlogs.inc.php
@@ -21,7 +21,7 @@
 	$functions_middle = '&nbsp;';
 	$functions_end = '';
 
-	$decrypt_log = '<img  src="tpl/stdstyle/images/free_icons/lock_open.png" class="icon16" alt="" title=""/>&nbsp;<a href="viewcache-test.php?cacheid={cacheid}&amp;nocryptlog=1#{decrypt_log_id}" onclick="var ch = document.getElementById(\'{decrypt_log_id}\').childNodes;for(var i=0;i < ch.length;++i) {var e = ch[i]; decrypt(e);} return false;">'.tr('decrypt').'</a>';	
+	$decrypt_log = '<a href="viewcache-test.php?cacheid={cacheid}&amp;nocryptlog=1#{decrypt_log_id}"><img src="tpl/stdstyle/images/free_icons/lock_open.png" class="icon16" alt="" title=""/></a>&nbsp;<a href="viewcache-test.php?cacheid={cacheid}&amp;nocryptlog=1#{decrypt_log_id}" onclick="var ch = document.getElementById(\'{decrypt_log_id}\').childNodes;for(var i=0;i < ch.length;++i) {var e = ch[i]; decrypt(e);} return false;">'.tr('decrypt').'</a>';	
 	$edit_log = '<a href="editlog.php?logid={logid}"><img src="tpl/stdstyle/images/action/16x16-properties.png" class="icon16" alt="" title=""/></a>&nbsp;<a href="editlog.php?logid={logid}">Edycja</a>&nbsp;';
 	$remove_log = '<a href="removelog.php?logid={logid}"><img src="tpl/stdstyle/images/log/16x16-trash.png" class="icon16" alt="{{Trash}}" title=""/></a>&nbsp;<a href="removelog.php?logid={logid}">Usuń</a>&nbsp;';
 	$upload_picture = '<a href="newpic.php?objectid={logid}&amp;type=1"><img src="tpl/stdstyle/images/action/16x16-addimage.png" class="icon16" alt="" title=""/></a>&nbsp;<a href="newpic.php?objectid={logid}&amp;type=1">Dodaj obrazek</a>&nbsp;';
