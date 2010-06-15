56789a13e2cae663632af2d217d4bf81e566ae23
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-06-15 11:23:45 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@2007 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- doc/sql/tables/cache_moved.sql
+++ doc/sql/tables/cache_moved.sql
@@ -4,11 +4,12 @@ CREATE TABLE IF NOT EXISTS `cache_moved` (
   `id` int(11) NOT NULL auto_increment,
   `cache_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `date_modified` datetime NOT NULL,
+  `log_id` int(11) default NULL,
+  `date` datetime NOT NULL,
   `longitude` double NOT NULL,
   `latitude` double NOT NULL,
   PRIMARY KEY  (`id`),
-  KEY `cache_id` (`cache_id`,`date_modified`),
+  KEY `cache_id` (`cache_id`,`date`),
   KEY `longitude` (`longitude`),
   KEY `latitude` (`latitude`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
