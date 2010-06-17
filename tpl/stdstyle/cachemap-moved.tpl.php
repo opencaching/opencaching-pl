3b4fe0c1d084aa66d02afcf7b2f3947a06ed430c
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-06-17 09:55:45 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@2067 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/cachemap-moved.tpl.php
+++ tpl/stdstyle/cachemap-moved.tpl.php
@@ -5,24 +5,24 @@ function initialize() {
 if (GBrowserIsCompatible()) {
 
 var icon = new GIcon();
- icon.image = "templates/icons/yellow.png";
- icon.shadow = "templates/icons/shadow.png";
+ icon.image = "tpl/stdstyle/images/google_maps/yellow.png";
+ icon.shadow = "tpl/stdstyle/images/google_maps/shadow.png";
  icon.iconSize = new GSize(12, 20);
  icon.shadowSize = new GSize(22, 20);
  icon.iconAnchor = new GPoint(6, 20);
  icon.infoWindowAnchor = new GPoint(5, 1);
 
 var icon2 = new GIcon();
- icon2.image = "templates/icons/red.png";
- icon2.shadow = "templates/icons/shadow.png";
+ icon2.image = "tpl/stdstyle/images/google_maps/red.png";
+ icon2.shadow = "tpl/stdstyle/images/google_maps/shadow.png";
  icon2.iconSize = new GSize(12, 20);
  icon2.shadowSize = new GSize(22, 20);
  icon2.iconAnchor = new GPoint(6, 20);
  icon2.infoWindowAnchor = new GPoint(5, 1);
 
 var icon3 = new GIcon();
- icon3.image = "templates/icons/green.png";
- icon3.shadow = "templates/icons/shadow.png";
+ icon3.image = "tpl/stdstyle/images/google_maps/green.png";
+ icon3.shadow = "tpl/stdstyle/images/google_maps/shadow.png";
  icon3.iconSize = new GSize(12, 20);
  icon3.shadowSize = new GSize(22, 20);
  icon3.iconAnchor = new GPoint(6, 20);
@@ -33,18 +33,18 @@ var map0 = new GMap2(document.getElementById("map0"));
 map0.addControl(new GSmallMapControl());
 map0.addControl(new GMapTypeControl());
 
-map0.setCenter(new GLatLng(), 6);
+map0.setCenter(new GLatLng({mapcenterLat},{mapcenterLon}), 6);
 obszar = new GLatLngBounds(); 
 {route}
  
-   var nowyZoom = map0.getBoundsZoomLevel(obszar);  
-   var nowyPunkt = obszar.getCenter();  
-   map0.setCenter(nowyPunkt,nowyZoom-1);  
+//   var nowyZoom = map0.getBoundsZoomLevel(obszar);  
+//   var nowyPunkt = obszar.getCenter();  
+//   map0.setCenter(nowyPunkt,nowyZoom-1);  
       }
     }
 
 
 </script>
 </head>
-<div id="map0" style="width: 500px; height: 500px"></div>
+<div id="map0" style="width: 780px; height: 500px"></div>
 
