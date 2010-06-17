c172093c3fcb6f7b01601e8969fb13b4e1202667
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-06-17 12:12:26 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@2080 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/cachemap-moved.tpl.php
+++ tpl/stdstyle/cachemap-moved.tpl.php
@@ -4,7 +4,7 @@ function initialize() {
 
 if (GBrowserIsCompatible()) {
 
-var icon = new GIcon();
+var icon_yellow = new GIcon();
  icon.image = "tpl/stdstyle/images/google_maps/yellow.png";
  icon.shadow = "tpl/stdstyle/images/google_maps/shadow.png";
  icon.iconSize = new GSize(12, 20);
@@ -12,7 +12,7 @@ var icon = new GIcon();
  icon.iconAnchor = new GPoint(6, 20);
  icon.infoWindowAnchor = new GPoint(5, 1);
 
-var icon2 = new GIcon();
+var icon_red = new GIcon();
  icon2.image = "tpl/stdstyle/images/google_maps/red.png";
  icon2.shadow = "tpl/stdstyle/images/google_maps/shadow.png";
  icon2.iconSize = new GSize(12, 20);
@@ -20,7 +20,7 @@ var icon2 = new GIcon();
  icon2.iconAnchor = new GPoint(6, 20);
  icon2.infoWindowAnchor = new GPoint(5, 1);
 
-var icon3 = new GIcon();
+var icon_green = new GIcon();
  icon3.image = "tpl/stdstyle/images/google_maps/green.png";
  icon3.shadow = "tpl/stdstyle/images/google_maps/shadow.png";
  icon3.iconSize = new GSize(12, 20);
@@ -29,18 +29,14 @@ var icon3 = new GIcon();
  icon3.infoWindowAnchor = new GPoint(5, 1);
 
  
-var map0 = new GMap2(document.getElementById("map0"));
-map0.addControl(new GSmallMapControl());
-map0.addControl(new GMapTypeControl());
+var map = new GMap2(document.getElementById("map"));
+map.addControl(new GSmallMapControl());
+map.addControl(new GMapTypeControl());
 
-map0.setCenter(new GLatLng({mapcenterLat},{mapcenterLon}), 6);
-obszar = new GLatLngBounds(); 
+map.setCenter(new GLatLng({mapcenterLat},{mapcenterLon}), 6);
 {route}
 {points}
- 
-//   var nowyZoom = map0.getBoundsZoomLevel(obszar);  
-//   var nowyPunkt = obszar.getCenter();  
-//   map0.setCenter(nowyPunkt,nowyZoom-1);  
+
       }
     }
 
@@ -54,5 +50,5 @@ obszar = new GLatLngBounds();
 <img src="tpl/stdstyle/images/google_maps/yellow.png" alt="b" width="12" height="20" title="point" /> = {{trp_points}}
 <img src="tpl/stdstyle/images/google_maps/green.png" alt="c" width="12" height="20" title="end"/> = {{recently_seen}}
 </p><bre/><br/>
-<div id="map0" style="width: 780px; height: 500px"></div>
+<div id="map" style="width: 780px; height: 500px"></div>
 </div>
