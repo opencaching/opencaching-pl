d92d74389037c58e25eecf738b517ca23955be01
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-06-22 04:52:35 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@2157 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- editlog-test.php
+++ editlog-test.php
@@ -619,7 +619,7 @@
 					foreach ($log_types AS $type)
 					{
 						// skip if permission=O and not owner
-						if($type['permission'] == 'O' && $log_record['user_id'] != $cache_user_id)
+						if($type['permission'] == 'B' && $log_record['user_id'] != $cache_user_id)
 							continue;
 						
 						if( $log_record['logtype'] != $type['id'] && $log_record['cachestatus'] != 1 )
@@ -657,6 +657,7 @@
 							if ($usr['admin']) {
 							if($type['id'] == 4 || $type['id'] == 7 || $type['id'] == 8|| $type['id'] == 9 || $type['id'] == 10|| $type['id'] == 11 )
 							{ continue;}} else {
+							if ($log_record['user_id'] == $cache_user_id && $type['id'] == 1 ){continue;}
 							if($type['id'] == 4 || $type['id'] == 7 || $type['id'] == 8|| $type['id'] == 9 || $type['id'] == 10|| $type['id'] == 11 || $type['id'] == 12)
 							{ continue;}}												
 							}
