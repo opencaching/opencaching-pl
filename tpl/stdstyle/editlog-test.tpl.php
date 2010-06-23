bccc9aeaa71f15034fc0f3c32e7868839b96d273
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-06-23 10:25:36 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@2169 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/editlog-test.tpl.php
+++ tpl/stdstyle/editlog-test.tpl.php
@@ -87,7 +87,23 @@ function showHide(id){
 } 
 function chkMoved()
     {
-         	var el;
+			var mode = document.logform.logtype.value;
+			var iconarray = new Array();
+				iconarray['1'] = '16x16-found.png';
+				iconarray['2'] = '16x16-dnf.png';
+				iconarray['3'] = '16x16-note.png';
+				iconarray['4'] = '16x16-moved.png';
+				iconarray['5'] = '16x16-need-maintenance.png';
+				iconarray['6'] = '16x16-need-maintenance.png';
+				iconarray['7'] = '16x16-go.png';
+				iconarray['8'] = '16x16-wattend.png';
+				iconarray['9'] = '16x16-trash.png';
+				iconarray['10'] = '16x16-published.png';
+				iconarray['11'] = '16x16-temporary.png';
+				iconarray['12'] = '16x16-octeam.png';
+			var image_log = "/tpl/stdstyle/images/log/" + iconarray[mode];
+			document.logform.actionicon.src = image_log;
+         var el;
 	el='coord_table';
 	if (document.logform.logtype.value == "4") 		
 	{document.getElementById(el).style.display='block';
@@ -113,7 +129,7 @@ function chkMoved()
 			<!--<select name="logtype" onChange="return _chkFound()">-->
 			<select onload="javascript:toogleLayer('ocena');" name="logtype" onchange="javascript:toogleLayer('ocena');">
 				{logtypeoptions}
-			</select>
+			</select>&nbsp;&nbsp;<img name='actionicon' src='' align="top" alt="">
 		</td>
 	</tr>
 	<tr><td class="spacer" colspan="2"></td></tr>
