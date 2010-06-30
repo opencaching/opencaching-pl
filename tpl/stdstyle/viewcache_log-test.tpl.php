db6f345071a2ba2ad7479fdf27c0ed702e7aecc2
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-06-30 05:26:45 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@2231 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/viewcache_log-test.tpl.php
+++ tpl/stdstyle/viewcache_log-test.tpl.php
@@ -1,6 +1,6 @@
-	<div class="content-txtbox-noshade line-box {show_deleted}">
-	<p class="content-title-noshade-size1">{logimage} {date} {ratingimage} <a href="viewprofile.php?userid={userid}">{username}</a> {type} {logfunctions}</p> 
-	<div class="viewcache_log-content">
+	<div class="logs">
+	<p class="content-title-noshade-size1 {show_deleted}">{logimage} {date} {ratingimage} <a href="viewprofile.php?userid={userid}">{username}</a> {type} {logfunctions}</p> 
+	<div class="viewcache_log-content {show_deleted}">
 	{log_coordinates}
 	{logtext}
 	{logpictures}
