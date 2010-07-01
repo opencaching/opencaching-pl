7f3c22a359de9b881f78d0ce8f1f619ab2525646
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-07-01 04:57:59 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@2263 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- newlogs.php
+++ newlogs.php
@@ -175,7 +175,7 @@ $rs = sql("SELECT cache_logs.id, cache_logs.cache_id AS cache_id,
 				        //$rating_picture
 				if ($log_record['recommended'] == 1 && $log_record['log_type']==1) 
 					{
-					$file_content .= '<td width="22"><img src="images/rating-star.png" border="0" alt=""/></td>';
+					$file_content .= '<td width="22"><img src="images/rating-star.png" border="0" alt="" title="Rekomendacja/></td>';
 					}
 					else
 					{
