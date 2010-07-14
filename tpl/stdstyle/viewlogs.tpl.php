f53688af75f4ea18c2d7449afbf2c91c284754a1
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-07-14 08:31:26 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@2420 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/viewlogs.tpl.php
+++ tpl/stdstyle/viewlogs.tpl.php
@@ -37,7 +37,7 @@ if ((F == " ") || (F == "&dhbg;")) {} else {if (E) {F = convertROT13Char(F)}}}}D
 			<div class="content2-container bg-blue02">
 				<p class="content-title-noshade-size1">
 				<img src="tpl/stdstyle/images/blue/logs.png" class="icon32" alt=""/>
-			&nbsp;Wpisy do logów dla skrzynki <a href="viewcache.php?cacheid={cacheid}">{cachename}</a>&nbsp;&nbsp;
+			&nbsp;{{log_entries}} <a href="viewcache.php?cacheid={cacheid}">{cachename}</a>&nbsp;&nbsp;
 			&nbsp;&nbsp;
 			{found_icon} {founds}x 
 			{notfound_icon} {notfounds}x 
@@ -47,4 +47,4 @@ if ((F == " ") || (F == "&dhbg;")) {} else {if (E) {F = convertROT13Char(F)}}}}D
 <div class="content2-container" id="viewcache-logs">
 		{logs}
 </div>
-<div id="viewlogs-end">[<a href="viewcache.php?cacheid={cacheid}">Przejdź do skrzynki</a>]</div>
+<div id="viewlogs-end">[<a class="links" href="viewcache.php?cacheid={cacheid}">[{{back_to_the_geocache_listing}}]</a>]</div>
