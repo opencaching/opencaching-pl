16e34b5c83f068e443b283373640b5fcba077efb
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-07-16 14:11:57 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@2453 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- newlogs.php
+++ newlogs.php
@@ -186,12 +186,15 @@ $rs = sql("SELECT cache_logs.id, cache_logs.cache_id AS cache_id,
 				$file_content .= '<td width="22"><img src="tpl/stdstyle/images/' . $log_record['icon_small'] . '" border="0" alt="" /></td>';
 				$file_content .= '<td width="22"><a class="links" href="viewcache.php?cacheid=' . htmlspecialchars($log_record['cache_id'], ENT_COMPAT, 'UTF-8') . '"><img src="tpl/stdstyle/images/' . $log_record['cache_icon_small'] . '" border="0" alt="" title="Kliknij aby zobaczyć skrzynke" /></a></td>';
 				$file_content .= '<td><b><a class="links" href="viewlogs.php?logid=' . htmlspecialchars($log_record['id'], ENT_COMPAT, 'UTF-8') . '" onmouseover="Tip(\'';
-				$file_content .= '<b>'.$log_record['user_name'].'</b>:<br/>';
+				$file_content .= '<b>'.$log_record['user_name'].'</b>: &nbsp;<br/>';
+				if ( $log_record['encrypt']==1 && ($usr['userid'] == $log_record['cache_owner'] || $usr['userid'] != $log_record['user_id']))				
+				$file_content .='<img src="tpl/stdstyle/images/free_icons/lock_open.png" alt="" />';
 				$data = cleanup_text(str_replace("\r\n", " ", $log_record['log_text']));
+				$data= str_replace("\n", " ",$data);
 				if ( $log_record['encrypt']==1 && ($usr['userid'] == $log_record['cache_owner'] || $usr['userid'] != $log_record['user_id']))
 				//crypt the log ROT13, but keep HTML-Tags and Entities
 				$data = str_rot13_html($data);
-				$file_content .= str_replace("\n", " ",$data);
+				$file_content .= "<br/>".$data;
 				$file_content .= '\', PADDING,5, WIDTH,280,SHADOW,true)" onmouseout="UnTip()">' . htmlspecialchars($log_record['cache_name'], ENT_COMPAT, 'UTF-8') . '</a></b></td>';
 				$file_content .= '<td><b><a class="links" href="viewprofile.php?userid='. htmlspecialchars($log_record['user_id'], ENT_COMPAT, 'UTF-8') . '">' . htmlspecialchars($log_record['user_name'], ENT_COMPAT, 'UTF-8'). '</a></b></td>';
 				$file_content .= "</tr>";
