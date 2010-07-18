d68b87c63ab532aa094218114fe85603487e1941
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-07-18 13:35:29 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@2466 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- log-test.php
+++ log-test.php
@@ -682,15 +682,15 @@
 						if( $res2['type'] != 6 && ($usr['userid'] == $cache_user_id || $res['founds'] > 0 || $res2['status'] == 2 || $res2['status'] == 3 || $res2['status'] == 4 || $res2['status'] == 6))
 						{
 							if ($log_type==3) {
-							$logtypeoptions .= '<option value="3" selected="selected">Komentarz</option>' . "\n";} else {
-							$logtypeoptions .= '<option value="3">Komentarz</option>' . "\n";}
+							$logtypeoptions .= '<option value="3" selected="selected">'.tr('comment_log').'</option>' . "\n";} else {
+							$logtypeoptions .= '<option value="3">'.tr('comment_log').'</option>' . "\n";}
 
 							if  ($res2['type'] == 8){
 								if ($log_type==4) {							
-							$logtypeoptions .= '<option value="4" selected="selected">Przeniesiona</option>' . "\n";} else{ 
-							$logtypeoptions .= '<option value="4">Przeniesiona</option>' . "\n";}}
-							$logtypeoptions .= '<option value="5">Potrzebny serwis</option>' . "\n";
-							if ($usr['admin']==true){$logtypeoptions .= '<option value="12">Komentarz COG</option>' . "\n";}
+							$logtypeoptions .= '<option value="4" selected="selected">'.tr('moved_log').'</option>' . "\n";} else{ 
+							$logtypeoptions .= '<option value="4">'.tr('moved_log').'</option>' . "\n";}}
+							$logtypeoptions .= '<option value="5">'.tr('need_maintenance_log').'</option>' . "\n";
+							if ($usr['admin']==true){$logtypeoptions .= '<option value="12">'.tr('comment_octeam_log').'</option>' . "\n";}
 							break;
 						}
 						// skip if permission=O and not owner
