586e9577eeca8e0e54edde05281874f7c5c768e4
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-07-18 16:44:09 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@2473 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/searchuser.tpl.php
+++ tpl/stdstyle/searchuser.tpl.php
@@ -1,8 +1,10 @@
 <?
 global $usr;
 ?>
-<div class="content2-pagetitle"><img src="tpl/stdstyle/images/blue/search3.png" class="icon32" alt="" /><img src="tpl/stdstyle/images/blue/profile.png" class="icon32" alt="" />&nbsp;{{find_user}}</div>
+<div class="content2-pagetitle"><img src="tpl/stdstyle/images/blue/search3.png" class="icon32" alt="" /><img src="tpl/stdstyle/images/blue/profile.png" class="icon32" alt="" />&nbsp;{{search_user}}</div>
 <div class="errormsg">{not_found}</div>
 <form name="optionsform" style="display:inline;" action='searchuser.php' method="post">
-		<input type="text" name="username" value="{username}" class="input200" /> <input type="submit" value="Szukaj" class="formbuttons" /><br/>
+		<input type="text" name="username" value="{username}" class="input200" /> 
+			<button type="submit" name="submit" value="{{search}}" style="font-size:12px;width:140px;"/><b>{{search}}</b></button>
+<br/>
 </form>
