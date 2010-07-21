4abe2cc924f21310395263c4d4e277a9aa450f25
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-07-21 10:31:27 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@2534 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/viewpendings.tpl.php
+++ tpl/stdstyle/viewpendings.tpl.php
@@ -1,14 +1,14 @@
-<div class="content2-pagetitle"><img src="tpl/stdstyle/images/blue/aprove-cache.png" class="icon32" alt="" />&nbsp;Skrzynki oczekujące na akceptację</div>
+<div class="content2-pagetitle"><img src="tpl/stdstyle/images/blue/aprove-cache.png" class="icon32" alt="" />&nbsp;{{pendings}}</div>
 	<div class="buffer"></div>
 	{confirm}
 	<div class="buffer"></div>
 	<table border='1' class="table" width="97%">
 		<tr>
-			<th >Cache</th>
-			<th >Data utworzenia</th>
-			<th >Właściciel</th>
-			<th >Akcja</th>
-			<th >Przypisane do</th>
+			<th >Geocache</th>
+			<th >{{date_created}}</th>
+			<th >{{owner}}</th>
+			<th >{{action}}</th>
+			<th >{{assigned_to}}</th>
 		</tr>
 		{content}
 	</table>
