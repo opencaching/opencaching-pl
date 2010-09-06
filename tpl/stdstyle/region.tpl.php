79ba4b9d9b7ffc1039841d4fff5452a6f8e28a60
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-09-06 08:22:08 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@2658 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/region.tpl.php
+++ tpl/stdstyle/region.tpl.php
@@ -21,11 +21,12 @@
 <p>
 
 	<div style="margin-top:4px;">
-		<font size="2" color="#000080"><b>Województwo dla współrzędnych: {lat} {lon}</b></font>
+		<font size="2" color="#000080"><b>Województwo dla współrzędnych: <br/>
+ {lat_dir} {lat_deg_int}&deg; {lat_min_float}&#39;&nbsp;&nbsp;{lon_dir} {lon_deg_int}&deg; {lon_min_float}&#39;</b></font>
 	</div>
 
 	<div style="margin-top:16px;">
-		<font size="2" color="#000080">Kraj</font><br/>
+		<font size="2" color="#000080"><b>Kraj</b></font><br/>
 		{country}<br/>
 	</div>
 	<div style="margin-top:4px;">
