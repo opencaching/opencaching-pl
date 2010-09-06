d2d3d377942f136ce1b3f44bf1007067e83ba391
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-09-06 07:31:50 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@2649 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

---
+++ tpl/stdstyle/region.tpl.php
@@ -0,0 +1,37 @@
+<?php
+
+
+/***************************************************************************
+	*
+	*   This program is free software; you can redistribute it and/or modify
+	*   it under the terms of the GNU General Public License as published by
+	*   the Free Software Foundation; either version 2 of the License, or
+	*   (at your option) any later version.
+	*
+	***************************************************************************/
+
+/****************************************************************************
+
+   Unicode Reminder メモ
+
+
+ ****************************************************************************/
+?>
+
+<p>
+
+	<div style="margin-top:4px;">
+		<font size="2" color="#000080"><b>Województwo z współrzędnych</b></font>
+	</div>
+
+	<div style="margin-top:16px;">
+		<font size="2" color="#000080">Kraj</font><br/>
+		{country}<br/>
+	</div>
+	<div style="margin-top:4px;">
+		<font size="2" color="#000080"><b>Województwo</b></font><br/>
+		{region} <br/>
+	</div>
+
+
+</p>
