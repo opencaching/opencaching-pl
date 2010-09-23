b088af23f22d0c4d3d96e2d606a8ee72e5a49f47
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-09-23 18:40:02 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@2806 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- lib/settings-example.inc.php
+++ lib/settings-example.inc.php
@@ -65,11 +65,14 @@
          // display 1 or not 0 info for beginner when register new cache
         $beginner_info=0;
 	
-	// display online users in footer pages
+	// display online users in footer pages  0=off, 1 = on
 	$onlineusers=0;
 	
-        // Number of forst caches will be send to approve , disable 0, all caches max 99999999
-        $NEED_APPROVE_LIMIT=3;
+	//block register new cache before first find xx nuber of caches, value -1 off this feature
+	$NEED_FIND_LIMIT=-1;
+	
+    // Number of forst caches will be send to approve , disable 0, all caches max 999999999
+    $NEED_APPROVE_LIMIT=0;
 		
 	// Hide coordinates for users not login
 	$hide_coords = false;
