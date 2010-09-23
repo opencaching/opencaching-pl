372bb148d9c655b36e3f62d2d42d7c1d7bafc527
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-09-23 17:44:47 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@2799 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- newcache-limit.php
+++ newcache-limit.php
@@ -37,8 +37,7 @@
 		else
 		{			
              // display info for begginner about number of find caches to possible register first cache     
-			$rsnfc = sql("SELECT COUNT(`cache_logs`.`cache_id`) as num_fcaches FROM cache_logs,caches WHERE cache_logs.cache_id=caches.cache_id AND (caches.type='1' OR caches.type='2' OR caches.type='3' OR caches.type='7' OR caches.type='8') AND cache_logs.type='1' AND cache_logs.deleted='0' AND `user_id` = ".sql_escape($usr['userid'])." 
-										AND status <> 4 AND status <> 5 AND status <> 6");
+			$rsnfc = sql("SELECT COUNT(`cache_logs`.`cache_id`) as num_fcaches FROM cache_logs,caches WHERE cache_logs.cache_id=caches.cache_id AND (caches.type='1' OR caches.type='2' OR caches.type='3' OR caches.type='7' OR caches.type='8') AND cache_logs.type='1' AND cache_logs.deleted='0' AND `cache_logs`.`user_id` = ".sql_escape($usr['userid'])."");
 			$rec = sql_fetch_array($rsnfc);
 			$num_find_caches = $rec['num_fcaches'];
 			$rsnc = sql("SELECT COUNT(`caches`.`cache_id`) as num_caches FROM `caches` WHERE `user_id` = ".sql_escape($usr['userid'])." 
