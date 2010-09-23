4662c06ae95382744ce2d61108eb3930d60fa1fb
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-09-23 12:49:25 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@2793 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/newcache_beginner.tpl.php
+++ tpl/stdstyle/newcache_beginner.tpl.php
@@ -23,19 +23,18 @@
 </table>
 <br /><br />
 <div style="width: 750px;">
-<p style="font-size: 14px; line-height:1.6em; text-align: justify;">Ponieważ zakładasz pierwsze swoje skrzynki na OpenCaching.pl polecamy Ci abyś zapoznał się z lekturą artykułów na naszym serwisie <a class="links" href="wiki.opencaching.pl">OpenGeoWiki</a>. Artykuły te przybliżą Ci zasady zakładania skrzynki, wyboru pojemników, typu skrzynki itd, dzięki temu Twoja skrzynka będzie poprawnie założona i OC Team, który będzie weryfikował te skrzynki nie będzie miał uwag. Pamiętaj że Twoja skrzynka jest Twoją wizytówką i inni użytkownicy będą ją oceniać w logach Twojej skrzynki.
+<p style="font-size: 14px; line-height:1.6em; text-align: justify;">Aby zarejstrować nowe skrzynki musisz najpierw mieć odnalezionych 10 skrzynek sposród następujących typów: Tradycyjna, Multicache, Quiz, Mobilna, Inny typ.
 <br /><br />
 <b>Twoje pierwsze 3 skrzynki bedą weryfikowane przez Zespół OC PL. Po zatwierdzeniu skrzynki otrzymasz informacje via e-mail o tym fakcie i będziesz mógł ją opublikować poprzez edycje skrzynki i zmiane jej statusu. W przypadku uwag do skrzynki Zespół OC PL będzie się z Tobą kontaktował w sprawie uzpełnienia informacji lub zmian w skrzynce.</b>
 
 </p>
 <br />
 <center>
-<H1> Czy chcesz zapoznać się z proponowanymi artykułami zanim założysz swoją skrzynkę?</H1>
 
 <table border="0" width="200"><tr><td>
-<form action="articles.php?page=beginner" method="post" enctype="application/x-www-form-urlencoded" name="newcacheform" dir="ltr">
-<input type="hidden" name="beginner" value="0"/>
-<button style="width:120;height:120; font-size:20px;"><b>TAK</b></button></form></td><td><form action="newcache.php" method="post" enctype="application/x-www-form-urlencoded" name="newcacheform" dir="ltr"><input type="hidden" name="beginner" value="0"/><button style="width:120;height:120; font-size:20px;"><b>NIE</b></button></td></tr>
+<form action="newcache.php" method="post" enctype="application/x-www-form-urlencoded" name="newcacheform" dir="ltr"><input type=
+"hidden" name="beginner" value="0"/><button style="width:200;height:120; font-size:15px;"><b>Kontynuacja...</b></button></td></tr>
 </table>
-</center></div>
+</center>
+</div>
 
