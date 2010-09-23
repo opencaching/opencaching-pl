b7ad4631cb6773ae72986408f937b695393d5840
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-09-23 12:41:02 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@2792 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/newcache_forbidden.tpl.php
+++ tpl/stdstyle/newcache_forbidden.tpl.php
@@ -1,12 +1,4 @@
 <?php
-/***************************************************************************
-											./tpl/stdstyle/newcache.tpl.php
-															-------------------
-		begin                : June 24 2004
-		copyright            : (C) 2004 The OpenCaching Group
-		forum contact at     : http://www.opencaching.com/phpBB2
-
-	***************************************************************************/
 
 /***************************************************************************
 	*
@@ -23,44 +15,6 @@
 
 	 submit a new cache
 
-	 replacements:
-
-	     name
-	     typeoptions
-	     sizeoptions
-	     show_all_countries_submit
-	     show_all_langs_submit
-	     latNsel
-	     latSsel
-	     lat_h
-	     lat_min
-	     lonEsel
-	     lonWsel
-	     lon_h
-	     lon_min
-	     countryoptions
-	     langoptions
-	     short_desc
-	     desc
-	     desc_html
-	     desc_message
-	     hints
-	     hidden_since
-	     toschecked
-	     reset
-	     submit_value
-	     hidden_since_message
-	     tos_message
-	     show_all_countries
-	     show_all_langs
-	     difficulty_options
-	     terrain_options
-             effort_message
-             search_time
-             way_length
-             type_message
-             size_message
-             diff_message
 
  ****************************************************************************/
 ?>
@@ -69,7 +23,7 @@
 	<tr><td class="content2-pagetitle"><img src="tpl/stdstyle/images/blue/cache.png" class="icon32" alt="" title="{{new_cache}}" align="middle" /><font size="4" <b>Nowa skrzynka</b></font></td></tr>
 	<tr><td class="spacer"></td></tr>
 	<tr>
-		<td>Nie masz możliwości zakładania skrzynek w serwisie Opencaching.pl. W razie jakichkolwiek pytań, prosimy o kontakt na rr@opencaching.pl.</td>
+		<td>Nie masz możliwości zakładania skrzynek w serwisie Opencaching.pl. W razie jakichkolwiek pytań, prosimy o kontakt na cog@opencaching.pl.</td>
 	</tr>
 	<tr><td class="spacer"></td></tr>
 </table>
