de91adf9fb4bba9e2e9d046ba0c5d3196765cf2a
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-11-17 13:57:21 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@2871 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- gallery_cache.php
+++ gallery_cache.php
@@ -155,7 +155,7 @@
 
 					$cachepicturelines = '';
 					$append_atag='';
-					$rscpictures = sql("SELECT `pictures`.`url`, `pictures`.`title`, `pictures`.`uuid`, `pictures`.`user_id`,`pictures`.`object_id` FROM `pictures` WHERE `pictures`.`object_id`=&1 AND `pictures`.`object_type`=2", $cache_id);
+					$rscpictures = sql("SELECT `pictures`.`url`, `pictures`.`title`, `pictures`.`uuid`, `pictures`.`user_id`,`pictures`.`object_id` FROM `pictures` WHERE `pictures`.`object_id`=&1 AND `pictures`.`object_type`=2 ORDER BY `pictures`.`date_created` DESC", $cache_id);
 
 					for ($j = 0; $j < mysql_num_rows($rscpictures); $j++)
 					{
@@ -165,7 +165,7 @@
 
                         $thisline = mb_ereg_replace('{link}', $pic_crecord['url'], $thisline);
                         $thisline = mb_ereg_replace('{longdesc}', str_replace("uploads","uploads",$pic_crecord['url']), $thisline);
-	                $thisline = mb_ereg_replace('{imgsrc}', 'thumbs2.php?'.$showspoiler.'uuid=' . urlencode($pic_crecord['uuid']), $thisline);
+	                $thisline = mb_ereg_replace('{imgsrc}', 'thumbs.php?'.$showspoiler.'uuid=' . urlencode($pic_crecord['uuid']), $thisline);
                         $thisline = mb_ereg_replace('{log}', $tmplog_username .": ". htmlspecialchars($record['text'], ENT_COMPAT, 'UTF-8'), $thisline);
                         if ($pic_crecord['title']=="") {$title="link";} else { $title=htmlspecialchars($pic_crecord['title'],ENT_COMPAT,'UTF-8');}
                         $thisline = mb_ereg_replace('{title}', "<a class=links href=viewlogs.php?logid=".$pic_crecord['object_id'].">".$title."</a>", $thisline);
@@ -195,7 +195,7 @@
 
                         $thisline = mb_ereg_replace('{link}', $pic_record['url'], $thisline);
                         $thisline = mb_ereg_replace('{longdesc}', str_replace("uploads","uploads",$pic_record['url']), $thisline);
-	                $thisline = mb_ereg_replace('{imgsrc}', 'thumbs2.php?'.$showspoiler.'uuid=' . urlencode($pic_record['uuid']), $thisline);
+	                $thisline = mb_ereg_replace('{imgsrc}', 'thumbs.php?'.$showspoiler.'uuid=' . urlencode($pic_record['uuid']), $thisline);
                         $thisline = mb_ereg_replace('{log}', $tmplog_username .": ". htmlspecialchars($record['text'], ENT_COMPAT, 'UTF-8'), $thisline);
                         if ($pic_record['title']=="") {$title="link";} else { $title=htmlspecialchars($pic_record['title'],ENT_COMPAT,'UTF-8');}
                         $thisline = mb_ereg_replace('{title}', "<a class=links href=viewlogs.php?logid=".$pic_record['object_id'].">".$title."</a>", $thisline);
