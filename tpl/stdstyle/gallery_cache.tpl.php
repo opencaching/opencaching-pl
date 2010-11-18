b1db91da1feea57d317aca65f5225fc11767c9c4
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-11-18 11:57:51 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@2891 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/gallery_cache.tpl.php
+++ tpl/stdstyle/gallery_cache.tpl.php
@@ -23,6 +23,7 @@
 				</p>
 			</div>
 <div class="content2-container">
+{cache_images_start}
 <div class="logs">
 <span style="font-size:12px;font-weight:bold;float:left;">{{images_cache}}</span><br/><br/>
 <center><table>
@@ -31,13 +32,16 @@
 
 </td></tr></table></center>	
 </div>
+{cache_images_end}
+{logs_images_start}
 <div class="logs">
 <span style="font-size:12px;font-weight:bold;">{{images_logs}}</span><br/><br/>
-<table>
+<center><table>
 <tr><td>
 
 		{logpictures}
-	</td></tr></table>
+	</td></tr></table></center>
 </div>
+{logs_images_end}
 </div>
 <div id="viewlogs-end">[<a class="links" href="viewcache.php?cacheid={cacheid}">{{back_to_the_geocache_listing}}</a>]</div>
