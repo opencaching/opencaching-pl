2d016ce73ca8c94e4fcdc63875ef4443cd1e348a
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-12-22 09:49:03 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@3007 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/newlogs_myn.tpl.php
+++ tpl/stdstyle/newlogs_myn.tpl.php
@@ -1,5 +1,5 @@
 <script type="text/javascript" src="lib/js/wz_tooltip.js"></script>
-<div class="content2-pagetitle"><img src="tpl/stdstyle/images/blue/logs.png" class="icon32" alt="" title="New Log entry" align="middle"/>&nbsp;{{latest_logs_myn}}</div>
+<div class="content2-pagetitle"><img src="tpl/stdstyle/images/blue/logs.png" class="icon32" alt="" title="New Log entry" align="middle"/>&nbsp;{{latest_logs}}</div>
 <!-- Text container -->
 	<p>
 		{pages}
