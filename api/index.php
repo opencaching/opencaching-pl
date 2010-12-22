f37c334a08b91ba7103d78c011c120d5b0aa5d83
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-12-22 07:19:54 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@2992 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- api/index.php
+++ api/index.php
@@ -1,11 +1,2 @@
 <?php
- define(‘BASE_DIR’, ‘../lib/zend’);
-
-  require_once(BASE_DIR . ‘lib/bootstrap.php’);
-#
-   $main = Zend_Controller_Front::getInstance();
-
-   $main->throwExceptions(false);
-    //GO!
-    $main->dispatch();
 ?>
