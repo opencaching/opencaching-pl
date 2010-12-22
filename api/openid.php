5589fbbc20e431e4e16aa2bc96e568cd324e770e
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-12-22 07:28:24 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@2997 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- api/openid.php
+++ api/openid.php
@@ -79,9 +79,9 @@ if ($auth->hasIdentity()) {
 
 //echo "<html><body><script>";
 echo "$status";
-echo '<form method="post"><fieldset style="width: 400px;">
+echo '<form method="post"><fieldset style="width: 450px;">
 <legend>OpenID Login</legend>
-<img src="/images/openid.png" alt="" />&nbsp;<input type="text" name="openid_identifier" value="">
+<img src="/images/openid.png" align="middle" alt="" />&nbsp;&nbsp;<input type="text" name="openid_identifier" value="">
 <input type="submit" name="openid_action" value="Login">
 <input type="submit" name="openid_action" value="Logout">
 <input type="submit" name="openid_action" value="Test">
