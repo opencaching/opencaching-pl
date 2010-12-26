9c6cd8a80941a239f4e10e5961f4836fd71bc49c
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-12-26 14:45:39 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@3031 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- doc/sql/tables/route_points.sql
+++ doc/sql/tables/route_points.sql
@@ -1,13 +1,11 @@
 SET NAMES 'utf8';
 DROP TABLE IF EXISTS `route_points`;
 CREATE TABLE IF NOT EXISTS `route_points` (
-  `route_id` int(11) NOT NULL auto_increment,
-  `user_id` int(11) NOT NULL default '0',
+  `route_id` int(11) default NULL,
    `point_nr` tinyint(1) default NULL,
   `lon` double default NULL,
   `lat` double default NULL,
-  PRIMARY KEY  (`route_id`,`user_id`),
-  KEY `user_id` (`user_id`),
+  KEY  (`route_id`),
   KEY `lon` (`lon`,`lat`),
   KEY `lat` (`lat`),
   KEY `point_nr` (`point_nr`)
