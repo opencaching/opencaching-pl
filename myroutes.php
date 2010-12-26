ac63bf5c427a839ea1f3b22ac466db3e0819e168
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-12-26 10:44:12 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@3026 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- myroutes.php
+++ myroutes.php
@@ -1,14 +1,5 @@
 <?php
 /***************************************************************************
-																./mylist.php
-															-------------------
-		begin                : July 17 2004
-		copyright            : (C) 2004 The OpenCaching Group
-		forum contact at     : http://www.opencaching.com/phpBB2
-
-	***************************************************************************/
-
-/***************************************************************************
 	*
 	*   This program is free software; you can redistribute it and/or modify
 	*   it under the terms of the GNU General Public License as published by
@@ -40,45 +31,47 @@
 		else
 		{
 			include($stylepath . '/mylist.inc.php');
-			$tplname = 'mylist';
+			$tplname = 'myroutes';
 			
-			$bml_id = 0;
-			tpl_set_var('title_text', $standard_title);
-
-			if (count($_SESSION['print_list']) == 0) {
-				tpl_set_var('list', $no_list);
-				tpl_set_var('print_delete_list', '');
-				tpl_set_var('export_list', '');
-			} else {
-				$cache_list = implode(",", $_SESSION['print_list']);
-				$rs = sql("SELECT `caches`.`cache_id` AS `cache_id`, `caches`.`name` AS `name`, `caches`.`last_found` AS `last_found` FROM `caches` WHERE `caches`.`cache_id` IN (".sql_escape($cache_list).") ORDER BY `caches`.`name`");
-				$list = '';
-				for ($i = 0; $i < mysql_num_rows($rs); $i++)
-				{
-					$record = sql_fetch_array($rs);
-					$tmp_list = $i % 2 == 0 ? $list_e : $list_o;
-					$tmp_list = mb_ereg_replace('{cachename}', htmlspecialchars($record['name'], ENT_COMPAT, 'UTF-8'), $tmp_list);
-					if ($record['last_found'] == NULL || $record['last_found'] == '0000-00-00 00:00:00')
-					{
-						$tmp_list = mb_ereg_replace('{lastfound}', htmlspecialchars($no_found_date, ENT_COMPAT, 'UTF-8'), $tmp_list);
-					}
-						else
-						{
-							$tmp_list = mb_ereg_replace('{lastfound}', htmlspecialchars(strftime($dateformat, strtotime($record['last_found'])), ENT_COMPAT, 'UTF-8'), $tmp_list);
-						}
-
-						$tmp_list = mb_ereg_replace('{urlencode_cacheid}', htmlspecialchars(urlencode($record['cache_id']), ENT_COMPAT, 'UTF-8'), $tmp_list);
-						$tmp_list = mb_ereg_replace('{cacheid}', htmlspecialchars($record['cache_id'], ENT_COMPAT, 'UTF-8'), $tmp_list);
-
-						$list .= $tmp_list . "\n";
-					}
-					tpl_set_var('list', $list);
-					tpl_set_var('print_delete_list', $print_delete_list);
-					tpl_set_var('export_list', $export_list);
-				}
-				
+			
+// Read file KML with route		
+if ( !$error ) {
+exec("/usr/bin/gpsbabel -i kml -f $upload_filename -x interpolate,distance=0.25k -o kml -F $upload_filename");
+$xml = simplexml_load_file($upload_filename);
+	foreach ( $xml->Document->Folder as $xmlelement ) {
+	foreach ( $xmlelement->Folder as $folder ) {
+	foreach ( $folder->Placemark->LineString->coordinates as $coordinates ) {
+		if ( $coordinates ) {
+		$coords_raw = explode(" ",trim($coordinates));
+		foreach ( $coords_raw as $coords_raw_part ) {
+		if ( $coords_raw_part ) {
+		$coords_raw_parts = explode(",",$coords_raw_part);
+		$coords[] = $coords_raw_parts[0];
+		$coords[] = $coords_raw_parts[1];
+		}}}}}}}
+		// end of read
+//we get the point data in to an array called $points:
+if (!$error){
+		for( $i=0; $i<count($coords)-1; $i=$i+2 ) {
+		$points[] = array("lon"=>$coords[$i],"lat"=>$coords[$i+1]);
+		if ( ($coords[$i]+0==0) OR ($coords[$i+1]+0==0) ) {
+		$error .= "Invalid Co-ords found in import file.<br>\n";
+		break;
 			}
+		}
+	}
+// add it to the route_points database:
+$point_num = 0;
+foreach ($points as $point) {
+$point_num++;
+$query = "INSERT into route_points
+(route_id,point_number,point_lat,point_lon) ".
+"VALUES ($new_id,$point_num,".addslashes($point["lat"]).",".addslashes($point["lon"]).");";
+$result=sql($query);
+}
+	
 		
+		}
 	}
 
 	//make the template and send it out
