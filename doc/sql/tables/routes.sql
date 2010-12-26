51aadc638baa286c681a49af32c0bbdc9421d988
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-12-26 14:46:12 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@3032 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

---
+++ doc/sql/tables/routes.sql
@@ -0,0 +1,12 @@
+SET NAMES 'utf8';
+DROP TABLE IF EXISTS `routes`;
+CREATE TABLE IF NOT EXISTS `routes` (
+  `route_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `name` varchar(60) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `radius` tinyint(1) default NULL,
+  PRIMARY KEY  (`route_id`),
+  KEY `name` (`name`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='routes name';
