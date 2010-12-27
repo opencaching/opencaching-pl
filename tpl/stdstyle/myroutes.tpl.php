d294d2c9e3f16501d6681c35753035c7208e944d
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-12-27 16:43:51 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@3047 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/myroutes.tpl.php
+++ tpl/stdstyle/myroutes.tpl.php
@@ -9,13 +9,12 @@
 	*  UTF-8 ąść
 	***************************************************************************/
 ?>
-
 <div class="content2-pagetitle"><img src="tpl/stdstyle/images/blue/route.png" class="icon32" alt="" />&nbsp;{{myroutes}}</div>
 <br/><p>&nbsp;&nbsp;<img src="tpl/stdstyle/images/free_icons/add.png" title=""/>&nbsp;<a class="links" href="myroutes_add.php">{{add_new_route}}</a></p>
 <br/>
-	{content}
-	
-<br/><br/><br/><div class="notice">{{see_desc_myroute}} <a href="http://wiki.opencaching.pl/index.php/Moje_trasy" target="_blank">{{here}}</a></div>
+{content}
+<br/><div>&nbsp;</div>
+<div class="notice">{{see_desc_myroute}} <a href="http://wiki.opencaching.pl/index.php/Moje_trasy" target="_blank">{{here}}</a></div>
 
 
 
