a8d084c375fde6c3d63b264c2cadceb504d1322f
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2010-12-31 09:10:55 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@3065 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- doc/sql/tables/routes.sql
+++ doc/sql/tables/routes.sql
@@ -5,8 +5,8 @@ CREATE TABLE IF NOT EXISTS `routes` (
   `user_id` int(11) DEFAULT NULL,
   `name` varchar(60) DEFAULT NULL,
   `description` varchar(255) DEFAULT NULL,
-  `radius` tinyint(1) default NULL,
-   `options` blob NOT NULL,
+  `radius` float DEFAULT NULL,
+  `options` blob NOT NULL,
   PRIMARY KEY  (`route_id`),
   KEY `name` (`name`),
   KEY `user_id` (`user_id`)
