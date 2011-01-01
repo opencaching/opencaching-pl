8dbb6e091919d234358057a873223b634b0ccee8
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2011-01-01 15:43:15 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@3080 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- test.php
+++ test.php
@@ -1,70 +1,126 @@
-<?php
-	require_once('./lib/common.inc.php');
-	
-function distance($lat1, $lon1, $lat2, $lon2, $unit) { 
 
-  $theta = $lon1 - $lon2; 
-  $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta)); 
-  $dist = acos($dist); 
-  $dist = rad2deg($dist); 
-  $miles = $dist * 60 * 1.1515;
-  if ($miles<0) $miles=0;
-  $unit = strtoupper($unit);
-
-  if ($unit == "K") {
-    return ($miles * 1.609344); 
-  } else if ($unit == "N") {
-      return ($miles * 0.8684);
-    } else {
-        return $miles;
-      }
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+<title>Google Maps JavaScript API Example: Advanced Directions</title>
+<script src=" http://maps.google.com/?file=api&v=2.x&key=ABQIAAAAKzfMHoyn1s1VSuNTwlFfzhTqTxhHAgqKNaAck663VX5jr8OSJBQrTiL58t4Rt3olsGRlxSuqVkU5Xg"
+type="text/javascript"></script>
+<style type="text/css">
+body {
+font-family: Verdana, Arial, sans serif;
+font-size: 11px;
+margin: 2px;
+}
+table.directions th {
+background-color:#EEEEEE;
 }
 
-$distance=0;
-	$route_id="15";
-			$rsc = sql("SELECT `lat`,`lon`
-					FROM `route_points` 
-					WHERE `route_id`='&1'
-			        ORDER BY point_nr", $route_id);
-			if (mysql_num_rows($rsc) !=0)
-			{	$record = sql_fetch_array($rsc);
-				$firsty=$record['lon'];
-				$firtsx=$record['lat'];
-			for ($i = 1; $i < mysql_num_rows($rsc); $i++)
-			{
-				$record = sql_fetch_array($rsc);
-				$secy=$record['lon'];
-				$secx=$record['lat'];
-				$distance1=calcDistance($firtsx,$firsty,$secx,$secy,1);
-				$distance=$distance+$distance1;
-				$firsty=$secy;
-				$firtsx=$secx;
-			}
-			$distance=round($distance,1);
-			sql("UPDATE `routes` SET `length`='&1' WHERE `route_id`='&2'",$distance,$route_id);
-			}
+img {
+color: #000000;
+}
+</style>
+<script type="text/javascript">
 
-/*
-function calcLatLong($long, $lat, $distance, $bearing) {
- $EARTH_RADIUS_EQUATOR = 6378140.0;
- $RADIAN = 180 / pi();
- $b = $bearing / $RADIAN;
- $long = $long / $RADIAN;
- $lat = $lat / $RADIAN;
- $f = 1/298.257;
- $e = 0.08181922;
-	
- $R = $EARTH_RADIUS_EQUATOR * (1 - $e * $e) / pow( (1 - $e*$e * pow(sin($lat),2)), 1.5);	
- $psi = $distance/$R;
- $phi = pi()/2 - $lat;
- $arccos = cos($psi) * cos($phi) + sin($psi) * sin($phi) * cos($b);
- $latA = (pi()/2 - acos($arccos)) * $RADIAN;
+var map;
+var gdir;
+var geocoder = null;
+var addressMarker;
 
- $arcsin = sin($b) * sin($psi) / sin($phi);
- $longA = ($long - asin($arcsin)) * $RADIAN;
- return array('longitude' => $longA, 'latitude' => $latA);
+function initialize() {
+if (GBrowserIsCompatible()) {
+map = new GMap2(document.getElementById("map_canvas"));
+gdir = new GDirections(map, document.getElementById("directions"));
+GEvent.addListener(gdir, "load", onGDirectionsLoad);
+GEvent.addListener(gdir, "error", handleErrors);
+
+setDirections("San Francisco", "Mountain View", "en_US");
+}
 }
 
-print_r(calcLatLong(17, 52, 129289, 30.34));
-*/
-?>
+function setDirections(fromAddress, toAddress, locale) {
+gdir.load("from: " + fromAddress + " to: " + toAddress,
+{ "locale": locale });
+}
+
+function handleErrors(){
+if (gdir.getStatus().code == G_GEO_UNKNOWN_ADDRESS)
+alert("No corresponding geographic location could be found for one of the specified addresses. This may be due to the fact that the address is relatively new, or it may be incorrect.\nError code: " + gdir.getStatus().code);
+else if (gdir.getStatus().code == G_GEO_SERVER_ERROR)
+alert("A geocoding or directions request could not be successfully processed, yet the exact reason for the failure is not known.\n Error code: " + gdir.getStatus().code);
+
+else if (gdir.getStatus().code == G_GEO_MISSING_QUERY)
+alert("The HTTP q parameter was either missing or had no value. For geocoder requests, this means that an empty address was specified as input. For directions requests, this means that no query was specified in the input.\n Error code: " + gdir.getStatus().code);
+
+// else if (gdir.getStatus().code == G_UNAVAILABLE_ADDRESS) <--- Doc bug... this is either not defined, or Doc is wrong
+// alert("The geocode for the given address or the route for the given directions query cannot be returned due to legal or contractual reasons.\n Error code: " + gdir.getStatus().code);
+
+else if (gdir.getStatus().code == G_GEO_BAD_KEY)
+alert("The given key is either invalid or does not match the domain for which it was given. \n Error code: " + gdir.getStatus().code);
+
+else if (gdir.getStatus().code == G_GEO_BAD_REQUEST)
+alert("A directions request could not be successfully parsed.\n Error code: " + gdir.getStatus().code);
+
+else alert("An unknown error occurred.");
+
+}
+
+function onGDirectionsLoad(){
+// Use this function to access information about the latest load()
+// results.
+
+// e.g.
+// document.getElementById("getStatus").innerHTML = gdir.getStatus().code;
+// and yada yada yada...
+}
+</script>
+
+</head>
+<body onload="initialize()" onunload="GUnload()">
+
+<h2>Maps API Directions Illustrated</h2>
+<form action="#" onsubmit="setDirections(this.from.value, this.to.value, this.locale.value); return false">
+
+<table>
+
+<tr><th align="right">From:&nbsp;</th>
+
+<td><input type="text" size="25" id="fromAddress" name="from"
+value="San Francisco"/></td>
+<th align="right">&nbsp;&nbsp;To:&nbsp;</th>
+<td align="right"><input type="text" size="25" id="toAddress" name="to"
+value="Mountain View" /></td></tr>
+
+<tr><th>Language:&nbsp;</th>
+<td colspan="3"><select id="locale" name="locale">
+
+<option value="en" selected>English</option>
+
+<option value="fr">French</option>
+
+<option value="de">German</option>
+<option value="pl">Polski</option>
+<option value="es">Spanish</option>
+</select>
+
+<input name="submit" type="submit" value="Get Directions!" />
+
+</td></tr>
+</table>
+
+
+</form>
+
+<br/>
+<table class="directions">
+<tr><th>Formatted Directions</th><th>Map</th></tr>
+
+<tr>
+<td valign="top"><div id="directions" style="width: 275px"></div></td>
+<td valign="top"><div id="map_canvas" style="width: 310px; height: 400px"></div></td>
+
+</tr>
+</table>
+</body>
+</html>
