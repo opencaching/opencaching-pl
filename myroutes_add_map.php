75163ce3eca188870ebb62918d3fa03795c5b414
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2011-01-03 09:43:26 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@3124 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- myroutes_add_map.php
+++ myroutes_add_map.php
@@ -31,7 +31,9 @@
 		else
 		{
 		
-			$tplname = 'myroutes_add';
+			$tplname = 'myroutes_add_map';
+			tpl_set_var('bodyMod', ' onload="initialize()" onunload="GUnload()"');
+
 			$user_id = $usr['userid'];
 			
 			
@@ -46,84 +48,19 @@
 
 				if (isset($_POST['back']))
 				{	
-							tpl_redirect('myroutes.php');
-							exit;
-				}				
-				// start submit
-				if (isset($_POST['submitform']))
-				{
-// insert route name		
-					
-						sql("INSERT INTO `routes` (
-													`route_id`,
-													`user_id`,
-													`name`,
-													`description`,
-													`radius` 
-												) VALUES (
-													'', '&1', '&2', '&3', '&4')",
-												$user_id,
-												$name,
-												$desc,
-												$radius);
-												
-$upload_filename=$_FILES['file']['tmp_name'];	
+				tpl_redirect('myroutes.php');
+				exit;
+				}
 
-// get route_id
-$route_id=sqlValue("SELECT route_id FROM `routes` WHERE name='$name' AND description='$desc' AND user_id=$user_id",0);
+// load xml from google maps
 
-// Read file KML with route, load in the KML file through the my_routes page, and run that KML file through GPSBABEL which has a tool interpolate data points in the route.	
-if ( !$error ) {
-exec("/usr/local/bin/gpsbabel -i kml,units=m -f ".$upload_filename." -x interpolate,distance=0.25k -o kml,units=m -F ".$upload_filename."");
-$xml = simplexml_load_file($upload_filename);
 
-	// get length route
-foreach ($xml->Document->Folder as $f){
-foreach ($f->Folder as $folder){
-$dis=$folder->description;
-$dis1=explode(" ",trim($dis));
-$len=(float)$dis1[27];
-	sql("UPDATE `routes` SET `length`='&1' WHERE `route_id`='&2'",$len,$route_id);
-	}}
 
-	
-	foreach ( $xml->Document->Folder as $xmlelement ) {
-	foreach ( $xmlelement->Folder as $folder ) {
-	foreach ( $folder->Placemark->LineString->coordinates as $coordinates ) {
-		if ( $coordinates ) {
-		$coords_raw = explode(" ",trim($coordinates));
-		foreach ( $coords_raw as $coords_raw_part ) {
-		if ( $coords_raw_part ) {
-		$coords_raw_parts = explode(",",$coords_raw_part);
-		$coords[] = $coords_raw_parts[0];
-		$coords[] = $coords_raw_parts[1];
-		}}}}}}}
-		// end of read
-//we get the point data in to an array called $points:
 
-if (!$error){
-		for( $i=0; $i<count($coords)-1; $i=$i+2 ) {
-		$points[] = array("lon"=>$coords[$i],"lat"=>$coords[$i+1]);
-		if ( ($coords[$i]+0==0) OR ($coords[$i+1]+0==0) ) {
-		$error .= "Invalid Co-ords found in import file.<br>\n";
-		break;
-			}
+
+
+
 		}
-	}
-// add it to the route_points database:
-		$point_num = 0;
-		foreach ($points as $point) {
-		$point_num++;
-		$query = "INSERT into route_points (route_id,point_nr,lat,lon)"."VALUES ($route_id,$point_num,".addslashes($point["lat"]).",".addslashes($point["lon"]).");";
-		$result=sql($query);
-		}
-		
-						tpl_redirect('myroutes.php');
-							exit;		
-
-				} //end submit
-
-			}
 	}
 
 	//make the template and send it out
