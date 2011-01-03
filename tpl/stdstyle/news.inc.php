3aec04e2bba6f5653a5c30a65234d89326c53593
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2011-01-03 07:11:58 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@3105 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/news.inc.php
+++ tpl/stdstyle/news.inc.php
@@ -28,5 +28,5 @@
  $tpl_newstopic = '<p class="content-title-noshade-size3">{date}</p><p class="content-txtbox-noshade-size5">{message}</p><br/>';
  $tpl_newstopic_without_topics = '<p class="content-title-noshade-size2"><span style="color: black;">{date}</span></p><p style="line-height: 1.6em;">{message} <br/> <b><a class="links" href="news.php">['.tr('more_news').' ...]</a></b></p><p>&nbsp;</p>';
  $tpl_newstopic_without_topic = '<p class="content-title-noshade-size1">{date}</p><p style="line-height: 1.6em;">{message}</p>';
- $tpl_newstopic_header = '<p class="content2-container-2col-left"><b>'.tr('news').'</b></p><div class="line-box"></div>';
+ $tpl_newstopic_header = '<p class="content-title-noshade-size3"><b>'.tr('news').'</b></p><';
 ?>
