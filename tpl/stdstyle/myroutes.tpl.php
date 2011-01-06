8c5fb0c963a0e9753acd5ebad0c4ee37858ae8cf
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2011-01-06 12:00:41 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@3185 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/myroutes.tpl.php
+++ tpl/stdstyle/myroutes.tpl.php
@@ -10,7 +10,7 @@
 	***************************************************************************/
 ?>
 <div class="content2-pagetitle"><img src="tpl/stdstyle/images/blue/route.png" class="icon32" alt="" />&nbsp;{{myroutes}}</div>
-<br/><p>&nbsp;&nbsp;&nbsp;<button type="button" style="font-weight:bold;font-size:12px;" onclick="window.location.href ='myroutes_add_map.php'">{{setup_new_route}}</button>&nbsp;&nbsp;&nbsp;<button type="button" style="font-weight:bold;font-size:12px;" onclick="window.location.href ='myroutes_add.php'">{{add_new_route_kml}}</button></p>
+<br/><p>&nbsp;&nbsp;&nbsp;<button type="button" style="font-weight:bold;font-size:12px;" onclick="window.location.href ='myroutes_add_map2.php'">{{setup_new_route}}</button>&nbsp;&nbsp;&nbsp;<button type="button" style="font-weight:bold;font-size:12px;" onclick="window.location.href ='myroutes_add.php'">{{add_new_route_kml}}</button></p>
 <br/>
 {content}
 <div class="notice">{{see_desc_myroute}} <a href="http://wiki.opencaching.pl/index.php/Moje_trasy" target="_blank">{{here}}</a></div>
