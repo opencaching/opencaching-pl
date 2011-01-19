99bf774fcb2edd5fde758c9c694273042bb51758
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2011-01-19 07:43:27 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@3265 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- newlogs.php
+++ newlogs.php
@@ -142,6 +142,7 @@ $rs = sql("SELECT cache_logs.id, cache_logs.cache_id AS cache_id,
 	                          cache_logs.type AS log_type,
 	                          cache_logs.date AS log_date,
  				`cache_logs`.`encrypt` `encrypt`,
+				cache_logs.user_id AS luser_id,
 				   cache_logs.text AS log_text,
 				  cache_logs.text_html AS text_html,
 	                          caches.name AS cache_name,
@@ -194,13 +195,13 @@ $rs = sql("SELECT cache_logs.id, cache_logs.cache_id AS cache_id,
 				$file_content .= '<td width="22"><a class="links" href="viewcache.php?cacheid=' . htmlspecialchars($log_record['cache_id'], ENT_COMPAT, 'UTF-8') . '"><img src="tpl/stdstyle/images/' . $log_record['cache_icon_small'] . '" border="0" alt="" title="Kliknij aby zobaczyć skrzynke" /></a></td>';
 				$file_content .= '<td><b><a class="links" href="viewlogs.php?logid=' . htmlspecialchars($log_record['id'], ENT_COMPAT, 'UTF-8') . '" onmouseover="Tip(\'';
 				$file_content .= '<b>'.$log_record['user_name'].'</b>: &nbsp;';
-				if ( $log_record['encrypt']==1 && $log_record['cache_owner']!=$usr['userid'] && $log_record['user_id']!=$usr['userid']){
+				if ( $log_record['encrypt']==1 && $log_record['cache_owner']!=$usr['userid'] && $log_record['luser_id']!=$usr['userid']){
 				$file_content .= "<img src=\'/tpl/stdstyle/images/free_icons/lock.png\' alt=\`\` /><br/>";}			
-				if ( $log_record['encrypt']==1 && ($log_record['cache_owner']==$usr['userid']|| $log_record['user_id']==$usr['userid'])){
+				if ( $log_record['encrypt']==1 && ($log_record['cache_owner']==$usr['userid']|| $log_record['luser_id']==$usr['userid'])){
 				$file_content .= "<img src=\'/tpl/stdstyle/images/free_icons/lock_open.png\' alt=\`\` /><br/>";}
 				$data = cleanup_text(str_replace("\r\n", " ", $log_record['log_text']));
 				$data = str_replace("\n", " ",$data);
-				if ( $log_record['encrypt']==1 && $log_record['cache_owner']!=$usr['userid'] && $log_record['user_id']!=$usr['userid'])
+				if ( $log_record['encrypt']==1 && $log_record['cache_owner']!=$usr['userid'] && $log_record['luser_id']!=$usr['userid'])
 				{//crypt the log ROT13, but keep HTML-Tags and Entities
 				$data = str_rot13_html($data);} else {$file_content .= "<br/>";}
 				$file_content .=$data;
