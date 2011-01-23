77c3e51c492cbd63988d2b31a9f6ed1ff4aeab30
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2011-01-23 17:39:22 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@3298 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- viewlogs.php
+++ viewlogs.php
@@ -259,30 +259,30 @@
 				$tmpnewpic = mb_ereg_replace('{logid}', $record['log_id'], $upload_picture);
 
 				
-				if( $record['deleted']!=1 )
+			if( $record['deleted']!=1 )
 				{
 					if ($record['user_id'] == $usr['userid'])
 					{
-						$logfunctions = $functions_start . $tmpedit . $functions_middle . $tmpremove . $functions_middle . $tmpnewpic;
+						$logfunctions = $functions_start . $tmpedit . $functions_middle; 
 
-						if ( $record['encrypt']==1 && $no_crypt_log == 0 )						
-						{$logfunctions .= $functions_middle . $tmpdecryptlog . $functions_middle . $functions_end;}else {$logfunctions .= $functions_middle . $functions_end;}
+					if ($record['type']!=12 && ($usr['userid']==$cache_record['cache_id'] || $usr['admin']==false)){					
+						$logfunctions .=$tmpremove . $functions_middle;}
+
+					if ($usr['admin']){					
+						$logfunctions .=$tmpremove . $functions_middle;}
+					 	$logfunctions .= $tmpnewpic . $functions_end;
 					}
 					else if( $usr['admin']) 
 					{
-						$logfunctions = $functions_start . $tmpedit . $functions_middle . $tmpremove;
-						if ( $record['encrypt']==1 && $no_crypt_log == 0 )						
-						{$logfunctions .= $functions_middle . $tmpdecryptlog . $functions_middle . $functions_end;}else {$logfunctions .= $functions_middle . $functions_end;}
-
-						} 
+						$logfunctions = $functions_start . $tmpedit . $functions_middle . $tmpremove . $functions_middle . $functions_end;
+					} 
 					elseif ($owner_id == $usr['userid'])
 					{
+
 						$logfunctions = $functions_start;
 						if ($record['type']!=12){
-						$logfunctions .= $tmpremove;} 
-
-						if ( $record['encrypt']==1 && $no_crypt_log == 0 )						
-						{$logfunctions .= $functions_middle . $tmpdecryptlog . $functions_middle . $functions_end;}else {$logfunctions .= $functions_middle . $functions_end;}
+ 						$logfunctions .= $tmpremove;}
+						$logfunctions .= $functions_end;
 					}
 				}
 				$tmplog = mb_ereg_replace('{logfunctions}', $logfunctions, $tmplog);
