6b73ea2886c115ac2d30c8d06adfb76ef984dc04
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2011-02-14 15:30:52 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@3360 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/newcache_beginner.tpl.php
+++ tpl/stdstyle/newcache_beginner.tpl.php
@@ -19,12 +19,12 @@
 ?>
 
 <table class="content" border="0">
-	<tr><td class="content2-pagetitle"><img src="tpl/stdstyle/images/blue/cache.png" class="icon32" alt="" title="{{new_cache}}" align="middle" /><font size="4" <b>Nowa skrzynka</b></font></td></tr>
+	<tr><td class="content2-pagetitle"><img src="tpl/stdstyle/images/blue/cache.png" class="icon32" alt="" title="{{new_cache}}" align="middle" /><font size="4" <b>Rejstracja nowej skrzynki - informacja</b></font></td></tr>
 	<tr><td class="spacer"></td></tr>
 </table>
 <br />
 <div class="searchdiv">
-<p style="font-size: 14px; line-height:1.6em; text-align: justify;"><b>Aby zarejestrować nowe skrzynki musisz najpierw znaleźć <font color="red"><?php echo $NEED_FIND_LIMIT; ?></font> skrzynek spośród następujących typów: 
+<p style="margin: 10px;font-size: 14px; line-height:1.6em; text-align: justify;"><b>Aby zarejestrować nowe skrzynki musisz najpierw znaleźć <font color="red"><?php echo $NEED_FIND_LIMIT; ?></font> skrzynek spośród następujących typów: 
 <font color="blue">
 <ul>
 <li>Tradycyjna, </li>
