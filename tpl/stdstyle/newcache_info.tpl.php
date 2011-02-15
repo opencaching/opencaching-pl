7b5afb0a2373b6d0e1d656101e71778c477e686a
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2011-02-15 08:19:38 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@3366 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

---
+++ tpl/stdstyle/newcache_info.tpl.php
@@ -0,0 +1,43 @@
+<?php
+
+/***************************************************************************
+	*
+	*   This program is free software; you can redistribute it and/or modify
+	*   it under the terms of the GNU General Public License as published by
+	*   the Free Software Foundation; either version 2 of the License, or
+	*   (at your option) any later version.
+	*
+	***************************************************************************/
+
+/****************************************************************************
+
+   Unicode Reminder ąść
+
+
+ ****************************************************************************/
+?>
+
+<table class="content" border="0">
+	<tr><td class="content2-pagetitle"><img src="tpl/stdstyle/images/blue/cache.png" class="icon32" alt="" title="{{new_cache}}" align="middle" /><font size="4" <b>Rejestracja nowej skrzynki - informacja</b></font></td></tr>
+	<tr><td class="spacer"></td></tr>
+</table>
+<br />
+<div class="searchdiv" style="background-color: #FFF9E3;">
+<p style="margin: 10px;font-size: 14px; line-height:1.6em; text-align: justify;">Dziękujemy za to że chcesz zarejstrować swoja skrzynkę na OC PL. Jeśli potrzebujesz pomocy w ukryciu lub rejestracji skrzynki sprawdź OpenGeoWiki artykuł: ....<br/>
+Aby twoja skrzynka była jak najlepszej jakości powinieneś rozważyć poniższe uwagi: 
+<font color="blue">
+<ul>
+<li>Czy skrzynka jest ulokowana w legalnym miejscu bez kolizji z lokalnym prawem,</li>
+<li>Czy skrzynka nie jest ulokowana na ternei parku Narodowego lub innego obszaru chronionego prawem,</li> 
+<li>Czy instnieją już w pobliżu inne skrzynki, </li>
+<li>Czy jesteś w stanie zapewnić suport dla skrzynki przez dłuższy czas, </li>
+<li>Czy wybrany przez Ciebie pojemnik jest właściwy do lokalizacji skrzynki, </li>
+<li>Czy skrzynka jest zgodna z Regulaminem serwisu OC PL?.</li>
+</ul></font>
+
+Prośmy zapoznaj się z Dobrymi Praktykami Geocaching'owimi oraz Kodeksem Geocacher na naszym OpenGeoWiki.
+</p>
+<br />
+</div>
+		<button type="submit" name="submitform" value="{submit}" style="font-size:14px;"><b>Wyszstkie powyższe elementy rozważyłem i chcę kontynuować rejestracje skrzynki</b></button>
+
