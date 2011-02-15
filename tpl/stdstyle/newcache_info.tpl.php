c43c182a72ba9a4c1340e2911a0558c576dc1587
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2011-02-15 11:05:31 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@3376 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/newcache_info.tpl.php
+++ tpl/stdstyle/newcache_info.tpl.php
@@ -38,5 +38,7 @@ Prosimy zapoznaj się z <a class="links" href="http://wiki.opencaching.pl/index.
 <br />
 </div>
 <br/><br/>
-		<center><button type="button" style="font-size:14px;" onclick="window.location.href ='newcache.php'"><b>Wszystkie powyższe elementy rozważyłem i chcę kontynuować rejestracje skrzynki</b></button>
+<form action="newcache.php" method="post" enctype="application/x-www-form-urlencoded" name="newcacheform" dir="ltr"><input type="hidden" name="newcache_info" value="0"/>
+		<center><button type="button" style="font-size:14px;"><b>Wszystkie powyższe elementy rozważyłem i chcę kontynuować rejestracje skrzynki</b></button>
 </center>
+</form>
