08ff93d4294c277e1b86f20ce20eea7470d0cdf0
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2011-02-17 10:02:06 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@3398 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

---
+++ tpl/stdstyle/newevents.tpl.php
@@ -0,0 +1,37 @@
+<?php
+/***************************************************************************
+
+   Unicode Reminder メモ
+
+	***************************************************************************/
+?>
+<div class="content2-pagetitle"><img src="tpl/stdstyle/images/blue/event.png" class="icon32" alt="Cache" title="Cache" align="middle"/>&nbsp;{{incomming_events}}</div>
+<!-- Text container -->
+	<p>
+		{pages}
+	</p>
+<div class="searchdiv">
+<table border="0" cellspacing="2" cellpadding="1" style="margin-left: 10px; line-height: 1.4em; font-size: 13px;" width="97%">
+<tr>
+<td><strong>Date</strong></td>
+<td>&nbsp;</td>
+<td>&nbsp;</td>
+<td>&nbsp;</td>
+<td>&nbsp;</td>
+<td><strong>Event</strong></td>
+<td>&nbsp;</td>
+<td><strong>Owner</strong></td>
+</tr>
+<tr>
+<td colspan="8"><hr></hr></td>
+</tr>
+		{newcaches}
+<tr>
+<td colspan="8"><hr></hr></td>
+</tr>
+</table>
+</div>
+	<p>
+		{pages}
+	</p>
+<!-- End Text Container -->
