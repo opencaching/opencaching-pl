0bbaa3a1ae5c8e000d27c4f9ab5b21f8035d739c
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2011-02-17 11:49:27 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@3408 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/newevents.tpl.php
+++ tpl/stdstyle/newevents.tpl.php
@@ -12,7 +12,7 @@
 <div class="searchdiv">
 <table border="0" cellspacing="2" cellpadding="1" style="margin-left: 10px; line-height: 1.4em; font-size: 13px;" width="95%">
 <tr>
-<td colspan="2"><strong>{{date_hidden_label}}</strong></td>
+<td colspan="2"><strong>Data</strong></td>
 <td><strong>{{event}}</strong></td>
 <td><strong>{{owner}}</strong>&nbsp;&nbsp;&nbsp;</td>
 <td colspan="3"><strong>{{latest_logs}}</strong></td>
