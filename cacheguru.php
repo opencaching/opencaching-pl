0b26daa429731d255970797e35ec655b880879a7
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2011-02-18 08:55:28 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@3439 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- cacheguru.php
+++ cacheguru.php
@@ -62,7 +62,7 @@ global $get_userid;
 			$number=$i+1;
 			$point .="var marker".$number." = new GMarker(point,".$icon."); map0.addOverlay(marker".$number.");\n\n";
 
-			$point .="GEvent.addListener(marker".$number.", \"click\", function() {marker".$number.".openInfoWindowHtml('<span style=\"font-size:12px;color:blue;\"><table><tr><td><img src=\"tpl/stdstyle/images/free_icons/book_open.png\" alt=\"img\"> <b>Przewodnik/Guru: <a href=\"viewprofile.php?userid=".$record['userid']."\">".$username."</a></td></tr><tr><td><img src=\"tpl/stdstyle/images/free_icons/email.png\" alt=\"img\"> Kontakt: <a href=\"mailto.php?userid=".$record['userid']."\">Napisz E-mail</a></b></td></tr></table></span><br/>');});\n bounds.extend(point);\n";
+			$point .="GEvent.addListener(marker".$number.", \"click\", function() {marker".$number.".openInfoWindowHtml('<br/><span style=\"font-size:12px;color:blue;\"><table><tr><td><img src=\"tpl/stdstyle/images/free_icons/book_open.png\" alt=\"img\"> <b>Przewodnik/Guru: <a href=\"viewprofile.php?userid=".$record['userid']."\">".$username."</a></td></tr><tr><td><img src=\"tpl/stdstyle/images/free_icons/email.png\" alt=\"img\"><b> Kontakt: <a href=\"mailto.php?userid=".$record['userid']."\">Napisz E-mail</a></b></td></tr></table></span>');});\n bounds.extend(point);\n";
 			}
 
 		tpl_set_var('points', $point);	
