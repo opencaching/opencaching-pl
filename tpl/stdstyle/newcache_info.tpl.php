b4b259e078d69ffac0a13c6a2ee619e10aaf7fe1
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2011-02-20 19:51:44 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@3506 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/newcache_info.tpl.php
+++ tpl/stdstyle/newcache_info.tpl.php
@@ -22,16 +22,16 @@
 	<tr><td class="spacer"></td></tr>
 </table>
 <br/>
-<div class="searchdiv" style="background-color: #FFF9E3;"><span style="font-size: 13px; line-height:1.7em; text-align: justify;">Drogi Użytkowniku,<br/>
+<div class="searchdiv" style="background-color: #FFF9E3;"><span style="font-weight:bold;font-size: 12px; line-height:1.7em; text-align: justify;">Drogi Użytkowniku,<br/>
 Dziękujemy za to, że chcesz zarejestrować swoja skrzynkę w serwisie opencaching.pl. Jeśli potrzebujesz pomocy w ukryciu lub rejestracji skrzynki, proszę zapoznaj się z informacjami zawartymi w <a class="links" href="http://wiki.opencaching.pl">OpenGeoWiki <img src="tpl/stdstyle/images/misc/linkicon.png" alt="link"></a> lub skontaktuj się z Centrum Obsługi Geocacher’a - adres email: cog@opencaching.pl
-Pamiętaj, że Twoja skrzynka świadczy o Tobie. Na pewno zależy Ci na tym, by była jak najlepszej jakości, a w związku z tym prosimy o to, abyś odpowiedział sobie samodzielnie na poniższe pytania :<ul style="font-size: 14px; line-height:1.6em; text-align: left;color:blue;">
+Pamiętaj, że Twoja skrzynka świadczy o Tobie. Na pewno zależy Ci na tym, by była jak najlepszej jakości, a w związku z tym prosimy o to, abyś odpowiedział sobie samodzielnie na poniższe pytania :<ul style="font-weight:bold;font-size: 12px; line-height:1.6em; text-align: left;color:blue;">
 <li>Czy skrzynka jest ulokowana w miejscu zgodnym z prawem, nie jest umieszczona na terenie prywatnym bez zgody jego właściciela ?,</li>
 <li>Czy skrzynka nie jest ulokowana na terenie Parku Narodowego, Rezerwatu, zabytku, cmentarza lub innego obszaru chronionego prawem ?,</li>
 <li>Czy w najbliższej okolicy znajdują się już inne skrzynki, czy zachowana jest minimalna odległość 150m) ?,</li>
 <li>Czy jesteś w stanie zapewnić serwis dla skrzynki przez cały czas jej istnienia w serwisie?,</li>
 <li>Czy wybrany przez Ciebie sposób ukrycia skrzynki jest właściwy dla wybranej lokalizacji skrzynki (odporność pojemnika na niską temperaturę, wilgoć) ?,</li>
 <li>Czy skrzynka jest zgodna z <a class="links" href="http://wiki.opencaching.pl/index.php/Regulamin_OC_PL">Regulaminem serwisu OC PL <img src="tpl/stdstyle/images/misc/linkicon.png" alt="link"></a> ?.</li>
-</ul><span style="font-size: 13px; line-height:1.6em; text-align: justify;">Prosimy zapoznaj się z Poradnikiem <a class="links" href="http://wiki.opencaching.pl/index.php/Zak%C5%82adanie_skrzynki">Jak założyć skrzynkę <img src="tpl/stdstyle/images/misc/linkicon.png" alt="link"></a> oraz <a class="links" href="http://wiki.opencaching.pl/index.php/Kodeks_geocachera">Kodeksem Geocacher'a <img src="tpl/stdstyle/images/misc/linkicon.png" alt="link"></a> na naszym OpenGeoWiki.
+</ul><span style="font-weight:bold;font-size: 12px; line-height:1.6em; text-align: justify;">Prosimy zapoznaj się z Poradnikiem <a class="links" href="http://wiki.opencaching.pl/index.php/Zak%C5%82adanie_skrzynki">Jak założyć skrzynkę <img src="tpl/stdstyle/images/misc/linkicon.png" alt="link"></a> oraz <a class="links" href="http://wiki.opencaching.pl/index.php/Kodeks_geocachera">Kodeksem Geocacher'a <img src="tpl/stdstyle/images/misc/linkicon.png" alt="link"></a> na naszym OpenGeoWiki.
 <br/>Pozdrawiamy<br/>OC Team</span>
 </div>
 <br/><br/>
