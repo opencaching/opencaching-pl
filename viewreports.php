73742b1260f458c353305a5d05ab83fb7eb67da8
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2011-02-22 05:58:54 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@3517 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- viewreports.php
+++ viewreports.php
@@ -82,7 +82,7 @@ global $bgcolor1, $bgcolor2;
 			$show_archive = " reports.status <> 2 AND ";
 			$sorting_order = "ASC";
 		}
-		$sql = "SELECT cache_status.id AS cs_id, cache_status.pl AS cache_status, reports.id as report_id, reports.user_id as user_id, reports.changed_by as changed_by, reports.changed_date as changed_date, reports.cache_id as cache_id, reports.type as type, reports.text as text, reports.submit_date as submit_date, reports.responsible_id as responsible_id, reports.status as status, user.username as username, user.user_id as user_id, caches.name as cachename, caches.status AS c_status FROM cache_status, reports, user, caches WHERE cache_status.id = caches.status AND ".sql_escape($show_archive)." user.user_id = reports.user_id AND caches.cache_id = reports.cache_id ORDER BY submit_date ".sql_escape($sorting_order);
+		$sql = "SELECT cache_status.id AS cs_id, caches.last_modified AS lastmodified,caches.user_id AS cache_ownerid,cache_status.pl AS cache_status, reports.id as report_id, reports.user_id as user_id, reports.changed_by as changed_by, reports.changed_date as changed_date, reports.cache_id as cache_id, reports.type as type, reports.text as text, reports.submit_date as submit_date, reports.responsible_id as responsible_id, reports.status as status, user.username as username, user.user_id as user_id, caches.name as cachename, caches.status AS c_status FROM cache_status, reports, user, caches WHERE cache_status.id = caches.status AND ".sql_escape($show_archive)." user.user_id = reports.user_id AND caches.cache_id = reports.cache_id ORDER BY submit_date ".sql_escape($sorting_order);
 		$query = mysql_query($sql) or die("DB error");
 		$row_num = 0;
 		while( $report = mysql_fetch_array($query) )
@@ -93,16 +93,16 @@ global $bgcolor1, $bgcolor2;
 				$bgcolor = "bgcolor2";
 		
 			$content .= "<tr>\n";
-			//$username_sql = "SELECT username FROM user WHERE user_id='".sql_escape($report['user_id'])."'";
-			//$username_query = mysql_query($username_sql) or die("DB error");
-			//$username = mysql_result($username_query,0);
+			$userloginsql = "SELECT last_login FROM user WHERE user_id='".sql_escape($report['cache_ownerid'])."'";
+			$userlogin_query = mysql_query($userloginsql) or die("DB error");
+			$userlogin = strftime("%Y-%m-%d", strtotime(mysql_result($userlogin_query,0)));
 			if( $usr['userid'] == $report['responsible_id'])
 				$addborder = "style='border-width:2px;'";
 			else
 				$addborder = "";
 			$content .= "<td ".$addborder." class='".$bgcolor."'><span class='content-title-noshade-size05'>".$report['report_id']."</span></td>\n";
 			$content .= "<td ".$addborder." class='".$bgcolor."'><span class='content-title-noshade-size05'>".$report['submit_date']."</span></td>\n";
-			$content .= "<td ".$addborder." class='".$bgcolor."'><a class='content-title-noshade-size05' href='viewcache.php?cacheid=".$report['cache_id']."'>".nonEmptyCacheName($report['cachename'])."</a></td>\n";
+			$content .= "<td ".$addborder." class='".$bgcolor."'><a class='content-title-noshade-size04' title=\"Skrzynka ostatnio modyfikowana: ".$report['lastmodified']."\" href='viewcache.php?cacheid=".$report['cache_id']."'>".nonEmptyCacheName($report['cachename'])."</a> <br/> <a title=\"Użytkownik logowal się ostatnio: ".$userlogin."\" class=\"links\" href=\"viewprofile.php?userid=".$report['cache_ownerid']."\">".getUsername($report['cache_ownerid'])."</a></td>\n";
 			$content .= "<td ".$addborder." class='".$bgcolor."'><span class='content-title-noshade-size05'>".colorCacheStatus($report['cache_status'], $report['c_status'])."</span></td>\n";
 			$content .= "<td ".$addborder." class='".$bgcolor."'><a class='content-title-noshade-size05' href='viewreport.php?reportid=".$report['report_id']."'>".writeReason($report['type'])."</a></td></font>\n";
 			$content .= "<td ".$addborder." class='".$bgcolor."'><a class='content-title-noshade-size05' href='viewprofile.php?userid=".$report['user_id']."'>".$report['username']."</a></td>\n";
