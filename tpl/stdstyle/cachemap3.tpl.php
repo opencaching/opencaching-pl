3647c437335b3544511bab0311df77beecc15201
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2011-03-08 05:29:57 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@3544 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/cachemap3.tpl.php
+++ tpl/stdstyle/cachemap3.tpl.php
@@ -466,7 +466,17 @@ ShowCoordsControl.prototype.setStyle_ = function(elem) {
 			});
 			
 
-
+			var circle={circle};
+			if(circle==1)  
+			{
+			var poli = ();
+			var punktCentralny = new GLatLng({coords});   
+				var poli = okrag(punktCentralny,150,'#99CCCC',2,0.8,'#9999CC',0.2,55);
+				map.addOverlay(poli);
+				var pointc = new GLatLng({coords});
+				var new_cache = new GMarker(pointc);
+				map.addOverlay(new_cache);
+			}
 
 			var onClickFunc = function(overlay,point) 
 			{
