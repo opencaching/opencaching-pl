b9290e6b6252f82bcda4527982f5727d327d1242
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2011-05-08 12:11:47 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@3580 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/lib/menu.php
+++ tpl/stdstyle/lib/menu.php
@@ -135,7 +135,7 @@ $menu = array(
 				'title' => tr('search_cache'),
 				'menustring' => tr('search_cache'),
 				'siteid' => 'search',
-				'onlylogged' => true,
+				'onlylogged' => false,
 				'visible' => true,
 				'filename' => 'search.php'
 			),
