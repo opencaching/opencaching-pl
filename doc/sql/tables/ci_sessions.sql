89a534009f6a3994ed744718c5b52d2b7021ca5a
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2011-06-20 11:58:51 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@3633 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

---
+++ doc/sql/tables/ci_sessions.sql
@@ -0,0 +1,11 @@
+SET NAMES 'utf8';
+DROP TABLE IF EXISTS `ci_sessions`;
+CREATE TABLE IF NOT EXISTS `ci_sessions` (
+  `session_id` varchar(40) NOT NULL DEFAULT '0',
+  `ip_address` varchar(16) NOT NULL DEFAULT '0',
+  `user_agent` varchar(50) NOT NULL,
+  `last_activity` int(10) unsigned NOT NULL DEFAULT '0',
+  `user_data` text NOT NULL,
+  PRIMARY KEY (`session_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
