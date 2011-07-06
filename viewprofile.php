272b90362df0e7f56cbe77426c7adf3aec949867
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2011-07-06 12:32:43 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@3671 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- viewprofile.php
+++ viewprofile.php
@@ -19,7 +19,15 @@
 		//user logged in?
 		if ($usr == false)
 		{
-		    $target = urlencode(tpl_get_current_page());
+
+function myUrlEncode($string) {
+    $entities = array('%21', '%2A', '%27', '%28', '%29', '%3B', '%3A', '%40', '%26', '%3D', '%2B', '%24', '%2C', '%2F', '%3F', '%25', '%23', '%5B', '%5D');
+    $replacements = array('!', '*', "'", "(", ")", ";", ":", "@", "&", "=", "+", "$", ",", "/", "?", "%", "#", "[", "]");
+	    return str_replace($entities, $replacements, urlencode($string));
+	    }
+
+
+		    $target = myUrlEncode(tpl_get_current_page());
 		    tpl_redirect('login.php?target='.$target);
 		}
 		else
