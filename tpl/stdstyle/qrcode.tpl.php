34f79044e19f80eb3cf979d3f65543ffeccf5c63
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2011-07-21 04:41:39 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@3699 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/qrcode.tpl.php
+++ tpl/stdstyle/qrcode.tpl.php
@@ -24,6 +24,6 @@
 	<br/><br/>
     <form action="qrcode.php" method="post">
         <span style="font-size:14px;width:120px">&nbsp;&nbsp;Dane w QR Code:</span>&nbsp;
-	<input name="data" value="{qrcode}" size="80"><br/>
+	<input name="data" value="{qrcode}" size="70" style="font-size:14px;"><br/><br/>
 	<span style="margin-left:125px;">&nbsp;</span><button type="submit" name="Generuj" value="Generuj" style="font-size:14px;width:120px"><b>Generuj</b></button>
 	</form><br/></div>
