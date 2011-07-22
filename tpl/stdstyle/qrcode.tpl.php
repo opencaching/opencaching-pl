3866518e7ef787ce1d0e8872148e33219afeba3c
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2011-07-22 12:34:38 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@3706 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/qrcode.tpl.php
+++ tpl/stdstyle/qrcode.tpl.php
@@ -27,5 +27,5 @@
 	<input name="data" value="{qrcode}" maxlength="77" size="70" style="font-size:14px;"><br/><br/>
 	<span style="margin-left:125px;">&nbsp;</span><button type="submit" name="Generuj" value="Generuj" style="font-size:14px;width:120px"><b>Generuj</b></button>
 	</form><br/>
-<img src="tpl/stdstyle/images/misc/16x16-info.png" class="icon16" alt="Info" /><a class="links" href="http://www.i-nigma.com/Downloadi-nigmaReader.html">Tu znajdziesz czytnik QR Code dla różnych telefony</a>
+<img src="tpl/stdstyle/images/misc/16x16-info.png" class="icon16" alt="Info" /><a class="links" href="http://www.i-nigma.com/Downloadi-nigmaReader.html">Tu znajdziesz czytnik QR Code dla różnych telefonów</a>
 </div>
