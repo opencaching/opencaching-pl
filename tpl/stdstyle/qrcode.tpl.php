190c28d9ec0bddec4841827d4eb1ffb6ed594f4c
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2011-07-23 05:04:18 +0000

    git-svn-id: https://opencaching-pl.googlecode.com/svn@3707 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/qrcode.tpl.php
+++ tpl/stdstyle/qrcode.tpl.php
@@ -19,13 +19,20 @@
 <div class="content2-pagetitle"><img src="tpl/stdstyle/images/blue/decrypt.png" class="icon32" alt="" title="" align="middle"/>&nbsp;Etykietka z QR Code</div>
 <p>&nbsp;</p>
 
-   <div class="searchdiv"><p>&nbsp;</p>
+   <div class="searchdiv">
+   <p style="font-size:12px;">Etykietkę z <a class="links" href="http://pl.wikipedia.org/wiki/QR_Code">QR Code</a> (zalaminowaną lub w woreczku stronowym lub jako naklejka) możesz użyć do 
+   zapisania krótkiej wskazówki max 77 znaków, współrzędnych następnego etapu multicache, hasło do logu itp. Do odczytania kodu
+   należy użyć czytnika zainstalowanego np w telefonie komórkowym.</p>
+    <p>&nbsp;</p>   
    <center>{imgqrcode}</center>
 	<br/><br/>
     <form action="qrcode.php" method="post">
         <span style="font-weight:bold;font-size:14px;width:120px">Dane w QR Code:</span>&nbsp;
 	<input name="data" value="{qrcode}" maxlength="77" size="70" style="font-size:14px;"><br/><br/>
 	<span style="margin-left:125px;">&nbsp;</span><button type="submit" name="Generuj" value="Generuj" style="font-size:14px;width:120px"><b>Generuj</b></button>
-	</form><br/>
+	</form><br/><br/>
 <img src="tpl/stdstyle/images/misc/16x16-info.png" class="icon16" alt="Info" /><a class="links" href="http://www.i-nigma.com/Downloadi-nigmaReader.html">Tu znajdziesz czytnik QR Code dla różnych telefonów</a>
+<p>&nbsp;</p>   
 </div>
+<p>&nbsp;</p>
+   
