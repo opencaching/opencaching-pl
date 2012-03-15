e36da3ae627b9fa580deb252aaf1c09ef8b37d8e
rygielski@gmail.com <rygielski@gmail.com@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2012-03-15 05:07:14 +0000

    (empty commit message)
    "(empty commit message)" lines were added as part of SVN->Git migration in 2015.
    
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@3759 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- post-commit.php
+++ post-commit.php
@@ -12,6 +12,4 @@ header("Content-Type: text/plain; charset=utf-8");
 # File /var/www/ocpl-update.sh contains "svn up /var/www/ocpl" command.
 # It is allowed to be sudo-executed by www-data without password.
 
-print "Running svn up...\n";
 print shell_exec("sudo /var/www/ocpl-update.sh up 2>&1");
-
