c20d7be0e08d08761abbce196e3e13032c4ed56b
rygielski@gmail.com <rygielski@gmail.com@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2012-03-18 15:47:50 +0000

    (empty commit message)
    "(empty commit message)" lines were added as part of SVN->Git migration in 2015.
    
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@3780 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- post-commit.php
+++ post-commit.php
@@ -9,21 +9,6 @@ set_time_limit(0);
 header("Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0");
 header("Content-Type: text/plain; charset=utf-8");
 
-if (isset($_GET['from']) && ($_GET['from'] == 'google'))
-{
-	# Google doesn't care what we do output. But it cares for timeouts.
-	# If I understood the docs correctly - when this request doesn't finish
-	# after 15 seconds, Google will send it again. We don't want that.
-	
-	header("Connection: close");
-	header("Content-Length: 9");
-	print "Received.";
-	flush();
-	ob_flush();
-	while (count(ob_list_handlers()) > 0)
-		ob_end_flush();
-}
-
 # File /var/www/ocpl-update.sh contains "svn up /var/www/ocpl" command
 # (and a couple of others). It is allowed to be sudo-executed by 'www-data'
 # without password.
