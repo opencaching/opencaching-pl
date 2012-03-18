f14deed35aae2e66e7cf9a0e147367c367451e87
rygielski@gmail.com <rygielski@gmail.com@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2012-03-18 14:55:32 +0000

    Testing post-commit and automatic updates.
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@3774 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- post-commit.php
+++ post-commit.php
@@ -9,7 +9,21 @@ set_time_limit(0);
 header("Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0");
 header("Content-Type: text/plain; charset=utf-8");
 
-# File /var/www/ocpl-update.sh contains "svn up /var/www/ocpl" command.
-# It is allowed to be sudo-executed by www-data without password.
+if (isset($_GET['from']) && ($_GET['from'] == 'google'))
+{
+	# Google doesn't care what we do output. But it cares for timeouts.
+	# If I understood the docs correctly - when this request doesn't finish
+	# after 15 seconds, Google will send it again. We don't want that.
+	
+	header("Connection: close");
+	header("Content-Length: 9");
+	print "Received.";
+	flush();
+	ob_flush();
+}
+
+# File /var/www/ocpl-update.sh contains "svn up /var/www/ocpl" command
+# (and a couple of others). It is allowed to be sudo-executed by 'www-data'
+# without password.
 
 print shell_exec("sudo /var/www/ocpl-update.sh up 2>&1");
