f327b3a8bc934a44f5095713878f1a6e94f58310
rygielski@gmail.com <rygielski@gmail.com@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2012-03-19 14:27:06 +0000

    Fixing previous commits (made by sp2ong). Part 1 - deleting doc and docs.
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@3798 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- doc/sql/tables/caches_attributes.sql
+++
@@ -1,9 +0,0 @@
-SET NAMES 'utf8';
-DROP TABLE IF EXISTS `caches_attributes`;
-CREATE TABLE IF NOT EXISTS `caches_attributes` (
-  `cache_id` int(11) NOT NULL,
-  `attrib_id` int(11) NOT NULL,
-  PRIMARY KEY  (`cache_id`,`attrib_id`),
-  KEY `attrib_id` (`attrib_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- doc/sql/tables/import_caches_date.sql
+++
@@ -1,7 +0,0 @@
-SET NAMES 'utf8';
-DROP TABLE IF EXISTS `import_caches_date`;
-CREATE TABLE  `import_caches_date` (
-`node_id` INT NOT NULL ,
-`updated` INT NOT NULL ,
-PRIMARY KEY (  `node_id` )
-) ENGINE = MYISAM
