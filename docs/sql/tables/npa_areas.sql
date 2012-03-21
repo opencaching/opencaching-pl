5449914345f8f2f9479521e64efe89a512e57b16
sp2ong@gmail.com <sp2ong@gmail.com@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2012-03-21 18:55:46 +0000

    dodanie nowej kolumny linkid
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@3821 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- docs/sql/tables/npa_areas.sql
+++ docs/sql/tables/npa_areas.sql
@@ -2,6 +2,7 @@ SET NAMES 'utf8';
 DROP TABLE IF EXISTS `npa_areas`;
 CREATE TABLE `npa_areas` (
   `id` int(10) unsigned NOT NULL auto_increment,
+   `linkid` int(4) DEFAULT NULL,
   `sitename` varchar(255) NOT NULL,
   `sitecode` varchar(255) NOT NULL,
   `sitetype` char(1) NOT NULL,
