1517a97f54c7ab5ccb6c24c0222d7926b3a10b70
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2012-04-05 16:46:34 +0000

    polaczenie plikow konfiguracyjnych ocpl z mobile w jeden
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@3949 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- mobile/lib/settings-example.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	$dynbasepath= '/srv/ocpl-dynamic-files/mobile/'; // path where is stored dynamic files
-	$dbserver = 'localhost'; // host adres
-	$dbusername = 'user'; // user name
-	$dbpasswd = 'ubuntu'; // password
-	$dbname = 'ocpl'; // database name
-
-?>
