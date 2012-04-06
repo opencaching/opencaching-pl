d9e87257aed881167a211ec2f905af62356fba8c
rygielski@gmail.com <rygielski@gmail.com@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2012-04-06 11:47:59 +0000

    Enhanced shrink_and_prepare.sh
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@3963 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- docs/vm/shrink_and_prepare.sh
+++ docs/vm/shrink_and_prepare.sh
@@ -8,6 +8,8 @@ sudo rm -f /root/.*_history /root/.selected_editor /root/.lesshst
 sudo rm -fR /root/.subversion/auth
 echo "Removing unimportant data..."
 echo "delete from okapi_cache where \`key\` != 'cron_schedule'" | mysql -pubuntu ocpl
+echo "truncate sys_logins" | mysql -pubuntu ocpl
+echo "truncate sys_sessions" | mysql -pubuntu ocpl
 sudo rm -fR /srv/ocpl-dynamic-files/okapi-dump*
 sudo rm -f /srv/ocpl-dynamic-files/statpics/*
 echo "Removing old packages..."
@@ -18,4 +20,13 @@ mysqlcheck -uroot -ptoor -o ocpl
 mysqladmin -uroot -ptoor flush-tables
 echo "Preparing for shrink (filling unused space with zeroes)..."
 cat /dev/zero | pv -s 50g > zero.fill; sync; sleep 1; sync; rm -f zero.fill
+echo "Preparing for shrink (filling swap partition with zeroes)..."
+if [ `cat /proc/swaps | grep "/dev/sda5" | wc -l` -eq 1 ]
+then
+	sudo swapoff /dev/sda5
+	sudo sswap -fllvz /dev/sda5
+	sudo swapon /dev/sda5
+else
+	echo "-> /dev/sda5 is NOT a swap partition on your system, skipping this step."
+fi
 echo "Done."
