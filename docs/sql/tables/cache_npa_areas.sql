e8b111f689b46c87a2953cc78055c1748c878c90
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2012-04-06 09:38:51 +0000

    zrzut strukury bazy z triggers w jeden plik i usuniecie poszczegolnych tablic
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@3960 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- docs/sql/tables/cache_npa_areas.sql
+++
@@ -1,9 +0,0 @@
-SET NAMES 'utf8';
-DROP TABLE IF EXISTS `cache_npa_areas`;
-CREATE TABLE IF NOT EXISTS `cache_npa_areas` (
-  `cache_id` int(10) unsigned NOT NULL,
-  `npa_id` int(10) unsigned NOT NULL,
-  `parki_id` int(10) unsigned NOT NULL,
-  `calculated` tinyint(1) NOT NULL,
-  PRIMARY KEY (`cache_id`,`npa_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
