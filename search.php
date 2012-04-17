8b37a6967473a056c966568adc3402df5232e2de
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2012-04-17 10:33:26 +0000

    (empty commit message)
    "(empty commit message)" lines were added as part of SVN->Git migration in 2015.
    
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@4031 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- search.php
+++ search.php
@@ -27,7 +27,8 @@
 	//prepare the templates and include all neccessary
 	require_once('./lib/common.inc.php');
 	require_once('./lib/search.inc.php');
-	global $lang; $ocWP;
+	global $lang; 
+	global $ocWP;
 	// SQL-Debug?
 	$sqldebug = false;
 	global $sql_debug;
@@ -329,7 +330,7 @@
 					$options['waypointtype'] = 'nc'; 
 				} 
 				$ocWP=strtolower($ocWP);
-				if( mb_ereg_match('((oc|'.$ocWP.'|gc)([a-z0-9]){4,4}|n([a-f0-9]){5,5}|([a-f0-9]){4,4})$', mb_strtolower($options['waypoint'])) ) 
+				if( mb_ereg_match('((oc|op|gc)([a-z0-9]){4,4}|n([a-f0-9]){5,5}|([a-f0-9]){4,4})$', mb_strtolower($options['waypoint'])) ) 
 				{
 					if ($options['waypointtype'] == $ocWP) 
 					{
