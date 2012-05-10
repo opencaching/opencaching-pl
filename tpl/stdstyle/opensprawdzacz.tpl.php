afbb36ac6e8e4db68bf904524fd7a9f44aeb6eb3
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2012-05-10 05:22:25 +0000

    drobne zmiany
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@4078 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/opensprawdzacz.tpl.php
+++ tpl/stdstyle/opensprawdzacz.tpl.php
@@ -5,15 +5,15 @@ OpenSprawdzacz
 {sekcja_1_start}
 <div class="content2-container bg-blue02">
 <p class="content-title-noshade-size1">
+<img src="tpl/stdstyle/images/blue/empty-32x32.png" class="icon32" alt="geocache" title="geocache" align="middle" /> 
 {{os_komunikat1}}
 </p></div>
 
 <p>{formularz}</p>
 <div class="content2-container bg-blue02">
 <p class="content-title-noshade-size1">
-<br/>
+<img src="tpl/stdstyle/images/blue/empty-32x32.png" class="icon32" alt="geocache" title="geocache" align="middle" /> 
 {{os_komunikat2}}
-<br/>
 </p></div>
 <br/><br/><br/>
 <div class="searchdiv">
