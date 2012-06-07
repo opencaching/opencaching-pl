a9cb78c5670a06a236553aa2bced05a93eaca0c5
rygielski@gmail.com <rygielski@gmail.com@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2012-06-07 22:04:10 +0000

    Automatic OKAPI Project update (r348)
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@4127 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- okapi/core.php
+++ okapi/core.php
@@ -23,6 +23,7 @@ use ErrorException;
 use OAuthServerException;
 use OAuthServer400Exception;
 use OAuthServer401Exception;
+use OAuthMissingParameterException;
 use OAuthConsumer;
 use OAuthToken;
 use OAuthServer;
@@ -514,7 +515,7 @@ class OkapiOAuthServer extends OAuthServer
 		} catch (OAuthMissingParameterException $e) {
 			# Note, that exception will be different if token is supplied
 			# and is invalid. We catch only a completely MISSING token parameter.
-			if (($e->getParamName == 'oauth_token') && (!$token_required))
+			if (($e->getParamName() == 'oauth_token') && (!$token_required))
 				$token = null;
 			else
 				throw $e;
@@ -657,7 +658,7 @@ class Okapi
 {
 	public static $data_store;
 	public static $server;
-	public static $revision = 347; # This gets replaced in automatically deployed packages
+	public static $revision = 348; # This gets replaced in automatically deployed packages
 	private static $okapi_vars = null;
 	
 	/** Get a variable stored in okapi_vars. If variable not found, return $default. */
