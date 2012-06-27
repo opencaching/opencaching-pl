26ea026ba05de63c243170c5963ca885f21a2a48
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2012-06-27 04:35:51 +0000

    zmiana liczby wyprowadzonych formatow danych GPX
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@4199 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/search.result.caches.tpl.php
+++ tpl/stdstyle/search.result.caches.tpl.php
@@ -105,8 +105,7 @@ if ($usr || !$hide_coords){ echo "
                  </td>
                  <td>
                    <span class=\"content-title-noshade txt-blue08\">GPX format</span>:<br/>        
-			<a class=\"links\" href=\"ocpl";?>{queryid}<?php echo ".gpx?startat=";?>{startat}<?php echo "&amp;count=max&amp;zip=1\" title=\"GPS Exchange Format(OC) .gpx\">OpenCaching</a> | 
-			<a class=\"links\" href=\"ocplgc";?>{queryid}<?php echo ".gpx?startat=";?>{startat}<?php echo "&amp;count=max&amp;zip=1\" title=\"GPS Exchange Format (Groundspeak) .gpx\">Geocaching.com</a> | 
+			<a class=\"links\" href=\"ocplgc";?>{queryid}<?php echo ".gpx?startat=";?>{startat}<?php echo "&amp;count=max&amp;zip=1\" title=\"GPS Exchange Format .gpx\">GPX</a> | 
 			<a class=\"links\" href=\"ocpl";?>{queryid}<?php echo ".zip?startat=";?>{startat}<?php echo "&amp;count=max&amp;zip=1\" title=\"Garmin ZIP file (GPX + zdjęcia)  .zip\">GARMIN</a>
                     </td>
           </tr>
