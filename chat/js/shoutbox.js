818c9063b05be14b1306f4e46419b9aca88f006f
sp2ong <sp2ong@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2012-07-03 07:03:38 +0000

    zmian kolejnosci wyswietlanych postow w shoubox
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@4222 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- chat/js/shoutbox.js
+++ chat/js/shoutbox.js
@@ -10,32 +10,3 @@
 
 	ajaxChat.handleLogout = function() {
 	}
-
-ajaxChat.addMessageToChatList = function(dateObject, userID, userName, userRole, messageID, messageText, channelID, ip) {
-// Prevent adding the same message twice:
-if(this.getMessageNode(messageID)) {
-return;
-}
-if(!this.onNewMessage(dateObject, userID, userName, userRole, messageID, messageText, channelID, ip)) {
-return;
-}
-this.updateDOM('chatList', this.getChatListMessageString(dateObject, userID, userName, userRole, messageID, messageText, channelID, ip),true)
-// This prepends the message instead of appending it to the list
-
-}
-
-ajaxChat.updateChatlistView = function() {
-if(this.dom['chatList'].childNodes && this.settings['maxMessages']) {
-while(this.dom['chatList'].childNodes.length > this.settings['maxMessages']) {
-// Remove the last child for reverse scroll (instead of the first child):
-this.dom['chatList'].removeChild(this.dom['chatList'].lastChild);
-//this.dom['chatList'].removeChild(this.dom['chatList'].firstChild);
-}
-}
-
-if(this.settings['autoScroll']) {
-// Always scroll to the top for reverse scroll:
-this.dom['chatList'].scrollTop = 0;
-//this.dom['chatList'].scrollTop = this.dom['chatList'].scrollHeight;
-}
-}
