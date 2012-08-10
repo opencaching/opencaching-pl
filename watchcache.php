c133b4e565ae82be34d6269c5cad9c3ead9f8bcc
wloczynutka@gmail.com <wloczynutka@gmail.com@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2012-08-10 20:34:32 +0000

    analogiczna poprawka do removecache.php, ten sam robaczek. nie rozumiem za bardzo linijek 40 i 41 bo przy takim zapytaniu chyba zawsze baza zwroci 1, ale zostawiem bo moze sie myle.
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@4258 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- watchcache.php
+++ watchcache.php
@@ -40,18 +40,27 @@
 			$rs = mysql_query('SELECT watcher FROM caches WHERE cache_id=\'' . sql_escape($cache_id) . '\'');
 			if (mysql_num_rows($rs) > 0)
 			{
-				$record = mysql_fetch_array($rs);
-				sql('UPDATE caches SET watcher=\'' . ($record['watcher'] + 1) . '\' WHERE cache_id=\'' . sql_escape($cache_id) . '\'');
+			 // sprawdzenie czy user nie obserwuje już keszynki
+			 // (check if user is not curently watching specified cache)
+			 $id_usera = sql_escape($usr['userid']);
+			 $id_keszynki = sql_escape($cache_id);
+			 $czy_user_obserwuje_kesz = mysql_num_rows(mysql_query("SELECT `id` FROM `cache_watches` WHERE `cache_id` = $id_keszynki AND `user_id` = $id_usera"));
+			 // jeśli tak, dodajemy wpis do bazy
+			 // (if so proceed to remove from database)	 
+			 if ($czy_user_obserwuje_kesz < 1)
+			    {
+				 $record = mysql_fetch_array($rs);
+				 sql('UPDATE caches SET watcher=\'' . ($record['watcher'] + 1) . '\' WHERE cache_id=\'' . sql_escape($cache_id) . '\'');
 
-				//add watch
-				sql('INSERT INTO `cache_watches` (`cache_id`, `user_id`, `last_executed`) VALUES (\'' . sql_escape($cache_id) . '\', \'' . sql_escape($usr['userid']) . '\', NOW())');
+				 //add watch
+				 sql('INSERT INTO `cache_watches` (`cache_id`, `user_id`, `last_executed`) VALUES (\'' . sql_escape($cache_id) . '\', \'' . sql_escape($usr['userid']) . '\', NOW())');
 
-				//add to user
-				$rs = sql('SELECT cache_watches FROM user WHERE user_id=\'' . sql_escape($usr['userid']) . '\'');
-				$record = mysql_fetch_array($rs);
-				sql('UPDATE user SET cache_watches=\'' . ($record['cache_watches'] + 1) . '\' WHERE user_id=\'' . sql_escape($usr['userid']) . '\'');
-				
-				tpl_redirect($target);
+				 //add to user
+				 $rs = sql('SELECT cache_watches FROM user WHERE user_id=\'' . sql_escape($usr['userid']) . '\'');
+				 $record = mysql_fetch_array($rs);
+				 sql('UPDATE user SET cache_watches=\'' . ($record['cache_watches'] + 1) . '\' WHERE user_id=\'' . sql_escape($usr['userid']) . '\'');
+				}
+			 tpl_redirect($target);
 			}
 		}
 	}
