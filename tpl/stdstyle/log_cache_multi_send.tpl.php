025e9c6f22532947c4f1ec66b14bf6718b798aa8
wloczynutka@gmail.com <wloczynutka@gmail.com@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2012-10-06 21:15:15 +0000

    maska selektroa pliku dla field notes.
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@4393 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/log_cache_multi_send.tpl.php
+++ tpl/stdstyle/log_cache_multi_send.tpl.php
@@ -23,7 +23,7 @@ Maksymalny rozmiar pliku to 50KB.<br />
 <br />
 <form enctype="multipart/form-data" method="POST" action="log_cache_multi.php">
 <input type="hidden" name="MAX_FILE_SIZE" value="51200" />
-<b>{{file_name}}</b>:<br/><input name="userfile" type="file" size="60"/><br/><br/>
+<b>{{file_name}}</b>:<br/><input name="userfile" type="file" size="60" accept="Text/plain" /><br/><br/>
 <button type="submit" value="Wyślij"  style="font-size:14px;width:160px"><b>{{submit}}</b></button>
 </form>
 
