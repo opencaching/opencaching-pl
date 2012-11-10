b746c5cf7e196067cdd6367db2e9264c9fc33e05
rygielski@gmail.com <rygielski@gmail.com@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2012-11-10 06:59:43 +0000

    Automatic OKAPI Project update (r493)
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@4419 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- okapi/services/replicate/replicate_common.inc.php
+++ okapi/services/replicate/replicate_common.inc.php
@@ -19,7 +19,7 @@ use okapi\Settings;
 class ReplicateCommon
 {
 	private static $chunk_size = 200;
-	private static $logged_cache_fields = 'code|names|location|type|status|url|owner|founds|notfounds|size|difficulty|terrain|rating|rating_votes|recommendations|req_passwd|descriptions|hints|images|trackables_count|trackables|alt_wpts|last_found|last_modified|date_created|date_hidden';
+	private static $logged_cache_fields = 'code|names|location|type|status|url|owner|founds|notfounds|size|size2|oxsize|difficulty|terrain|rating|rating_votes|recommendations|req_passwd|descriptions|hints|images|trackables_count|trackables|alt_wpts|last_found|last_modified|date_created|date_hidden';
 	
 	private static $logged_log_entry_fields = 'uuid|cache_code|date|user|type|comment';
 	
@@ -200,7 +200,8 @@ class ReplicateCommon
 			Okapi::mail_from_okapi(
 				"rygielski@mimuw.edu.pl",
 				"verify_clog_consistency",
-				"Number of invalid entries fixed: $sum"
+				"Number of invalid entries fixed: $sum\n\n".
+				print_r(Db::select_all("select * from okapi_vars"), true)
 			);
 		}
 	}
