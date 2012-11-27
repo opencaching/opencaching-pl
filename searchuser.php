24449ff8fbb5a5bee54e300997273ba5ca1b79f4
aopole@gmail.com <aopole@gmail.com@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2012-11-27 13:12:37 +0000

    Zmiana wyszukiwania uzytkownikow - teraz mozna wyszukiwac rowniez po fragmencie nazwy
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@4458 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- searchuser.php
+++ searchuser.php
@@ -1,47 +1,53 @@
 <?php
 //prepare the templates and include all neccessary
 	require_once('./lib/common.inc.php');
-
 		//Preprocessing
 	if ($error == false)
 	{
 		//user logged in?
 		if ($usr == false)
 		{
-		    $target = urlencode(tpl_get_current_page());
-		    tpl_redirect('login.php?target='.$target);
-		}
-		else
+			$target = urlencode(tpl_get_current_page());
+			tpl_redirect('login.php?target='.$target);
+		} else
 		{
-		$tplname = 'searchuser';
-		
-		$options['username'] = isset($_POST['username']) ? $_POST['username'] : '';
-		
-		if(!isset($options['username'])) {
-			$options['username']= '';	
-		}
-		
-		if ($options['username'] != '') {
-			$query = "SELECT user_id FROM user WHERE username LIKE '" . sql_escape($options['username']) . "'";;
-			$rs = sql($query);
-			if (mysql_num_rows($rs) != 0) { // Przekierowanie do profilu użytkownika
-				$record = sql_fetch_array($rs);
+			$tplname = 'searchuser';
+			$options['username'] = isset($_POST['username']) ? $_POST['username'] : '';
+			if(!isset($options['username'])) 
+			{
+				$options['username']= '';
+			}
+			if ($options['username'] != '') 
+			{
+				$query = "SELECT user_id, username FROM user WHERE username LIKE '%" . sql_escape($options['username']) . "%'";;
+				$rs = sql($query);
+				if (mysql_num_rows($rs) != 0) 
+				{
+					$ilosc=mysql_num_rows($rs);
+					if ( $ilosc==1 ) 
+					{
+						$linia='';
+						$record = sql_fetch_array($rs);
+						tpl_redirect('viewprofile.php?userid=' . htmlspecialchars($record['user_id'], ENT_COMPAT, 'UTF-8'));
+					} else {
+						$linia="Znaleziono $ilosc kont(a)</BR><UL>";
+						while ($record = sql_fetch_array($rs))
+						{
+							$linia.= "<LI><A HREF='viewprofile.php?userid=" . htmlspecialchars($record['user_id'], ENT_COMPAT, 'UTF-8') . "'>" . htmlspecialchars($record['username'], ENT_COMPAT, 'UTF-8') . "</A></LI>";
+						};
+					};
+					tpl_set_var('username', '');
+					tpl_set_var('not_found', $linia);
+				} else { // Nie znaleziono użytkownika
+					tpl_set_var('username', $options['username']);
+					tpl_set_var('not_found', '<b>Nie znaleziono użytkownika: '. $options['username'] .'</b><br/><br/>');
+				}
+				mysql_free_result($rs);
+			} else {
 				tpl_set_var('username', '');
 				tpl_set_var('not_found', '');
-				tpl_redirect('viewprofile.php?userid=' . htmlspecialchars($record['user_id'], ENT_COMPAT, 'UTF-8'));
-			} else { // Nie znaleziono użytkownika
-          tpl_set_var('username', $options['username']);
-          tpl_set_var('not_found', '<b>Nie znaleziono użytkownika: '. $options['username'] .'</b><br/><br/>');
 			}
-			mysql_free_result($rs);
-		} else {
-      tpl_set_var('username', '');
-      tpl_set_var('not_found', '');
 		}
-		
-		
-		}
-	
 	}
 	tpl_BuildTemplate();
 ?>
