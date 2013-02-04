bf4266bf98039115471944d5696f13131cb3a941
starypatyk@gmail.com <starypatyk@gmail.com@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2013-02-04 19:16:56 +0000

    Minor corrections to HTML syntax (missing angle brackets)
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@4557 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/articles/beginner.tpl.php
+++ tpl/stdstyle/articles/beginner.tpl.php
@@ -1,5 +1,5 @@
 <table class="content" border="0">
-	<tr><td class="content2-pagetitle"><img src="tpl/stdstyle/images/blue/cache.png" class="icon32" alt="" title="{{new_cache}}" align="middle" /><font size="4" <b>Nowa skrzynka - opisy pomocne przy zakładaniu skrzynki</b></font></td></tr>
+	<tr><td class="content2-pagetitle"><img src="tpl/stdstyle/images/blue/cache.png" class="icon32" alt="" title="{{new_cache}}" align="middle" /><font size="4"><b>Nowa skrzynka - opisy pomocne przy zakładaniu skrzynki</b></font></td></tr>
 	<tr><td class="spacer"></td></tr>
 </table>
 <br /><br />
