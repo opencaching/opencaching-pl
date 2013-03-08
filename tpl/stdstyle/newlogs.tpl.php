1ab4faa22dce9490d8ec64aac7d20a270e39d66a
henricusklomp@gmail.com <henricusklomp@gmail.com@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2013-03-08 23:43:54 +0000

    update translations
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@4619 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/newlogs.tpl.php
+++ tpl/stdstyle/newlogs.tpl.php
@@ -7,12 +7,12 @@
 	<div class="searchdiv">
 <table border="0" cellspacing="2" cellpadding="1" style="margin-left: 10px; line-height: 1.4em; font-size: 13px;" width="97%">
 <tr>
-<td><strong>Data</strong></td>
+<td><strong>{{date}}</strong></td>
 <td>&nbsp;</td>
 <td>&nbsp;</td>
 <td>&nbsp;</td>
 <td>&nbsp;</td>
-<td><strong>Geocache</strong></td>
+<td><strong>{{geocache}}</strong></td>
 <td><strong>{{logged_by}}</strong></td>
 </tr>
 <tr>
