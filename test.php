ff3d210c83264b77eac9a9a54fc6be9ca424f40f
wloczynutka@gmail.com <wloczynutka@gmail.com@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2013-03-08 23:06:52 +0000

    online test
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@4613 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- test.php
+++ test.php
@@ -2,6 +2,6 @@
 require_once 'lib/db.php';
 
 $a = new dataBase(true);
-$a->multiVariableQuery('SELECT * from `caches` where  `user_id` between :1 and :2) and type = :3', 1, 20, 2);
+$a->multiVariableQuery('SELECT * from `caches` where  `user_id` between :1 and :2 and type = :3', 1, 40, 2);
 
 ?>
