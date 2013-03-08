08193f867565b462827c28532274d2d72d8e62d1
wloczynutka@gmail.com <wloczynutka@gmail.com@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2013-03-08 23:36:28 +0000

    another test torture..
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@4617 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- test.php
+++ test.php
@@ -2,6 +2,14 @@
 require_once 'lib/db.php';
 
 $a = new dataBase(true);
+
+$cwpt = $a->paramQuery("SELECT `wp_oc` FROM `caches` WHERE `cache_id` = :cache_id", array('cache_id' => array ('value' => $cache_id, 'data_type' => 'integer')));
+$cache_waypt = $cwpt['result']['wp_oc'];
+
+print "one result: $cache_waypt <br>";
+  print 'uzyta pamiec: ' . memory_get_peak_usage () .'<br/><br/>';
+
+
 $zmienna = $a->multiVariableQuery('SELECT * from `caches` where  `user_id` between :1 and :2 and type = :3', 1, 5000, 2);
 
 print 'uzyta pamiec: ' . memory_get_peak_usage ();
