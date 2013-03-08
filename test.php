735dcb23efb62d45d20277b98e1ff022f7492535
wloczynutka@gmail.com <wloczynutka@gmail.com@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2013-03-08 23:23:03 +0000

    memory PDO test with variable.
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@4616 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- test.php
+++ test.php
@@ -2,6 +2,9 @@
 require_once 'lib/db.php';
 
 $a = new dataBase(true);
-$a->multiVariableQuery('SELECT * from `caches` where  `user_id` between :1 and :2 and type = :3', 1, 5000, 2);
+$zmienna = $a->multiVariableQuery('SELECT * from `caches` where  `user_id` between :1 and :2 and type = :3', 1, 5000, 2);
+
+print 'uzyta pamiec: ' . memory_get_peak_usage ();
+
 
 ?>
