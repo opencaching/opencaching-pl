f192e92520e05be113773e4bb0a47ded1c77e716
henricusklomp@gmail.com <henricusklomp@gmail.com@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2013-03-13 23:07:54 +0000

    More translations and some date notation changes
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@4645 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/articles/stat.tpl.php
+++ tpl/stdstyle/articles/stat.tpl.php
@@ -8,9 +8,9 @@
 <img src="tpl/stdstyle/images/free_icons/chart_bar.png" class="icon16" alt="" title="" align="middle" />&nbsp;<a class="links" href="articles.php?page=s3">{{user_ranking_by_number_of_finds_of_their_caches}}</a><br />
 <hr align ="left" style="border: 0; width: 500px;color: #000000; background-color: #000000;height: 1px;"/>
 <img src="tpl/stdstyle/images/free_icons/chart_bar.png" class="icon16" alt="" title="" align="middle" />&nbsp;<a class="links" href="articles.php?page=s4">{{cache_ranking_by_number_of_finds}}</a><br />
-<img src="tpl/stdstyle/images/free_icons/chart_bar.png" class="icon16" alt="" title="" align="middle" />&nbsp;<a class="links" href="articles.php?page=s11a">Ranking skrzynek według liczby odkryć w województwie</a><br />
+<img src="tpl/stdstyle/images/free_icons/chart_bar.png" class="icon16" alt="" title="" align="middle" />&nbsp;<a class="links" href="articles.php?page=s11a">{{cache_ranking_by_finds_per_region}}</a><br />
 <?php if ($usr !== false){ 
-echo '<img src="tpl/stdstyle/images/free_icons/chart_bar.png" class="icon16" alt="" title="" align="middle" />&nbsp;<a class="links" href="cacheratings.php">Ranking skrzynek według liczby rekomendacji</a><br />';
+echo '<img src="tpl/stdstyle/images/free_icons/chart_bar.png" class="icon16" alt="" title="" align="middle" />&nbsp;<a class="links" href="cacheratings.php">{{cache_ranking_by_number_of_recommendations}}</a><br />';
 echo '<img src="tpl/stdstyle/images/free_icons/chart_bar.png" class="icon16" alt="" title="" align="middle" />&nbsp;<a class="links" href="articles.php?page=s5">{{cache_ranking_by_calculated_indicator}}</a><br />';} ?>
 <hr align ="left" style="border: 0; width: 500px;color: #000000; background-color: #000000;height: 1px;"/>
 <img src="tpl/stdstyle/images/free_icons/chart_bar.png" class="icon16" alt="" title="" align="middle" />&nbsp;<a class="links" href="articles.php?page=s7">{{number_of_caches_by_region}}</a><br />
