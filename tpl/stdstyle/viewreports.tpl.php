8ace15f045224b483bab159ee8cdca40bd395e94
henricusklomp@gmail.com <henricusklomp@gmail.com@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2013-03-21 11:19:43 +0000

    admin translations #1
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@4666 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/viewreports.tpl.php
+++ tpl/stdstyle/viewreports.tpl.php
@@ -1,18 +1,18 @@
-<div class="content2-pagetitle"><img src="tpl/stdstyle/images/blue/rproblems.png" class="icon32" align="middle"/>&nbsp;Zgłoszenia problemów</div>
+<div class="content2-pagetitle"><img src="tpl/stdstyle/images/blue/rproblems.png" class="icon32" align="middle"/>&nbsp;{{admin_01}}</div>
 	<div class="buffer"></div>
-	<p>[Przejdź do <a href="viewreports.php?archiwum={archiwum}">{arch_curr}</a>]</p>
+	<p>[{{admin_02}} <a href="viewreports.php?archiwum={archiwum}">{arch_curr}</a>]</p>
 		<input type="hidden" name="cacheid" value="{cacheid}"/>
 		<table border='1' class="table" width="90%">
 			<tr>
 				<th >ID</th>
-				<th >Data zgłoszenia</th>
+				<th >{{admin_03}}</th>
 				<th >Cache</th>
-				<th >Status skrzynki</th>
-				<th >Rodzaj problemu</th>
-				<th >Zgłaszający</th>
-				<th >Prowadzący</th>
+				<th >{{admin_04}}</th>
+				<th >{{admin_05}}</th>
+				<th >{{admin_06}}</th>
+				<th >{{admin_07}}</th>
 				<th >Status</th>
-				<th >Zmieniony przez</th>
+				<th >{{admin_08}}</th>
 			</tr>
 			{content}
 		</table>
