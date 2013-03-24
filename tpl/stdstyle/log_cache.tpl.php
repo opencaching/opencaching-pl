1ed367d08cba3142bf61722ffdd1932c056aa5c7
henricusklomp@gmail.com <henricusklomp@gmail.com@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2013-03-24 09:12:34 +0000

    Another translation for international use
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@4684 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/log_cache.tpl.php
+++ tpl/stdstyle/log_cache.tpl.php
@@ -227,7 +227,7 @@ function toggleGeoKrety() {
 <table class="content" style="font-size: 12px; line-height: 1.6em;">
 	<tr><td colspan="2">&nbsp;</td></tr>
 	<tr>
-		<td colspan="2"><div class="notice" style="width: 500px;">Możesz zgłosić problem związany z skrzynką do właściciela skrzynki np <b>Uwagę do lokalizacji</b> (lub do Zespołu OC PL) wykorzystując ten formularz <img src="/tpl/stdstyle/images/blue/arrow.png" alt="" title=""/>&nbsp; <a class="links" href="reportcache.php?cacheid={cacheid}">Zgłoś problem</a> </div></td>
+		<td colspan="2"><div class="notice" style="width: 500px;">{{report_problem_notice}} <img src="/tpl/stdstyle/images/blue/arrow.png" alt="" title=""/>&nbsp; <a class="links" href="reportcache.php?cacheid={cacheid}">{{report_problem}}</a> </div></td>
 	</tr>
         <tr>                                                                                                                                                                                                                                 
 	                <td colspan="2"><div class="notice" style="width:500px;height:44px">{{empty_entry_notice}}</div></td>                                                                                                                                                                                                 
