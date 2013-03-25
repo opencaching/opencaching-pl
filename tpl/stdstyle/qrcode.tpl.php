0679e4f79ccbebf119f30a5a01f5b8a397ebe724
henricusklomp@gmail.com <henricusklomp@gmail.com@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2013-03-25 22:54:00 +0000

    Translation of qrcode in Dutch
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@4686 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/qrcode.tpl.php
+++ tpl/stdstyle/qrcode.tpl.php
@@ -16,22 +16,20 @@
  ****************************************************************************/
  ?>
  
-<div class="content2-pagetitle"><img src="tpl/stdstyle/images/misc/qrcode.png" class="icon32" alt="" title="" align="middle"/>&nbsp;Etykietka z QR Code</div>
+<div class="content2-pagetitle"><img src="tpl/stdstyle/images/misc/qrcode.png" class="icon32" alt="" title="" align="middle"/>&nbsp;{{qrcode_01}}</div>
 <p>&nbsp;</p>
 
    <div class="searchdiv">
-   <p style="font-size:12px;">Etykietkę z <a class="links" href="http://pl.wikipedia.org/wiki/QR_Code">QR Code</a> (zalaminowaną lub w woreczku stronowym lub jako naklejka) możesz użyć do 
-   zapisania krótkiej wskazówki max 77 znaków, współrzędnych następnego etapu multicache, hasło do logu itp. Do odczytania kodu
-   należy użyć czytnika zainstalowanego np w telefonie komórkowym.</p>
+   <p style="font-size:12px;">{{qrcode_02}}</p>
     <p>&nbsp;</p>   
    <center>{imgqrcode}</center>
 	<br/><br/>
     <form action="qrcode.php" method="post">
-        <span style="font-weight:bold;font-size:14px;width:120px">Dane w QR Code:</span>&nbsp;
+        <span style="font-weight:bold;font-size:14px;width:120px">{{qrcode_03}}:</span>&nbsp;
 	<input name="data" value="{qrcode}" maxlength="77" size="70" style="font-size:14px;"><br/><br/>
-	<span style="margin-left:125px;">&nbsp;</span><button type="submit" name="Generuj" value="Generuj" style="font-size:14px;width:120px"><b>Generuj</b></button>
+	<span style="margin-left:125px;">&nbsp;</span><button type="submit" name="Generuj" value="Generuj" style="font-size:14px;width:120px"><b>{{qrcode_04}}</b></button>
 	</form><br/><br/>
-<img src="tpl/stdstyle/images/misc/16x16-info.png" class="icon16" alt="Info" /><a class="links" href="http://www.i-nigma.com/Downloadi-nigmaReader.html">Tu znajdziesz czytnik QR Code dla różnych telefonów</a>
+<img src="tpl/stdstyle/images/misc/16x16-info.png" class="icon16" alt="Info" /><a class="links" href="http://www.i-nigma.com/Downloadi-nigmaReader.html">{{qrcode_05}}</a>
 <p>&nbsp;</p>   
 </div>
 <p>&nbsp;</p>
