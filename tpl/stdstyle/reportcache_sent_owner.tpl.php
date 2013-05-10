728fa419bb50e371c2a13c153bc9cd230be4fa39
henricusklomp@gmail.com <henricusklomp@gmail.com@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2013-05-10 08:19:49 +0000

    back to original
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@4824 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/reportcache_sent_owner.tpl.php
+++ tpl/stdstyle/reportcache_sent_owner.tpl.php
@@ -5,12 +5,12 @@
 				<col width="200">
 				<col>
 			</colgroup>
-			<tr><td class="content2-pagetitle" colspan="2"><img src="tpl/stdstyle/images/blue/rproblems.png" class="icon32" align="middle" /> <b>	{{report_resultpage}} <a href="viewcache.php?cacheid={cacheid}">{cachename}</a></b></td></tr>
+			<tr><td class="content2-pagetitle" colspan="2"><img src="tpl/stdstyle/images/blue/rproblems.png" class="icon32" align="middle" /> <b>	Zgłoszenie problemu dotyczącego skrzynki <a href="viewcache.php?cacheid={cacheid}">{cachename}</a></b></td></tr>
 			<tr><td colspan="2">&nbsp;</td></tr>
 			<tr><td colspan="2" class="info">
-				{{report_sent_owner}}
+				Zgłoszenie zostało wysłane do właściciela skrzynki.
 				<br />
-				[<a href="index.php">{{main_page}}</a>]&nbsp;[<a href="viewcache.php?cacheid={cacheid}">{{back_to_cache}}</a>]
+				[<a href="index.php">Strona główna</a>]&nbsp;[<a href="viewcache.php?cacheid={cacheid}">Powrót do skrzynki</a>]
 			</td></tr>
 			
 			<tr><td class="spacer" colspan="2"></td></tr>
