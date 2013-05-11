ca0590cd05256bee5314cba11838b8680c89a1e4
henricusklomp@gmail.com <henricusklomp@gmail.com@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2013-05-11 08:39:59 +0000

    Translated RSS feeds
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@4833 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/articles/rss.tpl.php
+++ tpl/stdstyle/articles/rss.tpl.php
@@ -2,14 +2,14 @@
 	<colgroup><col width="100"></colgroup>
 	<tr><td width="750" class="content2-pagetitle">
 	<img src="tpl/stdstyle/images/blue/rss.png" class="icon32" alt="OC" title="" align="middle" />
-	<font size="4">  <b>Kanały RSS OC PL</b></font></td></tr>
+	<font size="4">  <b>{{rss_01}}</b></font></td></tr>
 		<tr><td class="spacer"></td></tr>
 
 </table>
 <br />
-<h1><img src="tpl/stdstyle/images/free_icons/feed.png" class="icon16" alt="" title="" align="middle" />&nbsp;<a class="links" href="http://www.opencaching.pl/rss/newcaches.xml">Najnowsze skrzynki </a></h1>
-<h1><img src="tpl/stdstyle/images/free_icons/feed.png" class="icon16" alt="" title="" align="middle" />&nbsp;<a class="links" href="http://www.opencaching.pl/rss/newlogs.xml">Najnowsze logi </a> </h1>
-<h1><img src="tpl/stdstyle/images/free_icons/feed.png" class="icon16" alt="" title="" align="middle" />&nbsp;<a class="links" href="http://www.opencaching.pl/rss/newnews.xml">Najnowsze wiadomości </a></h1>
+<h1><img src="tpl/stdstyle/images/free_icons/feed.png" class="icon16" alt="" title="" align="middle" />&nbsp;<a class="links" href="http://www.opencaching.pl/rss/newcaches.xml">{{rss_02}} </a></h1>
+<h1><img src="tpl/stdstyle/images/free_icons/feed.png" class="icon16" alt="" title="" align="middle" />&nbsp;<a class="links" href="http://www.opencaching.pl/rss/newlogs.xml">{{rss_03}} </a> </h1>
+<h1><img src="tpl/stdstyle/images/free_icons/feed.png" class="icon16" alt="" title="" align="middle" />&nbsp;<a class="links" href="http://www.opencaching.pl/rss/newnews.xml">{{rss_04}} </a></h1>
 <br />
 
 
