d19423a4a56fef474db5cffded2c17a6cc67769a
wloczynutka <wloczynutka@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2013-05-23 20:01:26 +0000

    runwatch - preparing for multi language. (testing still but this time users will be affected)
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@4874 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- util.sec/watchlist/settings.inc.php
+++ util.sec/watchlist/settings.inc.php
@@ -1,15 +1,33 @@
 <?php
 	// Unicode Reminder ăĄă˘
+ 	global $watchlistMailfrom, $watchlistmailsubject;
+ 
+	$mailfrom = $watchlistMailfrom;
+	$mailsubject = $watchlistmailsubject. ': ' . date('Y-m-d');
 
-	$mailfrom = 'watch@opencaching.pl';
-	$mailsubject = '[OC PL] Watchlist ' . date('d.m.Y');
 
 	$debug = false;
-	$debug_mailto = 'ocpl@opencaching.pl';
+	$debug_mailto = 'rt@opencaching.pl';
 	
 	$nologs = 'Brak wpisów do logu.';
 	
-	$logowner_text = '{date} {user} zrobił wpis ({logtype}) do logu skrzynki "{wp}: {cachename}" .' . "\n" . 'OC link: http://www.opencaching.pl/viewcache.php?wp={wp}'. "\n" . 'OC Mobile link: http://m.opencaching.pl/viewcache.php?wp={wp}'. "\n\n" . '{text}' . "\n\n\n\n";
-	// $logowner_text = '{date} {user} '.tr('runwatch01').' ({logtype}) do logu skrzynki "{wp}: {cachename}" .' . "\n" . 'OC link: '.$absolute_server_URI.'{wp}'. "\n" . 'OC Mobile link: http://m.opencaching.pl/viewcache.php?wp={wp}'. "\n\n" . '{text}' . "\n\n\n\n";
-	$logwatch_text = '{date} {user} zrobił wpis ({logtype}) do logu skrzynki "{wp}: {cachename}" .' . "\n" . 'OC link: http://www.opencaching.pl/viewcache.php?wp={wp}'. "\n" . 'OC Mobile link: http://m.opencaching.pl/viewcache.php?wp={wp}' . "\n\n" . '{text}' . "\n\n\n\n";
+	$logwatch_text = $logowner_text = '
+	  <tr>
+	  	<td valign="top" style="border-right: 1px solid gray; border-spacing: 4px;" align="center">{date}</td>
+	  	<td valign="top" style="border-right: 1px solid gray; border-spacing: 4px;"><b>{user}</b></td>
+	  	<td valign="top" style="border-right: 1px solid gray; border-spacing: 4px;"><span style="color: {logtypeColor}"><b>{logtype}</b></span></td>
+	  	<td valign="top" style="border-right: 1px solid gray; border-spacing: 4px;"><a href="{absolute_server_URI}{wp}">{wp}<br/> {cachename}</a><br/><!--(<a href="http://m.opencaching.pl/viewcache.php?wp={wp}">OC Mobile</a>)--> </td>
+	  	<td valign="top" style="border-spacing: 4px;">
+	  	    {text}
+	  	</td>
+	  </tr>
+	  <tr>
+	  	<td valign="top" style="border-right: 1px solid gray; border-spacing: 4px;"> </td>
+	  	<td valign="top" style="border-right: 1px solid gray; border-spacing: 4px;"> </td>
+	  	<td valign="top" >
+	  </tr>
+	';
+	
+	//$logowner_text = '{date} {user} zrobił wpis ({logtype}) do logu skrzynki "{wp}: {cachename}" .' . "\n" . 'OC link: http://www.opencaching.pl/viewcache.php?wp={wp}'. "\n" . 'OC Mobile link: http://m.opencaching.pl/viewcache.php?wp={wp}'. "\n\n" . '{text}' . "\n\n\n\n";
+	//$logwatch_text = '{date} {user} zrobił wpis ({logtype}) do logu skrzynki "{wp}: {cachename}" .' . "\n" . 'OC link: http://www.opencaching.pl/viewcache.php?wp={wp}'. "\n" . 'OC Mobile link: http://m.opencaching.pl/viewcache.php?wp={wp}' . "\n\n" . '{text}' . "\n\n\n\n";
 ?>
