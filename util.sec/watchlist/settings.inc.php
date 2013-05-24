90333b0f9e935e991f5d4c25fb73b905709a3385
wloczynutka <wloczynutka@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2013-05-24 19:55:30 +0000

    runwatch - add paddings
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@4884 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- util.sec/watchlist/settings.inc.php
+++ util.sec/watchlist/settings.inc.php
@@ -11,19 +11,19 @@
 	
 	$logwatch_text = $logowner_text = '
 	  <tr>
-	  	<td valign="top" style="border-right: 1px solid gray; border-spacing: 4px; font-size: 9px;" align="center">{date}</td>
-	  	<td valign="top" style="border-right: 1px solid gray; border-spacing: 4px; font-size: 9px;"><b>{user}</b></td>
-	  	<td valign="top" style="border-right: 1px solid gray; border-spacing: 4px; font-size: 9px;"><span style="color: {logtypeColor}"><b>{logtype}</b></span></td>
-	  	<td valign="top" style="border-right: 1px solid gray; border-spacing: 4px; font-size: 9px;"><a href="{absolute_server_URI}{wp}">{wp}<br/> {cachename}</a><br/><!--(<a href="http://m.opencaching.pl/viewcache.php?wp={wp}">OC Mobile</a>)--> </td>
-	  	<td valign="top" style="border-spacing: 4px;">
+	  	<td valign="top" style="border-right: 1px solid gray; padding-right:4px; padding-left:4px; font-size: 9px;" align="center">{date}</td>
+	  	<td valign="top" style="border-right: 1px solid gray; padding-right:4px; padding-left:4px; font-size: 9px;"><b>{user}</b></td>
+	  	<td valign="top" style="border-right: 1px solid gray; padding-right:4px; padding-left:4px; font-size: 9px;"><span style="color: {logtypeColor}"><b>{logtype}</b></span></td>
+	  	<td valign="top" style="border-right: 1px solid gray; padding-right:4px; padding-left:4px; font-size: 9px;"><a href="{absolute_server_URI}{wp}">{wp}<br/> {cachename}</a><br/><!--(<a href="http://m.opencaching.pl/viewcache.php?wp={wp}">OC Mobile</a>)--> </td>
+	  	<td valign="top" style="padding-right:4px; padding-left:4px;">
 	  	    {text}
 	  	</td>
 	  </tr>
 	  <tr>
-	  	<td valign="top" style="border-right: 1px solid gray; border-spacing: 4px;"> </td>
-	  	<td valign="top" style="border-right: 1px solid gray; border-spacing: 4px;"> </td>
-	  	<td valign="top" style="border-right: 1px solid gray; border-spacing: 4px;"> </td>
-	  	<td valign="top" style="border-right: 1px solid gray; border-spacing: 4px;"> </td>
+	  	<td valign="top" style="border-right: 1px solid gray;"> </td>
+	  	<td valign="top" style="border-right: 1px solid gray;"> </td>
+	  	<td valign="top" style="border-right: 1px solid gray;"> </td>
+	  	<td valign="top" style="border-right: 1px solid gray;"> </td>
 	  	<td valign="top" >
 	  </tr>
 	';
