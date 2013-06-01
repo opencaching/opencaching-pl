399d3b52c3edf004360531589e933137b79c2c93
henricusklomp@gmail.com <henricusklomp@gmail.com@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2013-06-01 22:16:17 +0000

    Make the link to absolute server and userprofile work correct
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@4903 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/mailto.inc.php
+++ tpl/stdstyle/mailto.inc.php
@@ -33,14 +33,14 @@ $mailsubject = "[opencaching] ".tr('mailto_03')." '{from_username}': {subject}";
 
 $mailtext_email = tr('mailto_04')." {to_username},\n\n";
 $mailtext_email .= "'{from_username}' ".tr('mailto_05')." {from_email} ".tr('mailto_06')." $absolute_server_URI\n";
-$mailtext_email .= tr('mailto_07')." $absolute_server_URI viewprofile.php?userid={from_userid}\n";
+$mailtext_email .= tr('mailto_07'). $absolute_server_URI."viewprofile.php?userid={from_userid}\n";
 $mailtext_email .= tr('mailto_08')."\n";
 $mailtext_email .= "----------------------\n\n";
 $mailtext_email .= "{{text}}\n";
 
 $mailtext_anonymous = tr('mailto_04')." {to_username},\n\n";
 $mailtext_anonymous .= "'{from_username}' ".tr('mailto_06')." www.opencaching.pl\n";
-$mailtext_anonymous .= tr('mailto_07')." $absolute_server_URI viewprofile.php?userid={from_userid}\n";
+$mailtext_anonymous .= tr('mailto_07'). $absolute_server_URI."viewprofile.php?userid={from_userid}\n";
 $mailtext_anonymous .= tr('mailto_09')."\n";
 $mailtext_anonymous .= "----------------------\n";
 $mailtext_anonymous .= "{{text}}\n";
