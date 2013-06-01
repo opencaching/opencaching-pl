2aed1102e57a35d99b85d737ec5bdb074cbf4af7
henricusklomp@gmail.com <henricusklomp@gmail.com@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2013-06-01 01:01:33 +0000

    small correction. Link to real server will be done later
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@4900 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/mailto.inc.php
+++ tpl/stdstyle/mailto.inc.php
@@ -32,7 +32,7 @@ $errnotext = '<span class="errormsg">'.tr('mailto_02').'</span>';
 $mailsubject = "[opencaching] ".tr('mailto_03')." '{from_username}': {subject}";
 
 $mailtext_email = tr('mailto_04')." {to_username},\n\n";
-$mailtext_email .= "'{from_username} ".tr('mailto_05')." {from_email} ".tr('mailto_06')." $absolute_server_URI\n";
+$mailtext_email .= "'{from_username}' ".tr('mailto_05')." {from_email} ".tr('mailto_06')." $absolute_server_URI\n";
 $mailtext_email .= tr('mailto_07')." $absolute_server_URI viewprofile.php?userid={from_userid}\n";
 $mailtext_email .= tr('mailto_08')."\n";
 $mailtext_email .= "----------------------\n\n";
