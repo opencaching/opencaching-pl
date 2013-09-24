c6c1684b139415d5b371f55a41d860c7fb2a20c8
marcin.wozniak@gmail.com <marcin.wozniak@gmail.com@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2013-09-24 07:02:47 +0000

    5923 was already there.
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@5131 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- lib/common.inc.php
+++ lib/common.inc.php
@@ -338,7 +338,7 @@ if ((!isset($GLOBALS['oc_waypoint'])) && isset($GLOBALS['ocWP']))
 	
 /* temporary check for PowerTrails*/
 /* to be deleted after powerTrail module completed publishing to all users */
-$powerTrailBetaTesters = array(9067, 9078, 7699, 7969, 4029, 10737, 1038, 33407, 1769, 1, 3, 24777, 26496, 19613, 34803, 9778, 29133, 24076, 19738, 38859, 22940, 5923, 20932, 24515, 16517, 15695, 33346, 21165, 5923);
+$powerTrailBetaTesters = array(9067, 9078, 7699, 7969, 4029, 10737, 1038, 33407, 1769, 1, 3, 24777, 26496, 19613, 34803, 9778, 29133, 24076, 19738, 38859, 22940, 5923, 20932, 24515, 16517, 15695, 33346, 21165);
 if (in_array($usr['userid'], $powerTrailBetaTesters)){
 	$powerTrailModuleSwitchOn = true;
 } else {
