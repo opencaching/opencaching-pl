c277378b2f3b8915180537295a70e61c91487373
wloczynutka <wloczynutka@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2013-11-07 20:34:56 +0000

    PT diagnose
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@5333 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- test.php
+++ test.php
@@ -1,5 +1,32 @@
 <?php
-require_once(__DIR__.'/lib/db.php');;
+require_once(__DIR__.'/lib/db.php');
+$ptId = addslashes($_REQUEST['id']);
+
+$q1 = 'SELECT `PowerTrail_actionsLog` . * , `user`.`username` FROM `PowerTrail_actionsLog` , `user` WHERE `PowerTrail_actionsLog`.`userId` = `user`.`user_id` AND `PowerTrailId` =:1 ORDER BY actionDateTime ASC ';
+$q2 = 'SELECT * FROM `PowerTrail` WHERE `id` =:1'; 
+$q3 = 'SELECT * FROM `PowerTrail_owners` WHERE `PowerTrailId` = :1';
+
+$db=new dataBase;
+$db->multiVariableQuery($q1, $ptId);
+$r1 = $db->dbResultFetchAll();
+$db->multiVariableQuery($q2, $ptId);
+$r2 = $db->dbResultFetchAll();
+$db->multiVariableQuery($q3, $ptId);
+$r3 = $db->dbResultFetchAll();
+
+print '<pre>';
+print '$r1<br>';
+print_r($r1);
+print '<hr>$r2<br>';
+print_r($r2);
+print '<hr>$r3<br>';
+print_r($r3);
+print '</pre><hr>';
+
+print serialize(array($r1,$r2,$r3));
+exit;
+/*
+require_once(__DIR__.'/lib/db.php');
 require_once __DIR__.'/powerTrail/powerTrailBase.php';
 
 $user_id = addslashes($_REQUEST['u']);
@@ -14,11 +41,8 @@ print '<pre>';
 var_dump($pointsEarnedForPlacedCaches);
 
 exit;
+*/
 
-
-require_once 'powerTrail/powerTrailBase.php';
-// powerTrailBase::writePromoPt4mainPage();	35	// powerTrailBase::writePromoPt4mainPage();
-require_once 'util.sec/geokrety/processGeokretyErrors.php';	
 require_once 'util.sec/geokrety/processGeokretyErrors.php';
 $g = new processGeokretyErrors;	
 $g->run();
