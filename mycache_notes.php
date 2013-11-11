b3f5a4f0c9505a3afce805728f561f50c2763971
tripper1971g@gmail.com <tripper1971g@gmail.com@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2013-11-11 00:16:49 +0000

    notes of caches: an uniform list, PDO, deleting note from the list
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@5346 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- mycache_notes.php
+++ mycache_notes.php
@@ -11,7 +11,7 @@
 
 //prepare the templates and include all neccessary
 	require_once('./lib/common.inc.php');
-	
+	require_once('./lib/db.php');
 
 
 	//Preprocessing
@@ -28,29 +28,55 @@
 			$tplname = 'mycache_notes';
 			//get user record
 			$userid = $usr['userid'];
-
-			$notes_rs = sql("SELECT `cache_notes`.`cache_id` `cacheid`, `caches`.`name` `cache_name`, `cache_type`.`icon_small` `icon_large` FROM `cache_notes` INNER JOIN caches ON (`caches`.`cache_id` = `cache_notes`.`cache_id`), `cache_type`  WHERE `cache_notes`.`user_id`=&1 AND `cache_type`.`id`=`caches`.`type` GROUP BY `cacheid` ORDER BY `cacheid`,`date` DESC",$userid);
-			if (mysql_num_rows($notes_rs) != 0)
-			{	
-						$notes = '<table border="0" cellspacing="2" cellpadding="1" style="margin-left: 10px; line-height: 1.4em; font-size: 13px;" width="95%">';
-						$notes .= '<tr><td width="22">&nbsp;</td><td><strong>GeoCache</strong></td></tr><tr><td colspan="2"><hr></hr></td></tr>';
-
-						for ($i = 0; $i < mysql_num_rows($notes_rs); $i++)
-							{
-							
-							$notes_record = sql_fetch_array($notes_rs);
-							
-							$notes .= '<tr><td width="22"><img src="tpl/stdstyle/images/'.$notes_record['icon_large'].'" alt="" /></td><td align="left" valign="middle"><a class="links" href="viewcache.php?cacheid='.$notes_record['cacheid'].'">'.$notes_record['cache_name'].'</a></td></tr>';
-							}
-							$notes .= '<tr><td colspan="2"><hr></hr></td></tr></table><br /><br />';
-
-
-						tpl_set_var('notes_content', $notes);
-						mysql_free_result($notes_rs);
-
-				} else 	{ tpl_set_var('notes_content', '<br/><span style="font-size: 14px;">&nbsp;&nbsp;<strong>'.tr(no_note).'</strong></span><br/></br/>'); }
 				
+			
+			$db = new dataBase(); 			
+			if (isset( $_REQUEST["delete"]))
+			{
+				$note_id = $_REQUEST["delete"];				
+				//remove
+				$query = "DELETE FROM `cache_notes` WHERE `note_id`=:1";
+				$db->multiVariableQuery($query, $note_id );						
+			}
+			//else
+			{
 
+				//$notes_rs = sql("SELECT `cache_notes`.`cache_id` `cacheid`, `caches`.`name` `cache_name`, `cache_type`.`icon_small` `icon_large` FROM `cache_notes` INNER JOIN caches ON (`caches`.`cache_id` = `cache_notes`.`cache_id`), `cache_type`  WHERE `cache_notes`.`user_id`=&1 AND `cache_type`.`id`=`caches`.`type` GROUP BY `cacheid` ORDER BY `cacheid`,`date` DESC",$userid);
+				$query = "SELECT `cache_notes`.`cache_id` `cacheid`, `caches`.`name` `cache_name`, `cache_type`.`icon_small` `icon_large`, note_id FROM `cache_notes` INNER JOIN caches ON (`caches`.`cache_id` = `cache_notes`.`cache_id`), `cache_type`  WHERE `cache_notes`.`user_id`=:1 AND `cache_type`.`id`=`caches`.`type` GROUP BY `cacheid` ORDER BY `cacheid`,`date` DESC";
+				$db->multiVariableQuery($query, $userid);
+				
+				//if (mysql_num_rows($notes_rs) != 0)
+				$count = $db->rowCount();
+				if( $count != 0)
+				{	
+							//$notes = '<table border="0" cellspacing="2" cellpadding="1" style="margin-left: 10px; line-height: 1.4em; font-size: 13px;" width="95%">';
+							//$notes .= '<tr><td width="22">&nbsp;</td><td><strong>GeoCache</strong></td></tr><tr><td colspan="2"><hr></hr></td></tr>';
+							$notes ="";
+							$bgcolor1 = '#ffffff';
+							$bgcolor2 = '#eeeeee';
+							
+							//for ($i = 0; $i < mysql_num_rows($notes_rs); $i++)
+							for ($i = 0; $i < $count; $i++)
+							{
+								$bgcolor = ( $i% 2 )? $bgcolor1 : $bgcolor2;
+									
+								//$notes_record = sql_fetch_array($notes_rs);
+								$notes_record = $db->dbResultFetch();
+								
+								$notes .= '<tr><td style="background-color: {bgcolor}"><img src="tpl/stdstyle/images/'.$notes_record['icon_large'].'" alt="" /></td><td align="left"  style="background-color: {bgcolor}"><a  href="viewcache.php?cacheid={cacheid}">'.$notes_record['cache_name'].'</a></td><td style="background-color: {bgcolor}; text-align:center"><a class="links"  href="mycache_notes.php?delete={noteid}" onclick="return confirm(\''.tr("mycache_notes_01").'\');"><img style="vertical-align: middle;" src="tpl/stdstyle/images/log/16x16-trash.png" alt="" title='.tr('delete').' /></a></td></tr>';
+								$notes = mb_ereg_replace('{bgcolor}', $bgcolor, $notes);
+								$notes = mb_ereg_replace('{cacheid}', $notes_record["cacheid"], $notes);
+								$notes = mb_ereg_replace('{noteid}', $notes_record["note_id"], $notes);
+							}
+							//$notes .= '<tr><td colspan="3"><hr></hr></td></tr></table><br /><br />';
+	
+	
+							tpl_set_var('notes_content', $notes);
+							mysql_free_result($notes_rs);
+	
+					} else 	{ tpl_set_var('notes_content', '<br/><span style="font-size: 14px;">&nbsp;&nbsp;<strong>'.tr(no_note).'</strong></span><br/></br/>'); }
+			}	
+			unset( $db );
 		}			
 	}
 		
--- tpl/stdstyle/mycache_notes.tpl.php
+++ tpl/stdstyle/mycache_notes.tpl.php
@@ -14,9 +14,26 @@
 
 <div class="content2-pagetitle"><img src="tpl/stdstyle/images/blue/logs.png" class="icon32" alt="" />&nbsp;{{mycache_note}} </div>
 
+
+
 <div class="searchdiv">
-	{notes_content}
+<table class="table" border="0" cellspacing="0">
+	<colgroup>
+		<col width="10px"/>
+		<col width="680px"/>
+		<col width="40px"/>
+	</colgroup>
+	
+	<tr>
+		<td><p class="content-title-noshade"></p></td>
+		<td><p class="content-title-noshade">{{geocache}}</p></td>
+		<td nowrap="nowrap" class="content-title-noshade" style = "text-align: center" >{{delete}}</td>
+	</tr>
+{notes_content}
+</table>
 </div>
+
+
 <div class="notice" id="viewcache-attributesend">{{mycache_notes}} <a href="http://wiki.opencaching.pl/index.php/Notatki_skrzynki" target="_blank">{{here}}</a></div>
 
 
