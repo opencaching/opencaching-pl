1625dfb620c9106a457eea9e3db95bcc35f31e31
tripper1971g@gmail.com <tripper1971g@gmail.com@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2013-11-12 11:45:20 +0000

    I added google analitics in one of statistics.
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@5353 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/articles/s2.tpl.php
+++ tpl/stdstyle/articles/s2.tpl.php
@@ -1,3 +1,18 @@
+ 
+<script>
+//JG - włączenie śledzenia na potrzeby oszacowania użycia zasobów serwera 
+
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-45656189-1', 'opencaching.pl');
+  ga('send', 'pageview');
+
+</script>
+
+
 <table class="content">
 	<tr><td class="content2-pagetitle"><img src="tpl/stdstyle/images/blue/stat1.png" class="icon32" alt="Statystyki" title="Statystyki" align="middle" /><font size="4">  <b>{{statistics}}</b></font></td></tr>
 	<tr><td class="spacer"></td></tr>
