68f6031afbe523381ed67b1965852ea1ab507d8b
tripper1971g@gmail.com <tripper1971g@gmail.com@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2013-11-21 18:27:36 +0000

    Added GA to stats
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@5378 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- articles.php
+++ articles.php
@@ -82,18 +82,10 @@
 			$tplname = 'news';
 		} 
 	
-		$ga_scr = "<script> 
-
-					  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-					  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-					  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-					  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-					
-					  ga('create', 'UA-45656189-1', 'opencaching.pl');
-					  ga('send', 'pageview');
-				
-				</script>";
+		//$file_content = file_get_contents( "lib/js/ga.js" );
+		//$ga_scr = "<script> " . $file_content . " </script>";
 		
+		$ga_scr = " <script type='text/javascript' src = 'lib/js/ga.js'></script> ";
 		tpl_set_var( 'ga_script_header', $ga_scr);		
 		
 	//make the template and send it out
--- tpl/stdstyle/articles/s1.tpl.php
+++ tpl/stdstyle/articles/s1.tpl.php
@@ -2,6 +2,15 @@
 	<tr><td class="content2-pagetitle"><img src="tpl/stdstyle/images/blue/stat1.png" class="icon32" alt="{{stats}}" title="{{stats}}" align="middle" /><font size="4">  <b>{{statistics}}</b></font></td></tr>
 	<tr><td class="spacer"></td></tr>
 </table>
+<script type="text/javascript">
+TimeTrack( "START" );
+</script>
+
+<?php
+global $debug_page; 
+if ( $debug_page )
+	echo "<script type='text/javascript'>TimeTrack( 'DEBUG' );</script>";  
+?>
 
 <table class="table" width="760" style="line-height: 1.6em; font-size: 10px;">
 <tr>
@@ -9,4 +18,6 @@
 </td></tr>
 </table>
 
-
+<script type="text/javascript">
+TimeTrack( "END", "S1" );
+</script>
