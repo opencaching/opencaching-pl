08ed67137ed9d8d2f3d3dc8d7104a749ff23c3e9
feliks.przybyszewski@gmail.com <feliks.przybyszewski@gmail.com@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2013-11-25 23:26:06 +0000

    mycaches.php - sortable column with number of visits added (sql()->PDO) - 2nd attempt
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@5404 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/mycaches.tpl.php
+++ tpl/stdstyle/mycaches.tpl.php
@@ -1,13 +1,13 @@
 <?php
 /***************************************************************************
-        *                                                                                                      
-        *   This program is free software; you can redistribute it and/or modify        
-        *   it under the terms of the GNU General Public License as published by  
-        *   the Free Software Foundation; either version 2 of the License, or          
-        *   (at your option) any later version.
-        *  
-        *  UTF-8 ąść
-        ***************************************************************************/
+	*                                         				                                
+	*   This program is free software; you can redistribute it and/or modify  	
+	*   it under the terms of the GNU General Public License as published by  
+	*   the Free Software Foundation; either version 2 of the License, or	    	
+	*   (at your option) any later version.
+	*   
+	*  UTF-8 ąść
+	***************************************************************************/
 ?>
 <script type="text/javascript" src="lib/js/wz_tooltip.js"></script>
 <div class="content2-pagetitle"><img src="tpl/stdstyle/images/blue/cache.png" class="icon32" alt="" title="" align="middle"/>&nbsp;{{my_caches_status}}: <font color="black">{cache_stat}</font></div>
@@ -28,18 +28,19 @@
 <td colspan="2"><a class="links" href="mycaches.php?col=1{my_cache_sort}">{{date_hidden_label}}</a></td>
 <td></td>
 <td><a class="links" href="mycaches.php?col=2{my_cache_sort}">{{geocache}}</a></td>
-<td><a class="links" href="mycaches.php?col=3{my_cache_sort}"><img src="tpl/stdstyle/images/log/16x16-found.png"></a></td>
-<td><a class="links" href="mycaches.php?col=4{my_cache_sort}"><img src="images/rating-star.png"></a></td>
-<td><a class="links" href="mycaches.php?col=6{my_cache_sort}"><img src="images/gk.png"></a></td>
+<td align="middle"><a class="links" href="mycaches.php?col=3{my_cache_sort}" alt="{{mc_by_founds}}" title="{{mc_by_founds}}"><img src="tpl/stdstyle/images/log/16x16-found.png"></a></td>
+<td align="middle"><a class="links" href="mycaches.php?col=4{my_cache_sort}" alt="{{mc_by_reco}}" title="{{mc_by_reco}}"><img src="images/rating-star.png"></a></td>
+<td align="middle"><a class="links" href="mycaches.php?col=6{my_cache_sort}" alt="{{mc_by_gk}}" title="{{mc_by_gk}}"><img src="images/gk.png"></a></td>
+<td align="middle"><a class="links" href="mycaches.php?col=7{my_cache_sort}" alt="{{mc_by_visits}}" title="{{mc_by_visits}}"><img src="tpl/stdstyle/images/free_icons/vcard.png"></a></td>
 <td><a class="links" href="mycaches.php?col=5{my_cache_sort}">{{last_found}}</a></td>
 <td><strong>{{latest_logs}}</strong></td>
 </tr>
-<tr><td colspan="9"><hr></hr></td></tr>
+<tr><td colspan="10"><hr></hr></td></tr>
 {file_content}
-<tr><td colspan="9"><hr></hr></td></tr>
+<tr><td colspan="10"><hr></hr></td></tr>
 </table>
 </div>
-        <p>
-                {pages}
-        </p>
+	<p>
+		{pages}
+	</p>
 
