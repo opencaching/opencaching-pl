b3ea872217e0065afad108a381b11c4a204e3a70
feliks.przybyszewski@gmail.com <feliks.przybyszewski@gmail.com@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2013-12-08 08:34:08 +0000

    Added found-by-user indication in mycache_notes.php
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@5436 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- mycache_notes.php
+++ mycache_notes.php
@@ -12,7 +12,8 @@
 //prepare the templates and include all neccessary
 	require_once('./lib/common.inc.php');
 	require_once('./lib/db.php');
-
+require_once  __DIR__.'/lib/myn.inc.php';
+require_once  __DIR__.'/lib/db.php';
 
 	//Preprocessing
 	if ($error == false)
@@ -42,7 +43,12 @@
 			{
 
 				//$notes_rs = sql("SELECT `cache_notes`.`cache_id` `cacheid`, `caches`.`name` `cache_name`, `cache_type`.`icon_small` `icon_large` FROM `cache_notes` INNER JOIN caches ON (`caches`.`cache_id` = `cache_notes`.`cache_id`), `cache_type`  WHERE `cache_notes`.`user_id`=&1 AND `cache_type`.`id`=`caches`.`type` GROUP BY `cacheid` ORDER BY `cacheid`,`date` DESC",$userid);
-				$query = "SELECT `cache_notes`.`cache_id` `cacheid`, `caches`.`name` `cache_name`, `cache_type`.`icon_small` `icon_large`, note_id FROM `cache_notes` INNER JOIN caches ON (`caches`.`cache_id` = `cache_notes`.`cache_id`), `cache_type`  WHERE `cache_notes`.`user_id`=:1 AND `cache_type`.`id`=`caches`.`type` GROUP BY `cacheid` ORDER BY `cacheid`,`date` DESC";
+				$query = "SELECT `cache_notes`.`cache_id` `cacheid`,
+								 `caches`.`name` `cache_name`, 
+								 `cache_type`.`icon_small` `icon_large`, 
+								 `caches`.`type` `cache_type`,
+								 note_id 
+								 FROM `cache_notes` INNER JOIN caches ON (`caches`.`cache_id` = `cache_notes`.`cache_id`), `cache_type`  WHERE `cache_notes`.`user_id`=:1 AND `cache_type`.`id`=`caches`.`type` GROUP BY `cacheid` ORDER BY `cacheid`,`date` DESC";
 				$db->multiVariableQuery($query, $userid);
 				
 				//if (mysql_num_rows($notes_rs) != 0)
@@ -62,8 +68,14 @@
 									
 								//$notes_record = sql_fetch_array($notes_rs);
 								$notes_record = $db->dbResultFetch();
+				$cacheicon =  $cache_icon_folder;
+				if ($notes_record['cache_type']!="6") {
+					$cacheicon .=is_cache_found($notes_record['cacheid'], $userid) ? $foundCacheTypesIcons[$notes_record['cache_type']] : $CacheTypesIcons[$notes_record['cache_type']] ;
+				} else {
+					$cacheicon .=is_event_attended ($notes_record['cacheid'],$userid) ? $foundCacheTypesIcons["6"] : $CacheTypesIcons["6"] ;
+				};	
 								
-								$notes .= '<tr><td style="background-color: {bgcolor}"><img src="tpl/stdstyle/images/'.$notes_record['icon_large'].'" alt="" /></td><td align="left"  style="background-color: {bgcolor}"><a  href="viewcache.php?cacheid={cacheid}">'.$notes_record['cache_name'].'</a></td><td style="background-color: {bgcolor}; text-align:center"><a class="links"  href="mycache_notes.php?delete={noteid}" onclick="return confirm(\''.tr("mycache_notes_01").'\');"><img style="vertical-align: middle;" src="tpl/stdstyle/images/log/16x16-trash.png" alt="" title='.tr('delete').' /></a></td></tr>';
+								$notes .= '<tr><td style="background-color: {bgcolor}"><img src="'.$cacheicon.'" alt="" /></td><td align="left"  style="background-color: {bgcolor}"><a  href="viewcache.php?cacheid={cacheid}">'.$notes_record['cache_name'].'</a></td><td style="background-color: {bgcolor}; text-align:center"><a class="links"  href="mycache_notes.php?delete={noteid}" onclick="return confirm(\''.tr("mycache_notes_01").'\');"><img style="vertical-align: middle;" src="tpl/stdstyle/images/log/16x16-trash.png" alt="" title='.tr('delete').' /></a></td></tr>';
 								$notes = mb_ereg_replace('{bgcolor}', $bgcolor, $notes);
 								$notes = mb_ereg_replace('{cacheid}', $notes_record["cacheid"], $notes);
 								$notes = mb_ereg_replace('{noteid}', $notes_record["note_id"], $notes);
@@ -72,7 +84,7 @@
 	
 	
 							tpl_set_var('notes_content', $notes);
-							mysql_free_result($notes_rs);
+							//mysql_free_result($notes_rs);
 	
 					} else 	{ tpl_set_var('notes_content', '<br/><span style="font-size: 14px;">&nbsp;&nbsp;<strong>'.tr(no_note).'</strong></span><br/></br/>'); }
 			}	
