4adec9729ff8404502e465ae066bfd293210b284
tripper1971g@gmail.com <tripper1971g@gmail.com@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2013-12-13 22:26:45 +0000

    fix size
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@5448 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/myhome2.tpl.php
+++ tpl/stdstyle/myhome2.tpl.php
@@ -34,7 +34,7 @@
 	<tr>
 		<td class="header-small" colspan="2"> 
 		<img src="tpl/stdstyle/images/blue/logs.png" class="icon32" alt="Logs" title="Logs"/>&nbsp;
-		<span class="content-title-noshade-size0"> Znalazłeś {founds} skrzynek. 
+		<span class="content-title-noshade-size1"> Znalazłeś {founds} skrzynek. 
 		{events}		
 		[<a href="search.php?showresult=1&amp;expert=0&amp;output=HTML&amp;sort=byname&amp;finderid={userid}&amp;searchbyfinder=&amp;f_inactive=0&amp;f_ignored=0&amp;f_userfound=0&amp;f_userowner=0">Pokaż wszystkie</a>]</span>
 		</td>
@@ -42,7 +42,7 @@
 	<tr>
 		<td colspan="2">
 			{reports}<br /><br />
-			<span class="content-title-noshade-size1">
+			<span class="content-title-noshade-size2">
 			<b>{{your_newest_log_entries}}:</b><br />
 			</span>
 			<table class="table">
