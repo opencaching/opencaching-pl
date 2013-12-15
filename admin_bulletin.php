82d3a6723f9997214ba22b288c0032ec9dff3318
feliks.przybyszewski@gmail.com <feliks.przybyszewski@gmail.com@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2013-12-15 22:56:51 +0000

    Repacing hardcoded Polish text with tr function in admin_bulletin.php
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@5463 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- admin_bulletin.php
+++ admin_bulletin.php
@@ -10,7 +10,7 @@
 			// podgląd
 			$bulletin = addslashes($_POST['bulletin']);
 			
-			$_SESSION['bulletin'] = $bulletin;
+			$_SESSION['bulletin'] = $bulletin;	
 			tpl_set_var('bulletin', stripslashes(nl2br($bulletin)));
 			$tplname = 'admin_bulletin_preview';
 			tpl_BuildTemplate();
@@ -20,20 +20,22 @@
 		{
 			// wysłanie
 			$email_headers = "Content-Type: text/plain; charset=utf-8\r\n";
-			$email_headers .= "From: Opencaching.pl <rr@opencaching.pl>\r\n";
-			$email_headers .= "Reply-To: rr@opencaching.pl\r\n";
+			$email_headers .= "From: ". $site_name." <".$mail_rr.">\r\n";
+			$email_headers .= "Reply-To: ".$mail_rr."\r\n";
 			
 			$bulletin = ($_SESSION['bulletin']);
 			$sql = "INSERT INTO bulletins (content, user_id) VALUES ('".sql_escape($bulletin)."', ".sql_escape(intval($usr['userid'])).")";
 			@mysql_query($sql);
-			
-			$bulletin .= "\r\n\r\nJeśli nie chcesz więcej otrzymywać biuletynów informacyjnych z Opencaching.pl, zmień ustawienia konta na http://www.opencaching.pl/myprofile.php?action=change.";
+			$tr_newsletter_removal = tr('newsletter_removal');
+			$bulletin .= "\r\n\r\n".$tr_newsletter_removal." na ".$absolute_server_URI."myprofile.php?action=change.";
 			//get emails
 			$sql = "SELECT `email` FROM `user` WHERE `is_active_flag`=1 AND get_bulletin=1 AND rules_confirmed=1";
 			$query = @mysql_query($sql);
+			$tr_newsletter = $short_sitename." ".tr('Newsletter');
 			while( $email = @mysql_fetch_array($query))
 			{
-				mb_send_mail($email['email'], "[OC PL] Biuletyn informacyjny ".date("Y-m-d"), stripslashes($bulletin), $email_headers);
+				
+				mb_send_mail($email['email'], $tr_newsletter." ".date("Y-m-d"), stripslashes($bulletin), $email_headers);
 			}
 			$_SESSION['submitted'] = true;
 			tpl_set_var('bulletin', stripslashes($_SESSION['bulletin']));
