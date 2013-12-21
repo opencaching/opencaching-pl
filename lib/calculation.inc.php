902ebfe38149be87ab9e477afb9a82c8151bfe2d
feliks.przybyszewski@gmail.com <feliks.przybyszewski@gmail.com@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2013-12-21 11:50:59 +0000

    Testing own coord to quiz changes -1
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@5507 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- lib/calculation.inc.php
+++ lib/calculation.inc.php
@@ -152,6 +152,57 @@ function calcDistance($latFrom, $lonFrom, $latTo, $lonTo, $distanceMultiplier=1)
 	 return	$distance;
 	 }
 
+function getCalcDistanceSqlFormula($modEnabled, $lonFrom, $latFrom, $maxDistance, $distanceMultiplier=1, $lonField='longitude', 
+	$latField='latitude', $tableName = 'caches',$modLonField='longitude', $modLatField='latitude', $modTableName='cache_mod_cords')
+{
+	if ($modEnabled) 
+	{
+		return getSqlDistanceFormulaForModCoords($lonFrom, $latFrom, $maxDistance, $distanceMultiplier, $lonField, $latField, 
+			$tableName, $modLonField, $modLatField, $modTableName);
+	}
+	else
+	{
+		return getSqlDistanceFormula($lonFrom, $latFrom, $maxDistance, $distanceMultiplier, $lonField, $latField, $tableName);
+	}
+}
+
+function getSqlDistanceFormulaForModCoords($lonFrom, $latFrom, $maxDistance, $distanceMultiplier=1, $lonField='longitude', 
+	$latField='latitude', $tableName = 'caches',$modLonField='longitude', $modLatField='latitude', $modTableName='cache_mod_cords')
+{
+	$lonFrom = $lonFrom + 0;
+	$latFrom = $latFrom + 0;
+	$maxDistance = $maxDistance + 0;
+	$distanceMultiplier = $distanceMultiplier + 0;
+
+	if (!mb_ereg_match('^[a-zA-Z][a-zA-Z0-9_]{0,59}$', $lonField))
+		die('Fatal Error: invalid lonField');
+	if (!mb_ereg_match('^[a-zA-Z][a-zA-Z0-9_]{0,59}$', $latField))
+		die('Fatal Error: invalid latField');
+	if (!mb_ereg_match('^[a-zA-Z][a-zA-Z0-9_]{0,59}$', $tableName))
+		die('Fatal Error: invalid tableName');
+	if (!mb_ereg_match('^[a-zA-Z][a-zA-Z0-9_]{0,59}$', $modLonField))
+		die('Fatal Error: invalid modLonField');
+	if (!mb_ereg_match('^[a-zA-Z][a-zA-Z0-9_]{0,59}$', $modLatField))
+		die('Fatal Error: invalid modLatField');
+	if (!mb_ereg_match('^[a-zA-Z][a-zA-Z0-9_]{0,59}$', $modTableName))
+		die('Fatal Error: invalid modTableName');
+
+	$b1_rad = sprintf('%01.5F', (90 - $latFrom) * 3.14159 / 180);
+	$l1_deg = sprintf('%01.5F', $lonFrom);
+
+	$lonField = '`' . $tableName . '`.`' . $lonField . '`';
+	$latField = '`' . $tableName . '`.`' . $latField . '`';
+	
+	$modLonField = '`' . $modTableName . '`.`' . $modLonField . '`';
+	$modLatField = '`' . $modTableName . '`.`' . $modLatField . '`';
+	
+	$r = 6370 * $distanceMultiplier;
+
+	$retval = 'acos(cos(' . $b1_rad . ') * cos((90-IFNULL(' . $modLatField . ', ' . $latField . ')) * 3.14159 / 180) + sin(' . $b1_rad . ') * sin((90-IFNULL(' . $modLatField . ', ' . $latField . ')) * 3.14159 / 180) * cos((' . $l1_deg . ' - IFNULL(' . $modLonField . ', ' . $lonField . ')) * 3.14159 / 180)) * ' . $r;
+
+	return $retval;
+}
+
 function getSqlDistanceFormula($lonFrom, $latFrom, $maxDistance, $distanceMultiplier=1, $lonField='longitude', $latField='latitude', $tableName = 'caches')
 {
 	$lonFrom = $lonFrom + 0;
