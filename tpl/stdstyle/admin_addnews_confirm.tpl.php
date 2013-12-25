f3a0cb98d778330ffca64887526757ff5b2399f8
henricusklomp@gmail.com <henricusklomp@gmail.com@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2013-12-25 22:19:58 +0000

    Translated hardcoded Polish text inaddmin_adnews_confirm. PL, EN and NL
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@5557 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/admin_addnews_confirm.tpl.php
+++ tpl/stdstyle/admin_addnews_confirm.tpl.php
@@ -8,16 +8,16 @@
 <table class="content">
 <tr>
 	<td class="content2-pagetitle">
-		<img src="tpl/stdstyle/images/bluse/write.png" class="icon32" alt=""  /><font size="4">  <b>Dodaj newsa</b></font>
+		<img src="tpl/stdstyle/images/bluse/write.png" class="icon32" alt=""  /><font size="4">  <b>{{addnews_confirm_01}}</b></font>
 	</td>
 </tr>
 <tr><td class="spacer" colspan="2"></td></tr>
 <tr>
 	<td>
-		Dziękujemy za wysłanie nowej wiadomości. Postaramy się jak najszybciej przejrzeć tę informację.<br />
+		{{addnews_confirm_02}}<br />
 		{newscontent}
 		<br />
-		Powrót na <a href="index.php">Stronę główną</a>
+		{{addnews_confirm_03}} <a href="index.php">{{addnews_confirm_04}}</a>
 	</td>
 </tr>
 </table>
