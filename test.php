e3b299d0711c8f4742ee92d8ca12df78cdeb7492
wloczynutka <wloczynutka@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2014-01-17 21:51:48 +0000

    test
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@5642 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- test.php
+++ test.php
@@ -6,7 +6,7 @@ $database=new dataBase();
 //$ftfQuery = 'SELECT * FROM caches WHERE cache_id IN (SELECT `a`.`cache_id` FROM `cache_logs` AS `a` WHERE `user_id` =:1 AND `date` IN (SELECT MIN(date) FROM `cache_logs` AS b WHERE a.cache_id = b.cache_id ))';
 
 
-$ftfQuery = 'SELECT `a`.`cache_id`, a.date, caches.name FROM `cache_logs` AS `a`,  caches WHERE caches.cache_id = a.cache_id AND a.`user_id` =:1 AND `date` IN (SELECT MIN(date) FROM `cache_logs` AS b WHERE a.cache_id = b.cache_id ) ORDER BY date';
+$ftfQuery = 'SELECT `a`.`cache_id`, a.date, caches.name FROM `cache_logs` AS `a`,  caches WHERE caches.cache_id = a.cache_id AND a.`user_id` =:1 AND `date` IN (SELECT MIN(date) FROM `cache_logs` AS b WHERE a.cache_id = b.cache_id AND deleted=0 and type=1) ORDER BY date';
 
 $database->multiVariableQuery($ftfQuery, $userId);
 $ftfResult = $database->dbResultFetchAll();
