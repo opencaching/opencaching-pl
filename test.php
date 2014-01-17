c53bf79c9b3c658233a0bb2d9b862fa43379bc05
wloczynutka <wloczynutka@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2014-01-17 21:48:02 +0000

    test
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@5641 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

---
+++ test.php
@@ -0,0 +1,19 @@
+<?php
+require_once 'lib/db.php';
+
+$userId = addslashes($_REQUEST['id']);
+$database=new dataBase();
+//$ftfQuery = 'SELECT * FROM caches WHERE cache_id IN (SELECT `a`.`cache_id` FROM `cache_logs` AS `a` WHERE `user_id` =:1 AND `date` IN (SELECT MIN(date) FROM `cache_logs` AS b WHERE a.cache_id = b.cache_id ))';
+
+
+$ftfQuery = 'SELECT `a`.`cache_id`, a.date, caches.name FROM `cache_logs` AS `a`,  caches WHERE caches.cache_id = a.cache_id AND a.`user_id` =:1 AND `date` IN (SELECT MIN(date) FROM `cache_logs` AS b WHERE a.cache_id = b.cache_id ) ORDER BY date';
+
+$database->multiVariableQuery($ftfQuery, $userId);
+$ftfResult = $database->dbResultFetchAll();
+$c=0;
+foreach ($ftfResult as $ftfCache) {
+	$c++;
+	print '<a href=viewcache.php?cacheid='.$ftfCache['cache_id'].'>'.$ftfCache['name'].'</a> - '.$ftfCache['date'].'<br/>';
+}
+print '<br><br>tot: '.$c;
+var_dump($ftfResult);
