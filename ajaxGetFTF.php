8fefb717a899fe6c20c9c47642af1fa79bd8e0d7
wloczynutka <wloczynutka@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2014-01-19 22:00:20 +0000

    viewProfile witch ajax FTF bugfix
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@5652 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- ajaxGetFTF.php
+++ ajaxGetFTF.php
@@ -4,7 +4,7 @@ require_once 'lib/db.php';
 $userId = addslashes($_REQUEST['id']);
 $database=new dataBase();
 // $ftfQuery = 'SELECT `a`.`cache_id`, a.date, caches.name FROM `cache_logs` AS `a`,  caches WHERE caches.cache_id = a.cache_id AND a.`user_id` =:1 AND `date` IN (SELECT MIN(date) FROM `cache_logs` AS b WHERE a.cache_id = b.cache_id AND deleted=0 and type=1) ORDER BY date';
-$ftfQuery = 'SELECT `a`.`cache_id`, a.date, caches.name FROM `cache_logs` AS `a`, caches WHERE caches.cache_id = a.cache_id AND a.`user_id` =169 AND `date` = (SELECT MIN(date) FROM `cache_logs` AS b WHERE a.cache_id = b.cache_id AND deleted=0 and type=1) AND id = (select min(id) FROM `cache_logs` where cache_id = a.cache_id AND deleted=0 and type=1) ORDER BY date';
+$ftfQuery = 'SELECT `a`.`cache_id`, a.date, caches.name FROM `cache_logs` AS `a`, caches WHERE caches.cache_id = a.cache_id AND a.`user_id` =:1 AND `date` = (SELECT MIN(date) FROM `cache_logs` AS b WHERE a.cache_id = b.cache_id AND deleted=0 and type=1) AND id = (select min(id) FROM `cache_logs` where cache_id = a.cache_id AND deleted=0 and type=1) ORDER BY date';
 
 
 
