79c4c564d174b4769655162b61db90dd0f8320e9
wloczynutka <wloczynutka@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2014-01-20 23:23:42 +0000

    viewProfile witch ajax FTF bugfix
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@5656 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- ajaxGetFTF.php
+++ ajaxGetFTF.php
@@ -3,28 +3,12 @@ require_once 'lib/db.php';
 
 $userId = addslashes($_REQUEST['id']);
 $database=new dataBase();
-// $ftfQuery = 'SELECT `a`.`cache_id`, a.date, caches.name FROM `cache_logs` AS `a`,  caches WHERE caches.cache_id = a.cache_id AND a.`user_id` =:1 AND `date` IN (SELECT MIN(date) FROM `cache_logs` AS b WHERE a.cache_id = b.cache_id AND deleted=0 and type=1) ORDER BY date';
-$ftfQuery = 'SELECT `a`.`cache_id`, a.date, caches.name FROM `cache_logs` AS `a`, caches WHERE caches.cache_id = a.cache_id AND a.`user_id` =:1 AND `date` = (SELECT MIN(date) FROM `cache_logs` AS b WHERE a.cache_id = b.cache_id AND deleted=0 and type=1) AND id = (select min(id) FROM `cache_logs` where cache_id = a.cache_id AND deleted=0 and type=1) ORDER BY date';
-
-
-
+//$ftfQuery = 'SELECT `a`.`cache_id`, a.date, caches.name FROM `cache_logs` AS `a`, caches WHERE caches.cache_id = a.cache_id AND a.`user_id` =:1 AND `date` = (SELECT MIN(date) FROM `cache_logs` AS b WHERE a.cache_id = b.cache_id AND deleted=0 and type=1) AND id = (select min(id) FROM `cache_logs` where cache_id = a.cache_id AND deleted=0 and type=1) ORDER BY date';
+$ftfQuery = 'SELECT `a`.`cache_id` , a.date, caches.name FROM `cache_logs` AS `a` , caches WHERE caches.cache_id = a.cache_id AND a.`user_id` =:1 AND `date` = ( SELECT MIN( date ) FROM `cache_logs` AS b WHERE a.cache_id = b.cache_id AND deleted =0 AND TYPE =1 ) AND id = ( SELECT id FROM `cache_logs` WHERE `cache_id` = a.cache_id AND `type` =1 AND deleted =0 ORDER BY `cache_logs`.`date` ASC LIMIT 1 ) ORDER BY date';
 
 $database->multiVariableQuery($ftfQuery, $userId);
 $ftfResult = $database->dbResultFetchAll();
 
 print json_encode($ftfResult);
-
-/*
-$html = '<br>FTF x '.$database->rowCount().'<br><br><table class="ptCacheTable">';
-$bgColor='#eeeeff';
-foreach ($ftfResult as $ftfCache) {
-	if($bgColor == '#eeeeff') $bgColor = '#ffffff'; else $bgColor ='#eeeeff';
-	$data = explode(' ', $ftfCache['date']);
-	$html .= '<tr bgcolor="'.$bgColor.'"><td style="width: 60px;">'.$data[0].'</td><td style="width: 60px;">'.$data[1].'</td><td><a href=viewcache.php?cacheid='.$ftfCache['cache_id'].'>'.$ftfCache['name'].'</a></td></tr>';
-}
-$html .= '</table>';
-
-print $html;
-
-*/ 
-//var_dump($ftfResult);
+exit;
+?>
