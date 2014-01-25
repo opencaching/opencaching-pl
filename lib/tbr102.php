93191a9188e57c4290ff3de97a2ebce6c886d165
tripper1971g@gmail.com <tripper1971g@gmail.com@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2014-01-25 23:56:10 +0000

    BarChart
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@5685 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

---
+++ lib/tbr102.php
@@ -0,0 +1,270 @@
+<html>
+<head>
+</head>
+
+<body>
+<div id="idGCB"></div>
+
+<script type="text/javascript">
+TimeTrack( "START" );
+</script>
+
+<?php
+global $debug_page; 
+//if ( $debug_page )
+//	echo "<script type='text/javascript'>TimeTrack( 'DEBUG' );</script>";  
+?>
+
+
+<?php 
+echo "<script type='text/javascript'>";
+echo "GCTLoad( 'ChartBar', '', 1 );";
+echo "</script>";
+?>
+
+   
+<script type="text/javascript">
+		
+	var gcb = new GCT( 'idGCB' );
+	gcb.addColumn('string', 'UserName' );	
+
+	
+</script>
+	
+<?php
+$sEND = "";
+$sDateCondition = "";
+$sTypeCondition = "";
+
+global $lang;
+
+require_once('settings.inc.php');
+require_once('language.inc.php');
+require_once('cookie.class.php');
+
+if ($cookie->is_set('lang'))
+	$lang = $cookie->get('lang');
+
+require_once('db.php');
+
+
+
+$sUserIDLine = $_REQUEST[ "UserID"];
+$sDateFrom = $_REQUEST[ "DF"];
+$sDateTo = $_REQUEST[ "DT"];
+$sNameOfStat = $_REQUEST[ "stat"];
+
+$nDayInterval = 999;
+$sGranulate = "";
+
+if ( $sDateFrom <> ""  and  $sDateTo <> ""  )
+{
+	$dDateFrom = new DateTime( $sDateFrom );
+	$dDateTo = new DateTime( $sDateTo );
+	$interval = $dDateFrom->diff( $dDateTo );
+	$nDayInterval = $interval->format( '%a' );
+}
+
+if ( $nDayInterval < 65 )
+{
+	$sGranulate = " (week( cl.date) + 1) period ";
+} 
+else if ( $nDayInterval < 367 )
+{
+	$sGranulate = " month( cl.date) period ";
+}
+else 
+{
+	$sGranulate = " year( cl.date) period ";
+}
+
+
+//echo $interval->format('%R%a days');
+/*echo "<script type='text/javascript'>";
+echo "alert( '$sGranulate' );";
+echo "</script>";*/
+
+
+if ( $sDateFrom <> "" )
+	$sDateCondition .= "and date >='" .$sDateFrom."'"; 
+
+if ( $sDateTo <> "" )
+	$sDateCondition .= " and date < '".$sDateTo."' ";
+
+
+if ( $sNameOfStat == "NumberOfFinds" )
+	$sTypeCondition = " and  cl.type=1 ";
+
+if ( $sNameOfStat == "MaintenanceOfCaches" )
+	$sTypeCondition = " and  cl.type=6 ";
+
+
+$asUserID = explode(",", $sUserIDLine);
+
+
+if ( !strlen( $sUserIDLine )  )
+	$sEND = tr2('SelectUsers', $lang );
+
+if ( count( $asUserID ) > 30 )
+	$sEND = tr2('more30', $lang );
+	
+
+echo "<script type='text/javascript'>";
+if ( $sEND <> "" )
+{
+	echo "alert( '$sEND' );";
+	$asUserID = explode(",", "");	
+}
+
+echo "</script>";
+
+
+$sCondition = "";
+foreach( $asUserID as $sID )
+{
+	if( strlen( $sCondition ) )
+		$sCondition = $sCondition . " or "; 
+	
+	$sCondition = $sCondition . "cl.user_id = '". $sID."'"; 	
+}
+
+if( strlen( $sCondition ) )
+{
+	$sCondition = " and ( " . $sCondition . " )";
+}
+
+$sCondition .= $sDateCondition;
+
+
+/////////////////
+
+$dbc = new dataBase();
+
+/*$query = "SELECT * FROM cache_logs WHERE deleted=0 ";*/
+
+
+$query =
+"SELECT distinct " . $sGranulate . "
+		FROM
+		cache_logs cl
+
+		WHERE cl.deleted=0 "
+			 
+		.$sTypeCondition 
+		. $sCondition .
+				 
+		 " order by period";
+
+
+
+$dbc->multiVariableQuery($query);
+
+
+$aNrColumn= array();
+$i = 0;
+
+
+
+
+echo "<script type='text/javascript'>";
+
+
+while ( $record = $dbc->dbResultFetch() )
+{
+		
+	$nPeriod = $record[ 'period' ];
+
+	
+	
+	$aNrColumn[ $nPeriod ] = $i;
+	echo "gcb.addColumn('number', '$nPeriod');";
+	
+	$i = $i + 1;	
+}
+
+
+
+////////////////////
+
+
+
+//echo " var chartOpt = gcb.getChartOption();";
+//echo " chartOpt.vAxis.title= '".tr2('NrCaches',$lang)."';";
+echo "</script>";
+
+unset( $dbc );
+////////////////////////////
+
+foreach( $asUserID as $sID )
+{
+	$sCondition = " and cl.user_id = '". $sID."'";
+	$sCondition .= $sDateCondition;
+	
+	$dbc = new dataBase();
+	
+	$query =
+	"SELECT u.username username, u.user_id user_id,
+			" . $sGranulate . ",
+			COUNT(*) count
+			
+			FROM
+			cache_logs cl
+			join caches c on c.cache_id = cl.cache_id
+			join user u on cl.user_id = u.user_id
+	
+			WHERE cl.deleted=0 "
+				 
+			.$sTypeCondition 
+			. $sCondition .
+				
+			"GROUP BY period";
+	
+	$dbc->multiVariableQuery($query);
+	
+	
+	echo "<script type='text/javascript'>";
+	
+	
+	$nStart = 1;
+	while ( $record = $dbc->dbResultFetch() )
+	{
+		$nPeriod = $record['period'];
+		$nVal = $record['count'];
+		
+		if ( $nStart == 1 )
+		{
+			$sUserName = $record[ 'username' ];
+			$nUserId = $record[ 'user_id' ];
+							
+			echo "
+			gcb.addEmptyRow();
+			gcb.addToLastRow( 0, '$sUserName' );
+			";
+			
+			$nStart = 0;
+		}
+		
+		
+		$nrCol = $aNrColumn[ $nPeriod ];		
+		echo "gcb.addToLastRow( $nrCol+1 , $nVal );";		
+	}
+	
+	echo "</script>";
+	
+	unset( $dbc );
+}
+?>
+
+
+<script type="text/javascript">
+	gcb.drawChart( 1 );
+</script>  
+   
+<script type="text/javascript">
+TimeTrack( "END", "SB102" );
+</script>
+   
+</body>
+
+</html>
+
--- lib/th102.php
+++ lib/th102.php
@@ -5,21 +5,32 @@
 <body>
 <div id="idGCL"></div>
 
+<script type="text/javascript">
+TimeTrack( "START" );
+</script>
+
 <?php
+global $debug_page; 
+//if ( $debug_page )
+//	echo "<script type='text/javascript'>TimeTrack( 'DEBUG' );</script>";  
+?>
+
+
+<?php 
 echo "<script type='text/javascript'>";
 echo "GCTLoad( 'ChartLine', '', 1 );";
 echo "</script>";
 ?>
 
-
+   
 <script type="text/javascript">
+		
+	var gcl = new GCT( 'idGCL' );
+	gcl.addColumn('date', 'Date');	
 
-    var gcl = new GCT( 'idGCL' );
-    gcl.addColumn('date', 'Date');
-
-
+	
 </script>
-
+	
 <?php
 $sEND = "";
 $sDateCondition = "";
@@ -32,7 +43,7 @@ require_once('language.inc.php');
 require_once('cookie.class.php');
 
 if ($cookie->is_set('lang'))
-    $lang = $cookie->get('lang');
+	$lang = $cookie->get('lang');
 
 require_once('db.php');
 
@@ -46,33 +57,33 @@ $sNameOfStat = $_REQUEST[ "stat"];
 
 
 if ( $sDateFrom <> "" )
-    $sDateCondition .= "and date >='" .$sDateFrom."'";
+	$sDateCondition .= "and date >='" .$sDateFrom."'"; 
 
 if ( $sDateTo <> "" )
-    $sDateCondition .= " and date < '".$sDateTo."' ";
+	$sDateCondition .= " and date < '".$sDateTo."' ";
 
 
 if ( $sNameOfStat == "NumberOfFinds" )
-    $sTypeCondition = " and  cl.type=1 ";
+	$sTypeCondition = " and  cl.type=1 ";
 
 if ( $sNameOfStat == "MaintenanceOfCaches" )
-    $sTypeCondition = " and  cl.type=6 ";
+	$sTypeCondition = " and  cl.type=6 ";
 
 
 $asUserID = explode(",", $sUserIDLine);
 
 
 if ( !strlen( $sUserIDLine )  )
-    $sEND = tr2('SelectUsers', $lang );
+	$sEND = tr2('SelectUsers', $lang );
 
 if ( count( $asUserID ) > 10 )
-    $sEND = "Wybrano więcej niż dziesięciu - todo ENGLISH";
+	$sEND = tr2('more10', $lang );
 
 echo "<script type='text/javascript'>";
 if ( $sEND <> "" )
 {
-    echo "alert( '$sEND' );";
-    $asUserID = explode(",", "");
+	echo "alert( '$sEND' );";
+	$asUserID = explode(",", "");	
 }
 echo "</script>";
 
@@ -83,16 +94,16 @@ $aNrColumn=array();
 
 foreach( $asUserID as $sID )
 {
-    if( strlen( $sCondition ) )
-        $sCondition = $sCondition . " or ";
-
-    $sCondition = $sCondition . "cl.user_id = '". $sID."'";
+	if( strlen( $sCondition ) )
+		$sCondition = $sCondition . " or "; 
+	
+	$sCondition = $sCondition . "cl.user_id = '". $sID."'"; 	
 }
 
 if( strlen( $sCondition ) )
 {
-    $sConditionUser =" ( " . $sCondition . " )";
-    $sCondition =" and ( " . $sCondition . " )";
+	$sConditionUser =" ( " . $sCondition . " )";
+	$sCondition =" and ( " . $sCondition . " )";
 }
 
 $sCondition .= $sDateCondition;
@@ -109,8 +120,8 @@ $aUserName = array();
 
 while ( $record = $dbc->dbResultFetch() )
 {
-    $sID = $record[ 'user_id' ];
-    $aUserName[ $sID ] = $record[ 'username' ];
+	$sID = $record[ 'user_id' ];
+	$aUserName[ $sID ] = $record[ 'username' ];
 }
 unset( $dbc );
 
@@ -123,11 +134,11 @@ echo "<script type='text/javascript'>";
 $i = 0;
 foreach( $asUserID as $sID )
 {
-    $sName = $aUserName[ $sID ];
-    //$sName = $sID;
-    echo "gcl.addColumn('number', '$sName');";
-    $aNrColumn[ $sID ] = $i;
-    $i++;
+	$sName = $aUserName[ $sID ];
+	//$sName = $sID;
+	echo "gcl.addColumn('number', '$sName');";
+	$aNrColumn[ $sID ] = $i;
+	$i++;
 }
 
 
@@ -143,21 +154,21 @@ $dbc = new dataBase();
 
 $query =
 "SELECT year( cl.date) year, month( cl.date ) month, day( cl.date ) day,
-         u.username username, u.user_id user_id,
-        COUNT(*) count
+		 u.username username, u.user_id user_id,
+		COUNT(*) count
+		
+		FROM
+		cache_logs cl
+		join caches c on c.cache_id = cl.cache_id
+		join user u on cl.user_id = u.user_id
 
-        FROM
-        cache_logs cl
-        join caches c on c.cache_id = cl.cache_id
-        join user u on cl.user_id = u.user_id
+		WHERE cl.deleted=0 "
+			 
+		.$sTypeCondition 
+		. $sCondition .
 
-        WHERE cl.deleted=0 "
-
-        .$sTypeCondition
-        . $sCondition .
-
-        "GROUP BY year, month, day, user_id
-        order by year, month, day   ";
+		"GROUP BY year, month, day, user_id 
+		order by year, month, day 	";
 
 $dbc->multiVariableQuery($query);
 
@@ -166,7 +177,7 @@ $nCount = array();
 
 foreach( $asUserID as $sID )
 {
-    $anCount[ $sID ] = 0;
+	$anCount[ $sID ] = 0;
 }
 
 
@@ -176,27 +187,27 @@ echo "<script type='text/javascript'>";
 
 while ( $record = $dbc->dbResultFetch() )
 {
-    $nYear = $record['year'];
-    $nMonth = $record['month']-1;
-    $nDay = $record['day'];
+	$nYear = $record['year'];
+	$nMonth = $record['month']-1;
+	$nDay = $record['day'];
+	
+	$sNewDate = "new Date( $nYear, $nMonth, $nDay )";
+	$sUserName = $record[ 'username' ];
+	$nUserId = $record[ 'user_id' ];
+	 
+	$anCount[ $nUserId ] += $record[ 'count' ];
+	
 
-    $sNewDate = "new Date( $nYear, $nMonth, $nDay )";
-    $sUserName = $record[ 'username' ];
-    $nUserId = $record[ 'user_id' ];
-
-    $anCount[ $nUserId ] += $record[ 'count' ];
-
-
-    echo "
-            gcl.addEmptyRow();
-            gcl.addToLastRow( 0, $sNewDate );
-        ";
-
-
-    $nrCol = $aNrColumn[ $nUserId ];
-    $val = $anCount[ $nUserId ];
-    echo "gcl.addToLastRow( $nrCol+1 , $val );";
-
+	echo "
+			gcl.addEmptyRow();			
+			gcl.addToLastRow( 0, $sNewDate );		
+		";
+	
+	
+	$nrCol = $aNrColumn[ $nUserId ];
+	$val = $anCount[ $nUserId ];
+	echo "gcl.addToLastRow( $nrCol+1 , $val );";
+	
 }
 
 echo "</script>";
@@ -206,9 +217,13 @@ unset( $dbc );
 
 
 <script type="text/javascript">
-    gcl.drawChart( 1 );
+	gcl.drawChart( 1 );
+</script>  
+   
+<script type="text/javascript">
+TimeTrack( "END", "SL102" );
 </script>
-
+   
 </body>
 
 </html>
