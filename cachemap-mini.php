b19fe6b47579237b24375e21c529456b1493ec46
wloczynutka <wloczynutka@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2014-01-26 18:02:47 +0000

    try to code clean in cachemap-mini.php
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@5700 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- cachemap-mini.php
+++ cachemap-mini.php
@@ -1,81 +1,46 @@
 <?php
+/* cachemap-mini.php */
+require_once ('./lib/common.inc.php');
+require_once ('./lib/db.php');
 
-require_once('./lib/common.inc.php');
-
+db_disconnect();
+var_dump($cachemap_mapper);
 $tplname = 'cachemap-mini';
+$user_id = (isset($_REQUEST['userid']) && $_REQUEST['userid'] != '') ? $_REQUEST['userid'] : -1;
 
-global $usr;
-global $get_userid;
-global $filter;
-global $caches_list;
-global $language;
-global $lang;
-
-$user_id = '';
-
-$get_userid = $_REQUEST['userid'];
-//user logged in?
-    session_start();
-
-
-    tpl_set_var('sc', intval($_GET['sc']));
-
-    if( $get_userid == '')
-        $user_id = -1;
-    else
-        $user_id = $get_userid;
-
-    tpl_set_var('userid', $user_id);
-
-    $rs = mysql_query("SELECT `latitude`, `longitude`, `username` FROM `user` WHERE `user_id`='$user_id'");
-    $record = mysql_fetch_array($rs);
-    if( ($_REQUEST['lat'] != "" && $_REQUEST['lon'] != "") && ($_REQUEST['lat'] != 0 && $_REQUEST['lon'] != 0))
-    {
-        $coordsXY=$_REQUEST['lat'].",".$_REQUEST['lon'];
-        $coordsX=$_REQUEST['lat'];
-        if( $_REQUEST['inputZoom'] != "" )
-            tpl_set_var('zoom', $_REQUEST['inputZoom']);
-        else
-            tpl_set_var('zoom', 11);
+if (($_REQUEST['lat'] != "" && $_REQUEST['lon'] != "") && ($_REQUEST['lat'] != 0 && $_REQUEST['lon'] != 0)) {
+    $coordsXY = $_REQUEST['lat'] . "," . $_REQUEST['lon'];
+    $coordsX = $_REQUEST['lat'];
+    if ($_REQUEST['inputZoom'] != "") {
+        tpl_set_var('zoom', $_REQUEST['inputZoom']);
+    } else {
+        tpl_set_var('zoom', 11);
     }
-    else
-    {
-        $coordsXY="$record[latitude],$record[longitude]";
-        $coordsX="$record[latitude]";
-        if ($coordsX=="" || $coordsX==0)
-        {
-            $coordsXY=$country_coordinates;
-            tpl_set_var('zoom', $default_country_zoom);
-        }
-        else
-            tpl_set_var('zoom', 11);
+} else {
+    $rs = dataBase::select(array('latitude', 'longitude'), 'user', array(0 => array('fieldName' => 'user_id', 'fieldValue' => $user_id, 'operator' => '=')));
+    $record = $rs[0];
+    $coordsXY = "$record[latitude],$record[longitude]";
+    $coordsX = "$record[latitude]";
+    if ($coordsX == "" || $coordsX == 0) {
+        $coordsXY = $country_coordinates;
+        tpl_set_var('zoom', $default_country_zoom);
+    } else {
+        tpl_set_var('zoom', 11);
     }
+}
 
+tpl_set_var('userid', $user_id);
+tpl_set_var('doopen', "false");
+tpl_set_var('coords', $coordsXY);
+tpl_set_var("map_type", "0");
+tpl_set_var('cachemap_mapper', $cachemap_mapper);
 
-//  tpl_set_var('doopen', $_REQUEST['cacheid']?"true":"false");
-    tpl_set_var('doopen', "false");
-    tpl_set_var('coords', $coordsXY);
-    tpl_set_var('username', $record[username]);
-
-    tpl_set_var("map_type", "0");
-
-    tpl_set_var('cachemap_mapper', $cachemap_mapper);
-
-//  foreach($filter as $key)
-    /*if( isset( $_POST['submit'] ) )
-    {
-            $makeFilterResult = makeDBFilter();
-            setDBFilter($usr['userid'],$makeFilterResult);
-            $filter = $makeFilterResult;
-    }*/
-
-    /*SET YOUR MAP CODE HERE*/
-    tpl_set_var('cachemap_header', '<script src="//maps.googleapis.com/maps/api/js?sensor=false&amp;language='.$lang.'" type="text/javascript"></script>');
-    /*
-     * Generate dynamic URL to cachemap3.js file, this will make sure it will be reloaded by the browser.
-     * The time-stamp will be stripped by a rewrite rule in lib/.htaccess.
-     * */
-    tpl_set_var('lib_cachemap3_js', "lib/cachemap3." . date("YmdHis", filemtime($rootpath . 'lib/cachemap3.js')) . ".js");
-    tpl_BuildTemplate(true, true);
-
+/*SET YOUR MAP CODE HERE*/
+tpl_set_var('cachemap_header', '<script src="//maps.googleapis.com/maps/api/js?sensor=false&amp;language=' . $lang . '" type="text/javascript"></script>');
+/*
+ * Generate dynamic URL to cachemap3.js file, this will make sure it will be reloaded by the browser.
+ * The time-stamp will be stripped by a rewrite rule in lib/.htaccess.
+ * */
+tpl_set_var('lib_cachemap3_js', "lib/cachemap3." . date("YmdHis", filemtime($rootpath . 'lib/cachemap3.js')) . ".js");
+tpl_BuildTemplate(true, true);
 ?>
