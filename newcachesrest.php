ace1748683c2878ee38b35cb9ec02ac0d3b8f815
henricusklomp@gmail.com <henricusklomp@gmail.com@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2014-03-07 07:16:55 +0000

    changed date/time notation in newcachesrest.php
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@5796 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- newcachesrest.php
+++ newcachesrest.php
@@ -25,7 +25,7 @@
 
  ****************************************************************************/
 
-    global $lang, $rootpath;
+    global $lang, $rootpath, $dateFormat;
 
 
     //prepare the templates and include all neccessary
@@ -167,7 +167,7 @@
             $thisline = mb_ereg_replace('{userid}', $cache_record['userid'], $thisline);
             $thisline = mb_ereg_replace('{cachename}', htmlspecialchars($cache_record['name'], ENT_COMPAT, 'UTF-8'), $thisline);
             $thisline = mb_ereg_replace('{username}', htmlspecialchars($cache_record['username'], ENT_COMPAT, 'UTF-8'), $thisline);
-            $thisline = mb_ereg_replace('{date}', date('d-m-Y', strtotime($cache_record['date'])), $thisline);
+            $thisline = mb_ereg_replace('{date}', date($dateFormat, strtotime($cache_record['date'])), $thisline);
             $thisline = mb_ereg_replace('{imglink}', 'tpl/stdstyle/images/'.getSmallCacheIcon($cache_record['icon_large']), $thisline);
 //          $thisline = mb_ereg_replace('{{hidden_by}}', htmlspecialchars(tr('created_by'), ENT_COMPAT, 'UTF-8'), $thisline);
             $content .= $thisline . "\n";
