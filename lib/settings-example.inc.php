5f32948084e04988b01c6d36ccdce61b27664e20
wloczynutka@gmail.com <wloczynutka@gmail.com@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2014-03-28 21:24:13 +0000

    common setting. issue 56
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@5901 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- lib/settings-example.inc.php
+++ lib/settings-example.inc.php
@@ -1,4 +1,5 @@
 <?php
+require __DIR__.'/settingsDefault.inc.php';
 /***************************************************************************
     *
     *   This program is free software; you can redistribute it and/or modify
---
+++ lib/settingsDefault.inc.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * This script contains configurable variables. Keys of array can be overriden if neccessary in file settings.inc.php
+ *
+ */
+
+$config = array (
+    'blogMostRecentRecordsUrl' => 'http://blog.opencaching.pl/feed/', /* url where xml witch most recent blog enterie are placed */
+);
