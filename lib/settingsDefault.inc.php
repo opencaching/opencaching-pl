599dfac3408bd06838e8ebf35cd8aad524f9913d
andrixnet <andrixnet@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2014-04-05 12:48:45 +0000

    added RO to defaultLangugaeList
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@5988 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- lib/settingsDefault.inc.php
+++ lib/settingsDefault.inc.php
@@ -6,29 +6,29 @@
 
 $config = array (
     
-	/* url where xml witch most recent blog enterie are placed */
-	'blogMostRecentRecordsUrl' => 'http://blog.opencaching.pl/feed/', 
-	
-	/* to switch cache map v2 on set true otherwise false */
+    /* url where xml witch most recent blog enterie are placed */
+    'blogMostRecentRecordsUrl' => 'http://blog.opencaching.pl/feed/', 
+    
+    /* to switch cache map v2 on set true otherwise false */
     'map2SwithedOn' => true, 
-	
-	/* *******************************************************************
-	   * Node personalizations
-	   ******************************************************************* */
-	/* main logo picture (to be placed in /images/) */
+    
+    /* *******************************************************************
+       * Node personalizations
+       ******************************************************************* */
+    /* main logo picture (to be placed in /images/) */
     'headerLogo' => 'oc_logo.png', 
-	/* main logo; winter version, displayed during december and january. */
-	'headerLogoWinter' => 'oc_logo_winter.png', 
+    /* main logo; winter version, displayed during december and january. */
+    'headerLogoWinter' => 'oc_logo_winter.png', 
     /* main logo; prima aprilis version (april fools), displayed only on april 1st. */
-	'headerLogo1stApril' => 'oc_logo_1A.png', 
-	
-	/* website icon (favicon); (to be placed in /images/) 
-		Format: 16x16 pixels; PNG 8bit indexed or 24bit true color, transparency supported
-		A file /favicon.ico (windows icon ICO format, 16x16) should also exist as fallback 
-		mainly for MSIE */
-	'headerFavicon' => 'oc_icon.png', 
+    'headerLogo1stApril' => 'oc_logo_1A.png', 
+    
+    /* website icon (favicon); (to be placed in /images/) 
+        Format: 16x16 pixels; PNG 8bit indexed or 24bit true color, transparency supported
+        A file /favicon.ico (windows icon ICO format, 16x16) should also exist as fallback 
+        mainly for MSIE */
+    'headerFavicon' => 'oc_icon.png', 
     'defaultLangugaeList' => array (
-        'PL', 'EN', 'FR', 'DE', 'NL'
+        'PL', 'EN', 'FR', 'DE', 'NL', 'RO'
     )
     
  );
