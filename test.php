e397562d4ab5c7a878d61ef02bb8d248cfd2320f
wloczynutka@gmail.com <wloczynutka@gmail.com@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2014-04-22 18:07:50 +0000

    testing script
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@6065 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- test.php
+++ test.php
@@ -1,22 +1,4 @@
 <?php
+
 error_reporting(-1);
-print 'A';
-include __DIR__.'/tpl/stdstyle/etc/write_newblogs.inc.php';
-print 'B';
-exit;
-
-require '
-ocpl/util.sec/geokrety/processGeokretyErrors.php
-';
-
-exit;
-error_reporting(-1);
-require __DIR__.'/lib/ClassPathDictionary.php';
-spl_autoload_register(function ($className) {
-    include_once ClassPathDictionary::getClassPath($className);
-});
-
-$user_id = 9067;
-$rs=dataBase::select(array('latitude', 'longitude'), 'user', array(0 => array ('fieldName' => 'user_id', 'fieldValue' => $user_id, 'operator' => '=')));
-
-var_dump($rs);
+include __DIR__.'/util.sec/geokrety/processGeokretyErrors.php';
