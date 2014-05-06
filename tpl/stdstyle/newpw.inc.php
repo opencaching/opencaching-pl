0ee8255d601300a9f796493da728ae713ed4920b
henricusklomp@gmail.com <henricusklomp@gmail.com@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2014-05-06 09:27:00 +0000

    Added correct subject to email when changing password
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@6089 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- tpl/stdstyle/newpw.inc.php
+++ tpl/stdstyle/newpw.inc.php
@@ -28,7 +28,7 @@
     $getcode = tr('generate_code');
     $changepw = tr('chpw_01');
     $emailnotexist = '<div class="errormsg">'.tr('error_email_not_ok').'</div>';
-    $newpw_subject = tr('chpw_01');
+    $newpw_subject = tr('chpw_06');
     $emailsend = '<div class="notice" style="width:500px;height:44px;"><b>'.tr('chpw_02').'</b></div>';
     $pw_not_ok = '<div class="errormsg">'.tr('error_password_not_ok').'</div>';
     $pw_no_match = '<div class="errormsg">'.tr('error_password_diffs').'</div>';
