8d11273a7d765857fb68f384d30f32427523973a
henricusklomp@gmail.com <henricusklomp@gmail.com@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2014-05-12 15:16:26 +0000

    Translated not approved cacheemail
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@6126 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- viewpendings.php
+++ viewpendings.php
@@ -153,6 +153,7 @@ global $bgcolor1, $bgcolor2;
         $email_content = mb_ereg_replace('{cacheArchived_02}', tr('cacheArchived_02'), $email_content);
         $email_content = mb_ereg_replace('{cacheArchived_03}', tr('cacheArchived_03'), $email_content);
         $email_content = mb_ereg_replace('{cacheArchived_04}', tr('cacheArchived_04'), $email_content);
+	$email_content = mb_ereg_replace('{cacheArchived_05}', tr('cacheArchived_05'), $email_content);
 
         $query = sql("SELECT `email` FROM `user` WHERE `user_id`='&1'", $user_id);
         $owner_email = sql_fetch_array($query);
