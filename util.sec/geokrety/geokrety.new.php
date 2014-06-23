d94382d92778a1e4f248edf82a84236b0707b522
wloczynutka <wloczynutka@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2014-06-23 09:54:13 +0000

    load xml from geokrety debug
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@6214 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- util.sec/geokrety/geokrety.new.php
+++ util.sec/geokrety/geokrety.new.php
@@ -35,8 +35,17 @@
 
     /* new OC dedicated geokrety XML export */
     $url='http://geokrety.org/export_oc.php?modifiedsince=' . date('YmdHis', $modifiedsince - 1);
-    $gkxml=@simplexml_load_file($url);
-    if(!$gkxml) die("Geokrety export error! Failed to load XML file [simplexml_load_file()]: ".$url);
+
+
+	$xmlString = file_get_contents($url);
+	$gkxml = @simplexml_load_string($xmlString);
+
+
+//    $gkxml=@simplexml_load_file($url);
+    if(!$gkxml) {
+		print $xmlString;
+		die("Geokrety export error! Failed to load XML file [simplexml_load_file()]: ".$url);
+	}
 
     /* read geokrety data */
     foreach($gkxml->geokret as $geokret) {
