adccd2aa39afe56a4d289c3a94424a1b1b0c6fcd
boguslaw.szczepanowski@gmail.com <boguslaw.szczepanowski@gmail.com@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2014-06-28 22:41:02 +0000

    Save history of nick changes (#1)
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@6240 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

---
+++ sqlAlters/2014-06-28_nick_change_history.sql
@@ -0,0 +1,17 @@
+-- 2014-06-28 Save history of user's nick
+-- @author: Bogus z Polska
+ 
+ create table user_nick_history (
+id int(11) NOT NULL AUTO_INCREMENT COMMENT 'PK',
+user_id int(11) NOT NULL COMMENT 'FK to user.user_id',
+date_from datetime NOT NULL COMMENT 'Start date of the nick value',
+date_to datetime COMMENT 'End date of the nick value, NULL if current',
+username varchar(60) NOT NULL COMMENT 'The actual nick in a given period of time',
+change_comment TEXT COMMENT 'Change comment',
+change_by_user_id int(11) COMMENT 'User who changed the nick, FT to user.user_id',
+PRIMARY KEY (id),
+KEY user_nick_hist_user_id (user_id)
+)
+ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE user ENGINE=InnoDB;
