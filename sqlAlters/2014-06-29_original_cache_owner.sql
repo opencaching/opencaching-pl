56be27aea8670a9f1da28f7e7f4b5611a0d167ce
boguslaw.szczepanowski@gmail.com <boguslaw.szczepanowski@gmail.com@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2014-06-29 12:38:06 +0000

    Issue #128, keep original geocache's owner in DB, part 1
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@6242 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

---
+++ sqlAlters/2014-06-29_original_cache_owner.sql
@@ -0,0 +1,10 @@
+-- 2014-06-29 Issue #128, keep original geocache's owner in DB
+-- @author: Bogus z Polska
+ 
+alter table caches add column org_user_id int(11) COMMENT 'Origianl user_id, who created the geocache';
+
+create index chowner_user_id on chowner(user_id);  
+create index chowner_cache_id on chowner(cache_id);  
+
+alter table pictures engine=innodb;
+alter table chowner engine=innodb;
