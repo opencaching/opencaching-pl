770ce9b7560c1aa3fc1a398aee3d75751948f174
andrixnet <andrixnet@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2014-09-20 12:06:39 +0000

    Additional test and documentation on how to set up SVN automatic updates and have proper user ownership of the files on each node.
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@6320 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- docs/svn/ocpl-update.sh
+++ docs/svn/ocpl-update.sh
@@ -3,12 +3,12 @@
 # https://code.google.com/p/opencaching-pl/source/browse/trunk/post-commit.php
 
 # site name (FQDN)
-SITE_NAME=www.opencaching.ro
+SITE_NAME=www.opencaching.xx
 # hosting root directory
 SITE_ROOT=/var/www/site_root
 # user owner of the files.
 # must have primary group same as webserver user
-SITE_USER=ocro
+SITE_USER=oc_xx
 # sudo command to run "svn up" as SITE_USER
 # comment it if unused
 SUDO_CMD="sudo -u ${SITE_USER}"
