21e669b363678cba7061d5ca41cb9e12214a8446
andrixnet <andrixnet@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2014-09-20 11:26:19 +0000

    Example script on how to do svn updates while retaining the proper user ownership of the files on each node.
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@6319 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

---
+++ docs/svn/ocpl-update.sh
@@ -0,0 +1,36 @@
+# The name and location of this scripts should match the location in
+# the following script:
+# https://code.google.com/p/opencaching-pl/source/browse/trunk/post-commit.php
+
+# site name (FQDN)
+SITE_NAME=www.opencaching.ro
+# hosting root directory
+SITE_ROOT=/var/www/site_root
+# user owner of the files.
+# must have primary group same as webserver user
+SITE_USER=ocro
+# sudo command to run "svn up" as SITE_USER
+# comment it if unused
+SUDO_CMD="sudo -u ${SITE_USER}"
+
+# set to 1 to log updates
+DO_LOG=1
+# define log facility and level
+LOG_LEVEL="-p cron.info"
+
+if [ "$DO_LOG" == "1" ]; then 
+    echo "Updating ${SITE_NAME}..."			| logger ${LOG_LEVEL}
+    # run "svn up"
+    ${SUDO_CMD} svn up ${SITE_ROOT} 			| logger ${LOG_LEVEL}
+    echo "- SVN update completed."			| logger ${LOG_LEVEL}
+    echo 						| logger ${LOG_LEVEL}
+
+    echo "Running OKAPI update scripts..."		| logger ${LOG_LEVEL}
+    wget -O - -q http://${SITE_NAME}/okapi/update	| logger ${LOG_LEVEL}
+    echo "--- Done." 					| logger ${LOG_LEVEL}
+    echo 						| logger ${LOG_LEVEL}
+else
+    # run "svn up"
+    ${SUDO_CMD} svn up ${SITE_ROOT}
+    wget -O - -q http://${SITE_NAME}/okapi/update
+fi
