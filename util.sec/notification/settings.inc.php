d4edb5fd6e879bf5d57aaba0df57a5863d21cff5
tripper1971g@gmail.com <tripper1971g@gmail.com@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2014-10-05 15:45:10 +0000

    changed in newcache notification email from: $site_name to 'noreply@'.$site_name
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@6335 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- util.sec/notification/settings.inc.php
+++ util.sec/notification/settings.inc.php
@@ -2,12 +2,11 @@
     // Unicode Reminder ăĄă˘
     global $site_name;
 
-    $mailfrom = $site_name;
+    $mailfrom = 'noreply@'.$site_name;
     $mailsubject = tr('notify_newCache_13');
 
     $debug = false;
     $debug_mailto = 'ocpl@opencaching.pl';
 
     $nologs = tr('notify_newCache_11'). '. ';
-
 ?>
