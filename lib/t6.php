1d8ba33f164c93aec3a80264357606bf271574c4
wloczynutka@gmail.com <wloczynutka@gmail.com@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2014-11-04 22:03:18 +0000

    issue 170
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@6406 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- lib/t6.php
+++ lib/t6.php
@@ -10,26 +10,21 @@
 
 # This page took >60 seconds to render! Added daily caching.
 
-$cache_key = "t6.php-result";
+$cache_key = "t6.php-result-".$lang;
 $result = apc_fetch($cache_key);
 if ($result === false)
 {
     ob_start();
-
     $rsU = sql('SELECT COUNT(*) `count` FROM (SELECT COUNT(caches.user_id) FROM `caches` WHERE `status`=1 GROUP BY `user_id`) `users_with_founds`');
     $fC = sql('SELECT COUNT(*) `count` FROM `caches` WHERE `status`=1');
     $rsUs = mysql_fetch_array($rsU);
     $fCt = mysql_fetch_array($fC);
-
     $rsfCR = sql("SELECT COUNT(*) `count`, `cache_location`.`adm3` region, `cache_location`.`code3` code_region FROM `cache_location` INNER JOIN caches ON cache_location.cache_id=caches.cache_id WHERE `cache_location`.`code1`='PL' AND `caches`.`status`=1 AND `caches`.`type`<>6 AND `cache_location`.`adm3`!='' GROUP BY `cache_location`.`code3` ORDER BY count DESC");
-
     echo '<table width="97%"><tr><td align="center"><center><b> '.tr('number_of_caches_by_region').'</b> <br />'.tr('only_active_caches').'<br /> '.tr('users_who_created_caches').': ';
     echo $rsUs["count"];
     echo ' .::. '.tr('number_of_active_caches').': ';
     echo $fCt["count"];
-    echo '<br /><br />(Kliknij na nazwe województwa aby zobaczyć statytykę użytkowników w danym województwie)</center></td></tr></table><br><table border="1" bgcolor="white" width="97%">' . "\n";
-
-
+    echo '<br /><br />('.tr('Stats_s3_02').')</center></td></tr></table><br><table border="1" bgcolor="white" width="97%">' . "\n";
     echo '
     <tr class="bgcolor2">
         <td width="20%" align="right">
@@ -47,9 +42,8 @@ if ($result === false)
                     &nbsp;&nbsp;<b>'.$line["count"].'</b>&nbsp;&nbsp;
                 </td>
                 <td align="right">
-                    &nbsp;&nbsp;<b><a class=links href=articles.php?page=s9&region='.$line[code_region].'>'.$line[region].'</a></b>&nbsp;&nbsp;
+                    &nbsp;&nbsp;<b><a class=links href=articles.php?page=s9&region='.$line['code_region'].'>'.$line['region'].'</a></b>&nbsp;&nbsp;
                 </td>';
-
     }
 
     echo '</table>' . "\n";
