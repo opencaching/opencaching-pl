d9bc5f33c61d855e57d64907f884fa68ba863989
henricusklomp@gmail.com <henricusklomp@gmail.com@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2014-11-05 19:27:59 +0000

    Corrected a small mistake. reorgenised translation links and some translations for file t6.php
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@6408 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- lib/t6.php
+++ lib/t6.php
@@ -20,18 +20,18 @@ if ($result === false)
     $rsUs = mysql_fetch_array($rsU);
     $fCt = mysql_fetch_array($fC);
     $rsfCR = sql("SELECT COUNT(*) `count`, `cache_location`.`adm3` region, `cache_location`.`code3` code_region FROM `cache_location` INNER JOIN caches ON cache_location.cache_id=caches.cache_id WHERE `cache_location`.`code1`='PL' AND `caches`.`status`=1 AND `caches`.`type`<>6 AND `cache_location`.`adm3`!='' GROUP BY `cache_location`.`code3` ORDER BY count DESC");
-    echo '<table width="97%"><tr><td align="center"><center><b> '.tr('number_of_caches_by_region').'</b> <br />'.tr('only_active_caches').'<br /> '.tr('users_who_created_caches').': ';
+    echo '<table width="97%"><tr><td align="center"><center><b> '.tr('Stats_t6_01').'</b> <br />'.tr('Stats_t6_02').'<br /> '.tr('Stats_t6_03').': ';
     echo $rsUs["count"];
-    echo ' .::. '.tr('number_of_active_caches').': ';
+    echo ' .::. '.tr('Stats_t6_04').': ';
     echo $fCt["count"];
-    echo '<br /><br />('.tr('Stats_s3_02').')</center></td></tr></table><br><table border="1" bgcolor="white" width="97%">' . "\n";
+    echo '<br /><br />('.tr('Stats_t6_05').')</center></td></tr></table><br><table border="1" bgcolor="white" width="97%">' . "\n";
     echo '
     <tr class="bgcolor2">
         <td width="20%" align="right">
-            &nbsp;&nbsp;<b>'.tr('number_of_caches').'</b>&nbsp;&nbsp;
+            &nbsp;&nbsp;<b>'.tr('Stats_t6_06').'</b>&nbsp;&nbsp;
         </td>
         <td align="right">
-            &nbsp;&nbsp;<b>'.tr('region').'</b>&nbsp;&nbsp;
+            &nbsp;&nbsp;<b>'.tr('Stats_t6_07').'</b>&nbsp;&nbsp;
         </td>
     </tr><tr><td height="2"></td></tr>';
 
