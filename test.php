b0cffc9b8f1e96c8fcd2e5f5377b20561275408e
wloczynutka@gmail.com <wloczynutka@gmail.com@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2014-11-06 23:33:08 +0000

    phpifo
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@6415 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- test.php
+++ test.php
@@ -1,5 +1,7 @@
 <?php
 require_once 'lib/kint/Kint.class.php';
 error_reporting(-1);
-include __DIR__.'/util.sec/notification/run_notify.php';
+
+phpinfo();
+// include __DIR__.'/util.sec/notification/run_notify.php';
 // include __DIR__.'/util.sec/geokrety/processGeokretyErrors.php';
