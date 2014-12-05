8cffc5fdeeaca5eacdca8e58a02255a390979af0
boguslaw.szczepanowski@gmail.com <boguslaw.szczepanowski@gmail.com@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2014-12-05 21:42:24 +0000

    Rozszerzenie Purifiera
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@6474 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- lib/userInputFilters/userInputFilter.php
+++ lib/userInputFilters/userInputFilter.php
@@ -6,9 +6,77 @@ require_once __dir__.'/../htmlpurifier/library/HTMLPurifier.auto.php';
  * class designed to contain user input filters.
  *
  * @author Andrzej Łza Woźniak, 2014-11-17
+ * @author Boguslaw Szczepanowski, 2014-12-03
  */
 class userInputFilter 
 {
+    static private $config; 
+    static private $lastContext;
+    static private function createConfig() {
+        global $debug_page;
+        
+        $config = HTMLPurifier_Config::createDefault();
+        
+        // coś jest z CSSem spierdolone, i tyle...?
+        $config->def->add('CSS.DefinitionID', null, 'string', true);
+        
+        // TODO: jeżeli iframe nie wskazuje na YouTube, to trzeba go całkiem wywalać, a nie tylko atrybut src
+        $config->set('HTML.SafeIframe', true);
+        $config->set('URI.SafeIframeRegexp', '%^(https?:)?//(www\.youtube(?:-nocookie)?\.com/embed/|player\.vimeo\.com/video/)%'); //allow YouTube and Vimeo
+        $config->set('HTML.SafeObject', true);
+        $config->set('HTML.SafeEmbed', true);
+        $config->set('HTML.MaxImgLength', null);
+        $config->set('CSS.MaxImgLength', null);
+        $config->set('CSS.Proprietary', true);
+        $config->set('CSS.Trusted', true); // ???
+        $config->set('CSS.AllowTricky', true);
+        $config->set('CSS.AllowedFonts', null);
+        
+        $config->set('Core.CollectErrors', isset($debug_page) ? $debug_page : false); // tylko do debugowania - nie włączać na produkcji
+        
+        $config->set('Attr.AllowedFrameTargets', array('_blank'));
+        
+        // $config->set('HTML.Trusted', true); // <-- DO NOT ENABLE!
+        
+        $cssDefinition = $config->getCSSDefinition(true);
+        
+        // do not validate values, pass as is...
+        $cssDefinition->info['box-shadow'] =
+        $cssDefinition->info['text-shadow'] =
+            new HTMLPurifier_AttrDef_Text();
+        
+        $htmlDefinition = $config->getHTMLDefinition(true);
+        
+        // override default, very strict modules
+        $htmlDefinition->manager->registerModule(new OC_HTMLSafeEmbed(), true);
+        $htmlDefinition->manager->registerModule(new OC_HTMLSafeObject(), true);
+
+        return $config; 
+    }
+    
+    static function getConfig()
+    {
+        global $debug_page;
+        if (self::$config !== null){
+            return self::$config;
+        }
+        $useCache = !(isset($debug_page) ? $debug_page : false);
+        $cache_key = 'HTMLPurifierConfig';
+        $result = $useCache ? apc_fetch($cache_key) : false;
+        if ($result === false) {
+        	$result = self::createConfig();
+        	// finalize and lock the config
+        	$result->getHTMLDefinition(); 
+        	$result->getCSSDefinition();
+        	$result->getURIDefinition();
+
+        	if ($useCache){
+        	   apc_store($cache_key, $result, 60);  # cache it for 60 seconds
+        	}
+        }
+        return self::$config = $result;
+    }
+    
     /**
      * filter html string using HTMLPurifier.
      * refer to http://htmlpurifier.org/ for details and documentation.
@@ -16,19 +84,25 @@ class userInputFilter
      * @param string $dirtyHtml
      * @return string
      */
-    public static function purifyHtmlString($dirtyHtml)
+    public static function purifyHtmlString($dirtyHtml, &$context = null)
     {
-        $config = HTMLPurifier_Config::createDefault();
-        $config->set('HTML.SafeIframe', true);
-        $config->set('URI.SafeIframeRegexp', '%^(https?:)?//(www\.youtube(?:-nocookie)?\.com/embed/|player\.vimeo\.com/video/)%'); //allow YouTube and Vimeo
-        $config->set('Filter.YouTube', true);
-        $config->set('HTML.SafeObject', true);
-        $config->set('HTML.MaxImgLength', null);
-        $config->set('CSS.MaxImgLength', null);
-        $config->set('Attr.AllowedFrameTargets', array("_blank"));
+        $config = self::getConfig();
         $purifier = new HTMLPurifier($config);
         $cleanHtml = $purifier->purify($dirtyHtml);
+        if (($config->get('Core.CollectErrors')) && ($context !== null)){ 
+            $context['errors'] =& $purifier->context->get('ErrorCollector');
+        }
         return $cleanHtml;
+        
+        // 1. SVG się nie osadza -> done
+        // http://opencaching.pl/viewcache.php?cacheid=17638
+        // http://opencaching.pl/viewcache.php?cacheid=30273
+        // <embed src="http://broadcasting.miklobit.pl/broadcasting.php" type="image/svg+xml" width="210" height="210" />
+        
+        // 2. style="background-image:url('url');" -> style="background-image:url("url");" -> style="background-image: url(;"
+        // TODO: Wywalić htmlspecialchars_decode i powinno być gites majonez
+        
+        // 3. Znikają definicje scroll i text-shadow z CSSa -> done
     }
 
     /**
@@ -69,4 +143,101 @@ class userInputFilter
         return $cleanHtml;
     }
     
+}
+
+class OC_HTMLSafeEmbed extends HTMLPurifier_HTMLModule_SafeEmbed
+{
+    public function setup($config)
+    {
+        $newEmbed = HTMLPurifier_ElementDef::create(
+        		null,
+        		null,
+        		array(
+                        // TODO: jeżeli brak atrybutu type, lub nie spełnia kryterów, to usuń cały element ?
+        		        'type' => 'Enum#application/x-shockwave-flash,image/svg+xml',
+        				'width' => 'Length#1280',
+        				'height' => 'Length#1920',
+        				'allowscriptaccess' => 'Enum#never,always,sameDomain',
+        				'allownetworking' => 'Enum#all,internal,none',
+        		)
+        );
+        parent::setup($config);
+        $embed = &$this->info['embed'];
+        $embed->mergeIn($newEmbed);
+        unset($embed->attr_transform_post[count($embed->attr_transform_post)-1]);
+    }
+}
+
+class OC_HTMLSafeObject extends HTMLPurifier_HTMLModule_SafeObject
+{
+	public function setup($config)
+	{
+		$newObject = HTMLPurifier_ElementDef::create(
+				null,
+				null,
+				array(
+				        // TODO: j.w.
+						'type' => 'Enum#application/x-shockwave-flash,image/svg+xml',
+						'width' => 'Length#1280',
+						'height' => 'Length#1920',
+				)
+		);
+		parent::setup($config);
+		$object = &$this->info['object'];
+		$object->mergeIn($newObject);
+		
+		$param = &$this->info['param'];
+		$param->attr_transform_post[count($param->attr_transform_post)-1] = new OC_HTMLPurifier_AttrTransform_SafeParam();
+
+		unset($this->info_injector[count($this->info_injector)-1]);
+		$this->info_injector[] = new OC_HTMLPurifier_Injector_SafeObject();
+	}
+}
+
+class OC_HTMLPurifier_AttrTransform_SafeParam extends HTMLPurifier_AttrTransform_SafeParam
+{
+    protected $allowScriptAccess;
+    protected $allowNetworking;
+    
+    public function __construct()
+    {
+        parent::__construct();
+    	$this->allowScriptAccess = new HTMLPurifier_AttrDef_Enum(array('never','always','sameDomain'));
+    	$this->allowNetworking = new HTMLPurifier_AttrDef_Enum(array('all','internal','none'));
+    }
+    
+    public function transform($attr, $config, $context)
+    {
+    	switch ($attr['name']) {
+    		case 'allowScriptAccess':
+    		    $attr['value'] = $this->allowScriptAccess->validate($attr['value'], $config, $context);
+    		    return $attr;
+    		case 'allowNetworking':
+    		    $attr['value'] = $this->allowNetworking->validate($attr['value'], $config, $context);
+    		    return $attr;
+    	}
+    	$attr = parent::transform($attr, $config, $context);
+    	return $attr; 
+    }
+}
+
+class OC_HTMLPurifier_Injector_SafeObject extends HTMLPurifier_Injector_SafeObject {
+    
+    public function __construct()
+    {
+        unset($this->addParam['allowScriptAccess']);
+        unset($this->addParam['allowNetworking']);
+        $this->allowedParam['allowScriptAccess'] = true;
+        $this->allowedParam['allowNetworking'] = true;
+    }
+    
+    public function handleElement(&$token)
+    {
+        parent::handleElement($token);
+    }
+    
+    public function handleEnd(&$token)
+    {
+    	parent::handleEnd($token);
+    }
 }
