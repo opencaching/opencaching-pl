4624eac6e6f26bd8c345de6791f9b7268998713a
boguslaw.szczepanowski@gmail.com <boguslaw.szczepanowski@gmail.com@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2014-12-12 23:19:13 +0000

    Use HTML Purifier when saving geocache description, and do not use HTML Purifier when showing such geocaches.
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@6503 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

---
+++ sqlAlters/2014-12-12_cache_desc_purifier.sql
@@ -0,0 +1,9 @@
+-- 2014-12-12 Store information, that HTML Purifier has been used when saving geocache description
+-- @author: Bogus z Polska
+ 
+-- THIS IS SPARTAAAAAA!
+alter table `cache_desc` modify column desc_html tinyint(1) not null default '0' comment 'Format for desc column: 0-plain text, which is actualy HTML with only <br/>s; 1-unsafe HTML; 2-safe HTML';
+alter table `cache_desc` modify column desc_htmledit tinyint(1) not null default '0' comment 'Unused';
+alter table `cache_desc` modify column `desc` mediumtext default null comment 'HTML formatted geocache description';
+alter table `cache_desc` modify column short_desc mediumtext default null comment 'Plain text short description';
+alter table `cache_desc` modify column hint mediumtext default null comment 'HTML-escaped hint, will contain <br />s';
