0f3c8fcd06aef2d84c8254bdeedcccaba3c4f7b3
boguslaw.szczepanowski@gmail.com <boguslaw.szczepanowski@gmail.com@19cba9c3-8ce2-6c67-1c13-32ea8d8b491d>
2014-12-19 19:22:02 +0000

    Poprawka opisu kolumny
    git-svn-id: https://opencaching-pl.googlecode.com/svn/trunk@6512 19cba9c3-8ce2-6c67-1c13-32ea8d8b491d

--- sqlAlters/2014-12-12_cache_desc_purifier.sql
+++ sqlAlters/2014-12-12_cache_desc_purifier.sql
@@ -2,7 +2,7 @@
 -- @author: Bogus z Polska
  
 -- THIS IS SPARTAAAAAA!
-alter table `cache_desc` modify column desc_html tinyint(1) not null default '0' comment 'Format for desc column: 0-plain text, which is actualy HTML with only <br/>s; 1-unsafe HTML; 2-safe HTML';
+alter table `cache_desc` modify column desc_html tinyint(1) not null default '0' comment 'Format for desc column: 0-DO NOT USE: unknown format based on HTML; 1-unsafe HTML; 2-safe HTML';
 alter table `cache_desc` modify column desc_htmledit tinyint(1) not null default '0' comment 'Unused';
 alter table `cache_desc` modify column `desc` mediumtext default null comment 'HTML formatted geocache description';
 alter table `cache_desc` modify column short_desc mediumtext default null comment 'Plain text short description';
