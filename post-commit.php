3170a3a83a122b5e7bc71edf7f23a295d6c9f1d4
Wojciech Rygielski <rygielski@mimuw.edu.pl>
2015-07-27 18:46:40 +0200

    Update post-commit scripts.

--- post-commit.php
+++ post-commit.php
@@ -1,7 +1,7 @@
 <?php
 
-# This script is called by Google Code after each commit. All changes commited
-# to SVN are immediately updated on the production server.
+# This script is called by GitHub after each commit. All changes commited to
+# the master branch are immediately checked out at the production server.
 
 ignore_user_abort(true);
 set_time_limit(0);
@@ -9,9 +9,9 @@ set_time_limit(0);
 header("Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0");
 header("Content-Type: text/plain; charset=utf-8");
 
-# File /var/www/ocpl-update.sh contains "svn up /var/www/ocpl" command
-# (and a couple of others). It is allowed to be sudo-executed by 'www-data'
-# without password. This is for production only; if this is a developer
-# installation, then you should do updates by yourself (run 'svn up').
+# File /var/www/ocpl-update.sh contains proper "git pull" commands. It is
+# allowed to be sudo-executed by 'www-data' without password. This is for
+# production only; if this is a developer installation, then you should do
+# updates by yourself.
 
 print shell_exec("sudo /var/www/ocpl-update.sh 2>&1");
