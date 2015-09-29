5f110e4a0eaff1f1a73c03e8289572ed7f08d660
wloczynutka <wloczynutka@gmail.com>
2015-09-29 22:53:37 +0200

    include composer autoloader only if exist.

--- lib/ClassPathDictionary.php
+++ lib/ClassPathDictionary.php
@@ -1,7 +1,9 @@
 <?php
 
 /* include composer autoloader */
-require __DIR__ . '/../vendor/autoload.php';
+if(file_exists(__DIR__ . '/../vendor/autoload.php')){
+    require __DIR__ . '/../vendor/autoload.php';
+}
 
 /**
  *  Autoload class solution.
