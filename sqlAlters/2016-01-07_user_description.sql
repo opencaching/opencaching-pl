fcf0d3087b260475028523c0a78ed1d5eea33af2
MikloBit <miklobit@gmail.com>
2016-01-08 01:04:40 +0100

    User-description field too short #218

---
+++ sqlAlters/2016-01-07_user_description.sql
@@ -0,0 +1,6 @@
+-- 2016-01-07 Extend user-description 1024->4096
+
+-- github.com/opencaching/opencaching-pl/issues/218
+-- @author: miklobit
+
+ALTER TABLE  `user` CHANGE  `description`  `description` VARCHAR( 4096 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL ;
