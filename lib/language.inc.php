6eea69219d0b25c2ae75cf79fd7c46ca0a2e54c1
wloczynutka <wloczynutka@gmail.com>
2016-02-06 21:28:02 +0100

    change language files from plain text into php array to avoid parsing.

--- lib/language.inc.php
+++ lib/language.inc.php
@@ -5,32 +5,13 @@ $language = array();
 function load_language_file($lang)
 {
     global $language;
-
-    $cache_key = "oclang/$lang";
-    $result = apc_fetch($cache_key);
-    if ($result === false) {
-        $result = array();
-        $fhandle = fopen(dirname(__FILE__) . "/languages/" . $lang, "r");
-        if ($fhandle) {
-            while ($line = fgets($fhandle, 4096)) {
-                $pos = strpos($line, ' ');
-                $short = substr($line, 0, $pos);
-                $translation = substr($line, $pos + 1, -1);
-                $translation = rtrim($translation, "\r\n");
-                $result[$short] = $translation;
-            }
-            fclose($fhandle);
-        } else {
-            $result = null;
-        }
-        apc_store($cache_key, $result, 60);  # cache it for 60 seconds
-    }
-    if ($result) {
-        $language[$lang] = &$result;
-        return true;
-    } else {
-        return false;
-    }
+	$languageFilename = __DIR__ . "/languages/" . $lang.'.php';
+    if(!file_exists($languageFilename)){
+		return false;
+	}
+    include ($languageFilename);
+    $language[$lang] = $translations;
+    return true;
 }
 
 function available_languages()
@@ -38,8 +19,8 @@ function available_languages()
     $available_langs = array();
     if ($handle = opendir(dirname(__FILE__) . '/languages')) {
         while (false !== ($file = readdir($handle))) {
-            if (substr($file, 0, 1) != '.' && strlen($file) == 2) {
-                array_push($available_langs, $file);
+            if (substr($file, 0, 1) != '.' && strlen($file) == 6) {
+                array_push($available_langs, substr($file, 0,2));
             }
         }
         closedir($handle);
@@ -50,6 +31,8 @@ function available_languages()
 function tr($str)
 {
     global $language, $lang;
+
+
     if (isset($language[$lang][$str]) && $language[$lang][$str]) {
         $ref = &$language[$lang][$str];
         if (strpos($ref, "{") !== false)
@@ -75,5 +58,3 @@ function tr2($str, $lang)
         return $str . "No translation available (identifier: $str)-todo";
     }
 }
-
-?>
