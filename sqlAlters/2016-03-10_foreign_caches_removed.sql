4b7dff102c1392d5947554d09776e709ff1a1133
kojoty <p.juzwiak@gmail.com>
2016-03-08 18:51:53 +0100

    obsolate foregin_caches db tables remove + related code

---
+++ sqlAlters/2016-03-10_foreign_caches_removed.sql
@@ -0,0 +1,7 @@
+--
+-- Table foreign_caches is obsolate for many years
+-- This was used only by map_v2 - now this code is removed
+-- This table can be safety dropped
+--
+
+DROP TABLE foreign_caches;
