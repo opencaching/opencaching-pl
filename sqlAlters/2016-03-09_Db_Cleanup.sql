04206e28ff5aad409aaa50ef80c21f0a478d2fa8
kojoty <p.juzwiak@gmail.com>
2016-03-08 07:37:01 +0100

    remove ajax_chat* db tables

---
+++ sqlAlters/2016-03-09_Db_Cleanup.sql
@@ -0,0 +1,13 @@
+--
+-- There are a few unused tables which probably can be safety removed.
+--
+
+
+--
+-- chat is no more supported
+--
+DROP TABLE ajax_chat_bans;
+DROP TABLE ajax_chat_invitations;
+DROP TABLE ajax_chat_messages;
+DROP TABLE ajax_chat_online;
+DROP TABLE ajax_chat_users;
