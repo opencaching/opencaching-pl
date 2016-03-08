4f1379caca969bc671be75feaf4c675d80c79a51
kojoty <p.juzwiak@gmail.com>
2016-03-08 16:20:53 +0100

    nodes db table removed - this contained not used static data

---
+++ sqlAlters/2016-03-10_Db_Cleanup_part2.sql
@@ -0,0 +1,10 @@
+--
+-- There are a few unused tables which probably can be safety removed.
+-- This is part2. of changes
+--
+
+--
+-- nodes tabe contains never used in code static values
+--
+DROP TABLE nodes;
+
