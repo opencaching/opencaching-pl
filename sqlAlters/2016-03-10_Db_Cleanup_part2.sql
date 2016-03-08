6231b36c7b43be3ddc8793a3e28f592adbe4b769
kojoty <p.juzwiak@gmail.com>
2016-03-08 17:20:25 +0100

    removing of old ocpl voting db tables - not used in current code

--- sqlAlters/2016-03-10_Db_Cleanup_part2.sql
+++ sqlAlters/2016-03-10_Db_Cleanup_part2.sql
@@ -32,3 +32,24 @@ DROP TABLE sys_menu;
 -- sys_temptables table is never used in code
 --
 DROP TABLE sys_temptables;
+
+--
+-- tables rr_ocpl_* from years 2009-2015 are not used in current code
+-- I (kojoty) backup the data from these tables and now it can be safty dropped from server
+--
+DROP TABLE rr_ocpl_candidates_2009;
+DROP TABLE rr_ocpl_candidates_2010;
+DROP TABLE rr_ocpl_candidates_2011;
+DROP TABLE rr_ocpl_candidates_2012;
+DROP TABLE rr_ocpl_candidates_2013;
+DROP TABLE rr_ocpl_candidates_2014;
+DROP TABLE rr_ocpl_candidates_2015;
+
+DROP TABLE rr_ocpl_vote_2009;
+DROP TABLE rr_ocpl_vote_2010;
+DROP TABLE rr_ocpl_vote_2011;
+DROP TABLE rr_ocpl_vote_2012;
+DROP TABLE rr_ocpl_vote_2013;
+DROP TABLE rr_ocpl_vote_2014;
+DROP TABLE rr_ocpl_vote_2015;
+
