5df087a316a2134fc334948bdf47d8d51ced4844
kojoty <p.juzwiak@gmail.com>
2016-03-08 16:34:28 +0100

    object_types + search_doubles db tables removed - this tables are not used in code

--- sqlAlters/2016-03-10_Db_Cleanup_part2.sql
+++ sqlAlters/2016-03-10_Db_Cleanup_part2.sql
@@ -4,7 +4,17 @@
 --
 
 --
--- nodes tabe contains never used in code static values
+-- nodes table contains never used in code, static values
 --
 DROP TABLE nodes;
+
+--
+-- object_types table contains never used in code, static values
+--
+DROP TABLE object_types;
+
+--
+-- search_doubles table contains never used in code, static values
+--
+DROP TABLE search_doubles;
 
