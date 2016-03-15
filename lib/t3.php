399cc9ffa17de17899a0813c51fbe351c8d3ccb8
kojoty <p.juzwiak@gmail.com>
2016-03-16 00:52:40 +0100

    log warn fixes

--- lib/t3.php
+++ lib/t3.php
@@ -13,11 +13,11 @@ echo '<table width="97%"><tr><td align="center"><center><b>' . tr(Stats_t3_01) .
 echo '<table border="1" bgcolor="white" width="97%" style="font-size:11px; line-height:1.6em;">' . "\n";
 
 $linie = sql("SELECT COUNT(*) `count`, `user`.`username` FROM `cache_logs` INNER JOIN `caches` ON `cache_logs`.`cache_id`=`caches`.`cache_id` INNER JOIN `user` ON `caches`.`user_id`=`user`.`user_id` WHERE `cache_logs`.`deleted`=0 AND `cache_logs`.`type`=1 GROUP BY `user`.`user_id` ORDER BY `count` DESC, `user`.`username` ASC");
-echo '<tr><td class="bgcolor2" align="right"><b>' . tr(Stats_t3_02) . '</b>&nbsp;&nbsp;</td><td class="bgcolor2" align="center"><b>' . tr(Stats_t3_03) . '</b>&nbsp;&nbsp;</td><td class="bgcolor2" align="center"><b>' . tr(Stats_t3_04) . '</b>&nbsp;&nbsp;</td></tr><tr><td>';
+echo '<tr><td class="bgcolor2" align="right"><b>' . tr('Stats_t3_02') . '</b>&nbsp;&nbsp;</td><td class="bgcolor2" align="center"><b>' . tr(Stats_t3_03) . '</b>&nbsp;&nbsp;</td><td class="bgcolor2" align="center"><b>' . tr(Stats_t3_04) . '</b>&nbsp;&nbsp;</td></tr><tr><td>';
 $l2 = "";
 $licznik = 0;
 while ($linia = sql_fetch_assoc($linie)) {
-    $l1 = $linia[count];
+    $l1 = $linia['count'];
     if ($l2 != $l1) {
         $licznik++;
         echo "</td></tr><tr><td class=\"bgcolor2\" align=\"right\">&nbsp;&nbsp;<b>$licznik</b>&nbsp;&nbsp;</td><td class=\"bgcolor2\" align=\"right\">&nbsp;&nbsp;<b>$l1</b>&nbsp;&nbsp;</td>";
