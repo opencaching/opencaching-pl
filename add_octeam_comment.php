eeec4274504f06673b2eef849366214d3124c9b4
kojoty <p.juzwiak@gmail.com>
2016-03-20 00:53:24 +0100

    mysql_* remove: part4

--- add_octeam_comment.php
+++ add_octeam_comment.php
@@ -1,5 +1,7 @@
 <?php
 
+use Utils\Database\XDb;
+
 //prepare the templates and include all neccessary
 require_once('./lib/common.inc.php');
 
@@ -7,10 +9,9 @@ require_once('./lib/common.inc.php');
 if ($usr['admin']) {
     $_SESSION['submitted'] = false;
     $sql = "SELECT name FROM caches WHERE cache_id=" . intval($_REQUEST['cacheid']);
-    $cachename = @mysql_result(@mysql_query($sql), 0);
+    $cachename = XDb::xSimpleQueryValue($sql, 0);
     tpl_set_var('cachename', $cachename);
     tpl_set_var('cacheid', $_REQUEST['cacheid']);
     $tplname = 'add_octeam_comment';
     tpl_BuildTemplate();
 }
-?>
