ee05b770a86d4eae9eb8e37383482add92ef42c6
wloczynutka <wloczynutka@gmail.com>
2016-03-20 17:37:54 +0100

    add validation while user update power trail publish date. fixes #360

--- powerTrail/ajaxUpdateDate.php
+++ powerTrail/ajaxUpdateDate.php
@@ -1,21 +1,24 @@
 <?php
 session_start();
 if(!isset($_SESSION['user_id'])){
-    print 'no hacking please!';
-    exit;
+    die(json_encode(array('resultSuccess' => false, 'error' => 'User is not logged in')));
 }
 require_once __DIR__.'/../lib/ClassPathDictionary.php';
 $ptAPI = new powerTrailBase;
 
 $powerTrailId = $_REQUEST['projectId'];
-$newDate = $_REQUEST['newDate'] . ' 0:00:01';
-
+try{
+    $newDate = new DateTime($_REQUEST['newDate']);
+} catch (Exception $ex) {
+    die(json_encode(array('resultSuccess' => false, 'error' => 'Wrong date')));
+}
 
 // check if user is owner of selected power Trail
 if($ptAPI::checkIfUserIsPowerTrailOwner($_SESSION['user_id'], $powerTrailId) == 1) {
     $query = 'UPDATE `PowerTrail` SET `dateCreated`= :1 WHERE `id` = :2';
     $db = \lib\Database\DataBaseSingleton::Instance();
-    $db->multiVariableQuery($query, $newDate, $powerTrailId);
+    $db->multiVariableQuery($query, $newDate->format('Y-m-d H:i:s'), $powerTrailId);
+    die(json_encode(array('resultSuccess' => true, 'error' => null)));
+} else {
+    die(json_encode(array('resultSuccess' => false, 'error' => 'User is not powerTrail owner')));
 }
-
-?>
