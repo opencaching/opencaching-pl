9433762c9475d2464559017cc69dd5dc9e12f726
kojoty <p.juzwiak@gmail.com>
2016-03-20 01:49:54 +0100

    mysql_* remove: part9

--- add_rr_comment.php
+++ add_rr_comment.php
@@ -1,15 +1,16 @@
 <?php
 
+use Utils\Database\XDb;
+
 //prepare the templates and include all neccessary
 require_once('./lib/common.inc.php');
 
 if ($usr['admin']) {
     $_SESSION['submitted'] = false;
     $sql = "SELECT name FROM caches WHERE cache_id=" . intval($_REQUEST['cacheid']);
-    $cachename = @mysql_result(@mysql_query($sql), 0);
+    $cachename = XDb::xSimpleQueryValue($sql, 0);
     tpl_set_var('cachename', $cachename);
     tpl_set_var('cacheid', $_REQUEST['cacheid']);
     $tplname = 'add_rr_comment';
     tpl_BuildTemplate();
 }
-?>
