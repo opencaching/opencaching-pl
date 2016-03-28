e3b3b0713e4f9c16f59eeabe878ebb26d6d81730
kojoty <p.juzwiak@gmail.com>
2016-03-28 07:15:24 +0200

    mysql_* remove: part47

--- ocstats.php
+++ ocstats.php
@@ -1,5 +1,6 @@
 <?php
 
+use Utils\Database\XDb;
 // Unicode Reminder ??
 setlocale(LC_TIME, 'pl_PL.UTF-8');
 //setlocale(LC_TIME, 'pl_PL.ISO-8859-2');
@@ -14,27 +15,24 @@ $fontfile = "./lib/fonts/arial.ttf";
 # get userid and style from URL
 $userid = isset($_REQUEST['userid']) ? $_REQUEST['userid'] : 0;
 
-# get detailed info from DB
-$rs = sql("SELECT `username`, `statpic_logo`, `statpic_text` FROM `user` WHERE `user_id`='&1'", $userid);
-
 // nie licz spotkan, skrzynek jeszcze nieaktywnych, zarchiwizowanych i wstrzymanych
-$sql = "SELECT COUNT(*) FROM caches WHERE user_id='$userid' AND status <> 2 AND status <> 3 AND status <> 4 AND status <> 5 AND status <> 6 AND type <> 6";
-if ($odp = mysql_query($sql))
-    $hidden = mysql_result($odp, 0);
-else
-    $hidden = 0;
+$hidden = XDb::xMultiVariableQueryValue(
+    "SELECT COUNT(*) FROM caches
+    WHERE user_id= :1 AND status <> 2 AND status <> 3 AND status <> 4
+        AND status <> 5 AND status <> 6 AND type <> 6",
+    0, $userid);
 
-$sql = "SELECT COUNT(*) founds_count
-                    FROM cache_logs
-                    WHERE user_id=$userid AND type=1 AND deleted=0";
-if ($odp = mysql_query($sql))
-    $found = mysql_result($odp, 0);
-else
-    $found = 0;
+$found = XDb::xMultiVariableQueryValue(
+    "SELECT COUNT(*) founds_count FROM cache_logs
+    WHERE user_id= :1 AND type=1 AND deleted=0", 0, $userid);
 
+# get detailed info from DB
+$rs = XDb::xSql(
+    "SELECT `username`, `statpic_logo`, `statpic_text` FROM `user`
+    WHERE `user_id`= ? LIMIT 1", $userid);
 
-if (mysql_num_rows($rs) == 1) {
-    $record = sql_fetch_array($rs);
+if ($record = XDb::xFetchArray($rs)) {
+
     $username = $record['username'];
     $logo = isset($record['statpic_logo']) ? $record['statpic_logo'] : 0;
     $logotext = isset($record['statpic_text']) ? $record['statpic_text'] : 'Opencaching';
@@ -45,16 +43,17 @@ if (mysql_num_rows($rs) == 1) {
     $logotext = 'Opencaching';
 }
 
-mysql_free_result($rs);
+XDb::xFreeResults($rs);
 
 $userid = $userid + 0;
 
 if (!file_exists($dynbasepath . 'images/statpics/statpic' . $userid . '.jpg')) {
     // Bild existiert nicht => neu erstellen
-    $rs = sql("SELECT `tplpath`, `maxtextwidth` FROM `statpics` WHERE `id`='&1'", $logo);
+    $rs = XDb::xSql(
+        "SELECT `tplpath`, `maxtextwidth` FROM `statpics` WHERE `id`= ? LIMIT 1", $logo);
 
-    if (mysql_num_rows($rs) == 1) {
-        $record = sql_fetch_array($rs);
+    if ($record = XDb::xFetchArray($rs)) {
+
         $tplpath = $record['tplpath'];
         $maxtextwidth = $record['maxtextwidth'];
     } else {
@@ -62,7 +61,7 @@ if (!file_exists($dynbasepath . 'images/statpics/statpic' . $userid . '.jpg')) {
         $maxtextwidth = 60;
         $logo = 1;
     }
-    mysql_free_result($rs);
+    XDb::xFreeResults($rs);
 
     $im = ImageCreateFromGIF($tplpath);
     $clrWhite = ImageColorAllocate($im, 255, 255, 255);
@@ -79,7 +78,7 @@ if (!file_exists($dynbasepath . 'images/statpics/statpic' . $userid . '.jpg')) {
             $textsize = imagettfbbox($fontsize, 0, $fontfile, $text);
             ImageTTFText($im, $fontsize, 0, (imagesx($im) - ($textsize[2] - $textsize[0]) - 8 > $maxtextwidth) ? imagesx($im) - ($textsize[2] - $textsize[0]) - 8 : $maxtextwidth, 15, $clrBlack, $fontfile, $text);
             $fontsize = 8;
-            $text = tr(statpic_found) . $found . ' / ' . tr(statpic_hidden) . $hidden;
+            $text = tr('statpic_found') . $found . ' / ' . tr('statpic_hidden') . $hidden;
             $textsize = imagettfbbox($fontsize, 0, $fontfile, $text);
             ImageTTFText($im, $fontsize, 0, (imagesx($im) - ($textsize[2] - $textsize[0]) - 8 > $maxtextwidth) ? imagesx($im) - ($textsize[2] - $textsize[0]) - 8 : $maxtextwidth, 32, $clrBlack, $fontfile, $text);
             break;
@@ -93,7 +92,7 @@ if (!file_exists($dynbasepath . 'images/statpics/statpic' . $userid . '.jpg')) {
             $textsize = imagettfbbox($fontsize, 0, $fontfile, $logotext);
             ImageTTFText($im, $fontsize, 0, (imagesx($im) - ($textsize[2] - $textsize[0]) - 5 > $maxtextwidth) ? imagesx($im) - ($textsize[2] - $textsize[0]) - 5 : $maxtextwidth, 29, $clrBlack, $fontfile, $logotext);
             $fontsize = 8;
-            $text = tr(statpic_found) . $found . ' / ' . tr(statpic_hidden) . $hidden;
+            $text = tr('statpic_found') . $found . ' / ' . tr('statpic_hidden') . $hidden;
             $textsize = imagettfbbox($fontsize, 0, $fontfile, $text);
             ImageTTFText($im, $fontsize, 0, (imagesx($im) - ($textsize[2] - $textsize[0]) - 8 > $maxtextwidth) ? imagesx($im) - ($textsize[2] - $textsize[0]) - 8 : $maxtextwidth, 45, $clrBlack, $fontfile, $text);
             break;
@@ -116,7 +115,7 @@ if (!file_exists($dynbasepath . 'images/statpics/statpic' . $userid . '.jpg')) {
             $textsize = imagettfbbox($fontsize, 0, $fontfile, $text);
             ImageTTFText($im, $fontsize, 0, (imagesx($im) - ($textsize[2] - $textsize[0]) - 5 > $maxtextwidth) ? imagesx($im) - ($textsize[2] - $textsize[0]) - 5 : $maxtextwidth, 15, $clrBlack, $fontfile, $text);
             $fontsize = 8;
-            $text = tr(statpic_found) . $found . ' / ' . tr(statpic_hidden) . $hidden;
+            $text = tr('statpic_found') . $found . ' / ' . tr('statpic_hidden') . $hidden;
             $textsize = imagettfbbox($fontsize, 0, $fontfile, $text);
             ImageTTFText($im, $fontsize, 0, (imagesx($im) - ($textsize[2] - $textsize[0]) - 8 > $maxtextwidth) ? imagesx($im) - ($textsize[2] - $textsize[0]) - 8 : $maxtextwidth, 29, $clrBlack, $fontfile, $text);
             $fontsize = 8;
@@ -133,4 +132,4 @@ if (!file_exists($dynbasepath . 'images/statpics/statpic' . $userid . '.jpg')) {
 
 // Redirect auf das gespeicherte Bild
 tpl_redirect('images/statpics/statpic' . $userid . '.jpg');
-?>
+
