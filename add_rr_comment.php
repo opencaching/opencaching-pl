7b91b5364ad96f49ce4b88e507e6eb6ab6c3da88
kojoty <p.juzwiak@gmail.com>
2016-03-28 19:07:28 +0200

    mysql_* remove: part52

--- add_rr_comment.php
+++ add_rr_comment.php
@@ -7,8 +7,10 @@ require_once('./lib/common.inc.php');
 
 if ($usr['admin']) {
     $_SESSION['submitted'] = false;
-    $sql = "SELECT name FROM caches WHERE cache_id=" . intval($_REQUEST['cacheid']);
-    $cachename = XDb::xSimpleQueryValue($sql, 0);
+    $cachename = XDb::xMultiVariableQueryValue(
+        "SELECT name FROM caches WHERE cache_id= :1 ",
+        0, $_REQUEST['cacheid']);
+
     tpl_set_var('cachename', $cachename);
     tpl_set_var('cacheid', $_REQUEST['cacheid']);
     $tplname = 'add_rr_comment';
