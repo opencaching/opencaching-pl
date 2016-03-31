4064fb7af9cbc789206857dac68d4061ec11960a
kojoty <p.juzwiak@gmail.com>
2016-03-31 16:46:05 +0200

    searchplugin is not used and obsolate code - lets try to remove it

--- tpl/stdstyle/searchplugin.inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$errmsg_no_cache_found = '<span class="errormsg">Nie znaleziono żadnej skrzynki dla podanego Waypoint `{wp}`.</span>';
-$errmsg_many_caches_found = '<span class="errormsg">Wiele skrzynek znaleziono z Waypoint `{wp}`.</span>';
-$errmsg_unknown_format = 'Niepoprawny format Waypoint. Dopuszczalne formaty OPxxxx (OC PL), OCxxxx (OC DE), i GCxxxx (GC.com), gdzie xxxx numer .</span>';
-?>
