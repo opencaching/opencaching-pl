657b1c9986272233ee5e5950153ade0024c0e438
kojoty <p.juzwiak@gmail.com>
2016-04-01 05:12:11 +0200

    mysql_* remove: part62

--- my_logsFind.php
+++ my_logsFind.php
@@ -1,5 +1,6 @@
 <?php
 
+use Utils\Database\XDb;
 global $lang, $rootpath, $dateFormat;
 
 if (!isset($rootpath))
@@ -20,6 +21,10 @@ if ($error == false) {
         if (isset($_REQUEST['userid'])) {
             $user_id = $_REQUEST['userid'];
             tpl_set_var('userid', $user_id);
+        }else{
+            //no param userid - display data for currently logged user
+            $user_id = $usr['userid'];
+            tpl_set_var('userid', $user_id);
         }
 
         //get the news
@@ -29,9 +34,9 @@ if ($error == false) {
 
         function cleanup_text($str)
         {
-            $str = strip_tags($str, "<li>");
             $from[] = '<p>&nbsp;</p>';
             $to[] = '';
+            $str = strip_tags($str, "<li>");
             $from[] = '&nbsp;';
             $to[] = ' ';
             $from[] = '<p>';
@@ -83,31 +88,24 @@ if ($error == false) {
             for ($i = 0; $i < count($from); $i++)
                 $str = str_replace($from[$i], $to[$i], $str);
 
-            return filterevilchars($str);
+            return str_replace('[\\x00-\\x09|\\x0A-\\x0E-\\x1F]', '', $str); //filterevilchars
         }
 
-        function filterevilchars($str)
-        {
-            return str_replace('[\\x00-\\x09|\\x0A-\\x0E-\\x1F]', '', $str);
-        }
+        $username = XDb::xMultiVariableQueryValue(
+            "SELECT username FROM user WHERE user_id= :1 LIMIT 1", '', $user_id);
+        tpl_set_var('username', $username);
 
-        $rsGeneralStat = sql("SELECT  username FROM user WHERE user_id=&1", $user_id);
-
-        $user_record = sql_fetch_array($rsGeneralStat);
-        tpl_set_var('username', $user_record['username']);
-        mysql_free_result($rsGeneralStat);
         $LOGS_PER_PAGE = 50;
         $PAGES_LISTED = 10;
 
-        $rs = sql("SELECT count(id) FROM cache_logs, caches WHERE `cache_logs`.`cache_id`=`caches`.`cache_id`
+        $total_logs = XDb::xMultiVariableQueryValue(
+            "SELECT count(id) FROM cache_logs, caches WHERE `cache_logs`.`cache_id`=`caches`.`cache_id`
                 AND `cache_logs`.`deleted`=0
                 AND `cache_logs`.`type`=1
                 AND `caches`.`status` != 4
                 AND `caches`.`status` != 5
                 AND `caches`.`status` != 6
-                AND `cache_logs`.`user_id`='" . sql_escape($_REQUEST['userid']) . "'");
-        $total_logs = mysql_result($rs, 0);
-        mysql_free_result($rs);
+                AND `cache_logs`.`user_id`= :1 ", 0, $user_id);
 
         $pages = "";
         $total_pages = ceil($total_logs / $LOGS_PER_PAGE);
@@ -135,56 +133,55 @@ if ($error == false) {
             $pages .= '<a href="my_logsFind.php?userid=' . $user_id . '&amp;start=' . (($i - 1) * $LOGS_PER_PAGE) . '">{last_img}</a> ';
         else
             $pages .= '{last_img_inactive}';
-        $rs = sql("SELECT `cache_logs`.`id`
+
+
+        $rs = XDb::xSql(
+            "SELECT `cache_logs`.`id`
             FROM `cache_logs`, `caches`
             WHERE `cache_logs`.`cache_id`=`caches`.`cache_id`
                 AND `cache_logs`.`deleted`=0
                 AND `cache_logs`.`type`=1
-                  AND `caches`.`status` != 4
+                AND `caches`.`status` != 4
                 AND `caches`.`status` != 5
                 AND `caches`.`status` != 6
-                AND `cache_logs`.`user_id`='" . sql_escape($_REQUEST['userid']) . "'
+                AND `cache_logs`.`user_id`= ?
             ORDER BY  `cache_logs`.`date_created` DESC
-            LIMIT " . intval($start) . ", " . intval($LOGS_PER_PAGE));
-        $log_ids = '';
-        if (mysql_num_rows($rs) == 0)
-            $log_ids = '0';
-        for ($i = 0; $i < mysql_num_rows($rs); $i++) {
-            $record = sql_fetch_array($rs);
-            if ($i > 0) {
-                $log_ids .= ', ' . $record['id'];
-            } else {
-                $log_ids = $record['id'];
-            }
+            LIMIT " . intval($start) . ", " . intval($LOGS_PER_PAGE), $user_id );
+
+        $log_ids = array();
+        while( $record = XDb::xFetchArray($rs)){
+            $log_ids[] = $record['id'];
         }
-        mysql_free_result($rs);
+        XDb::xFreeResults($rs);
 
-        $rs = sql("SELECT cache_logs.id, cache_logs.cache_id AS cache_id,
-                              cache_logs.type AS log_type,
-                              cache_logs.date AS log_date,
-                 cache_logs.text AS log_text,
-                        cache_logs.text_html AS text_html,
-                              caches.name AS cache_name,
-                caches.user_id AS cache_owner,
-                cache_logs.encrypt encrypt,
-                              user.username AS user_name,
-                              cache_logs.user_id AS luser_id,
-                              caches.wp_oc AS wp_name,
-                              caches.type AS cache_type,
-                              cache_type.icon_small AS cache_icon_small,
-                              log_types.icon_small AS icon_small,
-                              IF(ISNULL(`cache_rating`.`cache_id`), 0, 1) AS `recommended`,COUNT(gk_item.id) AS geokret_in
-                      FROM ((cache_logs INNER JOIN caches ON (caches.cache_id = cache_logs.cache_id))) INNER JOIN user ON (cache_logs.user_id = user.user_id) INNER JOIN log_types ON (cache_logs.type = log_types.id) INNER JOIN cache_type ON (caches.type = cache_type.id) LEFT JOIN `cache_rating` ON `cache_logs`.`cache_id`=`cache_rating`.`cache_id` AND `cache_logs`.`user_id`=`cache_rating`.`user_id`
-                            LEFT JOIN   gk_item_waypoint ON gk_item_waypoint.wp = caches.wp_oc
-                            LEFT JOIN   gk_item ON gk_item.id = gk_item_waypoint.id AND
-                            gk_item.stateid<>1 AND gk_item.stateid<>4 AND gk_item.typeid<>2 AND gk_item.stateid !=5
-                      WHERE cache_logs.deleted=0 AND `cache_logs`.`type`=1 AND cache_logs.id IN (" . $log_ids . ") AND `cache_logs`.`user_id`='" . sql_escape($_REQUEST['userid']) . "'
-                       GROUP BY cache_logs.id ORDER BY cache_logs.date_created DESC");
-        if (mysql_num_rows($rs) != 0) {
-            $file_content = '';
-            for ($i = 0; $i < mysql_num_rows($rs); $i++) {
-                $log_record = sql_fetch_array($rs);
+        $rs = XDb::xSql(
+            "SELECT cache_logs.id, cache_logs.cache_id AS cache_id, cache_logs.type AS log_type,
+                    cache_logs.date AS log_date, cache_logs.text AS log_text, cache_logs.text_html AS text_html,
+                    caches.name AS cache_name, caches.user_id AS cache_owner, cache_logs.encrypt encrypt,
+                    user.username AS user_name, cache_logs.user_id AS luser_id,
+                    caches.wp_oc AS wp_name,
+                    caches.type AS cache_type,
+                    cache_type.icon_small AS cache_icon_small,
+                    log_types.icon_small AS icon_small,
+                    IF(ISNULL(`cache_rating`.`cache_id`), 0, 1) AS `recommended`,COUNT(gk_item.id) AS geokret_in
+            FROM (cache_logs INNER JOIN caches ON (caches.cache_id = cache_logs.cache_id))
+                INNER JOIN user ON (cache_logs.user_id = user.user_id)
+                INNER JOIN log_types ON (cache_logs.type = log_types.id)
+                INNER JOIN cache_type ON (caches.type = cache_type.id)
+                LEFT JOIN `cache_rating` ON `cache_logs`.`cache_id`=`cache_rating`.`cache_id`
+                    AND `cache_logs`.`user_id`=`cache_rating`.`user_id`
+                LEFT JOIN   gk_item_waypoint ON gk_item_waypoint.wp = caches.wp_oc
+                LEFT JOIN   gk_item ON gk_item.id = gk_item_waypoint.id
+                    AND gk_item.stateid<>1 AND gk_item.stateid<>4 AND gk_item.typeid<>2
+                    AND gk_item.stateid !=5
+            WHERE cache_logs.deleted=0 AND `cache_logs`.`type`=1
+                AND cache_logs.id IN (" .implode(',', $log_ids). ")
+                AND `cache_logs`.`user_id`= ?
+            GROUP BY cache_logs.id ORDER BY cache_logs.date_created DESC", $user_id);
 
+        $file_content = '';
+        while( $log_record = XDb::xFetchArray($rs)){
+            if ( !(($log_record['log_type'] == 12) && (!$usr['admin'])) ) { //ten warunek ukryje logi typu "komentarz COG" przed zwykłymi userami, natomiast adminom wyświetli wszystkie logi
                 $file_content .= '<tr>';
                 $file_content .= '<td style="width: 70px;">' . htmlspecialchars(date($dateFormat, strtotime($log_record['log_date'])), ENT_COMPAT, 'UTF-8') . '</td>';
 
@@ -195,7 +192,7 @@ if ($error == false) {
                 }
 
                 //$rating_picture
-                if ($log_record['recommended'] == 1) {
+                if ($log_record['recommended'] == 1 && $log_record['log_type'] == 1) {
                     $file_content .= '<td width="22"><img src="images/rating-star.png" border="0" alt=""/></td>';
                 } else {
                     $file_content .= '<td width="22">&nbsp;</td>';
@@ -219,8 +216,9 @@ if ($error == false) {
                 }
                 $file_content .=$data;
                 $file_content .= '\', PADDING,5, WIDTH,280,SHADOW,true)" onmouseout="UnTip()">' . htmlspecialchars($log_record['cache_name'], ENT_COMPAT, 'UTF-8') . '</a></b></td>';
+                $file_content .= '<td>&nbsp;</td>';
                 $file_content .= "</tr>";
-            }
+            } // koniec ifa ukrywajacego log o id 12 (komentarz COG)
         }
 
         $pages = mb_ereg_replace('{last_img}', $last_img, $pages);
@@ -235,4 +233,3 @@ if ($error == false) {
 }
 //make the template and send it out
 tpl_BuildTemplate();
-?>
