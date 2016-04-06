3e1398efd4cbb38fd0839d36d49c8867dfd1edf1
kojoty <p.juzwiak@gmail.com>
2016-04-07 00:35:18 +0200

    mysql_* remove: part94

--- graphs/new-caches-oc.php
+++ graphs/new-caches-oc.php
@@ -1,35 +1,34 @@
 <?php
 
+use Utils\Database\XDb;
 $rootpath = '../';
 require('../lib/common.inc.php');
 global $lang;
 
-//  setlocale(LC_TIME, 'pl_PL.UTF-8');
 setlocale(LC_TIME, 'pl_PL.utf-8');
-// setlocale(LC_TIME, 'en_EN.UTF-8');
+
 require('../lib/jpgraph/src/jpgraph.php');
 require('../lib/jpgraph/src/jpgraph_line.php');
 require('../lib/jpgraph/src/jpgraph_date.php');
 
-// for productive use 400 350
 $graph = new Graph(500, 400, "auto", 60 * 24);
 
-// for development
-//$graph = new Graph(500, 500);
-// Start von Opencaching
 $startDate = mktime(0, 0, 0, 5, 1, 2006);
 
+$rsCaches = XDb::xSql(
+    'SELECT COUNT(*) `count`, DAY(`date_created`) `day`, MONTH(`date_created`) `month`, YEAR(`date_created`) `year` FROM `caches`
+    WHERE caches.status=1
+    GROUP BY DAY(`date_created`), MONTH(`date_created`), YEAR(`date_created`)
+    ORDER BY YEAR(`date_created`) ASC, MONTH(`date_created`) ASC, DAY(`date_created`) ASC');
 
+$rsLogs = XDb::xSql(
+    'SELECT COUNT(*) `count`, DAY(`date_created`) `day`, MONTH(`date_created`) `month`, YEAR(`date_created`) `year` FROM `cache_logs`
+    WHERE (`type`=1 OR `type`=7) AND `deleted`=0
+    GROUP BY DAY(`date_created`), MONTH(`date_created`), YEAR(`date_created`)
+    ORDER BY YEAR(`date_created`) ASC, MONTH(`date_created`) ASC, DAY(`date_created`) ASC');
 
-//
-// Daten abfragen
-//
-$rsCaches = sql('SELECT COUNT(*) `count`, DAY(`date_created`) `day`, MONTH(`date_created`) `month`, YEAR(`date_created`) `year` FROM `caches` WHERE caches.status=1 GROUP BY DAY(`date_created`), MONTH(`date_created`), YEAR(`date_created`) ORDER BY YEAR(`date_created`) ASC, MONTH(`date_created`) ASC, DAY(`date_created`) ASC');
-
-$rsLogs = sql('SELECT COUNT(*) `count`, DAY(`date_created`) `day`, MONTH(`date_created`) `month`, YEAR(`date_created`) `year` FROM `cache_logs` WHERE (`type`=1 OR `type`=7) AND `deleted`=0 GROUP BY DAY(`date_created`), MONTH(`date_created`), YEAR(`date_created`) ORDER BY YEAR(`date_created`) ASC, MONTH(`date_created`) ASC, DAY(`date_created`) ASC');
-
-$rCaches = mysql_fetch_array($rsCaches);
-$rLogs = mysql_fetch_array($rsLogs);
+$rCaches = XDb::xFetchArray($rsCaches);
+$rLogs = XDb::xFetchArray($rsLogs);
 
 $cachesCount = 0;
 $logsCount = 0;
@@ -40,18 +39,17 @@ $xDate = array();
 $tickPositions = array();
 $tickLabels = array();
 
-// alle tage von 01-08-2005 bis heute durchgehen
 $days = 0;
 $date = $startDate;
 while ($date < time()) {
     while (($rCaches !== false) && (strtotime($rCaches['year'] . '-' . $rCaches['month'] . '-' . $rCaches['day']) < $date))
-        $rCaches = mysql_fetch_array($rsCaches);
+        $rCaches = XDb::xFetchArray($rsCaches);
 
     if ($rCaches['year'] . '-' . $rCaches['month'] . '-' . $rCaches['day'] == date('Y-n-j', $date))
         $cachesCount += $rCaches['count'];
 
     while (($rLogs !== false) && (strtotime($rLogs['year'] . '-' . $rLogs['month'] . '-' . $rLogs['day']) < $date))
-        $rLogs = mysql_fetch_array($rsLogs);
+        $rLogs = XDb::xFetchArray($rsLogs);
 
     if ($rLogs['year'] . '-' . $rLogs['month'] . '-' . $rLogs['day'] == date('Y-n-j', $date))
         $logsCount += $rLogs['count'];
@@ -68,8 +66,8 @@ while ($date < time()) {
     $days++;
     $date = mktime(0, 0, 0, 5, 1 + $days, 2006);
 }
-mysql_free_result($rsCaches);
-mysql_free_result($rsLogs);
+XDb::xFreeResults($rsCaches);
+XDb::xFreeResults($rsLogs);
 
 
 //
@@ -112,12 +110,7 @@ $graph->y2axis->scale->SetGrace(10, 0);
 $graph->y2axis->SetColor("darkgreen");
 
 
-//
-// Linien hinzufuegen
-//
-
 $lineCaches = new LinePlot($yDataCaches, $xDate);
-//  $lineCaches->SetFillColor("orange");
 $lineCaches->SetLegend(tr("graph_statistics_02"));
 $lineCaches->SetColor("blue");
 $lineCaches->SetStyle("solid");
@@ -125,7 +118,6 @@ $lineCaches->SetWeight(3);
 $lineCaches->SetStepStyle();
 $graph->Add($lineCaches);
 
-//  $foundEntries = sqlValue(sql("SELECT COUNT(*) FROM `cache_logs` WHERE `type`=1 AND `deleted`=0"), 0);
 $lineFound = new LinePlot($yDataLogs, $xDate);
 $lineFound->SetLegend(tr("graph_statistics_03"));
 $lineFound->SetColor("darkgreen");
@@ -144,25 +136,25 @@ $txtStat1->SetFont(FF_ARIAL, FS_NORMAL, 10);
 
 $lineHeight = $txtStat1->GetFontHeight($graph->img);
 
-$hiddenCaches = sqlValue("SELECT COUNT(*) FROM `caches` WHERE (`status`=1 OR `status`=2 OR `status`=3)", 0);
+$hiddenCaches = XDb::xSimpleQueryValue(
+    "SELECT COUNT(*) FROM `caches` WHERE (`status`=1 OR `status`=2 OR `status`=3)", 0);
+
 $txtStat2 = new Text(tr('graph_statistics_05') . str_replace(',', '.', number_format($hiddenCaches)));
 $txtStat2->SetPos(55, 55 + $lineHeight * 1.5);
 $txtStat2->SetFont(FF_ARIAL, FS_NORMAL, 8);
 $txtStat2->SetColor('blue');
 
-$activeCaches = sqlValue("SELECT COUNT(*) FROM `caches` WHERE `status`=1", 0);
+$activeCaches = XDb::xSimpleQueryValue(
+    "SELECT COUNT(*) FROM `caches` WHERE `status`=1", 0);
+
 $txtStat3 = new Text(tr('graph_statistics_06') . str_replace(',', '.', number_format($activeCaches)));
 $txtStat3->SetPos(55, 55 + $lineHeight * 2.5);
 $txtStat3->SetFont(FF_ARIAL, FS_NORMAL, 8);
 $txtStat3->SetColor('blue');
 
-//  $logEntries = sqlValue(sql("SELECT COUNT(*) FROM `cache_logs` WHERE `deleted`=0"), 0);
-//  $txtStat4 = new Text('Wpisy do log�w: ' . str_replace(',', '.', number_format($logEntries)));
-//  $txtStat4->SetPos(55, 55 + $lineHeight * 4.0);
-//  $txtStat4->SetFont(FF_GEORGIA, FS_NORMAL, 8);
-//  $txtStat4->SetColor('darkgreen');
+$foundEntries = XDb::xMultiVariableQueryValue(
+    "SELECT COUNT(*) FROM `cache_logs` WHERE `type`=1 AND `deleted`=0", 0);
 
-$foundEntries = sqlValue("SELECT COUNT(*) FROM `cache_logs` WHERE `type`=1 AND `deleted`=0", 0);
 $txtStat5 = new Text(tr('graph_statistics_07') . str_replace(',', '.', number_format($foundEntries)));
 $txtStat5->SetPos(55, 55 + $lineHeight * 5.0);
 $txtStat5->SetFont(FF_ARIAL, FS_NORMAL, 8);
@@ -171,7 +163,6 @@ $txtStat5->SetColor('darkgreen');
 $graph->AddText($txtStat1);
 $graph->AddText($txtStat2);
 $graph->AddText($txtStat3);
-//  $graph->AddText($txtStat4);
 $graph->AddText($txtStat5);
 
 
@@ -179,4 +170,3 @@ $graph->AddText($txtStat5);
 // Display the graph
 //
 $graph->Stroke();
-?>
