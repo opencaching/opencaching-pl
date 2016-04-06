32b6fb1416e56d5d737c75edcf3e151fd556b307
mzylowski <zylowskimichal@gmail.com>
2016-04-06 22:40:34 +0200

    Throw away obsolete table style in registration confirmation page, Better translations

--- tpl/stdstyle/register_confirm.tpl.php
+++ tpl/stdstyle/register_confirm.tpl.php
@@ -1,41 +1,27 @@
 <?php
 
 ?>
-<table class="content">
-    <colgroup>
-        <col width="150">
-        <col>
-    </colgroup>
-    <tr>
-        <td class="content2-pagetitle" colspan="2">
-            <img src="tpl/stdstyle/images/blue/profile.png" border="0" align="middle" width="32" height="32" alt="" title="" align="middle" /><font size="4"> <b>{{register_msg5}}</b></font>
-        </td>
-    </tr>
-    <tr><td class="spacer" colspan="2"></td></tr>
-
-    <tr>
-        <td colspan="2" class="help">
-            <div class="notice">
-                {{register_msg6}}
-            </div>
-        </td>
-    </tr>
-    <tr><td class="spacer" colspan="2"></td></tr>
-
-    <tr>
-        <td><img src="tpl/stdstyle/images/free_icons/user.png" class="icon16" alt="" title="" align="middle" />&nbsp;{{user}}:</td>
-        <td>{username}</td>
-    </tr>
-
-    <tr>
-        <td><img src="tpl/stdstyle/images/free_icons/email.png" class="icon16" alt="" title="" align="middle" />&nbsp;{{email_address}}:</td>
-        <td><b>{email}</b></td>
-    </tr>
-
-    <tr>
-        <td><img src="tpl/stdstyle/images/free_icons/world.png" class="icon16" alt="" title="" align="middle" />&nbsp;{{country_label}}:</td>
-        <td>{country}</td>
-    </tr>
-    <tr><td class="spacer" colspan="2"></td></tr>
-
-</table>
+<div class="content2-pagetitle"><img src="tpl/stdstyle/images/blue/profile.png" alt="" title="{{register_msg5}}" class="icon32"/>&nbsp;{{register_msg5}}</div>
+<div class="notice buffer">
+    {{register_msg6}}
+</div>
+<br />
+<div>
+    <div class="spacer">
+        <img src="tpl/stdstyle/images/free_icons/user.png" class="icon16" alt="" title="" align="middle" />&nbsp;
+        <label>{{user}}:</label>&nbsp;
+        {username}
+    </div>
+    <br />
+    <div class="spacer">
+        <img src="tpl/stdstyle/images/free_icons/email.png" class="icon16" alt="" title="" align="middle" />&nbsp;
+        <label>{{email_address}}:</label>&nbsp;
+        {email}
+    </div>
+    <br />
+    <div class="spacer">
+        <img src="tpl/stdstyle/images/free_icons/world.png" class="icon16" alt="" title="" align="middle" />&nbsp;
+        <label>{{country_label}}:</label>&nbsp;
+        {country}<br />
+    </div>
+</div>
