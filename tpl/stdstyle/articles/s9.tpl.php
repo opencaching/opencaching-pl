9ec46800f7a0151f1d98b957a0b5aa004ea11b69
kojoty <p.juzwiak@gmail.com>
2016-04-07 06:17:37 +0200

    mysql_* remove: part108

--- tpl/stdstyle/articles/s9.tpl.php
+++ tpl/stdstyle/articles/s9.tpl.php
@@ -1,3 +1,6 @@
+<?php
+use Utils\Database\XDb;
+?>
 <table class="content" width="97%">
     <tr><td class="content2-pagetitle"><img src="tpl/stdstyle/images/blue/stat1.png" class="icon32" alt="{{stats}}" title="{{stats}}" align="middle" /><font size="4">  <b>{{statistics}}</b></font></td></tr>
     <tr><td class="spacer"></td></tr>
@@ -23,16 +26,31 @@ if ($debug_page)
             require_once($rootpath . 'lib/common.inc.php');
             if (isset($_REQUEST['region']))
                 $region = $_REQUEST['region'];
+
             echo '<table width="97%"><tr><td align="center"><center><b> ' . tr('ranking_by_number_of_created_caches') . ' </b><br />tylko aktywne skrzynki<br />';
-            $woj = sqlValue("SELECT nuts_codes.name FROM nuts_codes WHERE code='$region'", 0);
+
+            $woj = XDb::xMultiVariableQueryValue(
+                "SELECT nuts_codes.name FROM nuts_codes WHERE code= :1 ", 0, $region);
+
             echo '<br /><b><font color="blue">' . $woj . '</font></b></center></td></tr></table>';
             echo '<table border="1" bgcolor="white" width="97%">' . "\n";
-            $wyniki = sql("SELECT count(*) count, user.username nick,caches.user_id userid FROM caches JOIN user USING(user_id) JOIN cache_location USING (cache_id) WHERE caches.status=1 AND caches.type<>6 AND cache_location.code3='$region' GROUP BY caches.user_id ORDER by count DESC, user.username ASC");
+
+            $wyniki = XDb::xSql(
+                "SELECT count(*) count, user.username nick,caches.user_id userid
+                FROM caches
+                    JOIN user USING(user_id)
+                    JOIN cache_location USING (cache_id)
+                WHERE caches.status=1 AND caches.type<>6
+                    AND cache_location.code3= ?
+                GROUP BY caches.user_id
+                ORDER by count DESC, user.username ASC", $region);
+
             $licznik = 0;
             $wartosc = 0;
             echo '<tr class="bgcolor2"><td align="right">&nbsp;&nbsp;<b>' . tr('ranking') . '</b>&nbsp;&nbsp;</td><td align="center">&nbsp;&nbsp;<b>' . tr('number_of_caches') . '</b>&nbsp;&nbsp;</td><td align="center">&nbsp;&nbsp;<b>' . tr('username') . '</b>&nbsp;&nbsp;</td></tr>';
             echo '<tr><td height="2"></td></tr>';
-            while ($wynik = sql_fetch_assoc($wyniki)) {
+
+            while ( $wynik = XDb::xFetchArray($wyniki) ) {
                 if ($wartosc == 0) {
                     $wartosc = $wynik['count'];
                     $licznik = 1;
