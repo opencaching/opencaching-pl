e97cfb85834ba9bc4b9ce91bfa4c78c0ad997175
kojoty <p.juzwiak@gmail.com>
2016-04-07 06:08:43 +0200

    mysql_* remove: part105

--- tpl/stdstyle/articles/s10.tpl.php
+++ tpl/stdstyle/articles/s10.tpl.php
@@ -1,3 +1,6 @@
+<?php
+use Utils\Database\XDb;
+?>
 <table class="content" width="97%">
     <tr><td class="content2-pagetitle"><img src="tpl/stdstyle/images/blue/stat1.png" class="icon32" alt="{{stats}}" title="{{stats}}" align="middle" /><font size="4">  <b>{{statistics}}</b></font></td></tr>
     <tr><td class="spacer"></td></tr>
@@ -28,40 +31,37 @@ if ($debug_page)
             if (isset($_REQUEST['region'])) {
                 $region = $_REQUEST['region'];
             }
-//  $region="PL61";
-            $woj = sqlValue("SELECT nuts_codes.name FROM nuts_codes WHERE code='$region'", 0);
+            $woj = XDb::xMultiVariableQueryValue(
+                "SELECT nuts_codes.name FROM nuts_codes WHERE code= :1 ", 0, $region);
 
-//$rsU = sql('SELECT COUNT(*) `count` FROM (SELECT COUNT(cache_logs.user_id) FROM `cache_logs` WHERE `type`=1 AND `deleted`=0 GROUP BY `user_id`) `users_with_founds`');
-//$fC = sql('SELECT COUNT(*) `count` FROM `cache_logs` WHERE `deleted`=0 AND (`type`=1 OR `type`=2)');
-            // $rsUs = mysql_fetch_array($rsU);
-            //  $fCt = mysql_fetch_array($fC);
-
-            echo '<center><table width="97%" border="0"><tr><td align="center"><center><b>' . tr(Stats_s10_01) . '  <b>';
-//  echo $rsUs[count];
+            echo '<center><table width="97%" border="0"><tr><td align="center"><center><b>' . tr('Stats_s10_01') . '  <b>';
             echo '<br /><br /><b><font color="blue">';
             echo $woj;
             echo '</font></b></center></td></tr></table>';
 
             echo '<table border="1" bgcolor="white" width="97%" style="font-size:11px; line-height:1.6em;">' . "\n";
 
-            $a = "SELECT COUNT(*) count, username, stat_ban, user.user_id FROM cache_location, caches, cache_logs, user " .
-                    "WHERE (`cache_location`.`code3`='$region' AND `cache_location`.`cache_id`=`caches`.`cache_id`) AND `cache_logs`.`deleted`=0 AND cache_logs.user_id=user.user_id AND (cache_logs.type=1 OR cache_logs.type=2) AND cache_logs.cache_id = caches.cache_id " .
-                    "GROUP BY user.user_id " .
-                    "ORDER BY 1 DESC, user.username ASC";
-
             echo "<br />";
 
-            $r = mysql_query($a) or die(mysql_error());
+            $r = XDb::xSql(
+                "SELECT COUNT(*) count, username, stat_ban, user.user_id FROM cache_location, caches, cache_logs, user
+                WHERE (`cache_location`.`code3`= ? AND `cache_location`.`cache_id`=`caches`.`cache_id`)
+                    AND `cache_logs`.`deleted`=0 AND cache_logs.user_id=user.user_id
+                    AND (cache_logs.type=1 OR cache_logs.type=2)
+                    AND cache_logs.cache_id = caches.cache_id
+                GROUP BY user.user_id
+                ORDER BY 1 DESC, user.username ASC", $region);
+
             echo '<tr class="bgcolor2">' .
-            '<td align="center">&nbsp;&nbsp;<b>' . tr(Stats_s10_02) . '</b>&nbsp;&nbsp;</td>' .
-            '<td align="center"><b>' . tr(Stats_s10_03) . '</b></td>' .
-            '<td align="center">&nbsp;&nbsp;<b>' . tr(Stats_s10_04) . '</b>&nbsp;&nbsp;</td></tr><tr><td>';
+            '<td align="center">&nbsp;&nbsp;<b>' . tr('Stats_s10_02') . '</b>&nbsp;&nbsp;</td>' .
+            '<td align="center"><b>' . tr('Stats_s10_03') . '</b></td>' .
+            '<td align="center">&nbsp;&nbsp;<b>' . tr('Stats_s10_04') . '</b>&nbsp;&nbsp;</td></tr><tr><td>';
 
             $l2 = ""; // number of users within the same rank
             $rank = 0; // rank number; increamented by one for each group of users having the same caches discovered
             $position = 1; // position ex aequo; incremented by number of users in each rank
 
-            while ($line = mysql_fetch_array($r)) {
+            while( $line = XDb::xFetchArray($r) ) {
                 $color = "black";
                 $banned = "";
                 if ($usr['admin'] || $line['stat_ban'] == 0) {
@@ -88,7 +88,6 @@ if ($debug_page)
             }
 
 // end table
-//echo "</td></tr>";
             echo "</table>\n";
             ?>
         </td></tr>
