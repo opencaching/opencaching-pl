67b008a680cf939b3729703f021e595714462c3a
kojoty <kojoty@users.noreply.github.com>
2016-04-14 22:35:35 +0200

    sql fix

--- sqlAlters/2016-04-14_fix_watcher_column.sql
+++ sqlAlters/2016-04-14_fix_watcher_column.sql
@@ -4,7 +4,7 @@
 --
 
 -- SET watcher field default
-ALTER TABLE caches CHANGE watcherwatcher INT(11) NULL DEFAULT '0';
+ALTER TABLE caches CHANGE watcher INT(11) NULL DEFAULT '0';
 
 -- fix all caches with watcher = null
 UPDATE `caches` SET `watcher` = 0 WHERE `watcher` IS NULL;
