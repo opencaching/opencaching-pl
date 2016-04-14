b0cd8564be1b925839d112b553a5d48e2e208f06
kojoty <kojoty@users.noreply.github.com>
2016-04-14 12:31:28 +0200

    restore PLConvert

--- lib/search.gpxgc.inc.php
+++ lib/search.gpxgc.inc.php
@@ -825,3 +825,20 @@ function append_output($str)
                 else
                         echo $str;
         }
+
+function PLConvert($source,$dest,$tekst)
+{
+    $source=strtoupper($source);
+    $dest=strtoupper($dest);
+    if($source==$dest) return $tekst;
+    $chars['POLSKAWY']    =array('a','c','e','l','n','o','s','z','z','A','C','E','L','N','O','S','Z','Z');
+    $chars['ISO-8859-2']  =array("\xB1","\xE6","\xEA","\xB3","\xF1","\xF3","\xB6","\xBC","\xBF","\xA1","\xC6","\xCA","\xA3","\xD1","\xD3","\xA6","\xAC","\xAF");
+    $chars['WINDOWS-1250']=array("\xB9","\xE6","\xEA","\xB3","\xF1","\xF3","\x9C","\x9F","\xBF","\xA5","\xC6","\xCA","\xA3","\xD1","\xD3","\x8C","\x8F","\xAF");
+    $chars['UTF-8']       =array('ą','ć','ę','ł','ń','ó','ś','ź','ż','Ą','Ć','Ę','Ł','Ń','Ó','Ś','Ź','Ż');
+    $chars['ENTITIES']    =array('ą','ć','ę','ł','ń','ó','ś','ź','ż','Ą','Ć','Ę','Ł','Ń','Ó','Ś','Ź','Ż');
+    if(!isset($chars[$source])) return false;
+    if(!isset($chars[$dest])) return false;
+        $tekst = str_replace('a', 'a', $tekst);
+        $tekst = str_replace('é', 'e', $tekst);
+    return str_replace($chars[$source],$chars[$dest],$tekst);
+}
