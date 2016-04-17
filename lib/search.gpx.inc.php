8d35ccd429dd865be413ed77b0da8c86bf0c7d48
kojoty <p.juzwiak@gmail.com>
2016-04-17 05:05:48 +0200

    PLConvert restore

--- lib/search.gpx.inc.php
+++ lib/search.gpx.inc.php
@@ -775,3 +775,44 @@ function append_output($str)
     else
         echo $str;
 }
+
+
+/*
+ Funkcja do konwersji polskich znakow miedzy roznymi systemami kodowania.
+ Zwraca skonwertowany tekst.
+
+ Argumenty:
+ $source - string - źródłowe kodowanie
+ $dest - string - źródłowe kodowanie
+ $tekst - string - tekst do konwersji
+
+ Obsługiwane formaty kodowania to:
+ POLSKAWY (powoduje zamianę polskich liter na ich łacińskie odpowiedniki)
+ ISO-8859-2
+ WINDOWS-1250
+ UTF-8
+ ENTITIES (zamiana polskich znaków na encje html)
+
+ Przyklad:
+ echo(PlConvert('UTF-8','ISO-8859-2','Zażółć gęślą jaźń.'));
+ */
+function PLConvert($source,$dest,$tekst)
+{
+    $source=strtoupper($source);
+    $dest=strtoupper($dest);
+    if($source==$dest) return $tekst;
+
+    $chars['POLSKAWY']    =array('a','c','e','l','n','o','s','z','z','A','C','E','L','N','O','S','Z','Z');
+    $chars['ISO-8859-2']  =array("\xB1","\xE6","\xEA","\xB3","\xF1","\xF3","\xB6","\xBC","\xBF","\xA1","\xC6","\xCA","\xA3","\xD1","\xD3","\xA6","\xAC","\xAF");
+    $chars['WINDOWS-1250']=array("\xB9","\xE6","\xEA","\xB3","\xF1","\xF3","\x9C","\x9F","\xBF","\xA5","\xC6","\xCA","\xA3","\xD1","\xD3","\x8C","\x8F","\xAF");
+    $chars['UTF-8']       =array('ą','ć','ę','ł','ń','ó','ś','ź','ż','Ą','Ć','Ę','Ł','Ń','Ó','Ś','Ź','Ż');
+    $chars['ENTITIES']    =array('ą','ć','ę','ł','ń','ó','ś','ź','ż','Ą','Ć','Ę','Ł','Ń','Ó','Ś','Ź','Ż');
+
+    if(!isset($chars[$source])) return false;
+    if(!isset($chars[$dest])) return false;
+
+    $tekst = str_replace('a', 'a', $tekst);
+    $tekst = str_replace('é', 'e', $tekst);
+
+    return str_replace($chars[$source],$chars[$dest],$tekst);
+}
