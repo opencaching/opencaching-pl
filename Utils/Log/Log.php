fcb8b5d8862df243f43822598878f916a965b50e
kojoty <p.juzwiak@gmail.com>
2016-04-21 21:25:03 +0200

    Log.php rename to proper value

--- Utils/Log/Log.php
+++ Utils/Log/Log.php
@@ -3,14 +3,19 @@
  * This is only a stub for util class used to log debug info...
  * TBD.
  */
+
 namespace Utils\Log;
+
 use Utils\Database\XDb;
+
 class Log
 {
     public static function logentry( $module, $eventid, $userid, $objectid1, $objectid2, $logtext, $details ){
+
         XDb::xSql(
             "INSERT INTO logentries (`module`, `eventid`, `userid`, `objectid1`, `objectid2`, `logtext`, `details`, `logtime`)
         VALUES ( ?, ?, ?, ?, ?, ?, ?, NOW())",
             $module, $eventid, $userid, $objectid1, $objectid2, $logtext, serialize($details));
     }
+
 }
