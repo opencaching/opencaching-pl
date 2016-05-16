427e70a9c65d9d967cad704f1ab1240ff02d1720
Michał Żyłowski <zylowskimichal@gmail.com>
2016-05-16 08:14:40 +0200

    quickfix for sql error when not logedin user want to to see geoPath page

--- powerTrail/powerTrailController.php
+++ powerTrail/powerTrailController.php
@@ -133,13 +133,13 @@ class powerTrailController {
             $cacheCountLimit = powerTrailBase::minimumCacheCount();
         }
         $userid = $this->user['userid'];
-        if(isset($_REQUEST['myPowerTrailsBool'])) {
+        if(isset($_REQUEST['myPowerTrailsBool']) && isset($userid)) {
             $myTrailsCondition = "and `id` NOT IN (SELECT `PowerTrailId` FROM `PowerTrail_owners`
             WHERE `userId` = $userid)";
         } else {
             $myTrailsCondition = "";
         }
-        if(isset($_REQUEST['gainedPowerTrailsBool'])) {
+        if(isset($_REQUEST['gainedPowerTrailsBool']) && isset($userid)) {
             $gainedTrailsCondition = "and `id` NOT IN (SELECT `PowerTrailId` FROM `PowerTrail_comments`
             WHERE `userId` = $userid and `commentType` = 2)";
         } else {
