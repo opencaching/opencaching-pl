dfa26dd8c6895bf5b7b8d67b16abb2233c55588e
wloczynutka <wloczynutka@gmail.com>
2016-06-06 14:26:37 +0200

    Replace GM elevation API with http://www.datasciencetoolkit.org/

--- lib/Objects/GeoCache/Altitude.php
+++ lib/Objects/GeoCache/Altitude.php
@@ -40,10 +40,31 @@ class Altitude
     }
 
     /**
+     * Retreive altitude from DataScienceToolkit Api, set $this->altitude and store it in db.
+     */
+    private function retreiveAltitudeFromDataScienceToolkit()
+    {
+        $latitude = $this->geoCache->getCoordinates()->getLatitude();
+        $longitude = $this->geoCache->getCoordinates()->getLongitude();
+        $dstElevationApiUrl = "http://www.datasciencetoolkit.org/coordinates2statistics/$latitude,$longitude?statistics=elevation";
+        $response = @json_decode(file_get_contents($dstElevationApiUrl));
+        $statisticObj = $response[0];
+        if(is_object($statisticObj) && isset($statisticObj->statistics->elevation->value)){
+            $altitude = $statisticObj->statistics->elevation->value;
+            $this->altitude = $altitude;
+            $this->storeAlitudeInDb();
+        }
+    }
+
+    /**
+     * Disabled / depreciated This method were replaced by function retreiveAltitudeFromDataScienceToolkit()
+     * Left temporary if swith back neccessary.
+     *
      * Retreive altitude from google Api, set $this->altitude and store it in db.
      */
     private function retreiveAltitudeFromGoogleApi()
     {
+        d('Depreciated, please use lib\Objects\GeoCache\Altitude::retreiveAltitudeFromDataScienceToolkit() instead. ');
         $googleElevationApiUrl = 'http://maps.googleapis.com/maps/api/elevation/xml?locations=';
         $latitude = number_format($this->geoCache->getCoordinates()->getLatitude(), 7, '.', '');
         $longitude = number_format($this->geoCache->getCoordinates()->getLongitude(), 7, '.', '');
@@ -83,7 +104,7 @@ class Altitude
      */
     public function pickAndStoreAltitude($userInputAltitude)
     {
-        $this->retreiveAltitudeFromGoogleApi();
+        $this->retreiveAltitudeFromDataScienceToolkit();
         if ( !is_null($userInputAltitude) &&
              $userInputAltitude < $this->altitude+50 &&
              $userInputAltitude > $this->altitude-50
--- test.php
+++ test.php
@@ -1,5 +1,13 @@
 <?php
 require_once 'lib/common.inc.php';
+
+
+$geoCache = new \lib\Objects\GeoCache\GeoCache(['cacheId' => (int) $_GET['cid']]);
+d($geoCache->getAltitude());
+$geoCache->getAltitude()->pickAndStoreAltitude(null);
+
+ddd($geoCache->getAltitude());
+
 echo "TEST:<hr/>";
 
 echo "<hr/>END!";
