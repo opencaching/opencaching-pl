a0e748cc4018b324c1edf36235e8484b0a230e57
kojoty <p.juzwiak@gmail.com>
2016-09-12 22:29:14 +0200

    fix log warning: Invalid arg... foreach() in write_newblogs.inc.php

--- tpl/stdstyle/etc/write_newblogs.inc.php
+++ tpl/stdstyle/etc/write_newblogs.inc.php
@@ -1,14 +1,6 @@
 <?php
-
-/* * *************************************************************************
- *
- *   This program is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation; either version 2 of the License, or
- *   (at your option) any later version.
- *
- * ************************************************************************* */
 /* this file to be run witch CRON. generate last blog entery list on main page */
+
 setlocale(LC_TIME, 'pl_PL.UTF-8');
 
 global $lang, $rootpath, $config;
@@ -26,13 +18,15 @@ $rss->load($config['blogMostRecentRecordsUrl']);
 $items = $rss->getItems();
 $html = '<ul style="font-size: 11px;">';
 $n = 0;
-foreach ($items as $index => $item) {
-    $pubDate = $item['pubDate'];
-    $pubDate = strftime("%d-%m-%Y", strtotime($pubDate));
-    $html .= '<li class="newcache_list_multi" style="margin-bottom:8px;"><img src="/tpl/stdstyle/images/free_icons/page.png" class="icon16" alt="news" title="news" /> ' . $pubDate . ' <a class=links href="' . $item['link'] . '" title="' . $item['title'] . '"><strong>' . $item['title'] . '</strong></a></li>';
-    $n = $n + 1;
-    if ($n == 5) {
-        break;
+if(is_array($items)){
+    foreach ($items as $index => $item) {
+        $pubDate = $item['pubDate'];
+        $pubDate = strftime("%d-%m-%Y", strtotime($pubDate));
+        $html .= '<li class="newcache_list_multi" style="margin-bottom:8px;"><img src="/tpl/stdstyle/images/free_icons/page.png" class="icon16" alt="news" title="news" /> ' . $pubDate . ' <a class=links href="' . $item['link'] . '" title="' . $item['title'] . '"><strong>' . $item['title'] . '</strong></a></li>';
+        $n = $n + 1;
+        if ($n == 5) {
+            break;
+        }
     }
 }
 $html.="</ul>";
