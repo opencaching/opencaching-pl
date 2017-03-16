36dc96f1e995fd26c67207537ebbaedbe0cc0483
Michał Żyłowski <zylowskimichal@gmail.com>
2017-03-16 18:44:06 +0100

    Refactor mail with notify to OC-Team from report cache page
    Signed-off-by: Michał Żyłowski <zylowskimichal@gmail.com>

--- reportcache.php
+++ reportcache.php
@@ -1,6 +1,10 @@
 <?php
 
+use lib\Objects\ApplicationContainer;
+use lib\Objects\GeoCache\GeoCache;
 use Utils\Database\XDb;
+use Utils\Email\EmailSender;
+
 function reason($reason)
 {
     switch ($reason) {
@@ -57,35 +61,11 @@ if ($usr == true) {
                             VALUES ( ? , ? , ? , ? )",
                             $usr['userid'], $cacheid, strip_tags($_POST['text']), $_POST['reason']);
 
-                        // wysłanie powiadomień
-                        $email_content = file_get_contents($stylepath . '/email/newreport_octeam.email');
-
-                        $email_content = mb_ereg_replace('{server}', $absolute_server_URI, $email_content);
-                        $email_content = mb_ereg_replace('{reportcache10}', tr('reportcache10'), $email_content);
-                        $email_content = mb_ereg_replace('{reportcache11}', tr('reportcache11'), $email_content);
-                        $email_content = mb_ereg_replace('{reportcache12}', tr('reportcache12'), $email_content);
-                        $email_content = mb_ereg_replace('{reportcache13}', tr('reportcache13'), $email_content);
-                        $email_content = mb_ereg_replace('{reportcache14}', tr('reportcache14'), $email_content);
-                        $email_content = mb_ereg_replace('{reportcache15}', tr('reportcache15'), $email_content);
-                        $email_content = mb_ereg_replace('{reportcache16}', tr('reportcache16'), $email_content);
-                        $email_content = mb_ereg_replace('{reportcache17}', tr('reportcache17'), $email_content);
-                        $email_content = mb_ereg_replace('{reportcache18}', tr('reportcache18'), $email_content);
-                        $email_content = mb_ereg_replace('{reportcache19}', tr('reportcache19'), $email_content);
-                        $email_content = mb_ereg_replace('{date}', date($datetimeFormat), $email_content);
-                        $email_content = mb_ereg_replace('{submitter}', $usr['username'], $email_content);
-                        $email_content = mb_ereg_replace('{cachename}', $cache['name'], $email_content);
-                        $email_content = mb_ereg_replace('{cache_wp}', $cache['wp_oc'], $email_content);
-                        $email_content = mb_ereg_replace('{cacheid}', $cacheid, $email_content);
-                        $email_content = mb_ereg_replace('{reason}', reason($_POST['reason']), $email_content);
-                        $email_content = mb_ereg_replace('{text}', strip_tags(addslashes($_POST['text'])), $email_content);
-                        // send email to RR
-
-                        $emailheaders = "Content-Type: text/plain; charset=utf-8\r\n";
-                        $emailheaders .= "From: " . $site_name . " <" . $cache_reporter['email'] . ">\r\n";
-                        $emailheaders .= "Reply-To: " . $site_name . " <" . $cache_reporter['email'] . ">";
-
-                        mb_send_mail($octeam_email, tr('reportcache07') . " (" . $cache['wp_oc'] . ")", $email_content, $emailheaders);
-                        // echo($octeam_email. tr('reportcache07'). $email_content. $emailheaders);
+                        EmailSender::sendNotifyAboutNewReportToOcTeam($stylepath.'/email/oc_team_notify_new_report.email.html',
+                            date($datetimeFormat), ApplicationContainer::Instance()->getLoggedUser(),
+                            new GeoCache(array('cacheId' => $cacheid)),
+                            reason($_POST['reason']),
+                            nl2br($_POST['text']));
                     } else
                         $tplname = 'reportcache_sent_owner';
                     //get email address of cache owner
