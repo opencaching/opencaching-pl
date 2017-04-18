b85abb68e62d7ba597f71a90cf8cf74de53f72db
harrieklomp <harrie@harrieklomp.be>
2017-04-18 07:02:08 +0200

    First step to add new waypoint type "Trailhead" to system.

---
+++ sqlAlters/2017-04-18_add_waypoint_type_trailhead.sql
@@ -0,0 +1,5 @@
+-- 2017-04-18 Add waypoint_type "Trailhead" to DB
+-- @author: Harrie Klomp
+
+
+INSERT INTO `waypoint_type` (`id`, `pl`, `en`, `nl`, `de`, `ro`, `icon`) VALUES (NULL, 'Początek ścieżki', 'Trailhead', 'Begin wandelpad', 'Anfang wanderpfad', 'Început de traseu', 'images/waypoints/wp_trailhead.png') 
