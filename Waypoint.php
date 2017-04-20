8320f9b9d66283da3ea43d853ec182f05d1b0e4f
harrieklomp <harrie@harrieklomp.be>
2017-04-20 17:18:28 +0200

    Add extra waypoint "Trailhead" to code.

---
+++ Waypoint.php
@@ -0,0 +1,203 @@
+<?php
+
+namespace lib\Objects\GeoCache;
+
+use Utils\Database\XDb;
+use lib\Objects\Coordinates\Coordinates;
+
+/**
+ * Geocache Waypoint (place of interest)
+ * represented in db by waypoints table
+ *
+ * @author Łza
+ */
+class Waypoint
+{
+    const TYPE_PHYSICAL = 1;
+    const TYPE_VIRTUAL = 2;
+    const TYPE_FINAL = 3;
+    const TYPE_INTERESTING = 4;
+    const TYPE_PARKING = 5;
+
+    const STATUS_VISIBLE = 1;
+    const STATUS_VISIBLE_HIDDEN_COORDS = 2;
+    const STATUS_HIDDEN = 3;
+
+    const OPENCHECKER_ENABLED = 1;
+
+    private $id;
+    private $geocache;
+    private $type;
+
+    /**
+     * @var Coordinates $coordinates
+     */
+    private $coordinates;
+    private $description;
+    private $status;
+    private $stage;
+    private $cacheId;
+    private $openChecker;
+
+    private $iconNames = array(
+        self::TYPE_PHYSICAL => 'images/waypoints/wp_physical.png',
+        self::TYPE_VIRTUAL => 'images/waypoints/wp_virtual.png',
+        self::TYPE_FINAL => 'images/waypoints/wp_final.png',
+        self::TYPE_INTERESTING => 'images/waypoints/wp_reference.png',
+        self::TYPE_PARKING => 'images/waypoints/wp_parking.png',
+        self::TYPE_TRAILHEAD => 'images/waypoints/wp_trailhead.png'
+    );
+
+
+
+    private static function FromDbRow($row)
+    {
+        $waypoint = new Waypoint();
+        $waypoint->loadFromDbRow($row);
+        return $waypoint;
+    }
+
+    private function loadFromDbRow($row)
+    {
+        $this->id           = $row['wp_id'];
+        $this->cacheId      = $row['cache_id'];
+        $this->type         = $row['type'];
+        $this->coordinates  = Coordinates::FromCoordsFactory($row['latitude'], $row['longitude']);
+        $this->description  = $row['desc'];
+        $this->status       = $row['status'];
+        $this->stage        = $row['stage'];
+        $this->openChecker  = $row['opensprawdzacz'];
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     *
+     * @return Coordinates
+     */
+    public function getCoordinates()
+    {
+        return $this->coordinates;
+    }
+
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    public function getStage()
+    {
+        return $this->stage;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function setType($type)
+    {
+        $this->type = $type;
+        return $this;
+    }
+
+    public function setCoordinates($coordinates)
+    {
+        $this->coordinates = $coordinates;
+        return $this;
+    }
+
+    public function setDescription($description)
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    public function setStatus($status)
+    {
+        $this->status = $status;
+        return $this;
+    }
+
+    public function setStage($stage)
+    {
+        $this->stage = $stage;
+        return $this;
+    }
+
+    public function getGeocache()
+    {
+        return $this->geocache;
+    }
+
+    public function setGeocache($geocache)
+    {
+        $this->geocache = $geocache;
+        return $this;
+    }
+
+    public function getIconName()
+    {
+        return $this->iconNames[$this->type];
+    }
+
+
+    public function isHidden(){
+        return $this->status == self::STATUS_HIDDEN;
+    }
+
+    public function areCoordsHidden(){
+        return $this->status == self::STATUS_VISIBLE_HIDDEN_COORDS;
+    }
+
+    public function getTypeTranslationKey()
+    {
+        return 'wayPointType'.$this->getType();
+    }
+
+    public function getDesc4Html()
+    {
+        return nl2br($this->description);
+    }
+
+    public static function GetWaypointsForCacheId(GeoCache $geoCache, $skipHiddenWps=true){
+
+
+        if($geoCache->getCacheType() == GeoCache::TYPE_MOVING){
+            // mobiles can't have waypoints...
+            return [];
+        }
+
+        $s = XDb::xSql(
+            "SELECT wp_id, type, longitude, latitude, `desc`, status, stage, opensprawdzacz, cache_id,
+                    waypoint_type.en wp_type, waypoint_type.icon wp_icon
+            FROM waypoints INNER JOIN waypoint_type ON (waypoints.type = waypoint_type.id)
+            WHERE cache_id = ? ORDER BY stage, wp_id",
+            $geoCache->getCacheId());
+
+        $results = [];
+        while($row = XDb::xFetchArray($s)){
+            $wp = Waypoint::FromDbRow($row);
+            if( !$wp->isHidden() || !$skipHiddenWps ){
+                $results[] = $wp;
+            }
+        }
+
+        return $results;
+    }
+
+}
---
+++ caches.inc.php
@@ -0,0 +1,145 @@
+<?php
+
+use Utils\Database\XDb;
+
+$default_cachestatus_id = 1;
+
+$log_types = array();
+$cache_types = array();
+$wp_types = array();
+$cache_status = array();
+$cache_size = array();
+
+// Sachesize-ID selected by default
+$default_logtype_id = 1;
+
+// new: get *_types from database
+if (!isset($cachetype))
+    $cachetype = '';
+
+$log_types = get_log_types_from_database();
+$cache_types = get_cache_types_from_database();
+$wp_types = get_wp_types_from_database($cachetype);
+$cache_status = get_cache_status_from_database();
+$cache_size = get_cache_size_from_database();
+
+function get_log_types_from_database()
+{
+    $log_types = array();
+
+    $resp = XDb::xSql("SELECT * FROM log_types ORDER BY id ASC");
+    while ($row = XDb::xFetchArray($resp)) {
+        $log_types[] = $row;
+    }
+    return $log_types;
+}
+
+function get_cache_types_from_database()
+{
+    $cache_types = array();
+
+    $resp = XDb::xSql("SELECT * FROM cache_type ORDER BY sort ASC");
+    while ($row = XDb::xFetchArray($resp)) {
+        $cache_types[] = $row;
+    }
+    return $cache_types;
+}
+
+function get_wp_types_from_database($cachetype)
+{
+    $wp_types = array();
+    if ($cachetype == '2' || $cachetype == '4' || $cachetype == '5' || $cachetype == '6' || $cachetype == '9') {
+        $param = "id=-1 OR id=4 OR id=5 OR id=6";
+    } else {
+        $param = "id=-1 OR id=1 OR id=2 OR id=3 OR id=4 OR id=5 OR id=6";
+    }
+    $resp = XDb::xSql("SELECT * FROM waypoint_type WHERE $param ORDER BY id ASC");
+    while ($row = XDb::xFetchArray($resp)) {
+        $wp_types[] = $row;
+    }
+    return $wp_types;
+}
+
+function get_cache_status_from_database()
+{
+    $cache_status = array();
+
+    $resp = XDb::xSql("SELECT * FROM cache_status ORDER BY id ASC");
+    while ($row = XDb::xFetchArray($resp)) {
+        $cache_status[] = $row;
+    }
+    return $cache_status;
+}
+
+function get_cache_size_from_database()
+{
+    $cache_size = array();
+
+    $resp = XDb::xSql("SELECT * FROM cache_size ORDER BY id ASC");
+    while ($row = XDb::xFetchArray($resp)) {
+        $cache_size[] = $row;
+    }
+    return $cache_size;
+}
+
+function log_type_from_id($id, $lang)
+{
+    global $log_types;
+    if (checkField('log_types', $lang))
+        $lang_db = $lang;
+    else
+        $lang_db = "en";
+
+    foreach ($log_types AS $type) {
+        if ($type['id'] == $id) {
+            return $type[$lang];
+        }
+    }
+}
+
+function cache_type_from_id($id, $lang)
+{
+    global $cache_types;
+    if (checkField('cache_type', $lang))
+        $lang_db = $lang;
+    else
+        $lang_db = "en";
+
+    foreach ($cache_types AS $cache_type) {
+        if ($cache_type['id'] == $id) {
+            return $cache_type[$lang_db];
+        }
+    }
+}
+
+function cache_size_from_id($id, $lang)
+{
+    global $cache_size;
+    if (checkField('cache_size', $lang))
+        $lang_db = $lang;
+    else
+        $lang_db = "en";
+
+    foreach ($cache_size AS $size) {
+        if ($size['id'] == $id) {
+            return $size[$lang_db];
+        }
+    }
+}
+
+function cache_status_from_id($id, $lang)
+{
+    global $cache_status;
+    if (checkField('cache_status', $lang))
+        $lang_db = $lang;
+    else
+        $lang_db = "en";
+
+    foreach ($cache_status AS $status) {
+        if ($status['id'] == $id) {
+            return $status[$lang_db];
+        }
+    }
+}
+
+
