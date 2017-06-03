cd4520d454a25d79394e962a8180074a64ae3f94
Wojciech Rygielski <rygielski@mimuw.edu.pl>
2017-06-03 21:57:02 +0200

    search.js: Extract a separate GetSelectedCacheIDs function
    This commit doesn't change a thing. It's just a bit easier to read.
    I wanted to have this logic in a separate function, in order to be able
    to call it from OKAPI GPX formatter (which I plan to commit later on).

--- tpl/stdstyle/js/search.js
+++ tpl/stdstyle/js/search.js
@@ -25,6 +25,16 @@ function EventPageFunction(event)
     document.getElementById("pageNumber").innerHTML = event['page']+1;
 }
 
+function GetSelectedCacheIDs()
+{
+    var selectedIDs = [];
+    var selectedItems =  gct.getSelection();
+    for (var i = 0; i < selectedItems.length; i++) {
+       selectedIDs.push(gct.getValue(selectedItems[i].row, 0));
+    }
+    return selectedIDs;
+}
+
 function CacheExport( type )
 {
     var link = 'search.php?searchto=searchbylist&showresult=1&f_inactive=0&f_ignored=0&f_userfound=0&f_userowner=0&f_watched=0&count=max&output=';
@@ -34,20 +44,9 @@ function CacheExport( type )
     else
         link = link + type;
 
-    var item;
-    var CacheID = [];
-    var RecArray =  gct.getSelection();
+    var CacheID = GetSelectedCacheIDs();
 
-
-    for (var i = 0; i < RecArray.length; i++)
-    {
-       item = RecArray[i];
-
-       val = gct.getValue( item.row, 0 );
-       CacheID[ i ] = val;
-    }
-
-    if ( RecArray.length )
+    if ( CacheID.length > 0 )
     {
         link = link+"&SearchCacheID="+CacheID;
         window.location.href = link;
