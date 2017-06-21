f1feb3e0cd013d994c03f864fcf4fa08662e6f87
deg <deg@best.pl>
2017-06-21 20:41:40 +0200

    Fixes PHP Notice:  Undefined variable: no_found_date and small HTML fix (This also tests /post-commit.php over https on OC PL)

--- tpl/stdstyle/myignores.inc.php
+++ tpl/stdstyle/myignores.inc.php
@@ -6,9 +6,10 @@ $bgcolor1 = '#ffffff';
 $ignore = '<tr>
                     <td style="background-color: {bgcolor}"><a href="viewcache.php?cacheid={urlencode_cacheid}">{cachename}</a></td>
                     <td style="background-color: {bgcolor}">&nbsp;</td>
-                    <td nowrap="nowrap" style="background-color: {bgcolor}; text-align: center" ><a href="removeignore.php?cacheid={cacheid}&target=myignores.php" onclick="return confirm(\'' . tr("myignores_1") . '\');"><img style="vertical-align: middle;" src="tpl/stdstyle/images/log/16x16-trash.png" alt="" title=' . tr('off_ignore') . ' /> </a></td>
+                    <td nowrap="nowrap" style="background-color: {bgcolor}; text-align: center" ><a href="removeignore.php?cacheid={cacheid}&target=myignores.php" onclick="return confirm(\'' . tr("myignores_1") . '\');"><img style="vertical-align: middle;" src="tpl/stdstyle/images/log/16x16-trash.png" alt="" title="' . tr('off_ignore') . '" /> </a></td>
                 </tr>';
 
 $no_ignores = '<div class="notice">' . tr('no_ignores') . '</div>';
 $title_text = tr('ignored_caches');
+$no_found_date = '---';
 ?>
