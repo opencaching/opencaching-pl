8e040a41a1e1fcc015c57d5f19db3444f4eeaa6f
deg <deg@best.pl>
2017-07-05 23:27:13 +0200

    First cleaning of OCPL code - in context #1079

--- newcache.php
+++ newcache.php
@@ -699,7 +699,7 @@ if ($error == false) {
                 //add record to caches table
                 XDb::xSql(
                     "INSERT INTO `caches` SET
-                        `cache_id` = '', `user_id` = ?, `name` = ?, `longitude` = ?, `latitude` = ?, `last_modified` = NOW(),
+                        `user_id` = ?, `name` = ?, `longitude` = ?, `latitude` = ?, `last_modified` = NOW(),
                         `date_created` = NOW(), `type` = ?, `status` = ?, `country` = ?, `date_hidden` = ?, `date_activate` = ?,
                         `founds` = 0, `notfounds` = 0, `watcher` = 0, `notes` = 0, `last_found` = NULL, `size` = ?, `difficulty` = ?,
                         `terrain` = ?, `uuid` = ?, `logpw` = ?, `search_time` = ?, `way_length` = ?, `wp_gc` = ?,
