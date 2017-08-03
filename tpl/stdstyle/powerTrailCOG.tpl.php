0af9048c57f676d9f09817fe42c1db24356b0d88
deg <deg@best.pl>
2017-08-03 23:30:11 +0200

    GeoPath COG look improvements and 100% HTML5 validation (#837)

--- tpl/stdstyle/powerTrailCOG.tpl.php
+++ tpl/stdstyle/powerTrailCOG.tpl.php
@@ -1,31 +1,11 @@
-<script type="text/javascript" src="lib/tinymce4/tinymce.min.js"></script>
-<script src="tpl/stdstyle/js/jquery-2.0.3.min.js"></script>
+<?php
+$view->callChunk('tinyMCE', false);
+?>
+
 <link rel="stylesheet" href="tpl/stdstyle/js/jquery_1.9.2_ocTheme/themes/cupertino/jquery.ui.all.css">
 <script src="tpl/stdstyle/js/jquery_1.9.2_ocTheme/ui/minified/jquery-ui.min.js"></script>
 <script src="tpl/stdstyle/js/jquery_1.9.2_ocTheme/ui/jquery.datepick-{language4js}.js"></script>
-<script src="https://maps.googleapis.com/maps/api/js?v=3.27&amp;key={googlemap_key}"></script>
-<link rel="stylesheet" href="tpl/stdstyle/css/powerTrail.css" type="text/css">
-<script type="text/javascript">
-    tinymce.init({
-        selector: "#reason",
-        height: "350",
-        menubar: false,
-        toolbar_items_size: "small",
-        browser_spellcheck: true,
-        relative_urls: false,
-        remove_script_host: false,
-        entity_encoding: "raw",
-        language: "{language4js}",
-        toolbar1: "newdocument | styleselect formatselect fontselect fontsizeselect",
-        toolbar2: "cut copy paste | bullist numlist | outdent indent blockquote | undo redo | link unlink anchor image code | preview ",
-        toolbar3: "bold italic underline strikethrough |  alignleft aligncenter alignright alignjustify | hr | subscript superscript | charmap emoticons | forecolor backcolor | nonbreaking ",
-        plugins: [
-            "advlist autolink autosave link image lists charmap print preview hr anchor pagebreak spellchecker",
-            "searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking",
-            "table directionality emoticons template textcolor paste textcolor"
-        ],
-    });
-</script>
+
 <script type="text/javascript">
     $(function () {
         $.datepicker.setDefaults($.datepicker.regional['pl']);
@@ -100,7 +80,6 @@
 
 </script>
 
-    <br><br>
     <!-- deleting entery comfirmation dialog  -->
     <div id="dialog-form" title="{{pt151}}" style="display: none">
         <form>
@@ -118,30 +97,28 @@
 
     <div style="display: {selPtDiv}">
         <form action="powerTrailCOG.php" id="selPt" class="form-group-sm">
-            {ptSelector} <a href="javascript:void(0);" onclick="$('#selPt').submit();" class="btn btn-default btn-sm">{{pt209}}</a>
+            {ptSelector} &nbsp;<a href="javascript:void(0);" onclick="$('#selPt').submit();" class="btn btn-default btn-sm">{{pt209}}</a>
         </form>
     </div>
 
-
-
-    <div style="display: {PtDetailsDiv}">
+  <div style="display: {PtDetailsDiv}">
         <input type="hidden" id="ptId" value="{ptId}">
-        <table>
+        <table class="table">
             <tr>
-                <td>{{pt008}}</td>
+                <td class="content-title-noshade">{{pt008}}</td>
                 <td>{ptName}</td>
             </tr>
             <tr>
-                <td>{{pt023}}</td>
+                <td class="content-title-noshade">{{pt023}}</td>
                 <td>{ptType}</td>
             </tr>
             <tr>
-                <td>{{cs_status}}</td>
+                <td class="content-title-noshade">{{status_label}}</td>
                 <td><span id="ptStatus">{ptStatus}</span>
                     <div id="ptStatSelectSpan" style="display: none;">
                         <span style="color:red;">{{pt221}}</span><br><hr>
                         {{pt238}}: {ptStatSelect}<br><br>
-                        <textarea id="reason" class="desc">{{pt220}}</textarea>
+                        <textarea id="reason" class="desc tinymce">{{pt220}}</textarea>
                         <a id="stbtn2" href="javascript:void(0);" onclick="$('#stbtn1').show();
                                 $('#ptStatus').show();
                                 $('#ptStatSelectSpan').hide();" class="editPtDataButton">{{pt031}}</a>
@@ -154,8 +131,13 @@
                 </td>
             </tr>
         </table>
-
-        <br><br> {{pt020}}<br><hr>
-        {{pt211}}<br><br>
-        {ptCaches}
+    <div class="buffer"></div>
+    <div class="content2-container bg-blue02">
+        <p class="content-title-noshade-size1"><img src="tpl/stdstyle/images/blue/basic2.png" class="icon32" alt=""/>{{pt020}}	</p>
     </div>
+    <div class="content2-container">
+      {ptCaches}
+      <div class="buffer"></div>
+      <div class="notice">{{pt211}}</div>
+    </div>
+  </div>
