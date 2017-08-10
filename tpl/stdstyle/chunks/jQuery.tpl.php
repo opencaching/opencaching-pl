5c39ae1ae3827c601338d9f2dfb0308c24c4e742
deg <deg@best.pl>
2017-08-10 17:52:15 +0200

    Upgrade jQuery to newest version, migrate to CDN and add test library (temporary) (#927)

--- tpl/stdstyle/chunks/jQuery.tpl.php
+++ tpl/stdstyle/chunks/jQuery.tpl.php
@@ -14,7 +14,8 @@ return function (){
 ?>
 
 <!-- jQuery chunk -->
-<script src="tpl/stdstyle/js/jquery-2.0.3.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://code.jquery.com/jquery-migrate-1.4.1.js"></script>
 <!-- End of jQuery chunk -->
 
 <?php
