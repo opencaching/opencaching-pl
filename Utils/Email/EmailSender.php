fd8c82c892ae32d25d67a6f9a17dae4772050971
deg <deg@best.pl>
2017-08-26 23:59:47 +0200

    New notifications about new caches

--- Utils/Email/EmailSender.php
+++ Utils/Email/EmailSender.php
@@ -14,6 +14,7 @@ use lib\Objects\GeoCache\GeoCacheLog;
 use lib\Objects\OcConfig\OcConfig;
 use lib\Objects\User\User;
 use lib\Objects\User\UserMessage;
+use Utils\Gis\Gis;
 
 class EmailSender
 {
@@ -413,6 +414,61 @@ class EmailSender
             $email->setSubject(tr('reportcache07').": ".$location);
         }
 
+        $email->setBody($formattedMessage->getEmailContent(), true);
+        $email->send();
+    }
+
+    public static function sendNewCacheNotify($emailTemplatePath, $notifiesList, User $user)
+    {
+        if (count($notifiesList) > 1) {
+            $pluralSuffix = '_pl';
+        } else {
+            $pluralSuffix = '';
+        }
+
+        $content = '';
+        foreach ($notifiesList as $item) {
+            $line = file_get_contents($emailTemplatePath . 'newcache_notify_item.html');
+            $line = mb_ereg_replace('{absolute_server_URI}', OcConfig::getAbsolute_server_URI(), $line);
+            $line = mb_ereg_replace('{cache_type}', tr($item->getCache()->getCacheTypeTranslationKey()), $line);
+            $line = mb_ereg_replace('{cache_type_icon}', Geocache::CacheIconByType($item->getCache()->getCacheType(), $item->getCache()->getStatus()), $line);
+            $line = mb_ereg_replace('{cache_wp}', $item->getCache()->getWaypointId(), $line);
+            $line = mb_ereg_replace('{cache_name}', $item->getCache()->getCacheName(), $line);
+            $line = mb_ereg_replace('{cache_size}', tr($item->getCache()->getSizeTranslationKey()), $line);
+            $line = mb_ereg_replace('{cache_direction}', Gis::bearing2Text(Gis::calcBearingBetween($user->getHomeCoordinates(), $item->getCache()->getCoordinates()), true), $line);
+            $line = mb_ereg_replace('{cache_distance}', round(Gis::distanceBetween($user->getHomeCoordinates(), $item->getCache()->getCoordinates())), $line);
+            $line = mb_ereg_replace('{cache_unit}', 'km', $line);
+            $line = mb_ereg_replace('{cache_diff_icon}', $item->getCache()->getDifficultyIcon(), $line);
+            $line = mb_ereg_replace('{cache_diff}', $item->getCache()->getDifficulty(), $line);
+            $line = mb_ereg_replace('{cache_ter_icon}', $item->getCache()->getTerrainIcon(), $line);
+            $line = mb_ereg_replace('{cache_ter}', $item->getCache()->getTerrain(), $line);
+            $line = mb_ereg_replace('{cache_author_id}', $item->getCache()->getOwnerId(), $line);
+            $line = mb_ereg_replace('{cache_author}', $item->getCache()->getOwner()->getUserName(), $line);
+            $line = mb_ereg_replace('{cache_author_activity}', tr('user_activity01'), $line);
+            $line = mb_ereg_replace('{cache_author_activity2}', tr('user_activity02'), $line);
+            $line = mb_ereg_replace('{cache_author_found}', $item->getCache()->getOwner()->getFoundGeocachesCount(), $line);
+            $line = mb_ereg_replace('{cache_author_dnf}', $item->getCache()->getOwner()->getNotFoundGeocachesCount(), $line);
+            $line = mb_ereg_replace('{cache_author_hidden}', $item->getCache()->getOwner()->getHiddenGeocachesCount(), $line);
+            $line = mb_ereg_replace('{cache_author_total}', $item->getCache()->getOwner()->getFoundGeocachesCount() + $item->getCache()->getOwner()->getNotFoundGeocachesCount() + $item->getCache()->getOwner()->getHiddenGeocachesCount(), $line);
+            $line = mb_ereg_replace('{cache_date}', $item->getCache()->getDatePlaced()->format(OcConfig::instance()->getDateFormat()), $line);
+            $content .= $line;
+        }
+
+        $subject = tr('notify_subject' . $pluralSuffix);
+        $subject = mb_ereg_replace('{site_name}', OcConfig::getSiteName(), $subject);
+
+        $formattedMessage = new EmailFormatter($emailTemplatePath . 'newcache_notify.email.html', true);
+        $formattedMessage->addFooterAndHeader($user->getUserName());
+        $formattedMessage->setVariable('intro', tr('notify_intro' . $pluralSuffix));
+        $formattedMessage->setVariable('absolute_server_URI', OcConfig::getAbsolute_server_URI());
+        $formattedMessage->setVariable('content', $content);
+
+        $email = new Email();
+        $email->addToAddr($user->getEmail());
+        $email->setReplyToAddr(OcConfig::getNoreplyEmailAddress());
+        $email->setFromAddr(OcConfig::getNoreplyEmailAddress());
+        $email->addSubjectPrefix(OcConfig::getMailSubjectPrefixForSite());
+        $email->setSubject($subject);
         $email->setBody($formattedMessage->getEmailContent(), true);
         $email->send();
     }
