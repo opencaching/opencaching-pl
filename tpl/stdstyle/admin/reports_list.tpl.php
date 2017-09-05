ae1de4ef1c706ed35c55d9f415bee8a27bbed8df
deg <deg@best.pl>
2017-09-05 12:38:16 +0200

    OC Team menu refactoring part 1b

--- tpl/stdstyle/admin/reports_list.tpl.php
+++ tpl/stdstyle/admin/reports_list.tpl.php
@@ -1,10 +1,18 @@
 <?php
 ?>
 <div class="content2-container">
+  <form action="/admin_reports.php" method="post">
   <div class="content2-pagetitle">
+    <div class="content-title-noshade-size0" style="float: right;">
+      {{admin_reports_found_reports}}: <?=$view->reportsCount?>&nbsp;&nbsp;
+      <div class="btn-group btn-group">
+        <button type="submit" class="btn btn-primary">{{filter}}</button>
+        <button type="submit" name="reset" class="btn btn-default">{{reset}}</button>
+      </div>
+    </div>
     <img src="tpl/stdstyle/images/blue/rproblems.png" class="icon32" alt=""> {{admin_reports_title_reportslist}}
   </div>
-  <form action="/admin_reports.php" method="post">
+  <?php if (isset($view->errorMsg)) { $view->callChunk('infoBar', null, null, $view->errorMsg); }?>
   <table class="table table-striped full-width">
     <tr>
       <th>{{admin_reports_lbl_id}}</th>
@@ -15,12 +23,10 @@
     </tr>
     <tr>
       <td style="text-align: center;">
-        <button type="submit" class="btn btn-primary btn-sm">{{filter}}</button>
-        <button type="submit" name="reset" class="btn btn-default btn-xs">{{reset}}</button>
+        <input type="text" placeholder="{{admin_reports_lbl_id}}" name="reportId" class="form-control input50">
       </td>
       <td>
         <input type="text" placeholder="{{waypointname_label}}" name="reportWp" value="<?=$_SESSION['reportWp']?>" class="form-control">
-        
       </td>
       <td><select name="reportType" class="form-control"><?=$view->typeSelect?></select></td>
       <td><select name="reportStatus" class="form-control"><?=$view->statusSelect?></select></td>
@@ -28,19 +34,19 @@
     </tr>
 <?php foreach ($view->reports as $report) { ?>
     <tr>
-      <td><a href="/admin_reports.php?action=showreport&id=<?=$report->getId()?>" class="links"><?=$report->getId()?></a></td>
+      <td><a href="/admin_reports.php?action=showreport&amp;id=<?=$report->getId()?>" class="links"><?=$report->getId()?></a></td>
       <td>
         <a href="/viewcache.php?wp=<?=$report->getCache()->getWaypointId()?>" class="links" target="_blank"><img src="/<?=$report->getCache()->getCacheIcon($view->user)?>" height=20 title="<?=tr($report->getCache()->getStatusTranslationKey())?>" alt="<?=tr($report->getCache()->getStatusTranslationKey())?>"> <?=$report->getCache()->getCacheName()?> (<?=$report->getCache()->getWaypointId()?>)</a><br>
         <?=$report->getCache()->getCacheLocationObj()->getLocationDesc(' &gt; ')?><br>
         <a href="/viewprofile.php?userid=<?=$report->getCache()->getOwnerId()?>" class="links" target="_blank"><?=$report->getCache()->getOwner()->getUserName()?></a>
       </td>
       <td>
-        <a href="/admin_reports.php?action=showreport&id=<?=$report->getId()?>" class="links"><?=tr($report->getReportTypeTranslationKey())?></a><br>
+        <a href="/admin_reports.php?action=showreport&amp;id=<?=$report->getId()?>" class="links"><?=tr($report->getReportTypeTranslationKey())?></a><br>
         <?=$report->getDateSubmit()->format($view->dateFormat)?><br>
         <a href="/viewprofile.php?userid=<?=$report->getUserIdSubmit()?>" class="links" target="_blank"><?=$report->getUserSubmit()->getUserName()?></a>
       </td>
       <td>
-        <a href="/admin_reports.php?action=showreport&id=<?=$report->getId()?>" class="links"><?=tr($report->getReportStatusTranslationKey())?></a><br>
+        <a href="/admin_reports.php?action=showreport&amp;id=<?=$report->getId()?>" class="links"><?=tr($report->getReportStatusTranslationKey())?></a><br>
         <?php if ($report->getDateChangeStatus() != null) { echo $report->getDateChangeStatus()->format($view->dateFormat);}?>
         <?php if ($report->getUserIdChangeStatus() != null) {?><br><a href="/viewprofile.php?userid=<?=$report->getUserIdChangeStatus()?>" class="links" target="_blank"><?=$report->getUserChangeStatus()->getUserName()?></a><?php }?>
       </td>
@@ -48,7 +54,6 @@
     </tr>
 <?php } ?>
   </table>
-    </form>
-  
   <?php $view->callChunk('pagination', $view->paginationModel); ?>
+  </form>
 </div>
