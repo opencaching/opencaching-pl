c56220494b69436bdd3646e4500668ed7e0bf265
kojoty <kojoty@users.noreply.github.com>
2017-12-01 13:13:31 +0100

    Export script (#1312)
    * sql trimmer for dev: random user emails + gzip input file support
    
    * comments fix

--- docs/sql/export/dumpCleaner.sh
+++ docs/sql/export/dumpCleaner.sh
@@ -9,15 +9,20 @@ SQL_FILE=$1
 TABLES_TO_TRIM_FILE=$2
 
 # result file
-TRIMMED_FILE="OC_DEV_DUMP_"$SQL_FILE
+TRIMMED_FILE="OC_DEV_DUMP_"${SQL_FILE%.*}
+
+# just print message
+function say () {
+    echo  `date +%F_%H:%M:%S`": $1"
+}
 
 # replace tricky processing breakers to peacefull chars
 function cleanFile() {
     FILE=$1
-    
+
     #replace double back-slashes to pipe "\\" -> "|"
     sed -E -i -e 's/\\\\/|/g' $FILE
-    
+
     # replace escaped quote to colon "\'" -> ":" 
     sed -E -i -e "s/\\\'/:/g" $FILE
 }
@@ -42,14 +47,17 @@ function trimFile () {
     # remove last pipe
     TRIM_PATTERN=${TRIM_PATTERN%?}
 
-    cat $SQL_FILE | grep -Ev "$TRIM_PATTERN" > $TRIMMED_FILE
+    zcat $SQL_FILE | grep -Ev "$TRIM_PATTERN" > $TRIMMED_FILE
 }
 
-# change all email addresses to DEFAUL-EMAIL
+# change all email addresses to DEFAULT-EMAIL - actually not used because user email needs to be unique!
 function blureEmails () {
-    # remove passwords from caches
+
+    # email address to set in all user rows
+    DEFAULT_EMAIL="" #by default email address is empty 
+
     EMAIL_REGEX="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}"
-    DEFAULT_EMAIL="email@server.com"
+    
     sed -E -i $TRIMMED_FILE -e "s/,'$EMAIL_REGEX',/,'$DEFAULT_EMAIL',/g"
 }
 
@@ -61,10 +69,10 @@ function changeCachesPassword() {
 
     TMP_FILE="tmp_`date +%s`.sql"
     TMP2_FILE="tmp2_`date +%s`.sql"
-    
+
     CACHES_PATTERN="INSERT INTO \`caches\`"
     CACHES_INSERT_LINE_MARKER="ALTER TABLE \`caches\` DISABLE KEYS"
-    
+
     cat $TRIMMED_FILE | grep "$CACHES_PATTERN" > $TMP_FILE
 
     cleanFile $TMP_FILE
@@ -99,47 +107,51 @@ function changeCachesPassword() {
     rm $TMP_FILE $TMP2_FILE
 }
 
-# change all user passwords to default value: haslo
+# change all user passwords to default value: haslo + email to: uid@localhost
 function changeUserPasswords() {
 
     #pass=haslo
     PASSWORD="ffb1b530c3116b4cd2e75b764c9ba0d647ab53053571a29532dbe8161996fe38540490fbecd45f4a4114d53d6e9bba6fb790a3b86ccda1b61c821ac02c1b53fa"
     SALT=""
     HASHING_ROUNDS=1
-    
+
     PASSWORD_COL=3
     SALT_COL=4
     HASH_RND_COL=5
-    
+    EMAIL_COL=6
+
     USER_TABLE_PATTERN="INSERT INTO \`user\`"
     USER_INSERT_LINE_MARKER="ALTER TABLE \`user\` DISABLE KEYS"
-    
+
     TMP_FILE="tmp_`date +%s`.sql"
     TMP2_FILE="tmp2_`date +%s`.sql"
-    
+
     cat $TRIMMED_FILE | grep "$USER_TABLE_PATTERN" > $TMP_FILE
-    
+
     cleanFile $TMP_FILE
 
     # replace all commas inside the quotes with semicolon
-    awk -F"'" -v OFS="'" '{ for (i=2; i<=NF; i+=2) gsub(",", ";", $i) } 1' $TMP_FILE > $TMP2_FILE
+    awk -i inplace -F"'" -v OFS="'" '{ for (i=2; i<=NF; i+=2) gsub(",", ";", $i) } 1' $TMP_FILE
 
-    mv $TMP2_FILE $TMP_FILE
-    
     # split long lines to one inserted row per line
     sed -i -e 's/),(/),\n(/g' $TMP_FILE
 
     # change passwords
-    awk 'BEGIN {OFS=FS=","} { 
+    awk -i inplace 'BEGIN {OFS=FS=","} {
+            uid=$1;
+            gsub(/.*\(/,"",uid);
             $'$PASSWORD_COL'="'\'$PASSWORD\''";
             $'$SALT_COL'="'\'$SALT\''";  
             $'$HASH_RND_COL'="'\'$HASHING_ROUNDS\''";
-            print 
-        }' $TMP_FILE > $TMP2_FILE
+            $'$EMAIL_COL'="'\''"uid"@localhost'\''";
+            print
+        }' $TMP_FILE
 
     # join lines inside TMP_FILE
-    cat $TMP2_FILE | tr '\n' ' ' > $TMP_FILE
-    
+    cat $TMP_FILE | tr '\n' ' ' > $TMP2_FILE
+
+    mv $TMP2_FILE $TMP_FILE
+
     # split lines per each INSERT...
     sed -i -e 's/\; INSERT INTO/\;\nINSERT INTO/g' $TMP_FILE
     
@@ -150,7 +162,7 @@ function changeUserPasswords() {
     sed -i "/$USER_INSERT_LINE_MARKER/ r $TMP_FILE" $TRIMMED_FILE
 
     # delete tmp files
-    rm $TMP_FILE $TMP2_FILE
+    rm $TMP_FILE
 }
 
 # remove most of cache log texts
@@ -158,7 +170,7 @@ function removeLogTexts () {
 
     LOG_TEXT_COL=6
     LOG_TEXT="TFTC"
-    
+
     LOGS_TABLE_PATTERN="INSERT INTO \`cache_logs\`"
     LOGS_INSERT_LINE_MARKER="ALTER TABLE \`cache_logs\` DISABLE KEYS"
 
@@ -185,10 +197,10 @@ function removeLogTexts () {
 
     # join lines inside TMP_FILE
     cat $TMP2_FILE | tr '\n' ' ' > $TMP_FILE
-    
+
     # split lines per each INSERT...
     sed -i -e 's/\; INSERT INTO/\;\nINSERT INTO/g' $TMP_FILE
-    
+
     # remove insert lines from trimmed file
     sed -i "/$LOGS_TABLE_PATTERN/d" $TRIMMED_FILE
 
@@ -197,17 +209,33 @@ function removeLogTexts () {
 
     # delete tmp files
     rm $TMP_FILE $TMP2_FILE
-}    
+}
 
- echo "sqlDumpCleaner by kojoty started!";
+function main {
+    say "SqlDumpCleaner by kojoty started!"
 
- echo "Initial sql dump file size:"
- printFileSize $SQL_FILE
+    say "Initial sql dump file size:"
+    printFileSize $SQL_FILE
+
+    say "Copy necessary data from gzip file"
+    trimFile
+
+    say "Change cache passwords"
+    changeCachesPassword
+
+    say "Change user passwords"
+    changeUserPasswords
+
+    say "Remove most of cache logs"
+    removeLogTexts
+
+    say "Compress result file"
+    gzip -9 -c $TRIMMED_FILE > $TRIMMED_FILE.gz
+
+    say "Final dev-dump file size"
+    printFileSize $TRIMMED_FILE.gz
+}
  
- trimFile
- changeCachesPassword
- changeUserPasswords
- removeLogTexts
- 
- echo "Final dev-dump file size"
- printFileSize $TRIMMED_FILE
+main
+
+
