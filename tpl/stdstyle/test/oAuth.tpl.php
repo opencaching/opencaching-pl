ad55b0fc135270fdf64e8e09b9ec30b4f7553e1d
kojoty <p.juzwiak@gmail.com>
2018-01-18 10:12:25 +0100

    #1320: minor fixes in test process - next part

--- tpl/stdstyle/test/oAuth.tpl.php
+++ tpl/stdstyle/test/oAuth.tpl.php
@@ -5,20 +5,24 @@ This is here for debug purpose only.
 Click button to check local config.
 
 <div>
-<a class="btn" href="<?=$view->fbLink?>">
-  <img class="loginBtnLogo" alt="Facebook"
-        src="/images/externalLogo/fb/FB-f-Logo__blue_50.png" />
-  <div class="loginBtnTxt">
-    Continue with Facebook
-  </div>
-</a>
+    <?php if($view->fbTestEn){ ?>
+    <a class="btn" href="<?=$view->fbLink?>">
+      <img class="loginBtnLogo" alt="Facebook"
+            src="/images/externalLogo/fb/FB-f-Logo__blue_50.png" />
+      <div class="loginBtnTxt">
+        Continue with Facebook
+      </div>
+    </a>
+    <?php } //if-fbTestEn ?>
 
-<a class="btn" href="<?=$view->gLink?>">
-  <img id="googleLogo" class="loginBtnLogo" alt="Google"
-        src="/images/externalLogo/google/btn_google_light_normal_ios.svg" />
-  <div class="loginBtnTxt">
-    Continue with Google
-  </div>
-</a>
+    <?php if($view->gTestEn) { ?>
+    <a class="btn" href="<?=$view->gLink?>">
+      <img id="googleLogo" class="loginBtnLogo" alt="Google"
+            src="/images/externalLogo/google/btn_google_light_normal_ios.svg" />
+      <div class="loginBtnTxt">
+        Continue with Google
+      </div>
+    </a>
+    <?php } //if-gTestEn ?>
 
 </div>
