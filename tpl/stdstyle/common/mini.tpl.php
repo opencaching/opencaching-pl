6e1b7ad4f419857d8a3261256f21b8d842f1eb27
Grzegorz deg <deg@best.pl>
2018-01-22 19:41:33 +0100

    Revert "Some main templates improvements"
    This reverts commit 55d08982fc02b4d1e8056a70a0285e4c5f7b5b3c.

--- tpl/stdstyle/common/mini.tpl.php
+++ tpl/stdstyle/common/mini.tpl.php
@@ -1,58 +1,66 @@
 <!DOCTYPE html>
 <html lang="<?=$view->getLang()?>" xml:lang="<?=$view->getLang()?>">
-<head>
-  <meta http-equiv="content-type" content="text/html; charset=utf-8">
-  <meta name="keywords" content="<?=$view->_keywords?>">
-  <meta name="author" content="<?=$view->_siteName?>">
-  <link rel="shortcut icon" href="/images/<?=$config['headerFavicon']?>">
-  <link rel="apple-touch-icon-precomposed" href="/images/oc_logo_144.png">
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8">
 
-  <title>{title}</title>
+        <meta name="keywords" content="geocaching, opencaching, skarby, poszukiwania, geocashing, longitude, latitude, utm, coordinates, treasure hunting, treasure, GPS, global positioning system, garmin, magellan, mapping, geo, hiking, outdoors, sport, hunt, stash, cache, geocaching, geocache, cache, treasure, hunting, satellite, navigation, tracking, bugs, travel bugs">
+        <meta name="author" content="{site_name}">
 
-  <link rel="stylesheet" type="text/css" media="screen" href="<?=$view->screenCss?>">
-  <link rel="stylesheet" type="text/css" media="print" href="<?=$view->printCss?>">
-  <link rel="stylesheet" type="text/css" media="screen,projection" href="<?=$view->screenCss?>">
-  <link rel="stylesheet" type="text/css" media="print" href="<?=$view->printCss?>">
+        <link rel="stylesheet" type="text/css" media="screen" href="<?=$view->screenCss?>">
+        <link rel="stylesheet" type="text/css" media="print" href="<?=$view->printCss?>">
 
-  <?php foreach( $view->getLocalCss() as $css ) { ?>
-    <link rel="stylesheet" type="text/css" href="<?=$css?>">
-  <?php } //foreach-css ?>
 
-  <?php
-      if( $view->isGoogleAnalyticsEnabled() ){
-          $view->callChunkOnce( 'googleAnalytics', $view->getGoogleAnalyticsKey() );
-      }
-      if( $view->isjQueryEnabled()){
-          $view->callChunk('jQuery');
-      }
-      if( $view->isjQueryUIEnabled()){
-          $view->callChunk('jQueryUI');
-      }
-      if( $view->isTimepickerEnabled()){
-          $view->callChunk('timepicker');
-      }
-      if( $view->isLightBoxEnabled()){
-          $view->callChunk('lightBoxLoader', true, false);
-      }
-      if( $view->isGMapApiEnabled()){
-          $view->callChunk('googleMapsApi', $GLOBALS['googlemap_key'], $view->getLang());
-      }
-  ?>
+        <link rel="shortcut icon" href="/images/<?=$config['headerFavicon']?>">
+        <link rel="apple-touch-icon-precomposed" href="/images/oc_logo_144.png">
 
-  <script type="text/javascript" src="lib/enlargeit/enlargeit.js" async="async"></script>
 
-  {htmlheaders}
-  {cachemap_header}
-</head>
+        <link rel="stylesheet" type="text/css" media="screen,projection" href="<?=$view->screenCss?>" />
+        <link rel="stylesheet" type="text/css" media="print" href="<?=$view->printCss?>" />
 
-<body {bodyMod}>
-  {template}
+        <link rel="SHORTCUT ICON" href="favicon.ico" />
+        <link rel="apple-touch-icon-precomposed" href="/images/oc_logo_144.png" />
 
-  <?php
-      // lightbox js should be loaded at th end of page
-      if( $view->isLightBoxEnabled()){
-          $view->callChunk('lightBoxLoader', false, true);
-      }
-  ?>
-</body>
+        <?php foreach( $view->getLocalCss() as $css ) { ?>
+          <link rel="stylesheet" type="text/css" href="<?=$css?>">
+        <?php } //foreach-css ?>
+
+        <?php
+            if( $view->isGoogleAnalyticsEnabled() ){
+                $view->callChunkOnce( 'googleAnalytics', $view->getGoogleAnalyticsKey() );
+            }
+
+            if( $view->isjQueryEnabled()){
+                $view->callChunk('jQuery');
+            }
+
+            if( $view->isjQueryUIEnabled()){
+                $view->callChunk('jQueryUI');
+            }
+            if( $view->isTimepickerEnabled()){
+                $view->callChunk('timepicker');
+            }
+            if( $view->isLightBoxEnabled()){
+                $view->callChunk('lightBoxLoader', true, false);
+            }
+
+            if( $view->isGMapApiEnabled()){
+                $view->callChunk('googleMapsApi', $GLOBALS['googlemap_key'], $view->getLang());
+            }
+        ?>
+
+        <script type="text/javascript" src="lib/enlargeit/enlargeit.js" async="async"></script>
+        <title>{title}</title>
+        {htmlheaders}
+        {cachemap_header}
+    </head>
+    <body{bodyMod}>
+        {template}
+
+        <?php
+        		// lightbox js should be loaded at th end of page
+            if( $view->isLightBoxEnabled()){
+                $view->callChunk('lightBoxLoader', false, true);
+            }
+        ?>
+    </body>
 </html>
