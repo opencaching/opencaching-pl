b82839a77a49417d8cf38efbc8cf9789c0487639
kojoty <kojoty@users.noreply.github.com>
2018-01-23 18:52:29 +0100

    temporary workaround for #1370

--- tpl/stdstyle/startPage/startPage.tpl.php
+++ tpl/stdstyle/startPage/startPage.tpl.php
@@ -252,7 +252,7 @@
       <?php if($view->feedsData) { ?>
           <?=$view->callSubTpl('/startPage/feeds')?>
       <?php }else{ //if-feedsData?>
-        <?php $view->callChunk('dynamicHtmlLoad', $view->feedsUrl, 'feedsContainer'); ?>
+        <?php //$view->callChunk('dynamicHtmlLoad', $view->feedsUrl, 'feedsContainer'); ?>
       <?php } //if-feedsData ?>
     </div>
     <!-- /feeds -->
