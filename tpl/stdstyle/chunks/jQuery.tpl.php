78901d39de505e11911b9287a6f6baf4dffbce57
Grzegorz deg <deg@best.pl>
2018-01-28 11:20:31 +0100

    jQuery version update

--- tpl/stdstyle/chunks/jQuery.tpl.php
+++ tpl/stdstyle/chunks/jQuery.tpl.php
@@ -14,8 +14,8 @@ return function (){
 ?>
 
 <!-- jQuery chunk -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<!-- <script src="https://code.jquery.com/jquery-migrate-1.4.1.js"></script> -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<!-- <script src="https://code.jquery.com/jquery-migrate-3.0.1.min.js"></script> -->
 <!-- End of jQuery chunk -->
 
 <?php
