c2759e8708ec5d6e40d6ad00ddcaeccd3fecf198
kojoty <p.juzwiak@gmail.com>
2018-02-07 22:51:13 +0100

    #1366: qrCode lib moved to composer

--- tpl/stdstyle/qrCodeGen/qrcode.tpl.php
+++ tpl/stdstyle/qrCodeGen/qrcode.tpl.php
@@ -1,43 +1,43 @@
 
 <div class="content2-pagetitle">
-    {{qrcode_title}}
+    <?=tr('qrcode_title')?>
 </div>
 
 <div id="qrCodeDesc">
-  <p>{{qrcode_desc}}</p>
+  <p><?=tr('qrcode_desc')?></p>
 </div>
 
 <div id="qrCodeimage">
-    <img src="{ocLabelImgUrl}" border="0" alt="" width="171" height="284" />
+    <img src="<?=$view->ocLabelImgUrl?>" alt="" width="171" height="284" />
 </div>
 
 
 <div id="qrCodeForm">
     <div>
-    <form action="qrcode.php" method="post">
-        <label for="qrCodeText">{{qrcode_textLabel}}:</label><br>
+    <form method="post">
+        <label for="qrCodeText"><?=tr('qrcode_textLabel')?>:</label><br>
 
-        <input name="qrCodeText" value="{qrCodeText}" maxlength="77"
+        <input id="qrCodeText" name="qrCodeText" value="<?=$view->qrCodeText?>" maxlength="77"
                 class="form-control input400"><br>
 
         <button type="submit" name="Generate" value="Generate"
-                class="btn btn-primary">{{qrcode_button}}</button>
+                class="btn btn-primary"><?=tr('qrcode_button')?></button>
     </form>
     </div>
 </div>
 
 <div>
-    <a href="{ocLabelImgUrl}" title="qrCode_image" download="QRcodeLabel.png">
-        <button class="btn btn-default">{{qrcode_downloadLabeled}}</button>
+    <a class="btn btn-default" href="<?=$view->ocLabelImgUrl?>" title="qrCode_image" download="QRcodeLabel.png">
+       <?=tr('qrcode_downloadLabeled')?>
     </a>
-    <a href="{qrCodeImgUrl}" title="qrCode_image" download="QRcode.png">
-        <button class="btn btn-default">{{qrcode_downloadQRCodeOnly}}</button>
+    <a class="btn btn-default" href="<?=$view->qrCodeImgUrl?>" title="qrCode_image" download="QRcode.png">
+        <?=tr('qrcode_downloadQRCodeOnly')?>
     </a>
 </div>
 
 <div>
-    <img src="tpl/stdstyle/images/misc/16x16-info.png" class="icon16" alt="Info" />
-    {{qrcode_appInfo}}
+    <img src="/tpl/stdstyle/images/misc/16x16-info.png" class="icon16" alt="Info" />
+    <?=tr('qrcode_appInfo')?>
 </div>
 
 
