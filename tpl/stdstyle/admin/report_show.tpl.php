a845d8d22af6e85aadf06a8833ba82d914a0c6b6
Grzegorz deg <deg@best.pl>
2018-02-08 16:49:02 +0100

    Admin reports fix after GeoCacheLog update and some improvements

--- tpl/stdstyle/admin/report_show.tpl.php
+++ tpl/stdstyle/admin/report_show.tpl.php
@@ -6,7 +6,7 @@ use lib\Objects\Admin\ReportPoll;
 use lib\Objects\Admin\ReportCommons;
 ?>
 <?php if ($view->includeGCharts) {?>
-<script src="https://www.gstatic.com/charts/loader.js"></script>
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 <script>
   google.charts.load('current', {'packages':['corechart']});
   <?php foreach ($view->inactivePolls as $inactPoll) {?>
@@ -15,7 +15,7 @@ use lib\Objects\Admin\ReportCommons;
   <?php }?>
 </script>
 <?php }?>
-<script src="/lib/js/wz_tooltip.js"></script>
+<script type="text/javascript" src="/lib/js/wz_tooltip.js"></script>
 <script src="<?=$view->reports_js?>"></script>
 <div id="report-confirm-poll"><p><strong>{{admin_reports_poll_confirm}}</strong></p>
   <p style="text-align: left">{{admin_reports_lbl_question}}: <em><span id="confirm-dlg-question"></span></em></p>
@@ -56,7 +56,7 @@ use lib\Objects\Admin\ReportCommons;
     </tr>
     <tr>
       <td class="content-title-noshade" style="text-align: right;">{{admin_reports_lbl_submiter}}</td>
-      <td><a href="/viewprofile.php?userid=<?=$view->report->getUserIdSubmit()?>" class="links" target="_blank"><?=$view->report->getUserSubmit()->getUserName()?></a> (<?php echo $view->report->getUserSubmit()->getFoundGeocachesCount() + $view->report->getUserSubmit()->getNotFoundGeocachesCount() + $view->report->getUserSubmit()->getHiddenGeocachesCount()?>)</td>
+      <td><a href="<?=$view->report->getUserSubmit()->getProfileUrl()?>" class="links" target="_blank"><?=$view->report->getUserSubmit()->getUserName()?></a> (<?php echo $view->report->getUserSubmit()->getFoundGeocachesCount() + $view->report->getUserSubmit()->getNotFoundGeocachesCount() + $view->report->getUserSubmit()->getHiddenGeocachesCount()?>)</td>
     </tr>
     <tr>
       <td class="content-title-noshade" style="text-align: right;">{{admin_reports_lbl_leader}}</td>
@@ -78,7 +78,7 @@ use lib\Objects\Admin\ReportCommons;
       <td class="content-title-noshade" style="text-align: right;">{{last_modified_label}}</td>
       <td>
         <?php if ($view->report->getDateLastChange() != null) { echo $view->report->getDateLastChange()->format($view->dateFormat); }?>
-        <?php if ($view->report->getUserIdLastChange() != null) {?>(<a href="/viewprofile.php?userid=<?=$view->report->getUserIdLastChange()?>" class="links" target="_blank"><?=$view->report->getUserLastChange()->getUserName()?></a>) <?php }?>
+        <?php if ($view->report->getUserIdLastChange() != null) {?>(<a href="<?=$view->report->getUserLastChange()->getProfileUrl()?>" class="links" target="_blank"><?=$view->report->getUserLastChange()->getUserName()?></a>) <?php }?>
       </td>
     </tr>
     <tr>
@@ -91,17 +91,17 @@ use lib\Objects\Admin\ReportCommons;
     <tr>
       <td class="content-title-noshade" style="text-align: right;">{{cache}}</td>
       <td>
-        <img src="/<?=$view->report->getCache()->getCacheIcon($view->user)?>" height=20 alt="">
-        <a href="/viewcache.php?wp=<?=$view->report->getCache()->getWaypointId()?>" class="links" target="_blank"><?=$view->report->getCache()->getCacheName()?> (<?=$view->report->getCache()->getWaypointId()?>)</a><br>
+        <img src="<?=$view->report->getCache()->getCacheIcon($view->user)?>" height=20 alt="">
+        <a href="<?=$view->report->getCache()->getCacheUrl()?>" class="links" target="_blank"><?=$view->report->getCache()->getCacheName()?> (<?=$view->report->getCache()->getWaypointId()?>)</a><br>
         <?=$view->report->getCache()->getCacheLocationObj()->getLocationDesc(' &gt; ')?><br>
         <?php if ($view->report->getCache()->getCacheType() == GeoCacheCommons::TYPE_EVENT) { ?>
-            <img src="/tpl/stdstyle/images/log/16x16-attend.png" class="icon16" alt="{{attendends}}"> x<?=$view->report->getCache()->getFounds()?>
-            <img src="/tpl/stdstyle/images/log/16x16-will_attend.png" class="icon16" alt="{{will_attend}}"> x<?=$view->report->getCache()->getNotFounds()?>
+            <img src="/tpl/stdstyle/images/log/16x16-attend.png" class="icon16" alt="{{attendends}}"> x<?=$view->report->getCache()->getFounds()?> 
+            <img src="/tpl/stdstyle/images/log/16x16-will_attend.png" class="icon16" alt="{{will_attend}}"> x<?=$view->report->getCache()->getNotFounds()?> 
             <img src="/tpl/stdstyle/images/log/16x16-note.png" class="icon16" alt="{{note}}"> x<?=$view->report->getCache()->getNotesCount()?>
             <br>
         <?php  } else { ?>
-            <img src="/tpl/stdstyle/images/log/16x16-found.png" class="icon16" alt="{{found}}"> x<?=$view->report->getCache()->getFounds()?>
-            <img src="/tpl/stdstyle/images/log/16x16-dnf.png" class="icon16" alt="{{not_found}}"> x<?=$view->report->getCache()->getNotFounds()?>
+            <img src="/tpl/stdstyle/images/log/16x16-found.png" class="icon16" alt="{{found}}"> x<?=$view->report->getCache()->getFounds()?> 
+            <img src="/tpl/stdstyle/images/log/16x16-dnf.png" class="icon16" alt="{{not_found}}"> x<?=$view->report->getCache()->getNotFounds()?> 
             <img src="/tpl/stdstyle/images/log/16x16-note.png" class="icon16" alt="{{note}}"> x<?=$view->report->getCache()->getNotesCount()?>
             <img src="/images/rating-star.png" class="icon16" alt="{{recommendations}}"> x<?=$view->report->getCache()->getRecommendations()?>
         <?php }?>
@@ -111,14 +111,14 @@ use lib\Objects\Admin\ReportCommons;
       <td class="content-title-noshade" style="text-align: right;">{{new_logs}}</td>
       <td>
         <?php foreach ($view->lastLogs as $log) { ?>
-          <img src="/<?=GeoCacheLogCommons::GetIconForType($log->getType())?>" alt="<?=tr(GeoCacheLogCommons::typeTranslationKey($log->getType()))?>" onmouseover="Tip('<b><?=$log->getUser()->getUserName()?></b>&nbsp;(<?=$log->getDate()->format($view->dateFormat)?>)<br><?=GeoCacheLogCommons::cleanLogTextForToolTip($log->getText())?>',OFFSETY, 25, OFFSETX, -135, PADDING,5, WIDTH,280,SHADOW,true)" onmouseout="UnTip()">
+          <img src="<?=GeoCacheLogCommons::GetIconForType($log->getType())?>" alt="<?=tr(GeoCacheLogCommons::typeTranslationKey($log->getType()))?>" onmouseover="Tip('<b><?=$log->getUser()->getUserName()?></b>&nbsp;(<?=$log->getDate()->format($view->dateFormat)?>)<br><?=GeoCacheLogCommons::cleanLogTextForToolTip($log->getText())?>',OFFSETY, 25, OFFSETX, -135, PADDING,5, WIDTH,280,SHADOW,true)" onmouseout="UnTip()"> 
         <?php  }?>
       </td>
     </tr>
     <tr>
       <td class="content-title-noshade" style="text-align: right;">{{owner}}</td>
       <td>
-        <a href="/viewprofile.php?userid=<?=$view->report->getCache()->getOwner()->getUserId()?>" class="links" target="_blank"><?=$view->report->getCache()->getOwner()->getUserName()?></a>
+        <a href="<?=$view->report->getCache()->getOwner()->getProfileUrl()?>" class="links" target="_blank"><?=$view->report->getCache()->getOwner()->getUserName()?></a>
         (<?php echo $view->report->getCache()->getOwner()->getFoundGeocachesCount() + $view->report->getCache()->getOwner()->getNotFoundGeocachesCount() + $view->report->getCache()->getOwner()->getHiddenGeocachesCount()?>)</td>
     </tr>
     <tr>
