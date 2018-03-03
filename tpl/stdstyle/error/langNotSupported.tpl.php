6693ec46103283cb33acfe1da5bc205ba7b27b97
Grzegorz deg <deg@best.pl>
2018-03-04 00:42:51 +0100

    Remove XSS bug

--- tpl/stdstyle/error/langNotSupported.tpl.php
+++ tpl/stdstyle/error/langNotSupported.tpl.php
@@ -23,13 +23,14 @@
     <h5>
         Please select one of supported language versions:
     </h5>
+    <span class="bottom-flags">
         <?php foreach($view->allLanguageFlags as $langFlag){ ?>
             <a rel="nofollow" href="<?=$langFlag['link']?>">
               <img class="img-navflag" src="<?=$langFlag['img']?>" alt="<?=$langFlag['name']?> version"
                    title="<?=$langFlag['name']?> version">&nbsp;<?=$langFlag['name']?>&nbsp;
             </a>
         <?php } //forach-lang-flags ?>
-
+    </span>
 
     <h5>
         If you would like to help us traslate opencaching to other languages feel free to
