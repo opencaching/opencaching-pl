2b80e1786853dba76bd55ac8839bb36fbf9176ae
Grzegorz deg <deg@best.pl>
2018-03-07 23:45:37 +0100

    revert myn.inc.php

--- lib/myn.inc.php
+++ lib/myn.inc.php
@@ -1,7 +1,68 @@
 <?php
+use Utils\Database\OcDb;
 
 class myninc
 {
+
+    /**
+     * =====================================================================================
+     * Funkcja sprawdzająca czy skrzynka jest znaleziona przez użytkownika
+     *
+     * dane wejściowe:
+     * id skrzynki
+     * id zalogowanego użytkownika
+     *
+     * zwraca true lub false
+     *
+     * =====================================================================================
+     */
+    private static function is_cache_found($cache_id, $user_id)
+    {
+        $q = 'SELECT user_id FROM cache_logs WHERE cache_id =:v1 AND user_id =:v2 AND type IN(1,7) AND Deleted=0';
+        $db = OcDb::instance();
+        $params = [];
+        $params['v1']['value'] = (integer) $cache_id;
+        $params['v1']['data_type'] = 'integer';
+        $params['v2']['value'] = (integer) $user_id;
+        $params['v2']['data_type'] = 'integer';
+        $s = $db->paramQuery($q, $params);
+        $rec = $db->dbResultFetch($s);
+        if (isset($rec['user_id'])) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * =====================================================================================
+     * Funkcja sprawdzająca czy użytkownik uczestniczył w wydarzeniu
+     *
+     * dane wejściowe:
+     * id skrzynki
+     * id zalogowanego użytkownika
+     *
+     * zwraca true lub false
+     *
+     * =====================================================================================
+     */
+    private static function is_event_attended($cache_id, $user_id)
+    {
+        $q = 'SELECT user_id FROM cache_logs WHERE cache_id =:v1 AND user_id =:v2 AND type = 7 AND Deleted=0';
+        $db = OcDb::instance();
+        $params = [];
+        $params['v1']['value'] = (integer) $cache_id;
+        $params['v1']['data_type'] = 'integer';
+        $params['v2']['value'] = (integer) $user_id;
+        $params['v2']['data_type'] = 'integer';
+        $s = $db->paramQuery($q, $params);
+        $rec = $db->dbResultFetch($s);
+        if (isset($rec['user_id'])) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
     public static function checkCacheStatusByUser($record, $userId)
     {
