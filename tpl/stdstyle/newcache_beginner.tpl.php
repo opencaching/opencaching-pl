ecebb24f250a5453dc6b88554d22b73563fa836f
Grzegorz deg <deg@best.pl>
2018-03-23 00:39:55 +0100

    Additional space in newcache_beginner.tpl

--- tpl/stdstyle/newcache_beginner.tpl.php
+++ tpl/stdstyle/newcache_beginner.tpl.php
@@ -15,6 +15,7 @@ use lib\Objects\GeoCache\GeoCacheCommons;
     <p><img src="<?=GeoCacheCommons::CacheIconByType(GeoCacheCommons::TYPE_QUIZ, GeoCacheCommons::STATUS_READY)?>" alt="<?=tr('quiz')?>" class="icon32"> <?=tr('quiz')?></p>
     <p><img src="<?=GeoCacheCommons::CacheIconByType(GeoCacheCommons::TYPE_MOVING, GeoCacheCommons::STATUS_READY)?>" alt="<?=tr('moving')?>" class="icon32"> <?=tr('moving')?></p>
     <p><img src="<?=GeoCacheCommons::CacheIconByType(GeoCacheCommons::TYPE_OTHERTYPE, GeoCacheCommons::STATUS_READY)?>" alt="<?=tr('unknown_type')?>" class="icon32"> <?=tr('unknown_type')?></p>
+    <div class="buffer"></div>
     <p>
       {{nc_begin_03}}
       <strong><?=$view->caches_find ?></strong>
