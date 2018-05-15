66ba9463ca73737bc303bfb22eb4ec56cc915749
rapotek <17698165+rapotek@users.noreply.github.com>
2018-05-15 16:04:58 +0200

    Vue.js libraries moved do CDN

--- tpl/stdstyle/chunks/vuejs.tpl.php
+++ tpl/stdstyle/chunks/vuejs.tpl.php
@@ -17,16 +17,30 @@ return function (){
 ?>
 
 <!-- Start of vue.js chunk -->
-<script src="<?php print Uri::getLinkWithModificationTime('/tpl/stdstyle/js/vue/vue.min.js')?>"></script>
-<script src="<?php print Uri::getLinkWithModificationTime('/tpl/stdstyle/js/vue/vue-resource.min.js')?>"></script>
-<script src="<?php print Uri::getLinkWithModificationTime('/tpl/stdstyle/js/vue/vuex.min.js')?>"></script>
+<script
+    src="https://unpkg.com/vue@2.5.16/dist/vue.min.js"
+    integrity="sha384-DwsLA0O/He+RjlS7pFkqEHfsCgdTMU+nSuUq/qkxvKSTED+s4vRttKEZtf4xTW1+"
+    crossorigin="anonymous"
+></script>
+<script
+    src="https://unpkg.com/vue-resource@1.5.0/dist/vue-resource.min.js"
+    integrity="sha384-w6PAHp9EeONrNm12NmQNxnIyu3xA6xGondegkDlwhGvcx1TvCdryOXnyCUcZDCCo"
+    crossorigin="anonymous"
+></script>
+<script
+    src="https://unpkg.com/vuex@2.0.0/dist/vuex.min.js"
+    integrity="sha384-VTkbLdY9G8IFsCoUDQVwFpReiiIuYpuIlclIXSmsUDeka16ux+be4CduSohbgkxO"
+    crossorigin="anonymous"
+></script>
 <script>
+<!--
 if (/MSIE \d|Trident.*rv:/.test(navigator.userAgent)) {
     document.write(
-'<script src="<?php print Uri::getLinkWithModificationTime('/tpl/stdstyle/js/vue/es6-promise.min.js')?>"><\/script>'
-+'<script src="<?php print Uri::getLinkWithModificationTime('/tpl/stdstyle/js/vue/es6-promise.auto.min.js')?>"><\/script>'
+'<script src="https://cdn.jsdelivr.net/npm/es6-promise@4/dist/es6-promise.min.js" integrity="sha384-2aj/bCHsl/sW9Br7V4KFv8G2BBHuiQ7ZOzY+OxTo7whNRvJY4FauRJbna1IVAvos" crossorigin="anonymous"></script>'
++'<script src="https://cdn.jsdelivr.net/npm/es6-promise@4/dist/es6-promise.auto.min.js" integrity="sha384-tQnfZyyk7ZX5leaWDkq9qAvwSkSvH0ouVfrxLn12X9Y2DS8nDa8pHXFH9LLKJdo/" crossorigin="anonymous"></script>'
     );
 }
+// -->
 </script>
 <!-- End of vue.js chunk -->
 
