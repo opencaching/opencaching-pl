4f6d7b56acd17e6778fb07392021e9ff83afd4a2
Grzegorz deg <deg@best.pl>
2018-09-13 21:51:01 +0200

    OC new look

---
+++ Config/banner.pl.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ *
+ * Texts for videobanner for OC PL
+ *
+ */
+
+$bannerTxt = [
+    "Największy na świecie bezpłatny serwis geocachingowy",
+    "Geościeżki - skupiające wiele keszy",
+    "Ponad 500 Geościeżek w Polsce!",
+    "Pełne statystyki, GPXy, wszystko za free!",
+    "Powiadomienia mailem o nowych keszach i logach",
+    "Centrum Obsługi Geokeszera wybierane przez Społeczność",
+    "100% funkcjonalności dostępnej jest bezpłatnie",
+    "Liberalne zasady publikacji keszy",
+];
--- Utils/View/View.php
+++ Utils/View/View.php
@@ -28,6 +28,7 @@ class View {
     private $_localJs = [];     // page-local JS scripts loaded from controller
 
     private $_showGdprPage = false;
+    private $_showVideoBannerState = false;
 
     private $_headerChunks = []; // chunks to load in <head> of the page
 
@@ -269,6 +270,29 @@ class View {
             $this->_loadTimepicker = false;
             $this->_loadFancyBox = false;
         }
+    }
+
+
+    /**
+     * Returns if should show video banner + slider on page
+     *
+     * @return boolean
+     */
+    public function showVideoBanner()
+    {
+        if (is_null(ApplicationContainer::Instance()->getLoggedUser()))
+            return true;
+        return $this->_showVideoBannerState;
+    }
+
+    /**
+     * Set to show (or not) video banner + slider on page
+     *
+     * @param boolean $state
+     */
+    public function setVideoBanner($state)
+    {
+        $this->_showVideoBannerState = boolval($state);
     }
 
     /**
