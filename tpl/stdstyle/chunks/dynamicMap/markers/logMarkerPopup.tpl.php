fbfded83149637dfe9c25bd73f61919224c9518a
rapotek <17698165+rapotek@users.noreply.github.com>
2018-09-14 23:00:36 +0200

    Small improvements to icons and popups

--- tpl/stdstyle/chunks/dynamicMap/markers/logMarkerPopup.tpl.php
+++ tpl/stdstyle/chunks/dynamicMap/markers/logMarkerPopup.tpl.php
@@ -2,14 +2,14 @@
 
 <div class="iw-container">
   <div class="iw-title">
-    <img class="iw-icon" src="{{icon}}" alt="{{wp}}" title="{{wp}}">
+    <img class="iw-icon dynamicMap_mapPopup-cacheIcon" src="{{icon}}" alt="{{wp}}" title="{{wp}}">
     <a href="{{link}}" target="_blank">
       <span class="iw-name">{{name}}</span></a>
     {{#if username}}({{username}}){{/if}}
   </div>
 
   {{#if log_link}}
-  <div class="iw-log">
+  <div class="iw-log dynamicMap_mapPopup-cacheLogBlock">
     <a href="{{log_link}}" target="_blank">
       <img src="{{log_icon}}" title="{{log_typeName}}" alt="{{log_typeName}}">
       <strong>{{log_username}}</strong> ({{log_date}})
@@ -19,4 +19,5 @@
     </a>
   </div>
   {{/if}}
+  <span class="dynamicMap_mapPopup-closer"></span>
 </div>
