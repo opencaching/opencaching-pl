e3b3c9f7eeafd0170372677618adf9f294f26f43
rapotek <17698165+rapotek@users.noreply.github.com>
2018-10-19 10:27:46 +0200

    Table cache_visits2 made more strict mode ready

---
+++ sqlAlters/2018-10-19_cache_visits2_strict.sql
@@ -0,0 +1,28 @@
+-- 2018-10-19
+-- @author: rapotek
+
+--
+-- Alters the cache_visits2 table user_id_ip column adding empty string as default.
+-- The alter is made to make the column ready for strict mode inserts with no value provided for it
+-- The procedure is only to ensure there are not nulls in the column, i.e. values not provided in inserts
+-- for not null varchar column are converted to empty strings by mysql in no strict mode. Otherwise there
+-- could be an inconsistency in data and duplicate 'C'-type rows for cache_id after alter is done.
+--
+DROP PROCEDURE IF EXISTS make_cache_visits2_more_strict;
+
+DELIMITER ;;
+
+CREATE PROCEDURE make_cache_visits2_more_strict()
+BEGIN
+    SELECT COUNT(*) INTO @nullUserIdIps FROM `cache_visits2` WHERE user_id_ip IS NULL;
+    IF @nullUserIdIps = 0 THEN
+        ALTER TABLE `cache_visits2` MODIFY `user_id_ip` VARCHAR(15) NOT NULL DEFAULT '' COMMENT 'user_id or used IP address';
+    END IF;
+END;;
+
+DELIMITER ;
+
+CALL make_cache_visits2_more_strict;
+
+DROP PROCEDURE IF EXISTS make_cache_visits2_more_strict;
+
