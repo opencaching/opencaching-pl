a3ba93122707c1786217b0cac766b65de1d751ee
Grzegorz deg <deg@best.pl>
2018-11-03 01:21:26 +0100

    Better PL texts on videobanner

--- Config/banner.pl.php
+++ Config/banner.pl.php
@@ -10,9 +10,9 @@ $bannerTxt = [
     "Największy na świecie niekomercyjny serwis geocachingowy",
     "GeoŚcieżki - skupiające wiele keszy",
     "Ponad 500 GeoŚcieżek w Polsce!",
-    "Pełne statystyki, GPXy, wszystko za free!",
+    "Pełne statystyki, GPXy, wszystko za darmo!",
     "Powiadomienia mailem o nowych keszach i logach",
     "Centrum Obsługi Geokeszera wybierane przez Społeczność",
     "100% funkcjonalności dostępne bezpłatnie",
-    "Liberalne zasady publikacji keszy",
+    "Przyjazne zasady publikacji keszy",
 ];
