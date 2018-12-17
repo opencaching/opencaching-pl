ff2b15843f6410d830677156e13f95defff46482
deg <deg@best.pl>
2018-12-17 22:45:33 +0100

    Cron Autoarchive refactor (fixes #1279, fixes #682)

--- Controllers/Cron/Jobs/AutoArchiveCachesJob.php
+++ Controllers/Cron/Jobs/AutoArchiveCachesJob.php
@@ -5,194 +5,233 @@
  */
 
 use Controllers\Cron\Jobs\Job;
-use lib\Objects\OcConfig\OcConfig;
 use lib\Objects\GeoCache\GeoCache;
-use Utils\Database\OcDb;
-use Utils\Log\Log;
+use lib\Objects\GeoCache\GeoCacheLog;
+use Utils\Email\Email;
+use Utils\Email\EmailFormatter;
 use Utils\Generators\Uuid;
 
 class AutoArchiveCachesJob extends Job
 {
-    private $step = array(
-        "START" => 0,
-        "AFTER_FIRST_MAIL_SENT" => 1,
-        "AFTER_SECOND_MAIL_SENT" => 2,
-        "ARCH_COMPLETE" => 3
-    );
+    const STEP_0_STAGED = 0;
+    const STEP_1_FIRST_MAIL_SENT = 1;
+    const STEP_2_SECOND_MAIL_SENT = 2;
+    const STEP_3_ARCHIVED = 3;
+    const ARCHIVE_EVENT = 100;
+
+    const TEMPLATE_DIR = __DIR__.'/../../../tpl/stdstyle/email/autoarchive/';
+
+    public function run()
+    {
+        $this->processCaches();
+
+        $this->processEvents();
+    }
 
     /**
-     * step:
+     * Processes caches to auto archive in 3 steps procedure:
      *   1: first notification (4 months)
      *   2: second notification (5 months)
-     *   3: autoarchive finished
+     *   3: auto archive
      */
-    public function run()
+    private function processCaches()
     {
         $this->removeLastEditedCachesFromList();
 
-        $db = OcDb::instance();
+        $stmt = $this->db->multiVariableQuery(
+            "SELECT `cache_id`, `last_modified`
+              FROM `caches`
+              WHERE `status` = :1
+                AND `last_modified` < NOW() - INTERVAL 4 MONTH",
+            GeoCache::STATUS_UNAVAILABLE);
 
-        $s = $db->simpleQuery(
-            "SELECT cache_id, last_modified FROM caches WHERE status = 2 AND last_modified < now() - interval 4 month");
+        while ($row = $this->db->dbResultFetch($stmt)) {
+            $now = new DateTime();
+            $lastModified = new DateTime($row['last_modified']);
+            $cache = GeoCache::fromCacheIdFactory($row['cache_id']);
+            $step = $this->db->multiVariableQueryValue(
+                "SELECT `step` FROM `cache_arch` WHERE `cache_id` = :1",
+                self::STEP_0_STAGED,
+                $cache->getCacheId()
+            );
 
-        $chachesToProcess = $db->dbResultFetchAll($s);
-        foreach ($chachesToProcess as $rs) {
-
-            $s = $db->multiVariableQuery(
-                "SELECT step FROM cache_arch WHERE cache_id = :1 LIMIT 1", (int) $rs['cache_id']);
-            $step_array = $db->dbResultFetchOneRowOnly($s);
-
-            if ($step_array) {
-                $step = (int) $step_array['step'];
-            } else {
-                $step = $this->step["START"];
+            if ($step == self::STEP_0_STAGED && $lastModified->diff($now)->days > 4 * 31) {
+                $this->proceedFirstStep($cache);
+            } elseif ($step == self::STEP_1_FIRST_MAIL_SENT && $lastModified->diff($now)->days > 5 * 31) {
+                $this->proceedSecondStep($cache);
+            } elseif ($step == self::STEP_2_SECOND_MAIL_SENT && $lastModified->diff($now)->days > 6 * 31) {
+                $this->archiveGeocache($cache);
             }
-            if (strtotime($rs['last_modified']) < time() - 6 * 31 * 24 * 60 * 60 && $step == $this->step["AFTER_SECOND_MAIL_SENT"]) {
-                $this->archiveGeocache($rs);
-            } elseif (strtotime($rs['last_modified']) < time() - 5 * 31 * 24 * 60 * 60 && $step == $this->step["AFTER_FIRST_MAIL_SENT"]) {
-                $this->proceedSecondStep($rs);
-            } elseif (strtotime($rs['last_modified']) < time() - 4 * 31 * 24 * 60 * 60 && $step == $this->step["START"]) {
-                $this->proceedFirstStep($rs);
-            }
+
+            unset($cache);
         }
 
-        $this->ArchEvent();
-    }
-
-
-    private function sendEmail($step, $cacheid)
-    {
-        $octeamEmailAddress = $this->ocConfig->getOcteamEmailAddress();
-        $siteName = $this->ocConfig->getSiteName();
-        $cache = new GeoCache(array('cacheId' => (int) $cacheid));
-        switch ($step) {
-            case $this->step["START"]:
-                $email_content = file_get_contents(__DIR__.'/../../../tpl/stdstyle/email/arch1.email');
-                break;
-            case $this->step["AFTER_FIRST_MAIL_SENT"]:
-                $email_content = file_get_contents(__DIR__.'/../../../tpl/stdstyle/email/arch2.email');
-                break;
-            case $this->step["AFTER_SECOND_MAIL_SENT"]:
-                $email_content = file_get_contents(__DIR__.'/../../../tpl/stdstyle/email/arch3.email');
-                break;
-        }
-        $email_content = mb_ereg_replace('{server}', $this->ocConfig->getAbsolute_server_URI(), $email_content);
-        $email_content = mb_ereg_replace('{autoArchive_01}', tr('autoArchive_01'), $email_content);
-        $email_content = mb_ereg_replace('{autoArchive_02}', tr('autoArchive_02'), $email_content);
-        $email_content = mb_ereg_replace('{autoArchive_03}', tr('autoArchive_03'), $email_content);
-        $email_content = mb_ereg_replace('{autoArchive_04}', tr('autoArchive_04'), $email_content);
-        $email_content = mb_ereg_replace('{autoArchive_05}', tr('autoArchive_05'), $email_content);
-        $email_content = mb_ereg_replace('{autoArchive_06}', tr('autoArchive_06'), $email_content);
-        $email_content = mb_ereg_replace('{autoArchive_07}', tr('autoArchive_07'), $email_content);
-        $email_content = mb_ereg_replace('{autoArchive_08}', tr('autoArchive_08'), $email_content);
-        $email_content = mb_ereg_replace('{autoArchive_09}', tr('autoArchive_09'), $email_content);
-        $email_content = mb_ereg_replace('{autoArchive_10}', tr('autoArchive_10'), $email_content);
-        $email_content = mb_ereg_replace('{cachename}', $cache->getCacheName(), $email_content);
-        $email_content = mb_ereg_replace('{cache_wp}', $cache->getWaypointId(), $email_content);
-        $email_content = mb_ereg_replace('{cacheid}', $cacheid, $email_content);
-        $email_content = mb_ereg_replace('{octeamEmailsSignature}', $this->ocConfig->getOcteamEmailsSignature(), $email_content);
-        $emailheaders = "Content-Type: text/plain; charset=utf-8\r\n";
-        $emailheaders .= "From: $siteName <$octeamEmailAddress>\r\n";
-        $emailheaders .= "Reply-To: $siteName <$octeamEmailAddress>";
-        $status = mb_send_mail($cache->getOwner()->getEmail(), tr('autoArchive_11'), $email_content, $emailheaders);
-        if(!$status){
-            error_log(__FILE__.':'.__LINE__.': Mail sending failure: to:'.$cache->getOwner()->getEmail());
-        }
-        Log::logentry(Log::EVENT_AUTOARCHIVE, $cache->getOwner()->getUserId(), $cache->getCacheId(), 0, 'Sending mail to ' . $cache->getOwner()->getEmail(), array('status' => $status));
-    }
-
-    private function loadCachesToProcess()
-    {
-        $db = OcDb::instance();
-
-        $s = $db->simpleQuery(
-            "SELECT cache_arch.step, caches.cache_id, caches.name, user.username
-            FROM `cache_arch`, caches, user
-            WHERE (step=1 OR step=2 OR step=3)
-                AND (caches.cache_id = cache_arch.cache_id)
-                AND (caches.user_id = user.user_id)
-            ORDER BY step ASC");
-        return $db->dbResultFetchAll($s);
+        return $this;
     }
 
     /**
-     * anulowanie procedury archiwizacji, jeśli opis skrzynki został zmodyfikowany w ciągu 6 miesięcy
+     * Send first notification (after 4 months)
+     *
+     * @param GeoCache $cache
+     * @return AutoArchiveCachesJob
+     */
+    private function proceedFirstStep(GeoCache $cache)
+    {
+        $this->updateCacheStepInDb($cache, self::STEP_1_FIRST_MAIL_SENT)
+            ->sendEmail($cache, self::STEP_1_FIRST_MAIL_SENT);
+
+        return $this;
+    }
+
+    /**
+     * Send second notification (after 5 months)
+     *
+     * @param GeoCache $cache
+     * @return AutoArchiveCachesJob
+     */
+    private function proceedSecondStep(GeoCache $cache)
+    {
+        $this->updateCacheStepInDb($cache, self::STEP_2_SECOND_MAIL_SENT)
+            ->sendEmail($cache, self::STEP_2_SECOND_MAIL_SENT);
+
+        return $this;
+    }
+
+    /**
+     * Archive Geocache (after 6 months)
+     *
+     * @param GeoCache $cache
+     * @return AutoArchiveCachesJob
+     */
+    private function archiveGeocache(GeoCache $cache)
+    {
+        $this->db->beginTransaction();
+
+        $this->updateCacheStepInDb($cache, self::STEP_3_ARCHIVED);
+        $cache->updateStatus(GeoCache::STATUS_ARCHIVED);
+        $this->db->multiVariableQuery(
+            "INSERT INTO `cache_logs`
+              (`cache_id`, `uuid`, `user_id`, `type`, `date`, `last_modified`,
+               `date_created`, `text`, `owner_notified`, `node`)
+              VALUES ( :1, :2, '-1', :3, NOW(), NOW(), NOW(), :4, 1, :5)",
+            $cache->getCacheId(),
+            Uuid::create(),
+            GeoCacheLog::LOGTYPE_ARCHIVED,
+            tr('autoArchiveLog'),
+            $this->ocConfig->getOcNodeId());
+
+        if ($this->db->commit()) {
+            $this->sendEmail($cache, self::STEP_3_ARCHIVED);
+        }
+
+        return $this;
+    }
+
+
+    private function sendEmail(GeoCache $cache, $reason)
+    {
+        $email = new Email();
+        if (!$email->addToAddr($cache->getOwner()->getEmail())) {
+            unlink($email);
+
+            return;
+        }
+
+        $email_template = self::TEMPLATE_DIR;
+        $subject = '';
+        switch ($reason) {
+            case self::STEP_1_FIRST_MAIL_SENT:
+                $email_template .= 'step1.email.html';
+                $subject = tr('autoArchiveSubject_12');
+                break;
+            case self::STEP_2_SECOND_MAIL_SENT:
+                $email_template .= 'step2.email.html';
+                $subject = tr('autoArchiveSubject_12');
+                break;
+            case self::STEP_3_ARCHIVED:
+                $email_template .= 'step3.email.html';
+                $subject = tr('autoArchiveSubject_3');
+                break;
+            case self::ARCHIVE_EVENT:
+                $email_template .= 'event.email.html';
+                $subject = tr('autoArchiveEventSubject');
+                break;
+        }
+        $subject .= ' '.$cache->getWaypointId().' '.$cache->getCacheName();
+
+        $formattedMessage = new EmailFormatter($email_template, true);
+        $formattedMessage->addFooterAndHeader($cache->getOwner()->getUserName(), true)
+            ->setVariable('cacheName', $cache->getCacheName())
+            ->setVariable('cacheWp', $cache->getGeocacheWaypointId())
+            ->setVariable('cacheUrl', $this->ocConfig->getAbsolute_server_URI().$cache->getCacheUrl());
+
+        $email->setFromAddr($this->ocConfig->getOcteamEmailAddress());
+        $email->setReplyToAddr($this->ocConfig->getOcteamEmailAddress());
+        $email->setFromAddr($this->ocConfig->getOcteamEmailAddress());
+        $email->setSubject($subject);
+        $email->addSubjectPrefix($this->ocConfig->getMailSubjectPrefixForSite());
+        $email->setBody($formattedMessage->getEmailContent(), true);
+        $email->send();
+    }
+
+    /**
+     * Cancel auto archive procedure if cache was modified
      */
     private function removeLastEditedCachesFromList()
     {
-        $db = OcDb::instance();
+        $this->db->simpleQuery(
+            "DELETE FROM `cache_arch` WHERE `cache_arch`.`cache_id` IN (
+                      SELECT `caches`.`cache_id` as `cache_id`
+                      FROM `caches`, `cache_arch`
+                      WHERE `cache_arch`.`cache_id` = `caches`.`cache_id`
+                        AND `caches`.`last_modified` >= NOW() - INTERVAL 4 MONTH 
+                      )"
+        );
 
-        $s = $db->simpleQuery(
-            'SELECT caches.cache_id as cacheId FROM caches, cache_arch
-            WHERE cache_arch.cache_id = caches.cache_id
-                AND last_modified >= now() - interval 4 month ');
-        $cachesToRm = $db->dbResultFetchAll($s);
-        foreach ($cachesToRm as $cacheToRm) {
-            $delSqlQuery = "DELETE FROM cache_arch WHERE cache_id = :1 LIMIT 1";
-            $db->multiVariableQuery($delSqlQuery, (int) $cacheToRm['cacheId']);
-        }
+        return $this;
     }
 
-    /**
-     * 6 months from last
-     * @param array $rs
-     */
-    private function archiveGeocache($rs)
+    private function updateCacheStepInDb(GeoCache $cache, $step)
     {
-        global $oc_nodeid;
-        $db = OcDb::instance();
+        $this->db->multiVariableQuery(
+            "REPLACE INTO `cache_arch` (`cache_id`, `step`) VALUES (:1, :2 )",
+            $cache->getCacheId(),
+            $step
+        );
 
-        $statusSqlQuery = "REPLACE INTO cache_arch (cache_id, step) VALUES ( :1, :2 )";
-        $archSqlQuery = "UPDATE caches SET status = 3 WHERE cache_id= :1 " ;
-        $logSqlQuery = "INSERT INTO cache_logs (cache_id, uuid, user_id, type, date, last_modified,
-                                    date_created, text, owner_notified, node)
-                        VALUES ( :1, :2, '-1', 9,NOW(),NOW(), NOW(), :3, 1, :4)";
-
-        $db->beginTransaction();
-
-        $db->multiVariableQuery($statusSqlQuery, (int) $rs['cache_id'], $this->step["ARCH_COMPLETE"]);
-        $db->multiVariableQuery($archSqlQuery, (int) $rs['cache_id']);
-        $db->multiVariableQuery($logSqlQuery, (int) $rs['cache_id'],  Uuid::create(), tr('autoArchive_12'), $oc_nodeid);
-
-        if ($db->commit()) {
-            $this->sendEmail($this->step["AFTER_SECOND_MAIL_SENT"], $rs['cache_id']);
-        }
-    }
-
-    /**
-     * second notification
-     * @param array $rs
-     */
-    private function proceedSecondStep($rs)
-    {
-        $this->updateCacheStepInDb($rs, $this->step["AFTER_SECOND_MAIL_SENT"]);
-        $this->sendEmail($this->step["AFTER_FIRST_MAIL_SENT"], $rs['cache_id']);
-    }
-
-    /**
-     * first notification
-     */
-    private function proceedFirstStep($rs)
-    {
-        $this->updateCacheStepInDb($rs, $this->step["AFTER_FIRST_MAIL_SENT"]);
-        $this->sendEmail($this->step["START"], $rs['cache_id']);
-    }
-
-    private function updateCacheStepInDb($rs, $step)
-    {
-        $db = OcDb::instance();
-        $db->multiVariableQuery(
-            "REPLACE INTO cache_arch (cache_id, step) VALUES (:1, :2 )", (int) $rs['cache_id'], $step);
+        return $this;
     }
 
     /**
      * archive all events older than 2 months
      */
-    function ArchEvent()
+    private function processEvents()
     {
-        $db = OcDb::instance();
-        $db->simpleQuery(
-            "UPDATE caches SET status = 3
-            WHERE status<>3 AND type = 6 AND date_hidden < now() - interval 2 month");
+        $stmt = $this->db->multiVariableQuery(
+            "SELECT `cache_id`
+            FROM `caches`
+            WHERE `type` = :1
+                  AND `status` <> :2
+                  AND `date_hidden` < NOW() - INTERVAL 2 MONTH",
+            GeoCache::TYPE_EVENT,
+            GeoCache::STATUS_ARCHIVED
+        );
+        while ($row = $this->db->dbResultFetch($stmt)) {
+            $cache = GeoCache::fromCacheIdFactory($row['cache_id']);
+            $cache->updateStatus(GeoCache::STATUS_ARCHIVED);
+
+            $this->db->multiVariableQuery(
+                "INSERT INTO `cache_logs`
+              (`cache_id`, `uuid`, `user_id`, `type`, `date`, `last_modified`,
+               `date_created`, `text`, `owner_notified`, `node`)
+              VALUES ( :1, :2, '-1', :3, NOW(), NOW(), NOW(), :4, 1, :5)",
+                $cache->getCacheId(),
+                Uuid::create(),
+                GeoCacheLog::LOGTYPE_ARCHIVED,
+                tr('autoArchiveEventLog'),
+                $this->ocConfig->getOcNodeId());
+
+            $this->sendEmail($cache, self::ARCHIVE_EVENT);
+        }
     }
 }
