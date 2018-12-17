9d318500af90645733e5a1193968a82ba901505f
deg <deg@best.pl>
2018-12-17 13:41:27 +0100

    Fix for PHP Notice:  Undefined variable: argc in /srv/oc.conspiracy.pl/util.sec/cron/run_cron.php on line 9

--- util.sec/cron/run_cron.php
+++ util.sec/cron/run_cron.php
@@ -5,7 +5,12 @@ require_once (__DIR__.'/../../lib/common.inc.php');
 
 // This script can be called with a single cronjob name as argument
 // for testing that job.
+if (PHP_SAPI == "cli" && $argc == 2) {
+    $job = $argv[1];
+} else {
+    $job = null;
+}
 
-$ctrl = new CronJobsController($argc == 2 ? $argv[1] : null);
+$ctrl = new CronJobsController($job);
 $ctrl->index();
 exit();
