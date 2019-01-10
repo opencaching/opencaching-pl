bfc4a148bea1e3ccfd3c93c38c0bea81e112cfb2
ocpl-crowdin-robot <46261132+ocpl-crowdin-robot@users.noreply.github.com>
2019-01-10 10:04:37 +0100

    New translations en.php (Polish)

--- lib/languages/pl.php
+++ lib/languages/pl.php
@@ -2670,4 +2670,8 @@ $translations = array(
 
     'pictures_intro' => 'Akceptowane formaty: {picallowedformats} - dla zdjęć rekomendowany format JPG .<br />Maksymalna dozwolona wielkość pliku {maxpicsizeMB} MB. Zalecane wymiary obrazków: {maxpicresolution} pikseli. <br />Po potwierdzeniu przetwarzanie może potrwać dłuższą chwilę.',
 
+    'page_error_1' => 'An error occured while processing your request.',
+    'page_error_2' => 'The OC site admins have been notified.',
+    'page_error_3' => 'If the problem persists, please <a href="/articles.php?page=contact">contact</a> the OC team and describe step by step how to reproduce this error.',
+    'page_error_back' => 'Go to the main page',
 );
