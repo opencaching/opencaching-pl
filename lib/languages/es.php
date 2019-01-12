60b6052cba07a5f136bcdcaf915a3d3764a975fd
ocpl-crowdin-robot <46261132+ocpl-crowdin-robot@users.noreply.github.com>
2019-01-12 14:42:09 +0100

    New translations en.php (Spanish)

--- lib/languages/es.php
+++ lib/languages/es.php
@@ -307,7 +307,7 @@ $translations = array(
     'size_incorrect' => 'No de tamano de buzón!',
     'diff_incorrect' => 'El nivel de dificultad, está mal!',
     'status_incorrect' => 'Status does not match published data.',
-    'virtual_cache_size' => 'El tamano del buzón de correo en el caso de la event y debe ser ajustado a \\"ningún cuadro de\\"!',
+    'virtual_cache_size' => 'El tamano del buzón de correo en el caso de la event y debe ser ajustado a "ningún cuadro de"!',
     'full_description' => 'Nombre completo',
     'mandatory_field' => 'La obligación',
     'full_desc_long_text' => 'Pulse aquí información acerca de por qué ukryłeś cuadro se encuentra en este lugar, y <span class="txt-red07"> toda la información necesaria </span> para encontrar la caja. La búsqueda de las cajas debe basarse en el conocimiento de sus coordenadas y el nombre .',
