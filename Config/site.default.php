ea1732f0cfa6eadc9d75b235f79901206ebae7b6
kojoty <p.juzwiak@gmail.com>
2019-01-14 20:50:35 +0100

    site.default.php - default config for site

--- Config/site.default.php
+++ Config/site.default.php
@@ -7,7 +7,18 @@ use lib\Objects\GeoCache\GeoCacheCommons;
  */
 
 /**
- * Theses sizes are not available for
+ * Configuration of general site properties in defaut version
+ */
+$site = [];
+
+
+/**
+ * Primary country (or countries) for this node
+ */
+$site['primaryCountries'] = ['PL'];
+
+/**
+ * Theses sizes are available for
  *    - creating caches
  *    - changing the type of a cache to this size
  *    - searching for caches.
@@ -22,3 +33,4 @@ $site['enabledCacheSizes'] = [
     GeoCacheCommons::SIZE_XLARGE,
     GeoCacheCommons::SIZE_NONE,
 ];
+
