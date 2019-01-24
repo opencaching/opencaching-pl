opencaching/opencaching-pl

commit aaf82371ce504fce635f2df0f1705ca765763134

improved Admin.DbUpdate

Controllers/Admin/DbUpdateController.php
@@ -55,16 +55,32 @@ class DbUpdateController extends BaseController
         $actions = [];  // dictionary of action => title
 
         $wasRun = ($update->wasRunAt() !== null);
-        if (!$wasRun) {
-            $actions['run'] = 'run';
-        } elseif (!$this->ocConfig->inDebugMode() && substr($update->getName(), 0, 3) < 100) {
-            $actions['run'] = 'run again';
-        }
+        $specialPurpose = substr($update->getName(), 0, 3) < 100;
 
-        // Devel / test actions, not available on production sites:
+        if (!$this->ocConfig->inDebugMode()) {
+            // production site
 
-        if ($this->ocConfig->inDebugMode()) {
             if (!$wasRun) {
+                $actions['run'] = 'run';
+            } else {
+                if ($update->getRuntype() == 'always' || $specialPurpose) {
+
+                    # For tests an special-purpose updates, it can make sense to
+                    # re-run them even on production sites, and to roll them back.
+
+                    $actions['run'] = 'run again';
+
+                    if ($update->hasRollback()) {
+                        $actions['rollback'] = 'rollback';
+                    }
+                }
+            }
+        } else {
+            // developer site
+
+            if (!$wasRun) {
+                $actions['run'] = 'run';
+
                 if ($update->hasRollback()) {
 
                     # There may be cases when a developer wants to test
@@ -73,13 +89,14 @@ class DbUpdateController extends BaseController
 
                     $actions['rollback'] = 'try rollback';
                 }
+
             } else {
                 # It can make sense to repeat an update, e.g. if there is no
                 # rollback method and the developer did a manual rollback.
 
                 $actions['run'] = 'run again';
 
-                if ($update->hasRollback() && !$update->isInGitMasterBranch()) {
+                if ($update->hasRollback() && (!$update->isInGitMasterBranch() || $specialPurpose)) {
 
                     # The workflow for rolling back an already deployed
                     # update is to create a new rollback-update and run that.
@@ -89,6 +106,7 @@ class DbUpdateController extends BaseController
                     $actions['rollback'] = 'rollback';
                 }
             }
+
             if (!$update->isInGitMasterBranch()) {
 
                 if (!($wasRun && $update->hasRollback())) {
@@ -99,6 +117,7 @@ class DbUpdateController extends BaseController
                     # helps to keep the database clean.
                 }
             }
+
             $actions['askRename'] = 'rename';
         }
 
@@ -131,14 +150,24 @@ class DbUpdateController extends BaseController
         // This action is allowed to run on production sites (by sysAdmins only).
         $this->securityCheck(false);
 
-        try {
-            if ($uuid) {
-                $messages = $this->getUpdateFromUuid($uuid)->run();
-            } else {
-                $messages = DbUpdates::run();
+        $update = $this->getUpdateFromUuid($uuid);
+
+        if (!isset($this->getAvailableActions($update)['run'])) {
+
+            # This can happen on page reload on a production site:
+            # Update was allowed to run, but must not re-run.
+
+            $messages = sprintf(tr('admin_dbupdate_norun'), $update->getName());
+        } else {
+            try {
+                if ($uuid) {
+                    $messages = $this->getUpdateFromUuid($uuid)->run();
+                } else {
+                    $messages = DbUpdates::run();
+                }
+            } catch (\Exception $e) {
+                $messages = get_class($e).": " . $e->getMessage() . "\n\n" . $e->getTraceAsString();
             }
-        } catch (\Exception $e) {
-            $messages = get_class($e).": " . $e->getMessage() . "\n\n" . $e->getTraceAsString();
         }
 
         $this->showAdminView($messages);
@@ -152,8 +181,26 @@ class DbUpdateController extends BaseController
 
     public function rollback($uuid)
     {
-        $this->securityCheck();
-        $messages = $this->getUpdateFromUuid($uuid)->rollback();
+        // This action is allowed to run on production sites for some special
+        // updates like tests (by sysAdmins only).
+        $this->securityCheck(false);
+
+        $update = $this->getUpdateFromUuid($uuid);
+
+        if (!isset($this->getAvailableActions($update)['rollback'])) {
+
+            # This can happen on page reload on a production site:
+            # Update was allowed to roll back, but only once.
+
+            $messages = sprintf(tr('admin_dbupdate_norollback'), $update->getName());
+        } else {
+            try {
+                $messages = $this->getUpdateFromUuid($uuid)->rollback();
+            } catch (\Exception $e) {
+                $messages = get_class($e).": " . $e->getMessage() . "\n\n" . $e->getTraceAsString();
+            }
+        }
+
         $this->showAdminView($messages);
 
         # See comment in run() method.

Utils/Database/DbUpdateHistory.php
@@ -11,8 +11,8 @@ use Utils\Generators\Uuid;
  * to the site. For high robustness, this (critical!) list is maintained
  * redundantly at two places:
  *
- *   - the database table db_update_history
- *   - the directory $dynbasepath/db_update_history
+ *   - in the database table db_update_history
+ *   - in the directory $dynbasepath/db_update_history
  *
  * Both are loaded and synchronized with the first call to a DbUpdateHistory
  * method. If an entry is missing in one of both (which means that data was

lib/languages/en.php
@@ -2530,6 +2530,8 @@ $translations = array(
     'admin_dbupdate_always' => 'always',
     'admin_dbupdate_time' => 'Was run at',
     'admin_dbupdate_run' => 'Run updates',
+    'admin_dbupdate_norun' => 'The update %s must not run (again).',
+    'admin_dbupdate_norollback' => 'The update %s must not be rolled back (again).',
     'admin_dbupdate_create' => 'Create new update',
     'admin_dbupdate_reload' => 'Reload list',
     'admin_dbupdate_develnote' => '<i>Italics</i> = Update is in development (not merged to Git master).',

tpl/stdstyle/sysAdmin/dbUpdate.tpl.php
@@ -47,6 +47,7 @@ use Utils\Uri\SimpleRouter;
         }
     </script>
 
+    <br />
     <input type="submit" class="btn btn-default btn-sm" value="{{rename}}" />
     <a class="btn btn-default btn-sm" href="<?= SimpleRouter::getLink('Admin.DbUpdate') ?>">{{cancel}}</a>
 
@@ -60,6 +61,7 @@ use Utils\Uri\SimpleRouter;
     <p><br />Do you really want to delete <b><?= $view->fileName ?></b> ?</p>
     <p><br /><i>Delete</i> will do a <b>git rm -f</b>, if the file is staged or commited in Git.</p>
 
+    <br />
     <a class="btn btn-default btn-sm" href="<?= SimpleRouter::getLink('Admin.DbUpdate', 'delete', $view->askDelete) ?>">{{delete}}</a>
     <a class="btn btn-default btn-sm" href="<?= SimpleRouter::getLink('Admin.DbUpdate') ?>">{{cancel}}</a>
 

commit d8dfb44025ec1afbccf2c22b81778e66759ecbe2

fixed Admin.DbUpdate bug introduced with aaf82371

Controllers/Admin/DbUpdateController.php
@@ -150,24 +150,24 @@ class DbUpdateController extends BaseController
         // This action is allowed to run on production sites (by sysAdmins only).
         $this->securityCheck(false);
 
-        $update = $this->getUpdateFromUuid($uuid);
+        try {
+            if (!$uuid) {
+                $messages = DbUpdates::run();
+            } else {
+                $update = $this->getUpdateFromUuid($uuid);
 
-        if (!isset($this->getAvailableActions($update)['run'])) {
+                if (!isset($this->getAvailableActions($update)['run'])) {
 
-            # This can happen on page reload on a production site:
-            # Update was allowed to run, but must not re-run.
+                    # This can happen on page reload on a production site:
+                    # Update was allowed to run, but must not re-run.
 
-            $messages = sprintf(tr('admin_dbupdate_norun'), $update->getName());
-        } else {
-            try {
-                if ($uuid) {
-                    $messages = $this->getUpdateFromUuid($uuid)->run();
+                    $messages = sprintf(tr('admin_dbupdate_norun'), $update->getName());
                 } else {
-                    $messages = DbUpdates::run();
+                    $messages = $this->getUpdateFromUuid($uuid)->run();
                 }
-            } catch (\Exception $e) {
-                $messages = get_class($e).": " . $e->getMessage() . "\n\n" . $e->getTraceAsString();
             }
+        } catch (\Exception $e) {
+            $messages = get_class($e).": " . $e->getMessage() . "\n\n" . $e->getTraceAsString();
         }
 
         $this->showAdminView($messages);