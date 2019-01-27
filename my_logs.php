78ae4d441e307c341b85f6409fcc7c5f667589c9
following <following@online.de>
2019-01-27 14:52:12 +0100

    sort own-logs list by cache_logs.date instead of date_created; fixes #83

--- my_logs.php
+++ my_logs.php
@@ -98,7 +98,6 @@ if ($usr == false) {
                 AND `caches`.`status` != 5
                 AND `caches`.`status` != 6
                 AND `cache_logs`.`user_id`= ?
-            ORDER BY  `cache_logs`.`date_created` DESC, `cache_logs`.`date` DESC, `cache_logs`.`id` DESC
             LIMIT ".intval($start).", ".intval($LOGS_PER_PAGE), $user_id);
 
     $log_ids = array();
@@ -110,6 +109,11 @@ if ($usr == false) {
     $file_content = '';
 
     if (!empty($log_ids)) {
+
+        $dateOrderSql =
+            $user_id == $usr['userid']
+            ?  '`cache_logs`.`date` DESC, `cache_logs`.`date_created` DESC'
+            :  '`cache_logs`.`date_created` DESC, `cache_logs`.`date` DESC';
 
         $rs = XDb::xSql(
             "SELECT cache_logs.id, cache_logs.cache_id AS cache_id, cache_logs.type AS log_type, cache_logs.date AS log_date,
@@ -129,7 +133,7 @@ if ($usr == false) {
                         AND gk_item.stateid<>1 AND gk_item.stateid<>4 AND gk_item.typeid<>2 AND gk_item.stateid !=5
                 WHERE cache_logs.deleted=0 AND cache_logs.id IN ( ".implode(',', $log_ids)." ) AND `cache_logs`.`user_id`= ? AND ".$logTypesCondition."
                 GROUP BY cache_logs.id
-                ORDER BY cache_logs.date_created DESC, `cache_logs`.`date` DESC, `cache_logs`.`id` DESC",
+                ORDER BY ".$dateOrderSql.", `cache_logs`.`id` DESC",
             $user_id);
 
         while ($log_record = XDb::xFetchArray($rs)) {
