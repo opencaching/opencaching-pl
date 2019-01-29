5bcf73160e8a6c91991f45a009b423cdfaa4f2c4
following <following@online.de>
2019-01-29 20:26:16 +0100

    fixed OC UK default country; add Danish workaround; updates #1913

---
+++ Config/site.uk.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+  * Configuration of general site properties of OC UK
+ */
+
+// UK is one of the few countries where the TLD differs from ISO-3166 code 
+
+$site['primaryCountries'] = ['GB'];
