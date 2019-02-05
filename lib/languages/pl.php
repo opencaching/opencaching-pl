746790d343df0864b7c5df6e876a60b2299ef7ba
ocpl-crowdin-robot <46261132+ocpl-crowdin-robot@users.noreply.github.com>
2019-02-05 18:12:56 +0100

    New translations en.php (Polish)

--- lib/languages/pl.php
+++ lib/languages/pl.php
@@ -2754,7 +2754,7 @@ $translations = array(
     'ogpx_otherOptions_recommendations' => 'Pokaż informację o liczbie przyznanych rekomendacji (w opisie skrzynki).',
     'ogpx_otherOptions_alt_wpts' => 'Dołącz wszystkie dodatkowe punkty/waypointy, które autor przypisał do skrzynki.',
     'ogpx_otherOptions_mark_found' => 'Oznacz skrzynki już znalezione (zostaną wygenerowane z symbolem otwartej skrzynki).',
-    'ogpx_additionalDownloadsHeaderHTML' => '<p>Gdy pobieranych jest powyżej 500 skrzynek, plik GPX jest dzielony na parę mniejszych plików. Każdy z nich musi być pobrany niezależnie:</p>',
+    'ogpx_additionalDownloadsHeaderHTML' => '<p>Gdy pobieranych jest powyżej 500 skrzynek, plik GPX jest dzielony na kilka mniejszych plików. Każdy z nich musi być pobrany niezależnie:</p>',
 
     'mobile_login' => '[zaloguj]',
     'mobile_logout' => '[wyloguj]',
