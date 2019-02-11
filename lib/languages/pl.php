7725c017fac878202ccac6a06cabee43db2df03d
ocpl-crowdin-robot <46261132+ocpl-crowdin-robot@users.noreply.github.com>
2019-02-11 11:54:06 +0100

    New translations en.php (Polish)

--- lib/languages/pl.php
+++ lib/languages/pl.php
@@ -381,6 +381,7 @@ $translations = array(
     'postActivation_mail_04' => 'Zanim założysz swoje pierwsze skrzynki w serwisie {site_name}, znajdź przynajmniej {NEED_FIND_LIMIT} skrzynek, by zdobyć doświadczenie.',
     'postActivation_mail_cacheguides' => 'Przewodnicy-wolontariusze po geocachingu',
 
+
     'password_confirm' => 'Potwierdź hasło',
     'register_msg1' => 'W celu rejestracji na {site_name} musisz posiadać rzeczywisty adres e-mail, na który zostanie wysłany link aktywacyjny. Na jeden adres e-mail możesz zarejestrować tylko jednego użytkownika.',
     'register_msg2' => 'Pola zaznaczone * są obowiązkowe.',
@@ -1827,6 +1828,7 @@ $translations = array(
     'statpic_hidden' => 'Ukrył:',
     'statpic_html_link' => '- Statystyka dla',
     'cog_user_name' => 'Centrum Obsługi Geocachera',
+    'page_not_found' => 'Page not found',
 
     'region_01' => 'Region dla współrzędnych',
     'region_03' => 'Region na podstawie danych lokalnych (NUTS)',
