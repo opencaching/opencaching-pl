7b4bc517a129de3654341758ea47fa3de9b377b4
kojoty <p.juzwiak@gmail.com>
2019-02-11 23:24:36 +0100

    folder refactor: /images -> /public/images

--- lib/staticmap.php
+++ lib/staticmap.php
@@ -45,7 +45,7 @@ Class staticMapLite
     );
 
     protected $tileDefaultSrc = 'mapnik';
-    protected $markerBaseDir = '../images/markers';
+    protected $markerBaseDir = __DIR__.'/../resources/images/staticMapMarkers';
     protected $atrribution = '(c) OpenStreetMap contributors';
 
     protected $markerPrototypes = array(
