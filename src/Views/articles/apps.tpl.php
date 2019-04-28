7573eb332f3e2cad5a37abb58c7dbe8cbef1269e
harrieklomp <harrie@harrieklomp.be>
2019-04-28 14:45:37 +0200

    Add US logo Is very small and well tested, so no problem for weekend rule.

---
+++ src/Views/articles/apps.tpl.php
@@ -0,0 +1,39 @@
+<table width="600" class="content">
+	<colgroup>
+		<col width="100">
+	</colgroup>
+	<tr><td width="750" class="content2-pagetitle">
+	<img src="public/images/free_icons/phone.png" class="icon32" alt="ABC" title="ABC" align="middle" />
+	<font size="4">  <b>Smartphone Apps</b></font></td></tr>
+	<tr><td class="spacer"></td></tr></table>
+<table class="content">
+<h1>These Smartphone Geocaching Apps offer full live support for Opencaching:</h1>
+<p style="font-size: 164x;"><ul style="font-size: 14px;">
+<li><a href="https://play.google.com/store/apps/details?id=cgeo.geocaching&hl=en" target="_blank">c:geo</a> For Android</li>
+<br />
+<li><a href="https://play.google.com/store/apps/details?id=com.gcbuddy.view" target="_blank">GeoCaching Buddy</a> For Android</li>
+<br />
+<li><a href="http://play.google.com/store/apps/details?id=geocaching.columbus" target="_blank">Columbus</a> For Android</li>
+<br />
+<li><a href="https://itunes.apple.com/app/id536646724" target="_blank">GeoCaches</a> For iphone</li>
+<br />
+<li><a href="http://www.geoviewcache.com" target="_blank">GeoView Cache</a> For iphone</li>
+<br />
+<li><a href="https://itunes.apple.com/us/app/geocaching-buddy/id298774229?mt=8" target="_blank">Geocaching Buddy</a> For iphone</li>
+<br />
+<li><a href="https://www.microsoft.com/en-us/store/p/geocaching-plus/9wzdncrfhxfb" target="_blank">Geocaching Plus</a> For Windows Phone</li>
+<br />
+<li><a href="https://www.microsoft.com/en-us/store/p/opencaching-arw/9nblggh0dqmd" target="_blank">OpenCaching ArW</a> For Windows Phone</li>
+</ul>
+</table>
+</p>
+<table class="content">
+<h1>Additionally, you can send our .GPX files to these Smartphone Apps:</h1>
+<p style="font-size: 14px;"><ul style="font-size: 14px;">
+<li><a href="https://itunes.apple.com/us/app/geocaching-with-geosphere/id307060076?mt=8" target="_blank">Geosphere</a> For iphone</li>
+<br />
+<li><a href="https://itunes.apple.com/us/app/cachly-simple-powerful-geocaching/id645384141?ls=1&mt=8" target="_blank">Cachly</a> For iphone</li>
+</ul>
+</table>
+</p>
+
---
+++ src/Views/articles/swag.tpl.php
@@ -0,0 +1,19 @@
+<table class="content">
+	<colgroup><col width="100"></colgroup>
+	<tr><td width="750" class="content2-pagetitle">
+	<img src="public/images/blue/colected.png" class="icon32" title="OCNA Swag!" align="middle" />
+	<font size="4">  <b>Purchase OCNA Swag!</b></font></td></tr>
+		<tr><td class="spacer"></td></tr>
+</table>
+<br />
+<br />
+<img style="float:left" src="/upload/bluet.png"> <center><p style="font-size: 14px;"><ul style="font-size: 14px;">
+The OCNA Swag store opened in August, 2013, and is hosted by <a href="http://opencachingna.spreadshirt.com" target="blank">Spreadshirt.com</a>. Spreadshirt.com is an International Company, founded in Germany in 2001. We of course use their American subsidiary based in Greensburg, Pennsylvania (suburban Pittsburgh). We don't just offer clothing in our Spreadshirt.com store, there you can find OCNA Buttons, Water Bottles, Beer Mugs, Coffee Cups, and more. They have our OCNA logo on file in high definition vector graphics, which was provided by our Graphic Artist Patrick Williams. We feel these items are <b>very</b> reasonably priced, much more so than their more well known competitor Cafepress.com. Please note that any issues with product quality or order fulfillment should be addressed to Spreadshirt.com customer service, not OCNA! </p> </center>
+<hr style="clear:both">
+<img style="float:left" src="/upload/front200clear.jpg"><center> <p style="font-size: 14px;"><ul style="font-size: 14px;"><br /> Our new 5th anniversary Pathtags sold out to the general public in April, 2016. We used the Pathtags fundraiser program, where we paid no money up front, and the tags were for sale in packs of 20 for $20. Only 400 were originally made. The site Admins placed a refill order in for 100 more tags (which included 10 bonus tags), So a total of 510 were manufactured. The admins will generously distribute those to promote the website. We thank all our users and supporters for helping us to sell out the first run, which did not happen for our OCNA 1st Edition Pathtags in 2013/2014.</p></center>
+<br />
+<br />
+<hr />
+<img style="float:left" src="/upload/thumb150_blueprint.jpg"> <center><p style="font-size: 14px;"><ul style="font-size: 14px;">
+Our 1st Edition Pathtags went on sale in in August, 2013, and sold out in March, 2014 (the last few packs were discounted). We also used the Pathtags fundraiser program with an initial run of 400. The site Admins did place a refill order, so 500 were originally made. These tags are now very rare! If you find one, consider yourself very lucky! They have technically never been retired, but it's very unlikely we will ever order more, in favor of commisioning a 3rd Edition OCNA Pathtag in the future. </p> </center>
+
