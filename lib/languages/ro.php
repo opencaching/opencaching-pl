105a3f91dd43c75529a5296c189ce9aaecaf37f8
ocpl-crowdin-robot <46261132+ocpl-crowdin-robot@users.noreply.github.com>
2019-05-18 12:33:50 +0200

    New translations en.php (Romanian)

--- lib/languages/ro.php
+++ lib/languages/ro.php
@@ -2186,6 +2186,7 @@ $translations = array(
     'lastLogList_geocacheName' => 'Geocache name',
     'lastLogList_foundBy' => 'Logs by user',
     'lastLogList_logEntry' => 'Log entry',
+    'lastLogList_logCreationDate' => 'Log created',
 
     'logbook_01' => 'Previzualizare jurnal',
     'logbook_02' => 'Tip',
