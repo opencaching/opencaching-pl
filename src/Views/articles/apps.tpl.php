c97ae6e095f5d52da767700ed72a59c5b4c44e4c
harrieklomp <harrie@harrieklomp.be>
2019-05-20 13:59:14 +0200

    Make page Smarthone apps available for translation

--- src/Views/articles/apps.tpl.php
+++ src/Views/articles/apps.tpl.php
@@ -4,42 +4,42 @@
 	</colgroup>
 	<tr><td width="750" class="content2-pagetitle">
 	<img src="public/images/free_icons/phone.png" class="icon32" alt="ABC" title="ABC" align="middle" />
-	<font size="4">  <b>Smartphone Apps</b></font></td></tr>
+	<font size="4">  <b>{{SmartPhone_Apps}}</b></font></td></tr>
 	<tr><td class="spacer"></td></tr></table>
 <table class="content">
-<h1>These Smartphone Geocaching Apps offer full live support for Opencaching:</h1>
+<h1>{{SmartPhone_Support}}</h1>
 <p style="font-size: 164x;"><ul style="font-size: 14px;">
-<li><a href="https://play.google.com/store/apps/details?id=cgeo.geocaching&hl=en" target="_blank">c:geo</a> For Android</li>
+<li><a href="https://play.google.com/store/apps/details?id=cgeo.geocaching&hl=en" target="_blank">c:geo</a> {{SmartPhone_Android}}</li>
 <br />
-<li><a href="https://play.google.com/store/apps/details?id=com.gcbuddy.view" target="_blank">GeoCaching Buddy</a> For Android</li>
+<li><a href="https://play.google.com/store/apps/details?id=com.gcbuddy.view" target="_blank">GeoCaching Buddy</a> {{SmartPhone_Android}}</li>
 <br />
-<li><a href="http://play.google.com/store/apps/details?id=geocaching.columbus" target="_blank">Columbus</a> For Android</li>
+<li><a href="http://play.google.com/store/apps/details?id=geocaching.columbus" target="_blank">Columbus</a> {{SmartPhone_Android}}</li>
 <br />
-<li><a href="https://itunes.apple.com/app/id536646724" target="_blank">GeoCaches</a> For iphone</li>
+<li><a href="https://itunes.apple.com/app/id536646724" target="_blank">GeoCaches</a> {{SmartPhone_Iphone}}</li>
 <br />
-<li><a href="http://www.geoviewcache.com" target="_blank">GeoView Cache</a> For iphone</li>
+<li><a href="http://www.geoviewcache.com" target="_blank">GeoView Cache</a> {{SmartPhone_Iphone}}</li>
 <br />
-<li><a href="https://itunes.apple.com/us/app/geocaching-buddy/id298774229?mt=8" target="_blank">Geocaching Buddy</a> For iphone</li>
+<li><a href="https://itunes.apple.com/us/app/geocaching-buddy/id298774229?mt=8" target="_blank">Geocaching Buddy</a> {{SmartPhone_Iphone}}</li>
 <br />
-<li><a href="https://www.microsoft.com/en-us/store/p/geocaching-plus/9wzdncrfhxfb" target="_blank">Geocaching Plus</a> For Windows Phone</li>
+<li><a href="https://www.microsoft.com/en-us/store/p/geocaching-plus/9wzdncrfhxfb" target="_blank">Geocaching Plus</a> {{SmartPhone_Windows}}</li>
 <br />
-<li><a href="https://www.microsoft.com/en-us/store/p/opencaching-arw/9nblggh0dqmd" target="_blank">OpenCaching ArW</a> For Windows Phone</li>
+<li><a href="https://www.microsoft.com/en-us/store/p/opencaching-arw/9nblggh0dqmd" target="_blank">OpenCaching ArW</a> {{SmartPhone_Windows}}</li>
 </ul>
 </table>
 </p>
 <table class="content">
-<h1>Additionally, you can send our .GPX files to these Smartphone Apps:</h1>
+<h1>{{SmartPhone_GPX}}</h1>
 <p style="font-size: 14px;"><ul style="font-size: 14px;">
-<li><a href="https://itunes.apple.com/us/app/geocaching-with-geosphere/id307060076?mt=8" target="_blank">Geosphere</a> For iphone</li>
+<li><a href="https://itunes.apple.com/us/app/geocaching-with-geosphere/id307060076?mt=8" target="_blank">Geosphere</a> {{SmartPhone_Iphone}}</li>
 <br />
-<li><a href="https://itunes.apple.com/us/app/cachly-simple-powerful-geocaching/id645384141?ls=1&mt=8" target="_blank">Cachly</a> For iphone</li>
+<li><a href="https://itunes.apple.com/us/app/cachly-simple-powerful-geocaching/id645384141?ls=1&mt=8" target="_blank">Cachly</a> {{SmartPhone_Iphone}}</li>
 </ul>
 </table>
 </p>
 <table class="content">
-<h1>Tools for Opencaching:</h1>
+<h1>{{SmartPhone_Tools}}</h1>
 <p style="font-size: 14px;"><ul style="font-size: 14px;">
-<li><a href="https://play.google.com/store/apps/details?id=eisbehr.gcc" target="_blank">GCC - GeoCache Calculator</a> For Android</li>
+<li><a href="https://play.google.com/store/apps/details?id=eisbehr.gcc" target="_blank">GCC - GeoCache Calculator</a> {{SmartPhone_Android}}</li>
 </ul>
 </table>
 </p>
