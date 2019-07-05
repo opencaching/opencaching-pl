3eb5928ddaf5e55ce6241a98addb8a0b709c6e16
harrieklomp <harrie@harrieklomp.be>
2019-07-05 14:40:51 +0200

    Some more OCUS config files

---
+++ config/guides.us.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * This is configuration of guides criterias.
+ *
+ * This is the guides criteria for OCUS.
+ *
+ */
+
+$guides['guideActivePeriod'] = 180;
+$guides['guideGotRecommendations'] = 3;
---
+++ config/i18n.us.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * This is configuration specific for OCUS node
+ * It contains only overrides to DEFAULT config
+ */
+
+
+/**
+ * List of supported languages
+ * Please note that for now still we have also translations in DB!
+ */
+$config['supportedLanguages'] = ['en', 'fr', 'nl', 'de', 'ro', 'pl'];
