7059ebaf05f04d275b3ee4103b9ed90aa551489a
harrieklomp <harrie@harrieklomp.be>
2019-07-05 23:35:16 +0200

    Update OCUS links with HTTPS.

--- config/links.us.php
+++ config/links.us.php
@@ -10,10 +10,10 @@
 
 
 //former $blogsite_url from settings.inc.php
-$links['blog'] = 'http://blog.opencaching.us';
+$links['blog'] = 'https://blog.opencaching.us';
 
 //former $forum_url from settings.inc.php
-$links['forum'] = 'http://forum.opencaching.us';
+$links['forum'] = 'https://forum.opencaching.us';
 
 // former $config['geokrety_url']
 $links['geokrety'] = 'https://geokrety.org';
@@ -22,7 +22,7 @@ $links['geokrety'] = 'https://geokrety.org';
  * This is local base for wiki links
  * (former $wiki_url in /lib/setting.inc.php)
  */
-$_wiki  = 'http://wiki.opencaching.us';
+$_wiki  = 'https://wiki.opencaching.us';
 
 $links['wiki']['main'] = $_wiki;
 
