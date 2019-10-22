33af486e7560ec2a5ceccf71b469fa67794f8d82
kojoty <kojoty@users.noreply.github.com>
2019-10-22 23:03:21 +0200

    fix of the exception in s7.php

--- src/Views/articles/s7.tpl.php
+++ src/Views/articles/s7.tpl.php
@@ -12,7 +12,6 @@
 
 use src\Utils\Database\XDb;
 use src\Utils\Cache\OcMemCache;
-use src\Models\OcConfig\OcConfig;
 use src\Utils\I18n\I18n;
 
 # This page took >60 seconds to render! Added daily caching.
