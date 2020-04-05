bb54fbd4ec9cc775fbbc3d2ea92532adf41b3d64
andrixnet <andrixnet@yahoo.com>
2020-04-05 15:53:30 +0300

    Disabling watchlist log at OCRO
    via /config

---
+++ config/watchlist.ro.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * Configuration setting for watchlist for OCRO node.
+ * It disables logging into logentries table.
+ * The rest of settings are stored in watchlist.default.php file
+ */
+$watchlist = [
+    'diag_file' => '',
+    'use_logentries' => false
+];
