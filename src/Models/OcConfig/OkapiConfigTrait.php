a144f9f35e71304cc67a1b1b98d536364762944d
kojoty <p.juzwiak@gmail.com>
2020-06-09 01:33:12 +0200

    Support for OKAPI cronJobsBlacklist on OCPL side.
    Now it is possible to disable some OKAPI cronjob by OCPL config files.

---
+++ src/Models/OcConfig/OkapiConfigTrait.php
@@ -0,0 +1,49 @@
+<?php
+namespace src\Models\OcConfig;
+
+/**
+ * This trait group access to local OKAPI config settings - settings here are used in okapi_settings.php
+ * BEWARE OF FUNCTIONS NAME COLLISION BETWEEN CONFIG TRAITS!
+ */
+trait OkapiConfigTrait {
+
+    protected $okapiConfig = null;
+
+    /**
+     * Returns blacklist of okpai cron jobs
+     *
+     * @return array|null
+     */
+    public static function getOkapiCronJobBlacklist()
+    {
+        return self::getOkapiConfigVar('cronJobsBlackList');
+    }
+    /**
+     * Returns okpai config properties
+     *
+     * @return array okapiConfig properties
+     */
+    protected function getOkapiConfig()
+    {
+        if (!$this->okapiConfig) {
+            $this->okapiConfig = self::getConfig("okapiConfig", "config");
+        }
+        return $this->okapiConfig;
+    }
+
+    /**
+     * Get Var from okapiConfig.* files
+     *
+     * @param string $varName
+     * @throws \Exception
+     * @return string|array
+     */
+    private static function getOkapiConfigVar($varName)
+    {
+        $okapiConfig = self::instance()->getOkapiConfig();
+        if (!is_array($okapiConfig)) {
+            throw new \Exception("Invalid $varName setting: see /config/okapiConfig.*");
+        }
+        return $okapiConfig[$varName];
+    }
+}
