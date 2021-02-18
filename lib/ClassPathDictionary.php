91001c843534a61645d5acffbdad9ceefe61ad60
Jeremiasz Major <jrh.mjr@gmail.com>
2021-02-18 23:01:52 +0100

    formatting

--- lib/ClassPathDictionary.php
+++ lib/ClassPathDictionary.php
@@ -26,7 +26,6 @@ if(file_exists(__DIR__.'/../vendor/autoload.php')){
  */
 class ClassPathDictionary
 {
-
     /**
      * When creating new class, use namespace leading to class php file.
      * This enables classes to be automatically loaded by getClassPath() method.
@@ -35,57 +34,59 @@ class ClassPathDictionary
      *
      * Please preserve alphabetical order.
      */
-    private static $classDictionary = array(
+    private static $classDictionary = [
         'powerTrailBase' => 'powerTrail/powerTrailBase.php',
         'powerTrailController' => 'powerTrail/powerTrailController.php',
         'sendEmail' => 'powerTrail/sendEmail.php',
-    );
+    ];
 
     public static function getClassPath($className)
     {
         $classPathArr = explode('\\', $className);
 
-        if ( count($classPathArr) > 1) { /* namespace solution */
+        if (count($classPathArr) > 1) { /* namespace solution */
+            $fileName = array_pop($classPathArr).'.php';
+            $classPath = __DIR__.'/../'.implode('/', $classPathArr);
 
-            $fileName = array_pop($classPathArr) . '.php';
-            $classPath = __DIR__ . '/../' . implode('/', $classPathArr);
+            $fileToInclude = $classPath.'/'.$fileName;
 
-            $fileToInclude = $classPath . '/' . $fileName;
-            if( file_exists($fileToInclude) ){
+            if (file_exists($fileToInclude)) {
                 return $fileToInclude;
             }
 
             // Okapi has lowercase filenames convention. If there is no such file,
             // try to find file with lowercase filename.
-            $fileToInclude = $classPath . '/' . lcfirst($fileName);
+            $fileToInclude = $classPath.'/'.lcfirst($fileName);
 
-            if( file_exists($fileToInclude) ){
+            if (file_exists($fileToInclude)) {
                 // Check if classname exists
                 return $fileToInclude;
             }
 
-            trigger_error(__METHOD__.": ERROR: Trying to load unknown class: $className");
+            trigger_error(__METHOD__.": ERROR: Trying to load unknown class: {$className}");
             return null;
         }
 
         // Try to look for this class in local dictionary.
-        if( isset(self::$classDictionary[$className] ) ){
-            $fileToInclude = __DIR__ . '/../' . self::$classDictionary[$className];
-            if( file_exists($fileToInclude) ){
+        if (isset(self::$classDictionary[$className])) {
+            $fileToInclude = __DIR__.'/../'.self::$classDictionary[$className];
+
+            if (file_exists($fileToInclude)) {
                 return $fileToInclude;
-            }else{
-                trigger_error(__METHOD__.": ERROR: Class $className found in dictionary, but file is missing!");
+            } else {
+                trigger_error(__METHOD__.": ERROR: Class {$className} found in dictionary, but the file is missing!");
             }
         }
 
-        trigger_error(__METHOD__.": ERROR: Trying to load unknown class: $className");
+        trigger_error(__METHOD__.": ERROR: Trying to load unknown class: {$className}");
         return null;
     }
 }
 
 spl_autoload_register(function ($className) {
+    $fileName = ClassPathDictionary::getClassPath($className);
 
-    $classFile = ClassPathDictionary::getClassPath($className);
-    if(!is_null($classFile))
-        include_once $classFile;
+    if (! is_null($fileName)) {
+        include_once $fileName;
+    }
 });
