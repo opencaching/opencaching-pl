39e69783878f38395c511d4d5f80721eb40d32a4
Jeremiasz Major <jrh.mjr@gmail.com>
2021-02-21 22:22:57 +0100

    formatting

--- config/links.pl.php
+++ config/links.pl.php
@@ -9,6 +9,6 @@
 /**
  * Additional wiki links used only by OCPL node
  */
-$links['wiki']['downloads'] = $_wiki.'/index.php/U%C5%BCyteczne_pliki_zwi%C4%85zane_z_OC_PL';
-$links['wiki']['history']   = $_wiki.'/index.php/Historia_serwisu_Opencaching.pl';
-$links['wiki']['impressum'] = $_wiki.'/index.php/Opencaching_PL';
+$links['wiki']['downloads'] = $_wiki . '/index.php/U%C5%BCyteczne_pliki_zwi%C4%85zane_z_OC_PL';
+$links['wiki']['history']   = $_wiki . '/index.php/Historia_serwisu_Opencaching.pl';
+$links['wiki']['impressum'] = $_wiki . '/index.php/Opencaching_PL';
--- config/links.uk.php
+++ config/links.uk.php
@@ -25,28 +25,28 @@ $links['wiki']['main'] = $_wiki;
 
 // overrides of defaults
 
-$links['wiki']['additionalWaypoints'] = $_wiki.'/index.php/Cache_Parameters#Additional_waypoints';
-$links['wiki']['cacheAttrib'] = $_wiki.'/index.php/Cache_Attributes';
-$links['wiki']['cacheAttrib_en'] = $_wiki.'/index.php/Cache_Attributes';
-$links['wiki']['cacheLogPass'] = $_wiki.'/index.php/Cache_Log_Passwords';
-$links['wiki']['cacheLogPass_en'] = $_wiki.'/index.php/Cache_Log_Passwords';
-$links['wiki']['cacheNotes'] = $_wiki.'/index.php/Cache_Notes';
-$links['wiki']['cacheParams'] = $_wiki.'/index.php/Cache_Parameters';
-$links['wiki']['cacheParams_en'] = $_wiki.'/index.php/Cache_Parameters';
-$links['wiki']['cacheTypes'] = $_wiki.'/index.php/Cache_Types';
-$links['wiki']['cachingCode'] = $_wiki.'/index.php/OC_UK_Mission_statement';
-$links['wiki']['forBeginers'] = $_wiki.'/index.php/Main_Page';
-$links['wiki']['geoPaths'] = $_wiki.'/index.php/GeoPath';
-$links['wiki']['myRoutes'] = $_wiki.'/index.php/My_Routes';
-$links['wiki']['placingCache'] = $_wiki.'/index.php/How_to_create_a_cache';
-$links['wiki']['ratingDesc'] = $_wiki.'/index.php/Cache_Ratings';
-$links['wiki']['ratingDesc_en'] = $_wiki.'/index.php/Cache_Ratings';
-$links['wiki']['rules'] = $_wiki.'/index.php/Terms_of_Use_OC_UK';
-$links['wiki']['rules_en'] = $_wiki.'/index.php/Terms_of_Use_OC_UK';
+$links['wiki']['additionalWaypoints'] = $_wiki . '/index.php/Cache_Parameters#Additional_waypoints';
+$links['wiki']['cacheAttrib'] = $_wiki . '/index.php/Cache_Attributes';
+$links['wiki']['cacheAttrib_en'] = $_wiki . '/index.php/Cache_Attributes';
+$links['wiki']['cacheLogPass'] = $_wiki . '/index.php/Cache_Log_Passwords';
+$links['wiki']['cacheLogPass_en'] = $_wiki . '/index.php/Cache_Log_Passwords';
+$links['wiki']['cacheNotes'] = $_wiki . '/index.php/Cache_Notes';
+$links['wiki']['cacheParams'] = $_wiki . '/index.php/Cache_Parameters';
+$links['wiki']['cacheParams_en'] = $_wiki . '/index.php/Cache_Parameters';
+$links['wiki']['cacheTypes'] = $_wiki . '/index.php/Cache_Types';
+$links['wiki']['cachingCode'] = $_wiki . '/index.php/OC_UK_Mission_statement';
+$links['wiki']['forBeginers'] = $_wiki . '/index.php/Main_Page';
+$links['wiki']['geoPaths'] = $_wiki . '/index.php/GeoPath';
+$links['wiki']['myRoutes'] = $_wiki . '/index.php/My_Routes';
+$links['wiki']['placingCache'] = $_wiki . '/index.php/How_to_create_a_cache';
+$links['wiki']['ratingDesc'] = $_wiki . '/index.php/Cache_Ratings';
+$links['wiki']['ratingDesc_en'] = $_wiki . '/index.php/Cache_Ratings';
+$links['wiki']['rules'] = $_wiki . '/index.php/Terms_of_Use_OC_UK';
+$links['wiki']['rules_en'] = $_wiki . '/index.php/Terms_of_Use_OC_UK';
 
 /**
  * additional links - not used in main code - used only in node-custom menu
  */
-$links['wiki']['downloads'] = $_wiki.'/index.php/Downloads';
-$links['wiki']['history'] = $_wiki.'/index.php/Main_Page';
-$links['wiki']['impressum'] = $_wiki.'/index.php/Main_Page';
+$links['wiki']['downloads'] = $_wiki . '/index.php/Downloads';
+$links['wiki']['history'] = $_wiki . '/index.php/Main_Page';
+$links['wiki']['impressum'] = $_wiki . '/index.php/Main_Page';
