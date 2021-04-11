8fb4e5285e285c8740f71dfaf0e18f59f42f210e
kojoty <kojoty@users.noreply.github.com>
2021-04-11 23:19:30 +0200

    Fix of the default config for watchlist

--- config/watchlist.default.php
+++ config/watchlist.default.php
@@ -5,18 +5,18 @@
  *
  * This is a default configuration.
  * It may be customized in node-specific configuration file.
- *
- * - 'diag_file':
- *      The path of a file used for operating time diagnosis
- *      set to empty string to disable diagnosis
- * - 'use_logentries':
- *      Set to true if you want to use Log::logentries in the code
- *      Set to false if Log::logentries can be omitted
  */
 
 $watchlist = [];
+/**
+ * The path of a file used for operating time diagnosis
+ * set to empty string to disable diagnosis
+ */
+$watchlist['diag_file'] = '';
 
-$watchlist['diag_file'] = '/var/log/ocpl/runwatch.log';
-
-$watchlist['use_logentries'] = true;
+/**
+ * Set to true if you want to use Log::logentries in the code
+ * Set to false if Log::logentries can be omitted
+ */
+$watchlist['use_logentries'] = false;
 
