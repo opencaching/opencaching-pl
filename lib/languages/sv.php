7c6cc97ecc3a44ebcacfbb78fa56532872ecfbad
ocpl-crowdin-robot <46261132+ocpl-crowdin-robot@users.noreply.github.com>
2021-04-11 21:44:05 +0200

    New translations en.php (Swedish)

--- lib/languages/sv.php
+++ lib/languages/sv.php
@@ -512,6 +512,11 @@ $translations = array(
     'admin_user_restrictions' => 'Restrictions',
     'admin_user_addnote' => 'OC Team note',
     'admin_user_noteok' => 'OC Team note added correctly',
+    'admin_user_rmUser' => 'Remove user account',
+    'admin_user_rmUserCancel' => 'Cancel',
+    'admin_user_rmUserConfirmed' => 'I confirm I want to remove this account',
+    'admin_user_confirmationTxt' => 'Are you sure you want to irrevocably remove this user account.',
+    'admin_user_confirmationEmail' => 'Be sure user request was send from address: %s',
 
     'titles' => 'Ämne',
     'content' => 'Message',
