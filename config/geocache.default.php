6432b8125f8f2f1a3edd9fef46cf7172e411e48d
kojoty <p.juzwiak@gmail.com>
2021-09-19 14:44:03 +0200

    #2295: default config for geocache reactivation rules

--- config/geocache.default.php
+++ config/geocache.default.php
@@ -49,3 +49,16 @@ $geocache['titledCacheMinFounds'] = 10;
  */
 $geocache['coordsHiddenForNonLogged'] = true;
 
+/**
+ * If reactivation rules are enabled (present) in geocache editor
+ */
+$geocache['reactivationRulesEnabled'] = false;
+
+/**
+ * This has sense ONLY if option $geocache['reactivationRulesEnabled'] == true.
+ *
+ * List of the translation keys with predefined reactivation rules texts.
+ * Order of the keys here will be reflacted in the geocache editor.
+ * If the list is empty only custom (user defined) option will be active.
+ */
+$geocache['reactivationRulesPredefinedOpts'] = [];
