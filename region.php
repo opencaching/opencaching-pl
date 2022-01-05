0f0521163ff543e3391b04fd5b2355a961ae657a
Grzegorz Deja - deg <deg-pl@users.noreply.github.com>
2022-01-05 14:12:23 +0100

    Client side coords validation in newcache.php. (#2328)
    * Client side coords validation in newcache.php.
    This should prevent error: "ErrorException: [/srv/ocuk/newcache.php:532] A non-numeric value encountered"
    
    * Improve newcache translation, http -> https change and PHP CS-fixer changes.
    I hope it will be OK for Crowdin
    
    * region.php cleaning
    Prevents error (and some other):
    "TypeError: [/srv/ocpl/src/Models/Coordinates/NutsLocation.php:81] Argument 1 passed to src\Models\Coordinates\NutsLocation::fromCoordsFactory() must be an instance of src\Models\Coordinates\Coordinates, null given, called in /srv/ocpl/region.php on line 37"
    
    * region.php cleaning - small CS Fixer changes

--- region.php
+++ region.php
@@ -1,56 +1,69 @@
 <?php
 
-use src\Models\Coordinates\Coordinates;
-use src\Models\Coordinates\NutsLocation;
 use src\Models\ApplicationContainer;
+use src\Models\Coordinates\Coordinates;
 use src\Models\Coordinates\GeoCode;
+use src\Models\Coordinates\NutsLocation;
 
-$tplname = 'region';
+require_once __DIR__ . '/lib/common.inc.php';
 
-require_once (__DIR__.'/lib/common.inc.php');
+if (! ApplicationContainer::GetAuthorizedUser()) {
+    echo 'Not authorized!';
 
-if(!ApplicationContainer::GetAuthorizedUser()){
-    print "Not authorized!";
     exit;
 }
 
 $lat_float = 0;
+$latOk = false;
+$lonOk = false;
+$coords = null;
+
 if (isset($_REQUEST['lat'])) {
     $lat_float = (float) $_REQUEST['lat'];
     $lat = $_REQUEST['lat'];
+    $latOk = true;
 }
 
 $lon_float = 0;
+
 if (isset($_REQUEST['lon'])) {
     $lon_float = (float) $_REQUEST['lon'];
-    $lon =  $_REQUEST['lon'];
+    $lon = $_REQUEST['lon'];
+    $lonOk = true;
 }
 
-$coords = Coordinates::FromCoordsFactory($lat, $lon);
-if(!is_null($coords)){
+if ($latOk && $lonOk) {
+    $coords = Coordinates::FromCoordsFactory($lat, $lon);
+}
+
+if (! is_null($coords)) {
     tpl_set_var('coords_str', $coords->getAsText(Coordinates::COORDINATES_FORMAT_DEG_MIN));
+
+    $nutsData = NutsLocation::fromCoordsFactory($coords);
+    tpl_set_var('nutsDesc', $nutsData->getDescription(' > '));
+
+    $googleGeocode = GeoCode::fromGoogleApi($coords);
+
+    if ($googleGeocode) {
+        tpl_set_var('googleDesc', $googleGeocode->getDescription(' > '));
+    } else {
+        tpl_set_var('googleDesc', '-');
+    }
+
+    $mapQuestGeoCode = GeoCode::fromMapQuestApi($coords);
+
+    if ($mapQuestGeoCode) {
+        tpl_set_var('mapQuestDesc', $mapQuestGeoCode->getDescription(' > '));
+    } else {
+        tpl_set_var('mapQuestDesc', '-');
+    }
 } else {
     tpl_set_var('coords_str', '');
-}
-
-// NUTS local data
-$nutsData = NutsLocation::fromCoordsFactory($coords);
-tpl_set_var('nutsDesc', $nutsData->getDescription(' > '));
-
-// Google geocode
-$googleGeocode = GeoCode::fromGoogleApi($coords);
-if($googleGeocode){
-    tpl_set_var('googleDesc', $googleGeocode->getDescription(' > '));
-}else{
+    tpl_set_var('nutsDesc', '-');
     tpl_set_var('googleDesc', '-');
-}
-
-$mapQuestGeoCode = GeoCode::fromMapQuestApi($coords);
-if($mapQuestGeoCode){
-    tpl_set_var('mapQuestDesc', $mapQuestGeoCode->getDescription(' > '));
-}else{
     tpl_set_var('mapQuestDesc', '-');
 }
 
 //make the template and send it out
+tpl_set_tplname('region');
 tpl_BuildTemplate();
