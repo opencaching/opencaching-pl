6479dd1ca130270583943bb34931cc1450260324
deg <deg@best.pl>
2022-03-30 17:19:24 +0200

    CS-Fixer fix

--- src/Models/Coordinates/GeoCodeServiceResult.php
+++ src/Models/Coordinates/GeoCodeServiceResult.php
@@ -5,8 +5,12 @@ namespace src\Models\Coordinates;
 class GeoCodeServiceResult
 {
     public string $name;
+
     public string $layer;
+
     public string $countryCode;
+
     public string $region;
+
     public array $bbox;
 }
