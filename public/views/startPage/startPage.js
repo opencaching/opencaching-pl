f8d49ad956ab70374072d7e9c99e5144c492ab8d
Stefo <stefo@stefo.pl>
2022-11-12 22:42:49 +0100

    OCPL-5 Various

--- public/views/startPage/startPage.js
+++ public/views/startPage/startPage.js
@@ -1,10 +1,27 @@
 $(document).ready(function(){
-	$('.totalStatsSlider').slick({
-		slidesToShow: 5,
-		slidesToScroll: 1,
-		autoplay: true,
-		autoplaySpeed: 3000,
-		dots: true,
-		arrows: false,
-	});
+    $('.totalStatsSlider').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots: true,
+        arrows: false,
+        responsive: [{
+            breakpoint: 970,
+			settings: {
+                slidesToShow: 4,
+            }
+        }, {
+            breakpoint: 800,
+			settings: {
+                slidesToShow: 3,
+            }
+        }, {
+            breakpoint: 500,
+			settings: {
+                slidesToShow: 2,
+				slidesToScroll: 2,
+            }
+        }]
+    });
 });
