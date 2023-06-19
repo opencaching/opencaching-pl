141b36e582c0ba9129d6487446a78d3e6cac5b66
ocpl-crowdin-robot <46261132+ocpl-crowdin-robot@users.noreply.github.com>
2023-06-19 21:45:50 +0200

    New translations en.php (German)

--- lib/languages/de.php
+++ lib/languages/de.php
@@ -2718,6 +2718,9 @@ Verwaltet die Opencaching-Website, kümmert sich um das Wiki, prüft neue Caches
     'startPage_newSearchesDesc' => 'Anzahl aller Fund- und Nichtfund-Logeinträge der letzten 30 Tage, ohne gelöschte Logs',
     'startPage_newoRecom' => 'Neue Emfpfehlungen',
     'startPage_newoRecomDesc' => 'Anzahl der in den letzten 30 Tagen vergebenen Cache-Empfehlungen',
+    'startPage_lastYearActiveUsers' => 'Users who found in last year',
+    'startPage_LastYearActiveUsersDesc' => 'Users who found at least one geocache in last year',
+
     'startPage_showMore' => 'Mehr ...',
     'startPage_latestCachesList' => 'Die neuesten Caches',
     'startPage_latestCacheSets' => 'Die neuesten Geopfade',
