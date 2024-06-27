962b6c6529cac9cc16fff5d39e7757617cd3892b
Stefo <stefo@stefo.pl>
2024-06-27 19:17:31 +0200

    opencaching-pl-2406: State Retention Issue for Collapsible S...

--- viewprofile.php
+++ viewprofile.php
@@ -57,7 +57,7 @@ if (isset($_REQUEST['save'])) {
     }
 
     if (isset($_REQUEST['checkMilestones'])) {
-        OcCookie::set('checkGeoPaths', ! $_REQUEST['checkMilestones'], true);
+        OcCookie::set('checkMilestones', ! $_REQUEST['checkMilestones'], true);
     }
 }
 
