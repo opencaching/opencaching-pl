2da7760cda6a5fadde4b990391d0dd0e2fe222c1
Rafał Borowski <moderator9781@gmail.com>
2025-07-10 18:32:05 +0000

    Remove dead links from links.tpl.php and tidy up layout and flag images a bit

--- src/Views/articles/links.tpl.php
+++ src/Views/articles/links.tpl.php
@@ -5,143 +5,113 @@
 <div class="searchdiv">
     <h2>Geocaching</h2>
 
-    <table style="font-size: 125%;">
+    <table class="links-table">
         <tbody>
             <tr>
-                <td style="width: 50%;">
+                <td class="links-col-1">
                     <b>Opencaching Network Sites:</b>
                 </td>
-                <td>
+                <td class="links-col-2">
                     <a href="https://opencaching.eu">www.opencaching.eu</a>
                 </td>
             </tr>
             <tr>
-                <td>
-                    <img src="/images/flags/cs.png" class="icon32" align="middle"> {{CZ}}
+                <td class="links-col-1">
+                    <img src="/images/flags/de.gif" /> {{DE}}
                 </td>
-                <td>
-                    <a href="https://opencaching.cz">www.opencaching.cz</a>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <img src="/images/flags/de.png" class="icon32" align="middle"> {{DE}}
-                </td>
-                <td>
+                <td class="links-col-2">
                     <a href="https://www.opencaching.de">www.opencaching.de</a>
                 </td>
             </tr>
             <tr>
-                <td>
-                    <img src="/images/flags/en.png" class="icon32" align="middle"> {{GB}}
+                <td class="links-col-1">
+                    <img src="/images/flags/en.gif" /> {{GB}}
                 </td>
-                <td>
+                <td class="links-col-2">
                     <a href="https://opencache.uk">www.opencache.uk</a>
                 </td>
             </tr>
             <tr>
-                <td>
-                    <img src="/images/flags/nl.png" class="icon32" align="middle"> {{NL}}
+                <td class="links-col-1">
+                    <img src="/images/flags/nl.gif" /> {{NL}}
                 </td>
-
-                <td>
+                <td class="links-col-2">
                     <a href="https://www.opencaching.nl">www.opencaching.nl </a>
                 </td>
             </tr>
             <tr>
-                <td>
-                    <img src="/images/flags/pl.png" class="icon32" align="middle"> {{PL}}
+                <td class="links-col-1">
+                    <img src="/images/flags/pl.gif" /> {{PL}}
                 </td>
-                <td>
+                <td class="links-col-2">
                     <a href="https://opencaching.pl">www.opencaching.pl</a>
                 </td>
             </tr>
             <tr>
-                <td>
-                    <img src="/images/flags/ro.png" class="icon32" align="middle"> {{RO}}
+                <td class="links-col-1">
+                    <img src="/images/flags/ro.gif" /> {{RO}}
                 </td>
-                <td>
+                <td class="links-col-2">
                     <a href="https://www.opencaching.ro">www.opencaching.ro</a>
                 </td>
             </tr>
             <tr>
-                <td>
-                    <img src="/images/flags/us.png" class="icon32" align="middle"> {{US}}
+                <td class="links-col-1">
+                    <img src="/images/flags/us.gif" /> {{US}}
                 </td>
-                <td>
+                <td class="links-col-2">
                     <a href="http://www.opencaching.us">www.opencaching.us</a>
                 </td>
             </tr>
         </tbody>
     </table>
 
-    <table style="font-size: 125%;">
+    <table class="links-table">
         <tbody>
             <tr>
-                <td style="width: 50%;">
+                <td class="links-col-1" colspan="2">
                     <b>{{links_02}}:</b>
                 </td>
+                <td class="links-col-2"></td>
             </tr>
             <tr>
-                <td>
-                    {{AU}}
+                <td class="links-col-1">
+                <img src="/images/flags/au.gif" /> {{AU}}
                 </td>
-                <td>
+                <td class="links-col-2">
                     <a href="https://geocaching.com.au">geocaching.com.au</a>
                 </td>
             </tr>
             <tr>
-                <td>
-                    {{HU}}
+                <td class="links-col-1">
+                    <img src="/images/flags/hu.gif" /> {{HU}}
                 </td>
-                <td>
+                <td class="links-col-2">
                     <a href="http://www.geocaching.hu">www.geocaching.hu</a>
                 </td>
             </tr>
             <tr>
-                <td>
-                    {{RO}}
+                <td class="links-col-1">
+                    <img src="/images/flags/ro.gif" /> {{RO}}
                 </td>
-                <td>
+                <td class="links-col-2">
                     <a href="http://geotrekking.plus.ro">geotrekking.plus.ro</a>
                 </td>
             </tr>
             <tr>
-                <td>
-                    {{EE}}
+                <td class="links-col-1">
+                    <img src="/images/flags/ee.gif" /> {{EE}}
                 </td>
-                <td>
-                    <a href="http://www.geopeitus.ee/english.html">www.geopeitus.ee</a>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    {{TR}}
-                </td>
-                <td>
-                    <a href="http://geocachingturkey.com">geocachingturkey.com</a>
+                <td class="links-col-2">
+                    <a href="http://www.geopeitus.ee/">www.geopeitus.ee</a>
                 </td>
             </tr>
             <tr>
-                <td>
-                    {{SU}}
+                <td class="links-col-1">
+                    <img src="/images/flags/ru.gif" /> {{SU}}
                 </td>
-                <td>
+                <td class="links-col-2">
                     <a href="http://www.geocaching.su">www.geocaching.su</a>
-                </td>
-            </tr>
-            <tr>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-            </tr>
-            <tr>
-                <td>
-                    {{links_03}}:
-                </td>
-                <td>
-                    <a href="https://www.terracaching.com">www.terracaching.com</a><br>
-                    <a href="http://geocaching.gpsgames.org">www.gpsgames.org</a><br>
-                    <a href="http://www.navicache.com">www.navicache.com</a><br>
                 </td>
             </tr>
         </tbody>
@@ -149,71 +119,60 @@
 
     <h2>Letterboxing</h2>
 
-    <table style="font-size: 125%;">
+    <table class="links-table">
         <tbody>
             <tr>
-                <td style="width: 50%;">
+                <td class="links-col-1" colspan="2">
                     {{links_04}}:
                 </td>
+                <td class="links-col-2"></td>
             </tr>
             <tr>
-                <td></td>
-                <td>
+                <td class="links-col-1"></td>
+                <td class="links-col-2">
                     <a href="http://www.letterboxing.org">www.letterboxing.org</a>
                 </td>
             </tr>
             <tr>
-                <td></td>
-                <td>
+                <td class="links-col-1"></td>
+                <td class="links-col-2">
                     <a href="https://www.atlasquest.com">www.atlasquest.com</a>
-                </td>
-            </tr>
-            <tr>
-                <td></td>
-                <td>
-                    <a href="http://www.dartmoorletterboxing.org/">www.dartmoorletterboxing.org</a>
                 </td>
             </tr>
         </tbody>
     </table>
 
-
     <h2>Travel Bugs</h2>
 
-    <table style="font-size: 125%;">
+    <table class="links-table">
         <tbody>
             <tr>
-                <td style="width: 50%;">
+                <td class="links-col-1">
                     {{links_05}}:
                 </td>
+                <td class="links-col-2"></td>
             </tr>
             <tr>
-                <td></td>
-                <td>
+                <td class="links-col-1"></td>
+                <td class="links-col-2">
                     <a href="https://www.geokrety.org">www.geokrety.org</a>
                 </td>
             </tr>
             <tr>
-                <td></td>
-                <td>
+                <td class="links-col-1"></td>
+                <td class="links-col-2">
                     <a href="http://www.geofish.net">www.geofish.net</a>
                 </td>
             </tr>
             <tr>
-                <td></td>
-                <td>
-                    <a href="http://www.geocachearmy.com">www.geocachearmy.com</a>
+                <td class="links-col-1"></td>
+                <td class="links-col-2">
+                    <a href="http://www.geolutins.com/">www.geolutins.com</a>
                 </td>
             </tr>
             <tr>
-                <td></td>
-                <td>
-                    <a href="http://www.geolutins.com/langue_valid.php?L=7">www.geolutins.com</a>
-                </td>
-            </tr>
-            <tr>
-                <td></td>
-                <td>
+                <td class="links-col-1"></td>
+                <td class="links-col-2">
                     <a href="http://www.pathtags.com">www.pathtags.com</a>
                 </td>
             </tr>
@@ -222,19 +181,62 @@
 
     <h2>{{links_06}}</h2>
 
-    <table style="font-size: 125%;">
+    <table class="links-table">
         <tbody>
             <tr>
-                <td>
+                <td class="links-col-1"></td>
+                <td class="links-col-2">
+                  <a href="https://www.terracaching.com">www.terracaching.com</a>
+                </td>
+            </tr>
+            <tr>
+                <td class="links-col-1"></td>
+                <td class="links-col-2">
                     <a href="https://www.cistes.net">www.cistes.net</a>
                 </td>
             </tr>
             <tr>
-                <td>
+                <td class="links-col-1"></td>
+                <td class="links-col-2">
                     <a href="https://geocachen.be/">geocachen.be</a>
+                </td>
+            </tr>
+            <tr>
+                <td class="links-col-1"></td>
+                <td class="links-col-2">
+                    <a href="https://geocachen.nl/">geocachen.nl</a>
                 </td>
             </tr>
         </tbody>
     </table>
 
 </div>
+
+<style>
+.links-table {
+    font-size: 125%;
+    width: 100%;
+}
+
+.links-col-1 {
+    width: 40%;
+    vertical-align: top;
+}
+
+.links-col-2 {
+    width: 60%;
+    vertical-align: top;
+}
+
+@media (max-width: 768px) {
+    .links-table {
+        table-layout: auto;
+    }
+
+    .links-col-1,
+    .links-col-2 {
+        width: auto;
+        display: block;
+    }
+}
+</style>
